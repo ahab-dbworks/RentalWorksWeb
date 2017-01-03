f23e1fcf7bda931b03386c30dcdb64246cb60e8d
Mike <Mike@DESKTOP-T2RBDF5>
2017-01-03 02:17:37 -0800

    Adds new developer config file templates Application.devsample.config for QuikScan and RentalWorksWeb.  Removes some folders that were being copied during builds from the framework directory.  Removes Index.htm in QuikScan from .gitignore since in QuikScan that file is not dynamically generated.

---
+++ RentalWorksQuikScan/ApplicationConfig.devsample.js
@@ -0,0 +1,5 @@
+﻿applicationConfig.debugMode = true;
+applicationConfig.designMode = false;
+applicationConfig.demoMode = false;
+applicationConfig.fwvirtualdirectory = 'fwjson/';
+applicationConfig.appvirtualdirectory = 'qs/';
