e2118a608d49e349ed0aa38a076b4eca841d116a
Mike Vermilion <mike@dbworks.com>
2017-02-02 11:11:58 -0800

    QS: Don't create Staging contract unless ordertran exists

--- RentalWorksQuikScan/Modules/Staging/Staging.cs
+++ RentalWorksQuikScan/Modules/Staging/Staging.cs
@@ -413,25 +413,22 @@ namespace RentalWorksQuikScan.Modules
         [FwJsonServiceMethod(RequiredParameters = "orderid,contractid")]
         public static void OrdertranExists(dynamic request, dynamic response, dynamic session)
         {
-            using (FwSqlCommand qry = new FwSqlCommand(FwSqlConnection.RentalWorks))
+            if (!(string.IsNullOrEmpty(request.contractid)))
             {
-                if (!(string.IsNullOrEmpty(request.contractid)))
+                using (FwSqlCommand sp = new FwSqlCommand(FwSqlConnection.RentalWorks, "contractisempty"))
                 {
-                    qry.Add("select ordertranexists = case when exists (select *");
-                    qry.Add("                                             from ordertran with (nolock)");
-                    qry.Add("                                             where outreceivecontractid = @contractid)");
-                    qry.Add("                              then 'T'");
-                    qry.Add("                              else 'F'");
-                    qry.Add("                         end");
-                    qry.AddParameter("@contractid", request.contractid);
-                    qry.Execute();
-                    response.ordertranExists = qry.GetField("ordertranexists").ToBoolean();
+                    sp.AddParameter("@contractid", request.contractid);
+                    sp.AddParameter("@isempty", SqlDbType.Char, ParameterDirection.Output);
+                    sp.Execute();
+                    response.ordertranExists = sp.GetParameter("@isempty").ToBoolean();
                 }
-                else
+            }
+            else
+            {
+                using (FwSqlCommand qry = new FwSqlCommand(FwSqlConnection.RentalWorks))
                 {
                     qry.Add("select ordertranexists = case when exists (select *");
-                    qry.Add("                                             from ordertran ot with (nolock) join ordercontract oc on (ot.outreceivecontractid = oc.contractid)");
-                    qry.Add("                                             where oc.orderid = @orderid)");
+                    qry.Add("                                           from dbo.funcstaged(@orderid, 'F'))");
                     qry.Add("                              then 'T'");
                     qry.Add("                              else 'F'");
                     qry.Add("                         end");
