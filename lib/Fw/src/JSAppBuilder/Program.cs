8c6d14dd3b95f88c380612dc589a6b8d9ccf480e
Mike Vermilion <mike@dbworks.com>
2017-04-14 14:40:20 -0700

    Fw: Adds ReplaceInFile.exe and updates JSAppBuilder.exe

--- lib/Fw/src/JSAppBuilder/Program.cs
+++ lib/Fw/src/JSAppBuilder/Program.cs
@@ -13,50 +13,57 @@ namespace JSAppBuilder
     {
         static void Main(string[] args)
         {
-            string pathJSAppBuilderConfig = string.Empty;
-            string solutionPath = string.Empty;
-            bool publish = false;
-            bool attachDebugger = false;
-            string version = args[2];
-            bool updateSchema = false;
-            for (int i = 0; i < args.Length; i++)
+            try
             {
-                switch (args[i])
+                string pathJSAppBuilderConfig = string.Empty;
+                string solutionPath = string.Empty;
+                bool publish = false;
+                bool attachDebugger = false;
+                string version = args[2];
+                bool updateSchema = false;
+                for (int i = 0; i < args.Length; i++)
                 {
-                    case "-ConfigFilePath":
-                        i++;
-                        pathJSAppBuilderConfig = args[i];
-                        break;
-                    case "-SolutionDir":
-                        i++;
-                        solutionPath = args[i];
-                        break;
-                    case "-Version":
-                        i++;
-                        version = args[i];
-                        break;
-                    case "-UpdateSchema":
-                        i++;
-                        updateSchema = args[i].ToLower().Equals("true");
-                        break;
-                    case "-Publish":
-                        i++;
-                        publish = args[i].ToLower().Equals("true");
-                        break;
-                    case "-AttachDebugger":
-                        i++;
-                        attachDebugger = args[i].ToLower().Equals("true");
-                        break;
+                    switch (args[i])
+                    {
+                        case "-ConfigFilePath":
+                            i++;
+                            pathJSAppBuilderConfig = args[i];
+                            break;
+                        case "-SolutionDir":
+                            i++;
+                            solutionPath = args[i];
+                            break;
+                        case "-Version":
+                            i++;
+                            version = args[i];
+                            break;
+                        case "-UpdateSchema":
+                            i++;
+                            updateSchema = args[i].ToLower().Equals("true");
+                            break;
+                        case "-Publish":
+                            i++;
+                            publish = args[i].ToLower().Equals("true");
+                            break;
+                        case "-AttachDebugger":
+                            i++;
+                            attachDebugger = args[i].ToLower().Equals("true");
+                            break;
+                    }
                 }
-            }
 
-            FwJSAppBuilder config = new FwJSAppBuilder();
-            config.BuildEngine = new VirtualBuildEngine();
-            config.Publish = publish;
-            config.Version = version;
-            config.UpdateSchema = updateSchema;
-            config.AttachDebugger = attachDebugger;
-            config.Build(pathJSAppBuilderConfig, solutionPath);
+                FwJSAppBuilder config = new FwJSAppBuilder();
+                config.BuildEngine = new VirtualBuildEngine();
+                config.Publish = publish;
+                config.Version = version;
+                config.UpdateSchema = updateSchema;
+                config.AttachDebugger = attachDebugger;
+                config.Build(pathJSAppBuilderConfig, solutionPath);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.Write(ex.Message + ex.StackTrace);
+            }
         }
     }
 }
