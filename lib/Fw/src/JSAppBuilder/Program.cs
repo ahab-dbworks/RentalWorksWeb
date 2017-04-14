f92d2459fea67e16a67c471873d6d71399678622
Mike Vermilion <mike@dbworks.com>
2017-04-14 12:25:03 -0700

    Fw: Updates the command line parameters for JSAppBuilder.exe

--- lib/Fw/src/JSAppBuilder/Program.cs
+++ lib/Fw/src/JSAppBuilder/Program.cs
@@ -16,31 +16,36 @@ namespace JSAppBuilder
             string pathJSAppBuilderConfig = string.Empty;
             string solutionPath = string.Empty;
             bool publish = false;
+            bool attachDebugger = false;
             string version = args[2];
             bool updateSchema = false;
             for (int i = 0; i < args.Length; i++)
             {
                 switch (args[i])
                 {
-                    case "-c":
+                    case "-ConfigFilePath":
                         i++;
                         pathJSAppBuilderConfig = args[i];
                         break;
-                    case "-s":
+                    case "-SolutionDir":
                         i++;
                         solutionPath = args[i];
                         break;
-                    case "-v":
+                    case "-Version":
                         i++;
                         version = args[i];
                         break;
-                    case "-u":
+                    case "-UpdateSchema":
                         i++;
                         updateSchema = args[i].ToLower().Equals("true");
                         break;
-                    case "-p":
+                    case "-Publish":
                         i++;
                         publish = args[i].ToLower().Equals("true");
+                        break;
+                    case "-AttachDebugger":
+                        i++;
+                        attachDebugger = args[i].ToLower().Equals("true");
                         break;
                 }
             }
@@ -50,6 +55,7 @@ namespace JSAppBuilder
             config.Publish = publish;
             config.Version = version;
             config.UpdateSchema = updateSchema;
+            config.AttachDebugger = attachDebugger;
             config.Build(pathJSAppBuilderConfig, solutionPath);
         }
     }
