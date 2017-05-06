f13642f90e5d68c3da6246422d46299be9481da2
Matt Young <matt@dbworks.com>
2017-05-05 17:15:41 -0700

    Adds code for validations batchar and batchvendorinvoice

---
+++ src/RentalWorksWeb/Source/Validations/BatchAR/BatchAR.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using Fw.Json.Services;
+using Fw.Json.SqlServer;
+using Fw.Json.Utilities;
+using Fw.Json.ValueTypes;
+
+namespace RentalWorksWeb.Source.Validations
+{
+    class BatchAR : FwValidation
+    {
+        //---------------------------------------------------------------------------------------------
+        protected override void setBrowseQry(FwSqlSelect selectQry)
+        {
+            base.setBrowseQry(selectQry);
+
+            selectQry.AddWhere("batchtype = 'AR'");
+            selectQry.AddWhere("locationid = @locationid");
+            selectQry.AddParameter("@locationid", session.security.webUser.locationid);
+        }
+        //---------------------------------------------------------------------------------------------
+    }
+}
+
---
+++ src/RentalWorksWeb/Source/Validations/BatchVendorInvoice/BatchVendorInvoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using Fw.Json.Services;
+using Fw.Json.SqlServer;
+using Fw.Json.Utilities;
+using Fw.Json.ValueTypes;
+
+namespace RentalWorksWeb.Source.Validations
+{
+    class BatchVendorInvoice : FwValidation
+    {
+        //---------------------------------------------------------------------------------------------
+        protected override void setBrowseQry(FwSqlSelect selectQry)
+        {
+            base.setBrowseQry(selectQry);
+
+            selectQry.AddWhere("batchtype = 'VENDORINVOICE'");
+            selectQry.AddWhere("locationid = @locationid");
+            selectQry.AddParameter("@locationid", session.security.webUser.locationid);
+        }
+        //---------------------------------------------------------------------------------------------
+    }
+}
+
