b49fd53d3b23fd923369b340c0292c7884e9098d
Mike Vermilion <mike@dbworks.com>
2017-07-06 22:30:15 -0700

    Updates Fw

---
+++ lib/Fw/src/FwStandard/Models/ValidateDuplicateRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FwStandard.Models
+{
+    public class ValidateDuplicateRequest
+    {
+        [Required]
+        Dictionary<string, ValidateDuplicateRequestField> fields { get; set; }
+    }
+
+    public class ValidateDuplicateRequestField
+    {
+        [Required]
+        public string datafield { get; set; }
+        [Required]
+        public object value { get; set; }
+        [Required]
+        public string type { get; set; }
+    }
+}
