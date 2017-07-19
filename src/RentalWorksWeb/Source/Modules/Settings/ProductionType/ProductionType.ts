7bba58d959e8ef606833e8afa1916be94f0d041e
Unknown <oliver@dbworks.com>
2017-07-19 11:08:58 -0700

    Added front-end for Deal Classification & Production Type

---
+++ src/RentalWorksWeb/Source/Modules/Settings/ProductionType/ProductionType.ts
@@ -0,0 +1,81 @@
+﻿declare var FwModule: any;
+declare var FwBrowse: any;
+
+class ProductionType {
+    Module: string;
+    apiurl: string;
+
+    constructor() {
+        this.Module = 'ProductionType';
+        this.apiurl = 'api/v1/productiontype';
+    }
+
+    getModuleScreen() {
+        var screen, $browse;
+
+        screen = {};
+        screen.$view = FwModule.getModuleControl(this.Module + 'Controller');
+        screen.viewModel = {};
+        screen.properties = {};
+
+        $browse = this.openBrowse();
+
+        screen.load = function () {
+            FwModule.openModuleTab($browse, 'ProductionType', false, 'BROWSE', true);
+            FwBrowse.databind($browse);
+            FwBrowse.screenload($browse);
+        };
+        screen.unload = function () {
+            FwBrowse.screenunload($browse);
+        };
+
+        return screen;
+    }
+
+    openBrowse() {
+        var $browse;
+
+        $browse = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Browse').html());
+        $browse = FwModule.openBrowse($browse);
+        FwBrowse.init($browse);
+
+        return $browse;
+    }
+
+    openForm(mode: string) {
+        var $form;
+
+        $form = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Form').html());
+        $form = FwModule.openForm($form, mode);
+
+        return $form;
+    }
+
+    loadForm(uniqueids: any) {
+        var $form;
+
+        $form = this.openForm('EDIT');
+        $form.find('div.fwformfield[data-datafield="ProductionTypeId"] input').val(uniqueids.ProductionTypeId);
+        FwModule.loadForm(this.Module, $form);
+
+        return $form;
+    }
+
+    saveForm($form: any, closetab: boolean, navigationpath: string) {
+        FwModule.saveForm(this.Module, $form, closetab, navigationpath);
+    }
+
+    loadAudit($form: any) {
+        var uniqueid;
+        uniqueid = $form.find('div.fwformfield[data-datafield="ProductionTypeId"] input').val();
+        FwModule.loadAudit($form, uniqueid);
+    }
+
+
+
+    afterLoad($form: any) {
+
+    }
+}
+
+(window as any).ProductionTypeController = new ProductionType();
