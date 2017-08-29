3f188d4b4c02bcca9558f9da0789a28400633fb5
Ahab Guirguis <ahab@dbworks.com>
2017-08-29 15:18:23 -0700

    Added Vendor Notes

---
+++ src/RentalWorksWebApi/Modules/Settings/VendorNoteGrid/VendorNoteGridLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using RentalWorksWebApi.Data;
+
+namespace RentalWorksWebApi.Modules.Settings.VendorNoteGrid
+{
+    [FwSqlTable("vendnoteview")]
+    public class VendorNoteGridLoader : RwDataReadWriteRecord
+    {
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "vendorid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, precision: 0, scale: 0)]
+        public string vendorid { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "vendnoteid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, precision: 0, scale: 0)]
+        public string vendnoteid { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notedate", modeltype: FwDataTypes.UTCDateTime, sqltype: "smalldatetime", maxlength: 4, precision: 16, scale: 0)]
+        public string notedate { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "name", modeltype: FwDataTypes.Text, sqltype: "varchar", maxlength: 65, precision: 0, scale: 0)]
+        public string name { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notesdesc", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 30, precision: 0, scale: 0)]
+        public string notesdesc { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notes", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 255, precision: 0, scale: 0)]
+        public string notes { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notify", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 1, precision: 0, scale: 0)]
+        public string notify { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notestbyid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, precision: 0, scale: 0)]
+        public string notestbyid { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "datestamp", modeltype: FwDataTypes.UTCDateTime, sqltype: "datetime", maxlength: 8, precision: 23, scale: 3)]
+        public string datestamp { get; set; }
+
+    }
+
+}
---
+++ src/RentalWorksWebApi/Modules/Settings/VendorNoteGrid/VendorNoteGridRecord.cs
@@ -0,0 +1,37 @@
+﻿using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using RentalWorksWebApi.Data;
+
+namespace RentalWorksWebApi.Modules.Settings.VendorNoteGrid
+{
+    [FwSqlTable("vendnote")]
+    public class VendorNoteGridRecord : RwDataReadWriteRecord
+    {
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "vendorid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, precision: 0, scale: 0, isPrimaryKey: true)]
+        public string VendorId { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "vendnoteid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, precision: 0, scale: 0)]
+        public string VendorNoteId { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notedate", modeltype: FwDataTypes.UTCDateTime, sqltype: "smalldatetime")]
+        public string NoteDate { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notestbyid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string NotestById { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notesdesc", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 30)]
+        public string NotesDesc { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notes", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 255)]
+        public string Notes { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "datestamp", modeltype: FwDataTypes.UTCDateTime, sqltype: "datetime")]
+        public string DateStamp { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "notify", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 1)]
+        public string Notify { get; set; }
+        //------------------------------------------------------------------------------------
+        
+    }
+}
