d4d96566633e2caaa1a2275aa7ad55d7914558c1
Justin Hoffman <justin@dbworks.com>
2017-09-11 02:24:38 -0700

    Payment Type API updates

--- src/RentalWorksWebApi/Modules/Settings/PaymentType/PaymentTypeLoader.cs
+++ src/RentalWorksWebApi/Modules/Settings/PaymentType/PaymentTypeLoader.cs
@@ -4,38 +4,44 @@ using System;
 
 namespace RentalWorksWebApi.Data.Settings
 {
-    [FwSqlTable("paytype")]
-    public class PaymentTypeRecord : RwDataReadWriteRecord
+    [FwSqlTable("paytypeview")]
+    public class PaymentTypeLoader : RwDataLoadRecord
     {
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "paytypeid", modeltype: FwDataTypes.Text, maxlength: 8, isPrimaryKey: true)]
+        [FwSqlDataField(column: "paytypeid", modeltype: FwDataTypes.Text, isPrimaryKey: true)]
         public string PaymentTypeId { get; set; } = "";
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "paytype", modeltype: FwDataTypes.Text, maxlength: 20, required: true)]
+        [FwSqlDataField(column: "paytype", modeltype: FwDataTypes.Text, required: true)]
         public string PaymentType { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "short", modeltype: FwDataTypes.Text, maxlength: 11, required: true)]
+        [FwSqlDataField(column: "short", modeltype: FwDataTypes.Text, required: true)]
         public string ShortName { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "pmttype", modeltype: FwDataTypes.Text, maxlength: 20, required: true)]
+        [FwSqlDataField(column: "pmttype", modeltype: FwDataTypes.Text, required: true)]
         public string PaymentTypeType { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "glaccountid", modeltype: FwDataTypes.Text, maxlength: 8)]
+        [FwSqlDataField(column: "glaccountid", modeltype: FwDataTypes.Text)]
         public string GlAccountId { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "glno", modeltype: FwDataTypes.Text)]
+        public string GlAccountNo { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "glacctdesc", modeltype: FwDataTypes.Text)]
+        public string GlAccountDescription { get; set; }
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "accttran", modeltype: FwDataTypes.Boolean)]
         public bool AccountingTransaction { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "exportpaymentmethod", modeltype: FwDataTypes.Text, maxlength: 30)]
+        [FwSqlDataField(column: "exportpaymentmethod", modeltype: FwDataTypes.Text)]
         public string ExportPaymentMethod { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "exportpaymenttype", modeltype: FwDataTypes.Text, maxlength: 30)]
+        [FwSqlDataField(column: "exportpaymenttype", modeltype: FwDataTypes.Text)]
         public string ExportPaymentType { get; set; }
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "includeinrwnet", modeltype: FwDataTypes.Boolean)]
         public bool IncludeInRentalWorksNet { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "rwnetcaption", modeltype: FwDataTypes.Text, maxlength: 20)]
+        [FwSqlDataField(column: "rwnetcaption", modeltype: FwDataTypes.Text)]
         public string RentalWorksNetCaption { get; set; }
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "inactive", modeltype: FwDataTypes.Boolean)]
--- src/RentalWorksWebApi/Modules/Settings/PaymentType/PaymentTypeLogic.cs
+++ src/RentalWorksWebApi/Modules/Settings/PaymentType/PaymentTypeLogic.cs
@@ -8,9 +8,12 @@ namespace RentalWorksWebApi.Modules.Settings.PaymentType
     {
         //------------------------------------------------------------------------------------
         PaymentTypeRecord paymentType = new PaymentTypeRecord();
+        PaymentTypeLoader paymentTypeLoader = new PaymentTypeLoader();
+
         public PaymentTypeLogic()
         {
             dataRecords.Add(paymentType);
+            dataLoader = paymentTypeLoader;
         }
         //------------------------------------------------------------------------------------
         [FwBusinessLogicField(isPrimaryKey: true)]
@@ -19,7 +22,16 @@ namespace RentalWorksWebApi.Modules.Settings.PaymentType
         public string PaymentType { get { return paymentType.PaymentType; } set { paymentType.PaymentType = value; } }
         public string ShortName { get { return paymentType.ShortName; } set { paymentType.ShortName = value; } }
         public string PaymentTypeType { get { return paymentType.PaymentTypeType; } set { paymentType.PaymentTypeType = value; } }
+        public string GlAccountId { get { return paymentType.GlAccountId; } set { paymentType.GlAccountId = value; } }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string GlAccountNo { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string GlAccountDescription { get; set; }
         public bool AccountingTransaction { get { return paymentType.AccountingTransaction; } set { paymentType.AccountingTransaction = value; } }
+        public string ExportPaymentMethod { get { return paymentType.ExportPaymentMethod; } set { paymentType.ExportPaymentMethod = value; } }
+        public string ExportPaymentType { get { return paymentType.ExportPaymentType; } set { paymentType.ExportPaymentType = value; } }
+        public bool IncludeInRentalWorksNet { get { return paymentType.IncludeInRentalWorksNet; } set { paymentType.IncludeInRentalWorksNet = value; } }
+        public string RentalWorksNetCaption { get { return paymentType.RentalWorksNetCaption; } set { paymentType.RentalWorksNetCaption = value; } }
         public bool Inactive { get { return paymentType.Inactive; } set { paymentType.Inactive = value; } }
         public string DateStamp { get { return paymentType.DateStamp; } set { paymentType.DateStamp = value; } }
         //------------------------------------------------------------------------------------
