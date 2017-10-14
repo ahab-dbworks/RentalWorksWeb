956acd42ff373d6474c55635bdb2ea30fb767002
Justin Hoffman <justin@dbworks.com>
2017-10-14 05:44:12 -0700

    Added ItemConsignor API

---
+++ src/RentalWorksWebApi/Modules/Home/ItemConsignor/ItemConsignorLogic.cs
@@ -0,0 +1,70 @@
+using FwStandard.BusinessLogic.Attributes; 
+using RentalWorksWebApi.Logic;
+namespace RentalWorksWebApi.Modules.Home.ItemConsignor
+{
+    public class ItemConsignorLogic : RwBusinessLogic
+    {
+        //------------------------------------------------------------------------------------ 
+        //ItemConsignorRecord itemConsignor = new ItemConsignorRecord();
+        ItemConsignorLoader itemConsignorLoader = new ItemConsignorLoader();
+        public ItemConsignorLogic()
+        {
+            //dataRecords.Add(itemConsignor);
+            dataLoader = itemConsignorLoader;
+        }
+        //------------------------------------------------------------------------------------ 
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string ConsignorId { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string ConsignorAgreementId { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string Consignor { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string AgreementNo { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string AgreementDescription { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string ItemId { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string ICode { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string Description { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string TrackedBy { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public bool TreatConsignedQtyAsOwned { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public int? QtyConsigned { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public int? ConsignorPercent { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public int? HousePercent { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public bool FlatRate { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public decimal? FlatRateAmount { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string RentalItemId { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string BarCode { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string SerialNumber { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string InventoryStatusId { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string InventoryStatus { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string InventoryStatusType { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string WarehouseId { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string Warehouse { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public string WarehouseCode { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public bool TextColor { get; set; }
+        [FwBusinessLogicField(isReadOnly: true)]
+        public int? Color { get; set; }
+        //------------------------------------------------------------------------------------ 
+    }
+}
