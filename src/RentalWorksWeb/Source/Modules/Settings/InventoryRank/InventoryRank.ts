bc087527edcaebcb856b9215d7a2bb987252b91e
antonio sandoval <asandoval@4wall.com>
2017-10-18 15:46:06 -0700

    Updated Inventory Rank Module and Vendor Note Grid In Vendor Module.

--- src/RentalWorksWeb/Source/Modules/Settings/InventoryRank/InventoryRank.ts
+++ src/RentalWorksWeb/Source/Modules/Settings/InventoryRank/InventoryRank.ts
@@ -42,8 +42,8 @@ class InventoryRank {
         return $browse;
     }
 
-    disableFields(): void {
-        jQuery('.disablefield').attr('data-required', 'false');
+    requiredFalse(): void {
+        jQuery('.requiredfalse').attr('data-required', 'false');
     }
 
     openForm(mode: string) {
@@ -52,8 +52,23 @@ class InventoryRank {
         $form = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Form').html());
         $form = FwModule.openForm($form, mode);
         if (mode == 'NEW') {
-            this.disableFields();
+            this.requiredFalse();
+            FwFormField.setValueByDataField($form, 'AFromValue', 0);
+            FwFormField.setValueByDataField($form, 'AToValue', 0);
+            FwFormField.setValueByDataField($form, 'BFromValue', 0);
+            FwFormField.setValueByDataField($form, 'BToValue', 0);
+            FwFormField.setValueByDataField($form, 'CFromValue', 0);
+            FwFormField.setValueByDataField($form, 'CToValue', 0);
+            FwFormField.setValueByDataField($form, 'DFromValue', 0);
+            FwFormField.setValueByDataField($form, 'DToValue', 0);
+            FwFormField.setValueByDataField($form, 'EFromValue', 0);
+            FwFormField.setValueByDataField($form, 'EToValue', 0);
+            FwFormField.setValueByDataField($form, 'FFromValue', 0);
+            FwFormField.setValueByDataField($form, 'FToValue', 0);
+            FwFormField.setValueByDataField($form, 'GFromValue', 0);
+            FwFormField.setValueByDataField($form, 'GToValue', 0);
         }        
+        
         return $form;
     }
 
@@ -63,7 +78,7 @@ class InventoryRank {
         $form = this.openForm('EDIT');
         $form.find('div.fwformfield[data-datafield="InventoryRankId"] input').val(uniqueids.InventoryRankId);
         FwModule.loadForm(this.Module, $form);        
-        this.disableFields();
+        this.requiredFalse();
         return $form;
     }
 
@@ -79,7 +94,7 @@ class InventoryRank {
 
     afterLoad($form: any) {
 
-        this.disableFields();
+        this.requiredFalse();
 
     }
 }
