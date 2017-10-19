7b4481e572c3febb1800feec7b8d9bacca6b934e
Mike Vermilion <mike@dbworks.com>
2017-10-18 18:05:26 -0700

    Adds properties to access the usersid, webusersid, and groupsid to the base controller.

--- src/RentalWorksWebApi/Controllers/RwController.cs
+++ src/RentalWorksWebApi/Controllers/RwController.cs
@@ -2,6 +2,7 @@
 using FwStandard.SqlServer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Linq;
 
 namespace RentalWorksWebApi.Controllers
 {
@@ -9,6 +10,36 @@ namespace RentalWorksWebApi.Controllers
     public class RwController : Controller  //todo: create FwController to inherit from
     {
         protected readonly ApplicationConfig _appConfig;
+        //------------------------------------------------------------------------------------
+        protected string UsersId
+        {
+            get
+            {
+                var claim = this.User.Claims.FirstOrDefault(x => x.Type == "http://www.dbworks.com/claims/usersid");
+                string usersid = (claim != null) ? claim.Value : string.Empty;
+                return usersid;
+            }
+        }
+        //------------------------------------------------------------------------------------
+        protected string WebUsersId
+        {
+            get
+            {
+                var claim = this.User.Claims.FirstOrDefault(x => x.Type == "http://www.dbworks.com/claims/webusersid");
+                string webusersid = (claim != null) ? claim.Value : string.Empty;
+                return webusersid;
+            }
+        }
+        //------------------------------------------------------------------------------------
+        protected string GroupsId
+        {
+            get
+            {
+                var claim = this.User.Claims.FirstOrDefault(x => x.Type == "http://www.dbworks.com/claims/groupsid");
+                string groupsid = (claim != null) ? claim.Value : string.Empty;
+                return groupsid;
+            }
+        }
         //------------------------------------------------------------------------------------
         public RwController(IOptions<ApplicationConfig> appConfig)
         {
