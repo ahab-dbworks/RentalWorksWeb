5b9cb1437c9cd56f24b380e40fa6d884911658ed
Jason Hoang <jhoang@4wall.com>
2017-10-25 14:42:39 -0700

    Updated G/L Distribution module

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Settings/GlDistributionMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Settings/GlDistributionMenu.cs
@@ -14,12 +14,9 @@ namespace RentalWorksWebLibrary.Security.Tree.RentalWorksWeb.Modules.Settings
             var nodeBrowseMenuBar = tree.AddMenuBar("{04283F29-8148-44C0-B1B4-CC64643CC967}", nodeBrowse.Id);
             var nodeBrowseSubMenu = tree.AddSubMenu("{D59F3FC2-D55D-4A12-A79C-54328B2F85A3}", nodeBrowseMenuBar.Id);
             var nodeBrowseExport = tree.AddSubMenuGroup("Export", "{0FC6417E-4B96-46F5-BD32-688E8115D58F}", nodeBrowseSubMenu.Id);
-            tree.AddDownloadExcelSubMenuItem("{2C156DE0-C929-4F2D-AFA3-DF98D1839957}", nodeBrowseExport.Id);
-            tree.AddNewMenuBarButton("{2322C04D-EE0F-428B-8CE8-27C1020D14B2}", nodeBrowseMenuBar.Id);
             tree.AddViewMenuBarButton("{22746185-5E0F-4D97-8D34-6977D432A0F9}", nodeBrowseMenuBar.Id);
             tree.AddEditMenuBarButton("{201FE7AA-AFC0-493A-8034-F4146E7DB57A}", nodeBrowseMenuBar.Id);
-            tree.AddDeleteMenuBarButton("{9331D6E6-8CF4-4DA8-BCA9-D510DA7A4DF2}", nodeBrowseMenuBar.Id);
-
+            
             // Form
             var nodeForm = tree.AddForm("{E403AD70-B6D7-4B39-A09D-589EB5857609}", MODULEID);
             var nodeFormMenuBar = tree.AddMenuBar("{2ED357B0-30B1-43BE-BCF7-D30AB4AFEFD4}", nodeForm.Id);
