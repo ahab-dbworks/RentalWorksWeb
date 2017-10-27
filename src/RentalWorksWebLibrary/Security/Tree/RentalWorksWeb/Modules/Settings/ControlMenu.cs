8628bdae47e7dc2462e7c827022975fde3265fbe
Jason Hoang <jhoang@4wall.com>
2017-10-27 13:53:46 -0700

    Removed Add and Delete functionality for the Control module

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Settings/ControlMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Settings/ControlMenu.cs
@@ -14,12 +14,9 @@ namespace RentalWorksWebLibrary.Security.Tree.RentalWorksWeb.Modules.Settings
             var nodeBrowseMenuBar = tree.AddMenuBar("{D02BE59C-5CFD-43F0-BA18-6147C405172A}", nodeBrowse.Id);
             var nodeBrowseSubMenu = tree.AddSubMenu("{C8C6DD27-79C0-4B8B-9464-E102ED86A388}", nodeBrowseMenuBar.Id);
             var nodeBrowseExport = tree.AddSubMenuGroup("Export", "{DE9D33EB-26ED-4B53-B8AF-0A5207F35632}", nodeBrowseSubMenu.Id);
-            tree.AddDownloadExcelSubMenuItem("{C1185EB1-DD56-43F3-BF1B-B766A8B4697F}", nodeBrowseExport.Id);
-            tree.AddNewMenuBarButton("{DB3FBC0E-7B35-46F5-AE2A-AB4966572972}", nodeBrowseMenuBar.Id);
             tree.AddViewMenuBarButton("{E9608D08-B235-454D-AE3E-75462BEE9638}", nodeBrowseMenuBar.Id);
             tree.AddEditMenuBarButton("{8DCE4D50-BBCE-4147-A05C-A0A00755EA11}", nodeBrowseMenuBar.Id);
-            tree.AddDeleteMenuBarButton("{00824D52-B838-4F14-B380-928777D2CC46}", nodeBrowseMenuBar.Id);
-
+            
             // Form
             var nodeForm = tree.AddForm("{58FEB457-0852-4A78-9322-E438A5E736E8}", MODULEID);
             var nodeFormMenuBar = tree.AddMenuBar("{6E613966-4DDE-4A44-AFFE-7DC527685329}", nodeForm.Id);
