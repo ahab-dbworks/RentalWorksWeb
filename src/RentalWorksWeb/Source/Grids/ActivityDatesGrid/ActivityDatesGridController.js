6f20f437dc1417ee837097da622c7dc6772758bb
Jason Hoang <jhoang@4wall.com>
2017-11-08 13:26:23 -0800

    Base for Activity Dates grid

---
+++ src/RentalWorksWeb/Source/Grids/ActivityDatesGrid/ActivityDatesGridController.js
@@ -0,0 +1,10 @@
+var ActivityDatesGrid = (function () {
+    function ActivityDatesGrid() {
+        this.Module = 'ActivityDatesGrid';
+        this.apiurl = 'api/v1/activitydates';
+    }
+    return ActivityDatesGrid;
+}());
+window.ActivityDatesGridController = new ActivityDatesGrid();
+//---------------------------------------------------------------------------------------------- 
+//# sourceMappingURL=ActivityDatesGridController.js.map
---
+++ src/RentalWorksWeb/Source/Grids/ActivityDatesGrid/ActivityDatesGridController.ts
@@ -0,0 +1,12 @@
+﻿class ActivityDatesGrid {
+    Module: string;
+    apiurl: string;
+
+    constructor() {
+        this.Module = 'ActivityDatesGrid';
+        this.apiurl = 'api/v1/activitydates';
+    }
+}
+
+(<any>window).ActivityDatesGridController = new ActivityDatesGrid();
+//----------------------------------------------------------------------------------------------
