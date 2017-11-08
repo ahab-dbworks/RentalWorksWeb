2d6e24a3a171940df00a31fa584da680fcd5d537
Jason Hoang <jhoang@4wall.com>
2017-11-08 13:26:37 -0800

    Base for Terms and Conditions grid

---
+++ src/RentalWorksWeb/Source/Grids/TermsAndConditionsGrid/TermsAndConditionsGridController.js
@@ -0,0 +1,10 @@
+var TermsAndConditionsGrid = (function () {
+    function TermsAndConditionsGrid() {
+        this.Module = 'TermsAndConditions';
+        this.apiurl = 'api/v1/ordertypelocation';
+    }
+    return TermsAndConditionsGrid;
+}());
+window.TermsAndConditionsGridController = new TermsAndConditionsGrid();
+//---------------------------------------------------------------------------------------------- 
+//# sourceMappingURL=TermsAndConditionsGridController.js.map
---
+++ src/RentalWorksWeb/Source/Grids/TermsAndConditionsGrid/TermsAndConditionsGridController.ts
@@ -0,0 +1,12 @@
+﻿class TermsAndConditionsGrid {
+    Module: string;
+    apiurl: string;
+
+    constructor() {
+        this.Module = 'TermsAndConditions';
+        this.apiurl = 'api/v1/ordertypelocation';
+    }
+}
+
+(<any>window).TermsAndConditionsGridController = new TermsAndConditionsGrid();
+//----------------------------------------------------------------------------------------------
