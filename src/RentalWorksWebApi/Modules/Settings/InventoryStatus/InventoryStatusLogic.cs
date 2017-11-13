f43f3f9dae42ac491c2bab1b8c5d47e69f6e5110
Justin Hoffman <justin@dbworks.com>
2017-11-13 00:26:56 -0800

    Added row background color to InventoryStatus Browse

--- src/RentalWorksWebApi/Modules/Settings/InventoryStatus/InventoryStatusLogic.cs
+++ src/RentalWorksWebApi/Modules/Settings/InventoryStatus/InventoryStatusLogic.cs
@@ -1,5 +1,7 @@
 ﻿using FwStandard.BusinessLogic.Attributes;
+using FwStandard.SqlServer;
 using RentalWorksWebApi.Logic;
+
 
 namespace RentalWorksWebApi.Modules.Settings.RentalStatus
 {
@@ -19,6 +21,9 @@ namespace RentalWorksWebApi.Modules.Settings.RentalStatus
         public string StatusType { get { return rentalStatus.StatusType; } set { rentalStatus.StatusType = value; } }
         public string Color { get { return rentalStatus.Color; } set { rentalStatus.Color = value; } }
         public bool WhiteText { get { return rentalStatus.WhiteText; } set { rentalStatus.WhiteText = value; } }
+        //[FwBusinessLogicField(isReadOnly: true)]
+        //public string TextColor { get { return (WhiteText ? FwConvert.OleColorToHtmlColor(System.Drawing.Color.White.) : FwConvert.OleColorToHtmlColor(0)); } }
+        //public string TextColor { get { return (WhiteText ? "rgb(255,255,255" : "rgb(0,0,0"); } }
         public string DateStamp { get { return rentalStatus.DateStamp; } set { rentalStatus.DateStamp = value; } }
         //------------------------------------------------------------------------------------
     }
