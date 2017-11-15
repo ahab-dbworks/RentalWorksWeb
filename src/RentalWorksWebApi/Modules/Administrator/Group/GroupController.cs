2dd77cdf589ba09227db79c64d77f3916ad1d1c6
Justin Hoffman <justin@dbworks.com>
2017-11-15 15:37:44 -0800

    Added Group API

---
+++ src/RentalWorksWebApi/Modules/Administrator/Group/GroupController.cs
@@ -0,0 +1,56 @@
+using FwStandard.Models; 
+using Microsoft.AspNetCore.Mvc; 
+using Microsoft.Extensions.Options; 
+using RentalWorksWebApi.Controllers; 
+using System.Threading.Tasks;
+namespace RentalWorksWebApi.Modules.Administrator.Group
+{
+    [Route("api/v1/[controller]")]
+    public class GroupController : RwDataController
+    {
+        public GroupController(IOptions<ApplicationConfig> appConfig) : base(appConfig) { }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/group/browse 
+        [HttpPost("browse")]
+        public async Task<IActionResult> BrowseAsync([FromBody]BrowseRequestDto browseRequest)
+        {
+            return await DoBrowseAsync(browseRequest, typeof(GroupLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // GET api/v1/group 
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery]int pageno, [FromQuery]int pagesize, [FromQuery]string sort)
+        {
+            return await DoGetAsync<GroupLogic>(pageno, pagesize, sort, typeof(GroupLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // GET api/v1/group/A0000001 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync([FromRoute]string id)
+        {
+            return await DoGetAsync<GroupLogic>(id, typeof(GroupLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/group 
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]GroupLogic l)
+        {
+            return await DoPostAsync<GroupLogic>(l);
+        }
+        //------------------------------------------------------------------------------------ 
+        // DELETE api/v1/group/A0000001 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute]string id)
+        {
+            return await DoDeleteAsync(id, typeof(GroupLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/group/validateduplicate 
+        [HttpPost("validateduplicate")]
+        public async Task<IActionResult> ValidateDuplicateAsync([FromBody]ValidateDuplicateRequest request)
+        {
+            return await DoValidateDuplicateAsync(request);
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
---
+++ src/RentalWorksWebApi/Modules/Administrator/Group/GroupRecord.cs
@@ -0,0 +1,48 @@
+using FwStandard.BusinessLogic; 
+using FwStandard.SqlServer; 
+using FwStandard.SqlServer.Attributes; 
+using RentalWorksWebApi.Data;
+namespace RentalWorksWebApi.Modules.Administrator.Group
+{
+    [FwSqlTable("groups")]
+    public class GroupRecord : RwDataReadWriteRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "groupsid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, isPrimaryKey: true)]
+        public string GroupId { get; set; } = "";
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "name", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 30)]
+        public string Name { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "memo", modeltype: FwDataTypes.Text, sqltype: "varchar", maxlength: 255)]
+        public string Memo { get; set; }
+        //------------------------------------------------------------------------------------
+        //[FwSqlDataField(column: "hidesecitems", modeltype: FwDataTypes.Boolean, sqltype: "char")]
+        //public bool Hidesecitems { get; set; }
+        ////------------------------------------------------------------------------------------ 
+        //[FwSqlDataField(column: "components", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 10)]
+        //public string Components { get; set; }
+        ////------------------------------------------------------------------------------------ 
+        //[FwSqlDataField(column: "menuitem", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 10)]
+        //public string Menuitem { get; set; }
+        ////------------------------------------------------------------------------------------ 
+        //[FwSqlDataField(column: "disablevalidationoptions", modeltype: FwDataTypes.Boolean, sqltype: "char")]
+        //public bool Disablevalidationoptions { get; set; }
+        ////------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "security", modeltype: FwDataTypes.Text, sqltype: "varchar", maxlength: -1)]
+        public string Security { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "hidenewmenuoptionsbydefault", modeltype: FwDataTypes.Boolean, sqltype: "char")]
+        public bool HideNewMenuOptionsByDefault { get; set; }
+        //------------------------------------------------------------------------------------ 
+        //[FwSqlDataField(column: "qehideaccessories", modeltype: FwDataTypes.Boolean, sqltype: "char")]
+        //public bool Qehideaccessories { get; set; }
+        ////------------------------------------------------------------------------------------ 
+        //[FwSqlDataField(column: "securitydefaultoff", modeltype: FwDataTypes.Boolean, sqltype: "char")]
+        //public bool Securitydefaultoff { get; set; }
+        ////------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "datestamp", modeltype: FwDataTypes.UTCDateTime, sqltype: "datetime")]
+        public string DateStamp { get; set; }
+        //------------------------------------------------------------------------------------ 
+    }
+}
