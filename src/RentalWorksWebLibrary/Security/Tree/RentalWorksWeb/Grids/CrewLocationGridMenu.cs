225a3e72b297454672f99d03f58a1d83aa0ed0c3
Unknown <oliver@dbworks.com>
2017-11-17 10:29:49 -0800

    Adds validation, fixes for crew position and labor grids

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/CrewLocationGridMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/CrewLocationGridMenu.cs
@@ -11,6 +11,8 @@ namespace RentalWorksWebLibrary.Security.Tree.RentalWorksWeb.Grids
         {
             var nodeGridMenuBar = tree.AddMenuBar("{58B2D26A-E373-497D-AF08-CD4E44417E22}", MODULEID);
             tree.AddEditMenuBarButton("{468E59BD-A83B-40A3-9639-CFA2C1D80DA8}", nodeGridMenuBar.Id);
+            tree.AddNewMenuBarButton("{27C3387C-6402-4F04-99C3-206216E8C649}", nodeGridMenuBar.Id);
+            tree.AddDeleteMenuBarButton("{A63B3B97-AF16-4D88-BC33-C0EBB7297493}", nodeGridMenuBar.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/CrewPositionGridMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/CrewPositionGridMenu.cs
@@ -11,6 +11,8 @@ namespace RentalWorksWebLibrary.Security.Tree.RentalWorksWeb.Grids
         {
             var nodeGridMenuBar = tree.AddMenuBar("{F9873F73-4466-4CDC-AE4F-0445BD2ECBF9}", MODULEID);
             tree.AddEditMenuBarButton("{F17D11EF-1A10-46B1-B682-5B3919A57318}", nodeGridMenuBar.Id);
+            tree.AddNewMenuBarButton("{F04F8656-2B68-4E6E-92AE-72EC8B7D0E37}", nodeGridMenuBar.Id);
+            tree.AddDeleteMenuBarButton("{9483C712-5DF2-4555-AC2D-FBD235146CCB}", nodeGridMenuBar.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
