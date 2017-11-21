40ee99bdaaa655149b61c81d76fb0b4b08f2639b
Unknown <oliver@dbworks.com>
2017-11-21 15:20:09 -0800

    Adds searching by field, search now requires enter keypress instead of keystroke

--- src/RentalWorksWeb/Source/Modules/Administrator/SettingsPage/SettingsPageController.ts
+++ src/RentalWorksWeb/Source/Modules/Administrator/SettingsPage/SettingsPageController.ts
@@ -27,6 +27,7 @@ class SettingsPage {
         //screen.$view = FwHybridMasterController.getMasterView(combinedViewModel);
         screen.$view = FwModule.getModuleControl(this.Module + "Controller");
         screen.properties = {};
+        screen.moduleCaptions = {};
         $settings = this.openSettings();
 
 
@@ -35,12 +36,14 @@ class SettingsPage {
             FwModule.openModuleTab($settings, 'Settings', false, 'SETTINGS', true)
             var node = FwApplicationTree.getNodeById(FwApplicationTree.tree, '730C9659-B33B-493E-8280-76A060A07DCE');
             var modules = FwApplicationTree.getChildrenByType(node, 'Module');
+            var moduleTemplates = {};
             console.log(modules)
             for (var i = 0; i < modules.length; i++) {
-                var moduleName = modules[i].properties.controller.slice(0, -10)
-                FwSettings.renderModuleHtml($settings.find(".fwsettings"), modules[i].properties.caption, moduleName, modules[i].properties.color)
-                FwSettings.renderRecordHtml($settings, moduleName)
+                var moduleName = modules[i].properties.controller.slice(0, -10);
+                FwSettings.renderModuleHtml($settings.find(".fwsettings"), modules[i].properties.caption, moduleName, modules[i].properties.color);
+                FwSettings.renderRecordHtml($settings, moduleName);
             }
+            FwSettings.getCaptions(screen);
 
             //FwAppData.apiMethod(false, 'GET', applicationConfig.appbaseurl + applicationConfig.appvirtualdirectory + 'SettingsPage.json', null, null, function onSuccess(response) {
             //    response.Modules.forEach(function (module) {
@@ -49,8 +52,34 @@ class SettingsPage {
             //        FwSettings.renderRecordHtml($settings, module.Title)
             //    })
             //}, null, jQuery('html'));
-            
 
+            $settings.on('keypress', '#moduleSearch', function (e) {                
+                if (e.which === 13) {
+                    e.preventDefault();
+                    var $settings, val;
+                    $settings = jQuery('small#description');
+                    val = jQuery.trim(this.value).toUpperCase();
+                    if (val === "") {
+                        $settings.closest('div.panel-group').show();
+                    }
+                    if (val.length > 2) {
+                        var results = [];
+                        $settings.closest('div.panel-group').hide();
+                        for (var caption in screen.moduleCaptions) {
+                            if (caption.indexOf(val) !== -1) {
+                                for (var moduleName in screen.moduleCaptions[caption]) {
+                                    results.push(moduleName.toUpperCase());
+                                }
+                            }
+                        }
+                        for (var i = 0; i < results.length; i++) {
+                            $settings.filter(function () {
+                                return -1 != jQuery(this).text().toUpperCase().indexOf(results[i]);
+                            }).closest('div.panel-group').show()
+                        }
+                    }
+                }      
+            })
         };
         screen.unload = function () {
 
