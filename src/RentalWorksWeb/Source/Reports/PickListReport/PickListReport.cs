4b66312e11636f7b12f637e6d8478126a77466bd
Jason Hoang <jhoang@4wall.com>
2017-12-20 17:15:49 -0800

    Updated Pick List Report print formatting and fields

--- src/RentalWorksWeb/Source/Reports/PickListReport/PickListReport.cs
+++ src/RentalWorksWeb/Source/Reports/PickListReport/PickListReport.cs
@@ -14,13 +14,31 @@ namespace Web.Source.Reports
         //---------------------------------------------------------------------------------------------
         protected override string renderHeaderHtml(string styletemplate, string headertemplate, FwReport.PrintOptions printOptions)
         {
+            FwSqlSelect select;
+            FwSqlCommand qry;
+            FwJsonDataTable dtDetails;
+
+            qry = new FwSqlCommand(FwSqlConnection.RentalWorks, FwQueryTimeouts.Report);
+            select = new FwSqlSelect();
+            select.Add("select top 1 *");
+            select.Add("from  picklistrptview with (nolock)");
+            select.Add("where picklistid = @picklistid");
+            select.Add("order by orderno, pickdate, rectypesequence, itemorder, masterno");
+            select.AddParameter("@picklistid", request.parameters.PickListId);
+
+            select.Parse();
+            dtDetails = qry.QueryToFwJsonTable(select, true);
+
             StringBuilder sb;
             string html;
       
             sb = new StringBuilder(base.renderHeaderHtml(styletemplate, headertemplate, printOptions));
+
             sb.Replace("[LBLREPORTNAME]", getReportName());
+      
 
             html = sb.ToString();
+            html = this.applyTableToTemplate(html, "header", dtDetails);
 
             return html;
         }
@@ -32,14 +50,12 @@ namespace Web.Source.Reports
             StringBuilder sb;
 
             dtPickListReport = GetPickListReport();
-
-            html        = base.renderBodyHtml(styletemplate, bodytemplate, printOptions);
+     
+            html = base.renderBodyHtml(styletemplate, bodytemplate, printOptions);
             sb          = new StringBuilder(base.renderBodyHtml(styletemplate, bodytemplate, printOptions));
-        
-
-
+            sb.Replace("[TotalRows]", "Total Rows: " + dtPickListReport.Rows.Count);
             html        = sb.ToString();
-            html        = this.applyTableToTemplate(html, "details", dtPickListReport);
+            html = this.applyTableToTemplate(html, "details", dtPickListReport);
 
             return html;
         }
@@ -51,14 +67,14 @@ namespace Web.Source.Reports
             FwJsonDataTable dtDetails;
 
             qry = new FwSqlCommand(FwSqlConnection.RentalWorks, FwQueryTimeouts.Report);
-            qry.AddColumn("orderdate",       false, FwJsonDataTableColumn.DataTypes.Date);
-            qry.AddColumn("estrentfrom",     false, FwJsonDataTableColumn.DataTypes.Date);
-            qry.AddColumn("estrentto",       false, FwJsonDataTableColumn.DataTypes.Date);
-            qry.AddColumn("billperiodstart", false, FwJsonDataTableColumn.DataTypes.Date);
-            qry.AddColumn("billperiodend",   false, FwJsonDataTableColumn.DataTypes.Date);
-            qry.AddColumn("contractdate",    false, FwJsonDataTableColumn.DataTypes.Date);
-            qry.AddColumn("image",           false, FwJsonDataTableColumn.DataTypes.JpgDataUrl);
-            qry.AddColumn("itemvalue",       false, FwJsonDataTableColumn.DataTypes.CurrencyStringNoDollarSign);
+            //qry.AddColumn("orderdate",       false, FwJsonDataTableColumn.DataTypes.Date);
+            //qry.AddColumn("estrentfrom",     false, FwJsonDataTableColumn.DataTypes.Date);
+            //qry.AddColumn("estrentto",       false, FwJsonDataTableColumn.DataTypes.Date);
+            //qry.AddColumn("billperiodstart", false, FwJsonDataTableColumn.DataTypes.Date);
+            //qry.AddColumn("billperiodend",   false, FwJsonDataTableColumn.DataTypes.Date);
+            //qry.AddColumn("contractdate",    false, FwJsonDataTableColumn.DataTypes.Date);
+            //qry.AddColumn("image",           false, FwJsonDataTableColumn.DataTypes.JpgDataUrl);
+            //qry.AddColumn("itemvalue",       false, FwJsonDataTableColumn.DataTypes.CurrencyStringNoDollarSign);
 
             select = new FwSqlSelect();
             select.Add("select *");
