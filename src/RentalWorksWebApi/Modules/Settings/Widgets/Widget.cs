87913c0f94c51bd9053efa829c51c8a3ad33d955
Justin Hoffman <justin@dbworks.com>
2017-12-21 07:51:20 -0800

    First pass, hard-coded test of a Widget control and API.

---
+++ src/RentalWorksWebApi/Modules/Settings/Widgets/Widget.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi.Modules.Settings.Widgets
+{
+    //------------------------------------------------------------------------------------
+    public class WidgetDataSet
+    {
+        public List<int> Data { get; set; }
+        public List<string> BackgroundColor { get; set; }
+        public List<string> BorderColor { get; set; }
+        public int BorderWidth { get; set; }
+        public WidgetDataSet()
+        {
+            Data = new List<int>();
+            BackgroundColor = new List<string>();
+            BorderColor = new List<string>();
+            BorderWidth = 1;
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetData
+    {
+        public List<string> Labels { get; set; }
+        public List<WidgetDataSet> DataSets { get; set; }
+        public WidgetData()
+        {
+            Labels = new List<string>();
+            DataSets = new List<WidgetDataSet>();
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetTitle
+    {
+        public bool Display { get; set; }
+        public string Text { get; set; }
+        public WidgetTitle()
+        {
+            Display = true;
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetLegend
+    {
+        public bool Display { get; set; }
+        public WidgetLegend()
+        {
+            Display = true;
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetAxisTicks
+    {
+        public bool BeginAtZero { get; set; }
+        public WidgetAxisTicks()
+        {
+            BeginAtZero = true;
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetAxis
+    {
+        public WidgetAxisTicks Ticks { get; set; }
+        public WidgetAxis()
+        {
+            Ticks = new WidgetAxisTicks();
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetScales
+    {
+        public List<WidgetAxis> YAxes { get; set; }
+        public WidgetScales()
+        {
+            YAxes = new List<WidgetAxis>();
+            YAxes.Add(new WidgetAxis());
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class WidgetOptions
+    {
+        public WidgetTitle Title { get; set; }
+        public WidgetLegend Legend { get; set; }
+        public WidgetScales Scales { get; set; }
+        public bool Responsive { get; set; }
+        public bool MaintainAspectRatio { get; set; }
+        public WidgetOptions()
+        {
+            Title = new WidgetTitle();
+            Legend = new WidgetLegend();
+            Scales = new WidgetScales();
+        }
+    }
+    //------------------------------------------------------------------------------------
+    public class Widget
+    {
+        public string ChartType { get; set; }
+        public WidgetData Data { get; set; }
+        public WidgetOptions Options { get; set; }
+
+        public Widget()
+        {
+            Data = new WidgetData();
+            Options = new WidgetOptions();
+        }
+    }
+    //------------------------------------------------------------------------------------
+
+}
---
+++ src/RentalWorksWebApi/Modules/Settings/Widgets/WidgetController.cs
@@ -0,0 +1,94 @@
+﻿using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace WebApi.Modules.Settings.Widgets
+{
+
+
+    [Route("api/v1/[controller]")]
+    public class WidgetController : AppDataController
+    {
+        public WidgetController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+
+        //------------------------------------------------------------------------------------
+        private async Task<IActionResult> DoGetWidget(string widgetName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Widget w = new Widget();
+                w.ChartType = "bar";
+                w.Data.Labels.Add("Confirmed");
+                w.Data.Labels.Add("Hold");
+                w.Data.Labels.Add("Active");
+                w.Data.Labels.Add("Complete");
+                w.Data.Labels.Add("Closed");
+                w.Data.Labels.Add("Cancelled");
+
+                w.Options.Title.Text = "Orders By Status";
+
+                List<int> data = new List<int>();
+                data.Add(251);  // confirmed
+                data.Add(5);    // hold
+                data.Add(170);  // active
+                data.Add(102);  // complete
+                data.Add(84);   // closed
+                data.Add(200);  // cancelled
+
+                w.Data.DataSets.Add(new WidgetDataSet());
+                w.Data.DataSets[0].Data = data;
+
+
+                List<string> backgroundColor = new List<string>();
+                backgroundColor.Add("rgba(255, 99, 132, 0.2)");
+                backgroundColor.Add("rgba(54, 162, 235, 0.2)");
+                backgroundColor.Add("rgba(255, 206, 86, 0.2)");
+                backgroundColor.Add("rgba(75, 192, 192, 0.2)");
+                backgroundColor.Add("rgba(153, 102, 255, 0.2)");
+                backgroundColor.Add("rgba(255, 159, 64, 0.2)");
+
+                w.Data.DataSets[0].BackgroundColor = backgroundColor;
+
+                List<string> borderColor = new List<string>();
+                borderColor.Add("rgba(255,99,132,1)");
+                borderColor.Add("rgba(54, 162, 235, 1)");
+                borderColor.Add("rgba(255, 206, 86, 1)");
+                borderColor.Add("rgba(75, 192, 192, 1)");
+                borderColor.Add("rgba(153, 102, 255, 1)");
+                borderColor.Add("rgba(255, 159, 64, 1)");
+
+                w.Data.DataSets[0].BorderColor = borderColor;
+
+
+                return new OkObjectResult(w);
+
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------
+
+        // GET api/v1/widget/ordersbystatus
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetAsync([FromRoute]string widgetName)
+        {
+            return await DoGetWidget(widgetName);
+        }
+
+    }
+}
