eb5636a343e96a92d89f1eafd2ec009261334840
Justin Hoffman <justin@dbworks.com>
2018-02-02 15:43:55 -0800

    Updated logic to display peek "..." button based on default system setting

--- src/RentalWorksWeb/ApplicationConfig.sample.js
+++ src/RentalWorksWeb/ApplicationConfig.sample.js
@@ -1,1 +1,5 @@
-﻿
+﻿applicationConfig.debugMode = true;
+applicationConfig.designMode = false;
+applicationConfig.demoMode = false;
+applicationConfig.apiurl = 'http://localhost:57949/';
+applicationConfig.defaultPeek = true;
