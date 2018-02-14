f5e0d1ef84282e05e866db549a9907a677db77d8
Jason Hoang <jhoang@4wall.com>
2018-02-14 09:27:21 -0800

    Added back missing Order Item grid

---
+++ src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.ts
@@ -0,0 +1,12 @@
+﻿﻿class OrderItemGrid {
+    Module: string;
+    apiurl: string;
+
+    constructor() {
+        this.Module = 'OrderItemGrid';
+        this.apiurl = 'api/v1/orderitem';
+    }
+}
+
+(<any>window).OrderItemGridController = new OrderItemGrid();
+//----------------------------------------------------------------------------------------------
