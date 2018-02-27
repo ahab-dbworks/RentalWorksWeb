ee5683e73721f28e255cf68ca71dc25cc9e44794
Justin Hoffman <justin@dbworks.com>
2018-02-27 15:31:15 -0800

    Added Customer filter for Deal API

--- src/RentalWorksWebApi/Modules/Home/Deal/DealLoader.cs
+++ src/RentalWorksWebApi/Modules/Home/Deal/DealLoader.cs
@@ -470,5 +470,13 @@ namespace WebApi.Modules.Home.Deal
         [FwSqlDataField(column: "securitydepositamt", modeltype: FwDataTypes.Decimal)]
         public decimal? SecurityDepositAmount { get; set; }
         //------------------------------------------------------------------------------------ 
+        protected override void SetBaseSelectQuery(FwSqlSelect select, FwSqlCommand qry, FwCustomFields customFields = null, BrowseRequest request = null)
+        {
+            base.SetBaseSelectQuery(select, qry, customFields, request);
+            select.Parse();
+            //select.AddWhere("(xxxtype = 'ABCDEF')"); 
+            addFilterToSelect("CustomerId", "customerid", select, request);
+        }
+        //------------------------------------------------------------------------------------ 
     }
 }
