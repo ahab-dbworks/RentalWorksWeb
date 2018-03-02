c338f982602f56ff95282fe496eca1e24df98cde
Joshua Pace <jpace@4wall.com>
2018-03-02 09:51:08 -0800

    Customer Note Grid bug fix in regard to dates not rendering in view

--- src/RentalWorksWeb/Source/Grids/CustomerNoteGrid/CustomerNoteGridController.ts
+++ src/RentalWorksWeb/Source/Grids/CustomerNoteGrid/CustomerNoteGridController.ts
@@ -1,19 +1,14 @@
 ﻿class CustomerNoteGrid {
-    Module: string;
-    apiurl: string;
-
-    constructor() {
-        this.Module = 'CustomerNoteGrid';
-        this.apiurl = 'api/v1/customernote';
-    }
+    Module: string = 'CustomerNoteGrid';
+    apiurl = 'api/v1/customernote';
 
     onRowNewMode($control: JQuery, $tr: JQuery) {
-        // need to default the date and user, haven't tested the date code below, so leaving commented for now
-        //var date = new Date();
-        //var formattedDate = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
-        //$tr.find('[data-formdatafield="NoteDate"] input.value').val(formattedDate);
-        //$tr.find('[data-formdatafield="NotesBy"] input.value').val();
-        //$tr.find('[data-formdatafield="NotesBy"] input.text').val();
+         // need to default the date and user, haven't tested the date code below, so leaving commented for now
+    //    var date = new Date();
+    //    var formattedDate = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+    //    $tr.find('[data-formdatafield="NoteDate"] input.value').val(formattedDate);
+    //    $tr.find('[data-formdatafield="NotesBy"] input.value').val();
+    //    $tr.find('[data-formdatafield="NotesBy"] input.text').val();
     }
 }
 
