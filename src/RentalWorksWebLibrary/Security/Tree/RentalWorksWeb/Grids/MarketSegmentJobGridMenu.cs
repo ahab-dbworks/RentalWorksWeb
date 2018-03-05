62e76c4f7151d74b2575c3e783b29d1fa28b5290
Joshua Pace <jpace@4wall.com>
2018-03-05 15:46:34 -0800

    Adds edit, delete abilities to Market Segment Job Grid, deletes unneeded .js. map files, Adds Default Type radio element to widget

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/MarketSegmentJobGridMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/MarketSegmentJobGridMenu.cs
@@ -11,6 +11,8 @@ namespace WebLibrary.Security.Tree.RentalWorksWeb.Grids
         {
             var nodeGridMenuBar = tree.AddMenuBar("{0A014A97-9FC2-4ADA-A225-8CD2ADCC6A84}", MODULEID);
             tree.AddEditMenuBarButton("{FD6BFA89-4627-4AB5-879B-8720ACDFDAB6}", nodeGridMenuBar.Id);
+            tree.AddNewMenuBarButton("{A5A508A7-66B6-45CB-9B69-E3C092695592}", nodeGridMenuBar.Id);
+            tree.AddDeleteMenuBarButton("{B953B4A9-3717-4A66-A98C-75E6383FE218}", nodeGridMenuBar.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
