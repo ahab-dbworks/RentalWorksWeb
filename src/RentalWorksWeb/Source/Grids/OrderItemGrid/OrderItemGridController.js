59e672c09382e72e5839bb4c65a181228f8a1b75
Jason Hoang <jhoang@4wall.com>
2018-03-07 12:24:17 -0800

    Updated Order Item grid to properly display defaulted warehouse fields

--- src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.js
+++ src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.js
@@ -8,15 +8,18 @@ var OrderItemGrid = (function () {
             var $form = $control.closest('.fwform');
             var toDate = FwFormField.getValueByDataField($form, 'EstimatedStopDate');
             var fromDate = FwFormField.getValueByDataField($form, 'EstimatedStartDate');
-            var warehouse = FwFormField.getValueByDataField($form, 'WarehouseId');
+            var warehouse = FwFormField.getValueByDataField($form, 'Warehouse');
+            var warehouseId = FwFormField.getValueByDataField($form, 'WarehouseId');
             if ($generatedtr.hasClass("newmode")) {
                 $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
                 $generatedtr.find('.field[data-browsedatafield="FromDate"] input').val(fromDate);
                 $generatedtr.find('.field[data-browsedatafield="ToDate"] input').val(toDate);
                 $generatedtr.find('.field[data-browsedatafield="QuantityOrdered"] input').val("1");
                 $generatedtr.find('.field[data-browsedatafield="SubQuantity"] input').val("0");
-                $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input').val(warehouse);
-                $generatedtr.find('.field[data-browsedatafield="ReturnWarehouseId"] input').val(warehouse);
+                $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input').val(warehouseId);
+                $generatedtr.find('.field[data-browsedatafield="ReturnToWarehouseId"] input').val(warehouseId);
+                $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input.text').val(warehouse);
+                $generatedtr.find('.field[data-browsedatafield="ReturnToWarehouseId"] input.text').val(warehouse);
             }
         });
     };
