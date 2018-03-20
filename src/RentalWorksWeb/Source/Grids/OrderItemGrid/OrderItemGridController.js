ce4812c33be5342f6c7f343a199b2e3347215a9c
Jason Hoang <jhoang@4wall.com>
2018-03-20 11:46:30 -0700

    Fixed issue with validations on Order Item grid for Template module

--- src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.js
+++ src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.js
@@ -6,14 +6,18 @@ var OrderItemGrid = (function () {
     OrderItemGrid.prototype.generateRow = function ($control, $generatedtr) {
         $generatedtr.find('div[data-browsedatafield="InventoryId"]').data('onchange', function ($tr) {
             var $form = $control.closest('.fwform');
-            var toDate = FwFormField.getValueByDataField($form, 'EstimatedStopDate');
-            var fromDate = FwFormField.getValueByDataField($form, 'EstimatedStartDate');
+            if ($form[0].dataset.controller !== "TemplateController") {
+                var toDate = FwFormField.getValueByDataField($form, 'EstimatedStopDate');
+                var fromDate = FwFormField.getValueByDataField($form, 'EstimatedStartDate');
+            }
             var warehouse = FwFormField.getValueByDataField($form, 'Warehouse');
             var warehouseId = FwFormField.getValueByDataField($form, 'WarehouseId');
             if ($generatedtr.hasClass("newmode")) {
                 $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
-                $generatedtr.find('.field[data-browsedatafield="FromDate"] input').val(fromDate);
-                $generatedtr.find('.field[data-browsedatafield="ToDate"] input').val(toDate);
+                if ($form[0].dataset.controller !== "TemplateController") {
+                    $generatedtr.find('.field[data-browsedatafield="FromDate"] input').val(fromDate);
+                    $generatedtr.find('.field[data-browsedatafield="ToDate"] input').val(toDate);
+                }
                 $generatedtr.find('.field[data-browsedatafield="QuantityOrdered"] input').val("1");
                 $generatedtr.find('.field[data-browsedatafield="SubQuantity"] input').val("0");
                 $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input').val(warehouseId);
