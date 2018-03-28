3339669fcba75a590cf9a08677a786b09bc096a0
Unknown <jpace@4wall.com>
2018-03-28 16:35:09 -0700

    Adjusts page-size for both repaircost and repairpart grid to allow for totaling large number of entries

--- src/RentalWorksWeb/Source/Grids/RepairCostGrid/RepairCostGridController.ts
+++ src/RentalWorksWeb/Source/Grids/RepairCostGrid/RepairCostGridController.ts
@@ -1,13 +1,13 @@
 ﻿class RepairCostGrid {
-  Module: string = 'RepairCostGrid';
-  apiurl: string = 'api/v1/repaircost';
+    Module: string = 'RepairCostGrid';
+    apiurl: string = 'api/v1/repaircost';
 
-  generateRow($control, $generatedtr) {
-    $generatedtr.find('div[data-browsedatafield="RateId"]').data('onchange', function ($tr) {
-      $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
-      $generatedtr.find('.field[data-browsedatafield="Unit"] input').val($tr.find('.field[data-browsedatafield="Unit"]').attr('data-originalvalue'));
-    });
-  };
+    generateRow($control, $generatedtr) {
+        $generatedtr.find('div[data-browsedatafield="RateId"]').data('onchange', $tr => {
+            $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="Unit"] input').val($tr.find('.field[data-browsedatafield="Unit"]').attr('data-originalvalue'));
+        });
+    };
 }
 
 var RepairCostGridController = new RepairCostGrid();
--- src/RentalWorksWeb/Source/Grids/RepairPartGrid/RepairPartGridController.ts
+++ src/RentalWorksWeb/Source/Grids/RepairPartGrid/RepairPartGridController.ts
@@ -1,17 +1,17 @@
 ﻿class RepairPartGrid {
-  Module: string = 'RepairPartGrid';
-  apiurl: string = 'api/v1/repairpart';
+    Module: string = 'RepairPartGrid';
+    apiurl: string = 'api/v1/repairpart';
   
-  generateRow($control, $generatedtr, $form) {
-    var warehouse = JSON.parse(sessionStorage.getItem('warehouse')).warehouse;
-    var warehouseId = JSON.parse(sessionStorage.getItem('warehouse')).warehouseid;
+    generateRow($control, $generatedtr, $form) {
+        const warehouse = JSON.parse(sessionStorage.getItem('warehouse')).warehouse;
+        const warehouseId = JSON.parse(sessionStorage.getItem('warehouse')).warehouseid;
 
-    $generatedtr.find('div[data-browsedatafield="InventoryId"]').data('onchange', function ($tr) {
-      $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
-      $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input.text').val(warehouse);
-      $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input.value').val(warehouseId);
-    });
-  };
+        $generatedtr.find('div[data-browsedatafield="InventoryId"]').data('onchange', $tr => {
+            $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input.text').val(warehouse);
+            $generatedtr.find('.field[data-browsedatafield="WarehouseId"] input.value').val(warehouseId);
+        });
+    };
 }
  
 var RepairPartGridController = new RepairPartGrid();
