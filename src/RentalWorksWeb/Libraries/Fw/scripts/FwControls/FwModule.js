fca812934a7c13bfff153546207ab363a4dfd119
Oliver Wang <oliver@dbworks.com>
2018-04-04 16:36:02 -0700

    Removes cancel button from pop up after settings page

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwModule.js
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwModule.js
@@ -863,7 +863,9 @@ var FwModule = (function () {
                 $confirmation = FwConfirmation.renderConfirmation('Close Tab', 'Want to save your changes to "' + tabname + '"?');
                 $save = FwConfirmation.addButton($confirmation, 'Save');
                 $dontsave = FwConfirmation.addButton($confirmation, 'Don\'t Save');
-                $cancel = FwConfirmation.addButton($confirmation, 'Cancel');
+                if ($form.parent().data('type') !== 'settings-row') {
+                    $cancel = FwConfirmation.addButton($confirmation, 'Cancel');
+                }
                 $save.on('click', function () {
                     var controller, isvalid;
                     controller = $form.attr('data-controller');
