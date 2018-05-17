a456b69ad3e54debbffcb83be8e384b82b9c15b1
Mike Vermilion <mike@dbworks.com>
2018-05-16 17:57:36 -0700

    Formats DealController

--- src/RentalWorksWebApi/Modules/Home/Deal/DealController.cs
+++ src/RentalWorksWebApi/Modules/Home/Deal/DealController.cs
@@ -1,56 +1,56 @@
-using FwStandard.Models; 
-using Microsoft.AspNetCore.Mvc; 
-using Microsoft.Extensions.Options; 
-using WebApi.Controllers; 
-using System.Threading.Tasks; 
-namespace WebApi.Modules.Home.Deal 
-{ 
-[Route("api/v1/[controller]")] 
-public class DealController : AppDataController 
-{ 
-public DealController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { } 
-//------------------------------------------------------------------------------------ 
-// POST api/v1/deal/browse 
-[HttpPost("browse")] 
-public async Task<IActionResult> BrowseAsync([FromBody]BrowseRequest browseRequest) 
-{ 
-return await DoBrowseAsync(browseRequest, typeof(DealLogic)); 
-} 
-//------------------------------------------------------------------------------------ 
-// GET api/v1/deal 
-[HttpGet] 
-public async Task<IActionResult> GetAsync([FromQuery]int pageno, [FromQuery]int pagesize, [FromQuery]string sort) 
-{ 
-return await DoGetAsync<DealLogic>(pageno, pagesize, sort, typeof(DealLogic)); 
-} 
-//------------------------------------------------------------------------------------ 
-// GET api/v1/deal/A0000001 
-[HttpGet("{id}")] 
-public async Task<IActionResult> GetAsync([FromRoute]string id) 
-{ 
-return await DoGetAsync<DealLogic>(id, typeof(DealLogic)); 
-} 
-//------------------------------------------------------------------------------------ 
-// POST api/v1/deal 
-[HttpPost] 
-public async Task<IActionResult> PostAsync([FromBody]DealLogic l) 
-{ 
-return await DoPostAsync<DealLogic>(l); 
-} 
-//------------------------------------------------------------------------------------ 
-// DELETE api/v1/deal/A0000001 
-[HttpDelete("{id}")] 
-public async Task<IActionResult> DeleteAsync([FromRoute]string id) 
-{ 
-return await DoDeleteAsync(id, typeof(DealLogic)); 
-} 
-//------------------------------------------------------------------------------------ 
-// POST api/v1/deal/validateduplicate 
-[HttpPost("validateduplicate")] 
-public async Task<IActionResult> ValidateDuplicateAsync([FromBody]ValidateDuplicateRequest request) 
-{ 
-return await DoValidateDuplicateAsync(request); 
-} 
-//------------------------------------------------------------------------------------ 
-} 
-} 
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+namespace WebApi.Modules.Home.Deal
+{
+    [Route("api/v1/[controller]")]
+    public class DealController : AppDataController
+    {
+        public DealController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/deal/browse 
+        [HttpPost("browse")]
+        public async Task<IActionResult> BrowseAsync([FromBody]BrowseRequest browseRequest)
+        {
+            return await DoBrowseAsync(browseRequest, typeof(DealLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // GET api/v1/deal 
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery]int pageno, [FromQuery]int pagesize, [FromQuery]string sort)
+        {
+            return await DoGetAsync<DealLogic>(pageno, pagesize, sort, typeof(DealLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // GET api/v1/deal/A0000001 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync([FromRoute]string id)
+        {
+            return await DoGetAsync<DealLogic>(id, typeof(DealLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/deal 
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]DealLogic l)
+        {
+            return await DoPostAsync<DealLogic>(l);
+        }
+        //------------------------------------------------------------------------------------ 
+        // DELETE api/v1/deal/A0000001 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute]string id)
+        {
+            return await DoDeleteAsync(id, typeof(DealLogic));
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/deal/validateduplicate 
+        [HttpPost("validateduplicate")]
+        public async Task<IActionResult> ValidateDuplicateAsync([FromBody]ValidateDuplicateRequest request)
+        {
+            return await DoValidateDuplicateAsync(request);
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
