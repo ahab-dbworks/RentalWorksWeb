02353603ee0228a395afbad5cc450ef019902dc6
Unknown <oliver@dbworks.com>
2018-05-24 16:46:38 -0700

    Rental warehouse grid editable, styling adjustments on grid

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/RentalInventoryWarehouseGridMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/RentalInventoryWarehouseGridMenu.cs
@@ -9,7 +9,10 @@ namespace WebLibrary.Security.Tree.RentalWorksWeb.Grids
         //---------------------------------------------------------------------------------------------
         public override void BuildBranch(FwSecurityTree tree)
         {
-            var nodeGridMenuBar = tree.AddMenuBar("{ED2DF78A-FFB2-472B-8848-3C8D9988A68E}", MODULEID);
+            var nodeGridMenuBar = tree.AddMenuBar("{92EF711F-C336-4DCA-B25F-58EEE5E89328}", MODULEID);
+            tree.AddNewMenuBarButton("{54337E04-FD01-4456-BCBF-BF76FAAF6C56}", nodeGridMenuBar.Id);
+            tree.AddEditMenuBarButton("{2624D496-5A68-4931-AB2B-C6DD1C5B0D65}", nodeGridMenuBar.Id);
+            tree.AddDeleteMenuBarButton("{3749DAFE-542E-4EA9-B0DC-A3476FB9154B}", nodeGridMenuBar.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
