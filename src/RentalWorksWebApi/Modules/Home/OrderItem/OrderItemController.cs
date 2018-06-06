5a42ec101c3e8317fa0752093505ccff75e80849
Justin Hoffman <justin@dbworks.com>
2018-06-06 04:42:48 -0700

    added endpoints to calculate Extended and Discount on Quote/Order items grids

--- src/RentalWorksWebApi/Modules/Home/OrderItem/OrderItemController.cs
+++ src/RentalWorksWebApi/Modules/Home/OrderItem/OrderItemController.cs
@@ -1,8 +1,11 @@
-using FwStandard.Models; 
-using Microsoft.AspNetCore.Mvc; 
-using Microsoft.Extensions.Options; 
-using WebApi.Controllers; 
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
 using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+
 namespace WebApi.Modules.Home.OrderItem
 {
     [Route("api/v1/[controller]")]
@@ -57,6 +60,94 @@ namespace WebApi.Modules.Home.OrderItem
         public async Task<IActionResult> ValidateDuplicateAsync([FromBody]ValidateDuplicateRequest request)
         {
             return await DoValidateDuplicateAsync(request);
+        }
+        //------------------------------------------------------------------------------------ 
+
+        public class OrderItemExtended
+        {
+            public string RateType;
+            public DateTime? FromDate;
+            public DateTime? ToDate;
+            public Decimal? Quantity;
+            public Decimal? Rate;
+            public Decimal? DaysPerWeek;
+            public Decimal? DiscountPercent;
+            public Decimal? WeeklyExtended;
+            public Decimal? WeeklyDiscount;
+            public Decimal? MonthlyExtended;
+            public Decimal? MonthlyDiscount;
+            public Decimal? PeriodExtended;
+            public Decimal? PeriodDiscount;
+        }
+
+        // GET api/v1/orderitem/calculateextended
+        [HttpGet("calculateextended")]
+        public IActionResult CalculateExtended(string RateType, DateTime? FromDate, DateTime? ToDate, Decimal? Quantity, Decimal? Rate, Decimal? DaysPerWeek, Decimal? DiscountPercent)
+        {
+            try
+            {
+                OrderItemExtended extended = new OrderItemExtended();
+                extended.RateType = RateType;
+                extended.FromDate = FromDate;
+                extended.ToDate = ToDate;
+                extended.Quantity = Quantity;
+                extended.Rate = Rate;
+                extended.DaysPerWeek = DaysPerWeek;
+                extended.DiscountPercent = DiscountPercent;
+
+                //calculate extendeds here
+                extended.WeeklyExtended = 1;
+                extended.WeeklyDiscount = 1.5M;
+                extended.MonthlyExtended = 2;
+                extended.MonthlyDiscount = 2.5M;
+                extended.PeriodExtended = 3;
+                extended.PeriodDiscount = 3.5M;
+
+
+                return new OkObjectResult(extended);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+
+        // GET api/v1/orderitem/calculatediscountpercent
+        [HttpGet("calculatediscountpercent")]
+        public IActionResult CalculateDiscountPercent(string RateType, DateTime? FromDate, DateTime? ToDate, Decimal? Quantity, Decimal? Rate, Decimal? DaysPerWeek, Decimal? WeeklyExtended, Decimal? MonthlyExtended, Decimal? PeriodExtended)
+        {
+            try
+            {
+                OrderItemExtended extended = new OrderItemExtended();
+                extended.RateType = RateType;
+                extended.FromDate = FromDate;
+                extended.ToDate = ToDate;
+                extended.Quantity = Quantity;
+                extended.Rate = Rate;
+                extended.DaysPerWeek = DaysPerWeek;
+                extended.WeeklyExtended = WeeklyExtended;
+                extended.MonthlyExtended = MonthlyExtended;
+                extended.PeriodExtended = PeriodExtended;
+
+                //calculate discount percent here
+                extended.DiscountPercent = 99;
+
+
+                return new OkObjectResult(extended);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
         }
         //------------------------------------------------------------------------------------ 
     }
