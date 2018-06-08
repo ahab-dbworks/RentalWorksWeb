640ca2d3c42355fd5d54fdb382eac9cc8e358648
Unknown <oliver@dbworks.com>
2018-06-08 16:25:57 -0700

    Updates time column with military time formatting, requires further work for saving

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwBrowseColumns/FwBrowseColumn_time.js
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwBrowseColumns/FwBrowseColumn_time.js
@@ -13,15 +13,27 @@ FwBrowseColumn_time.setFieldViewMode = function($browse, $field, $tr, html) {
     $field.html(originalvalue);
 };
 //---------------------------------------------------------------------------------
-FwBrowseColumn_time.setFieldEditMode = function($browse, $field, $tr, html) {
-    var originalvalue = (typeof $field.attr('data-originalvalue')  === 'string') ? $field.attr('data-originalvalue') : '';
-    html.push('<input class="value" type="time"');
+FwBrowseColumn_time.setFieldEditMode = function ($browse, $field, $tr, html) {
+    var timepickerTimeFormat, inputmaskTimeFormat;
+    var originalvalue = (typeof $field.attr('data-originalvalue') === 'string') ? $field.attr('data-originalvalue') : '';
+    html.push('<div class="fwformfield-control">');
+    html.push('<input class="value" type="text" autocapitalize="none"');
     if ($browse.attr('data-enabled') === 'false') {
         html.push(' disabled="disabled"');
     }
     html.push(' />');
+    html.push('</div>');
+    if ($field.attr('data-timeformat') === '24') {
+        timepickerTimeFormat = 'H:i:s';
+        inputmaskTimeFormat = 'hh:mm';
+    } else {
+        timepickerTimeFormat = 'h:i A';
+        inputmaskTimeFormat = 'hh:mm t';
+    }
     html = html.join('');
     $field.html(html);
+    $field.find('input.value').inputmask(inputmaskTimeFormat);
     $field.find('input.value').val(originalvalue);
+
 };
 //---------------------------------------------------------------------------------
