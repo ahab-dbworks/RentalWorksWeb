6cc445ca695ddf7d088abd44dd4926661ac2bd19
Joshua Pace <jpace@4wall.com>
2018-06-13 16:21:51 -0700

    defaults date and current user on new note add in contacts

--- src/RentalWorksWeb/Source/Grids/ContactNoteGrid/ContactNoteGridController.js
+++ src/RentalWorksWeb/Source/Grids/ContactNoteGrid/ContactNoteGridController.js
@@ -3,6 +3,14 @@ var ContactNoteGrid = (function () {
         this.Module = 'ContactNoteGrid';
         this.apiurl = 'api/v1/contactnote';
     }
+    ContactNoteGrid.prototype.onRowNewMode = function ($control, $tr) {
+        var today = FwFunc.getDate();
+        var usersid = sessionStorage.getItem('usersid');
+        var name = sessionStorage.getItem('name');
+        $tr.find('[data-formdatafield="NoteDate"] input.value').val(today);
+        $tr.find('[data-browsedisplayfield="NotesBy"] input.value').val(usersid);
+        $tr.find('[data-browsedisplayfield="NotesBy"] input.text').val(name);
+    };
     return ContactNoteGrid;
 }());
 var ContactNoteGridController = new ContactNoteGrid();
--- src/RentalWorksWeb/Source/Grids/ContactNoteGrid/ContactNoteGridController.ts
+++ src/RentalWorksWeb/Source/Grids/ContactNoteGrid/ContactNoteGridController.ts
@@ -1,10 +1,15 @@
 ﻿class ContactNoteGrid {
-    Module: string;
-    apiurl: string;
+    Module: string = 'ContactNoteGrid';
+    apiurl: string = 'api/v1/contactnote';
 
-    constructor() {
-        this.Module = 'ContactNoteGrid';
-        this.apiurl = 'api/v1/contactnote';
+    onRowNewMode($control: JQuery, $tr: JQuery) {
+        const today = FwFunc.getDate();
+        const usersid = sessionStorage.getItem('usersid');                 // J. Pace 6/13/18  C4E0E7F6-3B1C-4037-A50C-9825EDB47F44
+        const name = sessionStorage.getItem('name');
+
+        $tr.find('[data-formdatafield="NoteDate"] input.value').val(today);
+        $tr.find('[data-browsedisplayfield="NotesBy"] input.value').val(usersid);
+        $tr.find('[data-browsedisplayfield="NotesBy"] input.text').val(name);
     }
 }
 
