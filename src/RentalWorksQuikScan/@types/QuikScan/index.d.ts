18c0bdd097cb8bc4446ecf01916a4302d6f03a22
Mike Vermilion <mike@dbworks.com>
2018-07-06 15:25:51 -0700

    QS: Changes to tsconfig and typings moved from Global.ts to ./@types

---
+++ src/RentalWorksQuikScan/@types/QuikScan/index.d.ts
@@ -0,0 +1,41 @@
+﻿declare var masterController: WebMaster;
+declare var FwAudit: any;
+declare var FwCharge: any;
+declare var FwContextMenu: any;
+declare var FwLanguages: any;
+declare var FwMenu: any;
+declare var FwMobileMasterController: any;
+declare var FwSecurity: any;
+declare var FwServices: any;
+declare var FwTabs: any;
+declare var FwValidation: any;
+
+declare var applicationConfig: any;
+declare var FastClick: any;
+declare var cdvapp;
+
+declare var RwAccountController: any;
+declare var RwConstants: any;
+declare var RwFillContainer: any;
+declare var RwHome: any;
+declare var RwInventoryController: any;
+declare var RwLanguages: any;
+declare var RwOrderController: any;
+declare var RwPartialController: any;
+declare var RwSelectOrder: any;
+declare var RwSelectPhyInv: any;
+declare var RwSelectPO: any;
+declare var RwQuote: any;
+declare var RwQuoteMenu: any;
+declare var RwRFID: any;
+declare var RwRoutes: any;
+declare var RwServices: any;
+
+declare var AssetSetLocation: any;
+declare var AssignItems: any;
+declare var Exchange: any;
+declare var ReceiveOnSet: any;
+declare var TimeLog: any;
+
+declare var DTDevices: any;
+declare var TslReader: any;
--- src/RentalWorksQuikScan/scripts/Globals.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=Globals.js.map
