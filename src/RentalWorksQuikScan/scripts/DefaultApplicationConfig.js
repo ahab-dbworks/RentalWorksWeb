90060e5a3a86d5a3919feeec682b741b33d68029
Mike Vermilion <mike@dbworks.com>
2018-07-06 15:27:34 -0700

    QS: Temporarily converted DefaultApplicationConfig.ts back to JavaScript to resolve an issue with release builds until there is time to troubleshoot the issue with 404s on the source map.

--- src/RentalWorksQuikScan/scripts/DefaultApplicationConfig.js
+++ src/RentalWorksQuikScan/scripts/DefaultApplicationConfig.js
@@ -1,25 +1,20 @@
-var ApplicationConfig = (function () {
-    function ApplicationConfig() {
-        this.appbaseurl = '';
-        this.fwvirtualdirectory = '';
-        this.appvirtualdirectory = '';
-        this.debugMode = false;
-        this.designMode = false;
-        this.demoMode = false;
-        this.devMode = false;
-        this.demoEmail = 'qsdemo@dbworks.com';
-        this.demoPassword = 'QSDEMO';
-        this.ajaxTimeoutSeconds = 20;
-        this.version = '';
-        this.apiurl = '';
-        this.photoWidth = 1024;
-        this.photoHeight = 1024;
-        this.photoQuality = 100;
-        this.iPodPhotoQualit = 100;
-        this.iPhonePhotoQuality = 100;
-        this.defaultPeek = false;
-    }
-    return ApplicationConfig;
-}());
-var applicationConfig = new ApplicationConfig();
-//# sourceMappingURL=DefaultApplicationConfig.js.map
+﻿var applicationConfig = {
+    appbaseurl: '',
+    fwvirtualdirectory: '',
+    appvirtualdirectory: '',
+    debugMode: false,
+    designMode: false,
+    demoMode: false,
+    devMode: false,
+    demoEmail: 'qsdemo@dbworks.com',
+    demoPassword: 'QSDEMO',
+    ajaxTimeoutSeconds: 20,
+    version: '',
+    apiurl: '',
+    photoWidth: 1024,
+    photoHeight: 1024,
+    photoQuality: 100,
+    iPodPhotoQuality: 100,
+    iPhonePhotoQuality: 100,
+    defaultPeek: false
+};
