8faa85062369766e9e56f103f0a1212c1546566b
Mike Vermilion <mike@dbworks.com>
2018-07-18 17:35:08 -0700

    Adds SecurityTreeBuilder project

---
+++ SecurityTreeBuilder/SecurityTreeLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecurityTreeBuilder
+{
+    public class SecurityTreeLoader
+    {
+        public static List<Type> LoadTypes(string rootNamespace, Type subClassOf)
+        {
+            List<Type> types = typeof(WebApi.Program).Assembly.GetTypes()
+                .Where(t => t.FullName.StartsWith(rootNamespace) && t.IsSubclassOf(subClassOf))
+                .OrderBy(o => o.Name)
+                .ToList<Type>();
+
+            return types;
+        }
+    }
+}
