6b7941573913d353979a088cad2dbcd9c0b7128b
Justin Hoffman <justin@dbworks.com>
2018-08-08 12:44:56 -0700

    Added SoundColor field

---
+++ src/RentalWorksWebApi/Modules/Settings/Sound/SoundLoader.cs
@@ -0,0 +1,40 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Collections.Generic;
+namespace WebApi.Modules.Settings.Sound
+{
+    [FwSqlTable("soundview")]
+    public class SoundLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "soundid", modeltype: FwDataTypes.Text, isPrimaryKey: true)]
+        public string SoundId { get; set; } = "";
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "sound", modeltype: FwDataTypes.Text)]
+        public string Sound { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "filename", modeltype: FwDataTypes.Text)]
+        public string FileName { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "systemsound", modeltype: FwDataTypes.Boolean)]
+        public bool? SystemSound { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "soundcolor", modeltype: FwDataTypes.OleToHtmlColor)]
+        public string SoundColor { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "datestamp", modeltype: FwDataTypes.UTCDateTime)]
+        public string DateStamp { get; set; }
+        //------------------------------------------------------------------------------------ 
+        protected override void SetBaseSelectQuery(FwSqlSelect select, FwSqlCommand qry, FwCustomFields customFields = null, BrowseRequest request = null)
+        {
+            base.SetBaseSelectQuery(select, qry, customFields, request);
+            select.Parse();
+            //select.AddWhere("(xxxtype = 'ABCDEF')"); 
+            //addFilterToSelect("UniqueId", "uniqueid", select, request); 
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
--- src/RentalWorksWebApi/Modules/Settings/Sound/SoundLogic.cs
+++ src/RentalWorksWebApi/Modules/Settings/Sound/SoundLogic.cs
@@ -6,9 +6,12 @@ namespace WebApi.Modules.Settings.Sound
     {
         //------------------------------------------------------------------------------------ 
         SoundRecord sound = new SoundRecord();
+        SoundLoader soundLoader = new SoundLoader();
+
         public SoundLogic()
         {
             dataRecords.Add(sound);
+            dataLoader = soundLoader;
         }
         //------------------------------------------------------------------------------------ 
         [FwBusinessLogicField(isPrimaryKey: true)]
@@ -17,6 +20,7 @@ namespace WebApi.Modules.Settings.Sound
         public string Sound { get { return sound.Sound; } set { sound.Sound = value; } }
         public string FileName { get { return sound.FileName; } set { sound.FileName = value; } }
         public bool? SystemSound { get { return sound.SystemSound; } set { sound.SystemSound = value; } }
+        public string SoundColor { get; set; }
         public string DateStamp { get { return sound.DateStamp; } set { sound.DateStamp = value; } }
         //------------------------------------------------------------------------------------ 
         //protected override bool Validate(TDataRecordSaveMode saveMode, ref string validateMsg) 
