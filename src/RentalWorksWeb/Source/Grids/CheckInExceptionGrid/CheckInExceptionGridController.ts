3c3f06a35904a7e701aa67d6064a5eb5966b9693
Jason Hoang <jhoang@4wall.com>
2018-08-15 12:22:32 -0700

    Added CheckInExceptionGrid

---
+++ src/RentalWorksWeb/Source/Grids/CheckInExceptionGrid/CheckInExceptionGridController.ts
@@ -0,0 +1,12 @@
+﻿class CheckInExceptionGrid {
+    Module: string;
+    apiurl: string;
+
+    constructor() {
+        this.Module = 'CheckInExceptionGrid';
+        this.apiurl = 'api/v1/checkinexception';
+    }
+}
+
+var CheckInExceptionGridController = new CheckInExceptionGrid();
+//----------------------------------------------------------------------------------------------
---
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/CheckInExceptionGridMenu.cs
@@ -0,0 +1,19 @@
+﻿using FwStandard.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebLibrary.Security.Tree.RentalWorksWeb.Grids
+{
+    class CheckInExceptionGrid : FwSecurityTreeBranch
+    {
+        //---------------------------------------------------------------------------------------------
+        public CheckInExceptionGrid() : base("{E6A2B313-ADEC-41DD-824E-947097E63060}") { }
+        //---------------------------------------------------------------------------------------------
+        public override void BuildBranch(FwSecurityTree tree)
+        {
+            var nodeGridMenuBar = tree.AddMenuBar("{6530DC2B-CE31-4249-8836-D1E607EAE5F3}", MODULEID);
+        }
+        //---------------------------------------------------------------------------------------------
+    }
+}
