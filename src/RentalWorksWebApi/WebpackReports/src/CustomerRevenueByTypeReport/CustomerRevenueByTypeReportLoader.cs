aa5dfc51dc8fc107cd6dba69074eaf814d44a9c3
Justin Hoffman <justin@dbworks.com>
2018-08-23 16:27:43 -0700

    Added Customer Revenue By Type api

---
+++ src/RentalWorksWebApi/WebpackReports/src/CustomerRevenueByTypeReport/CustomerRevenueByTypeReportLoader.cs
@@ -0,0 +1,150 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Collections.Generic;
+using System;
+using WebLibrary;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+
+namespace WebApi.Modules.Reports.CustomerRevenueByTypeReport
+{
+    [FwSqlTable("customerrevenuebytypeview")]
+    public class CustomerRevenueByTypeReportLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "invoiceid", modeltype: FwDataTypes.Text)]
+        public string InvoiceId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "invoiceno", modeltype: FwDataTypes.Text)]
+        public string InvoiceNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "locationid", modeltype: FwDataTypes.Text)]
+        public string OfficeLocationId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "location", modeltype: FwDataTypes.Text)]
+        public string OfficeLocation { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "departmentid", modeltype: FwDataTypes.Text)]
+        public string DepartmentId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "department", modeltype: FwDataTypes.Text)]
+        public string Department { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderid", modeltype: FwDataTypes.Text)]
+        public string OrderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderno", modeltype: FwDataTypes.Text)]
+        public string OrderNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "ordertypeid", modeltype: FwDataTypes.Text)]
+        public string OrderTypeId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "customerid", modeltype: FwDataTypes.Text)]
+        public string CustomerId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "customer", modeltype: FwDataTypes.Text)]
+        public string Customer { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealid", modeltype: FwDataTypes.Text)]
+        public string DealId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "deal", modeltype: FwDataTypes.Text)]
+        public string Deal { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billingstart", modeltype: FwDataTypes.Date)]
+        public string BillingStart { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billingend", modeltype: FwDataTypes.Date)]
+        public string BillingEnd { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "agentid", modeltype: FwDataTypes.Text)]
+        public string AgentId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "agent", modeltype: FwDataTypes.Text)]
+        public string Agent { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rental", modeltype: FwDataTypes.Decimal)]
+        public decimal? Rental { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "sales", modeltype: FwDataTypes.Decimal)]
+        public decimal? Sales { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "space", modeltype: FwDataTypes.Decimal)]
+        public decimal? Facilities { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "labor", modeltype: FwDataTypes.Decimal)]
+        public decimal? Labor { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "misc", modeltype: FwDataTypes.Decimal)]
+        public decimal? Miscelleaneous { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "assetsale", modeltype: FwDataTypes.Decimal)]
+        public decimal? AssetSale { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "parts", modeltype: FwDataTypes.Decimal)]
+        public decimal? Parts { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "tax", modeltype: FwDataTypes.Decimal)]
+        public decimal? Tax { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "total", modeltype: FwDataTypes.Decimal)]
+        public decimal? Total { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealtype", modeltype: FwDataTypes.Text)]
+        public string DealType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "nonbillable", modeltype: FwDataTypes.Boolean)]
+        public bool? NonBillable { get; set; }
+        //------------------------------------------------------------------------------------ 
+        public override async Task<FwJsonDataTable> BrowseAsync(BrowseRequest request, FwCustomFields customFields = null)
+        {
+            DateTime fromDate = DateTime.MinValue; 
+            DateTime toDate = DateTime.MaxValue; 
+
+            if (request != null)
+            {
+                if (request.uniqueids != null)
+                {
+                    IDictionary<string, object> uniqueIds = ((IDictionary<string, object>)request.uniqueids);
+                    if (uniqueIds.ContainsKey("FromDate"))
+                    {
+                        fromDate = FwConvert.ToDateTime(uniqueIds["FromDate"].ToString());
+                    }
+                    if (uniqueIds.ContainsKey("ToDate"))
+                    {
+                        toDate = FwConvert.ToDateTime(uniqueIds["ToDate"].ToString());
+                    }
+                }
+            }
+
+            FwJsonDataTable dt = null;
+
+            using (FwSqlConnection conn = new FwSqlConnection(this.AppConfig.DatabaseSettings.ConnectionString))
+            {
+                using (FwSqlCommand qry = new FwSqlCommand(conn, "getcustomerrevenuebymonthrpt", this.AppConfig.DatabaseSettings.QueryTimeout))
+                {
+                    qry.AddParameter("@fromdate", SqlDbType.Date, ParameterDirection.Input, fromDate);
+                    qry.AddParameter("@todate", SqlDbType.Date, ParameterDirection.Input, toDate);
+                    PropertyInfo[] propertyInfos = typeof(CustomerRevenueByTypeReportLoader).GetProperties();
+                    foreach (PropertyInfo propertyInfo in propertyInfos)
+                    {
+                        FwSqlDataFieldAttribute sqlDataFieldAttribute = propertyInfo.GetCustomAttribute<FwSqlDataFieldAttribute>();
+                        if (sqlDataFieldAttribute != null)
+                        {
+                            qry.AddColumn(sqlDataFieldAttribute.ColumnName, propertyInfo.Name, sqlDataFieldAttribute.ModelType, sqlDataFieldAttribute.IsVisible, sqlDataFieldAttribute.IsPrimaryKey, false);
+                        }
+                    }
+                    dt = await qry.QueryToFwJsonTableAsync(false, 0);
+                }
+            }
+            return dt;
+
+
+        }
+        //------------------------------------------------------------------------------------
+    }
+}
