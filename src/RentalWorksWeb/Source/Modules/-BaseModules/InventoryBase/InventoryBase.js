6327c1a683f07194a93c8d23d533d07556f39d95
Justin Hoffman <justin@dbworks.com>
2018-08-24 04:48:59 -0700

    Updated TS inheritance on Inventory modules

--- src/RentalWorksWeb/Source/Modules/-BaseModules/InventoryBase/InventoryBase.js
+++ src/RentalWorksWeb/Source/Modules/-BaseModules/InventoryBase/InventoryBase.js
@@ -1,7 +1,80 @@
 class InventoryBase {
     constructor() {
+        this.Module = 'BaseInventory';
         this.ActiveView = 'ALL';
+        this.caption = 'Base Inventory';
+        this.AvailableFor = '';
     }
+    getModuleScreen() {
+        var screen, $browse, self = this;
+        screen = {};
+        screen.$view = FwModule.getModuleControl(this.Module + 'Controller');
+        screen.viewModel = {};
+        screen.properties = {};
+        $browse = this.openBrowse();
+        screen.load = function () {
+            FwModule.openModuleTab($browse, self.caption, false, 'BROWSE', true);
+            FwBrowse.databind($browse);
+            FwBrowse.screenload($browse);
+        };
+        screen.unload = function () {
+            FwBrowse.screenunload($browse);
+        };
+        return screen;
+    }
+    ;
+    openBrowse() {
+        let self = this;
+        let $browse = FwBrowse.loadBrowseFromTemplate(this.Module);
+        $browse = FwModule.openBrowse($browse);
+        this.ActiveView = 'ALL';
+        $browse.data('ondatabind', function (request) {
+            request.activeview = self.ActiveView;
+        });
+        FwBrowse.addLegend($browse, 'Item', '#ffffff');
+        FwBrowse.addLegend($browse, 'Accessory', '#fffa00');
+        FwBrowse.addLegend($browse, 'Complete', '#0080ff');
+        FwBrowse.addLegend($browse, 'Kit', '#00c400');
+        FwBrowse.addLegend($browse, 'Misc', '#ff0080');
+        if (this.AvailableFor === "R") {
+            FwBrowse.addLegend($browse, 'Container', '#ff8040');
+        }
+        return $browse;
+    }
+    ;
+    openForm(mode) {
+        var $form, controller;
+        $form = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Form').html());
+        $form = FwModule.openForm($form, mode);
+        if (mode === 'NEW') {
+            this.setupNewMode($form);
+        }
+        controller = $form.attr('data-controller');
+        if (typeof window[controller]['openFormInventory'] === 'function') {
+            window[controller]['openFormInventory']($form);
+        }
+        this.events($form);
+        return $form;
+    }
+    ;
+    loadForm(uniqueids) {
+        var $form;
+        $form = this.openForm('EDIT');
+        $form.find('div.fwformfield[data-datafield="InventoryId"] input').val(uniqueids.InventoryId);
+        FwModule.loadForm(this.Module, $form);
+        return $form;
+    }
+    ;
+    saveForm($form, parameters) {
+        FwModule.saveForm(this.Module, $form, parameters);
+    }
+    ;
+    loadAudit($form) {
+        var uniqueid;
+        uniqueid = $form.find('div.fwformfield[data-datafield="InventoryId"] input').val();
+        FwModule.loadAudit($form, uniqueid);
+    }
+    ;
     events($form) {
         let classificationValue, trackedByValue;
         $form.find('[data-datafield="OverrideProfitAndLossCategory"] .fwformfield-value').on('change', function () {
@@ -45,6 +118,178 @@ class InventoryBase {
                 $form.find('.asset-submodule').hide();
             }
         });
+    }
+    addBrowseMenuItems($menuObject) {
+        var self = this;
+        var $all = FwMenu.generateDropDownViewBtn('All', true);
+        var $item = FwMenu.generateDropDownViewBtn('Item', true);
+        var $accessory = FwMenu.generateDropDownViewBtn('Accessory', false);
+        var $complete = FwMenu.generateDropDownViewBtn('Complete', false);
+        var $kitset = FwMenu.generateDropDownViewBtn('Kit', false);
+        var $misc = FwMenu.generateDropDownViewBtn('Misc', false);
+        var $container = FwMenu.generateDropDownViewBtn('Container', false);
+        $all.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'ALL';
+            FwBrowse.search($browse);
+        });
+        $item.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'ITEM';
+            FwBrowse.search($browse);
+        });
+        $accessory.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'ACCESSORY';
+            FwBrowse.search($browse);
+        });
+        $complete.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'COMPLETE';
+            FwBrowse.search($browse);
+        });
+        $kitset.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'KIT';
+            FwBrowse.search($browse);
+        });
+        $misc.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'MISC';
+            FwBrowse.search($browse);
+        });
+        $container.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'CONTAINER';
+            FwBrowse.search($browse);
+        });
+        FwMenu.addVerticleSeparator($menuObject);
+        var viewSubitems = [];
+        viewSubitems.push($all);
+        viewSubitems.push($item);
+        viewSubitems.push($accessory);
+        viewSubitems.push($complete);
+        viewSubitems.push($kitset);
+        viewSubitems.push($misc);
+        if (this.AvailableFor === "R") {
+            viewSubitems.push($container);
+        }
+        var $view;
+        $view = FwMenu.addViewBtn($menuObject, 'View', viewSubitems);
+        return $menuObject;
+    }
+    ;
+    setupNewMode($form) {
+        FwFormField.enable($form.find('[data-datafield="Classification"]'));
+        $form.find('div[data-datafield="Classification"] .fwformfield-value').on('change', function () {
+            var $this = jQuery(this);
+            $form.find('.completeskitstab').show();
+            $form.find('.containertab').hide();
+            $form.find('.completetab').hide();
+            $form.find('.kittab').hide();
+            if ($this.prop('checked') === true && $this.val() === 'I') {
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').show();
+            }
+            if ($this.prop('checked') === true && $this.val() === 'A') {
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').show();
+            }
+            if ($this.prop('checked') === true && $this.val() === 'C') {
+                $form.find('.completetab').show();
+                $form.find('.completeskitstab').hide();
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'K') {
+                $form.find('.kittab').show();
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'N') {
+                $form.find('.containertab').show();
+                $form.find('.completeskitstab').hide();
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'S') {
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'M') {
+                FwFormField.setValueByDataField($form, 'TrackedBy', 'QUANTITY');
+                FwFormField.disable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').show();
+            }
+        });
+    }
+    afterLoadSetClassification($form) {
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'I' || FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'A') {
+            FwFormField.enable($form.find('[data-datafield="Classification"]'));
+            $form.find('.completeradio').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.miscradio').hide();
+        }
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'N') {
+            $form.find('.containertab').show();
+            $form.find('.completeskitstab').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.completeradio').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.miscradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'C') {
+            $form.find('.completetab').show();
+            $form.find('.completeskitstab').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.miscradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'K') {
+            $form.find('.kittab').show();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.miscradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'M') {
+            $form.find('.completeradio').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            FwFormField.setValueByDataField($form, 'TrackedBy', 'QUANTITY');
+            FwFormField.disable($form.find('div[data-datafield="TrackedBy"]'));
+        }
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'S') {
+            $form.find('.completeradio').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
     }
 }
 var InventoryBaseController = new InventoryBase();
--- src/RentalWorksWeb/Source/Modules/-BaseModules/InventoryBase/InventoryBase.ts
+++ src/RentalWorksWeb/Source/Modules/-BaseModules/InventoryBase/InventoryBase.ts
@@ -1,6 +1,92 @@
 ﻿class InventoryBase {
+    Module: string = 'BaseInventory';
     ActiveView: string = 'ALL';
+    caption: string = 'Base Inventory';
+    AvailableFor: string = '';
 
+    //----------------------------------------------------------------------------------------------
+    getModuleScreen() {
+        var screen, $browse, self = this;
+
+        screen = {};
+        screen.$view = FwModule.getModuleControl(this.Module + 'Controller');
+        screen.viewModel = {};
+        screen.properties = {};
+
+        $browse = this.openBrowse();
+
+        screen.load = function () {
+            FwModule.openModuleTab($browse, self.caption, false, 'BROWSE', true);
+            FwBrowse.databind($browse);
+            FwBrowse.screenload($browse);
+        };
+        screen.unload = function () {
+            FwBrowse.screenunload($browse);
+        };
+
+        return screen;
+    };
+    //----------------------------------------------------------------------------------------------
+    openBrowse() {
+        let self = this;
+        let $browse: JQuery = FwBrowse.loadBrowseFromTemplate(this.Module);
+        $browse = FwModule.openBrowse($browse);
+
+        this.ActiveView = 'ALL'; // Resets view to all when revisting module page
+
+        $browse.data('ondatabind', function (request) {
+            request.activeview = self.ActiveView;
+        });
+        FwBrowse.addLegend($browse, 'Item', '#ffffff');
+        FwBrowse.addLegend($browse, 'Accessory', '#fffa00');
+        FwBrowse.addLegend($browse, 'Complete', '#0080ff');
+        FwBrowse.addLegend($browse, 'Kit', '#00c400');
+        FwBrowse.addLegend($browse, 'Misc', '#ff0080');
+        if (this.AvailableFor === "R") {
+            FwBrowse.addLegend($browse, 'Container', '#ff8040');
+        }
+
+        return $browse;
+    };
+    //----------------------------------------------------------------------------------------------
+    openForm(mode: string) {
+        var $form, controller;
+        $form = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Form').html());
+        $form = FwModule.openForm($form, mode);
+
+        if (mode === 'NEW') {
+            this.setupNewMode($form);
+        }
+
+        controller = $form.attr('data-controller');
+        if (typeof window[controller]['openFormInventory'] === 'function') {
+            window[controller]['openFormInventory']($form);
+        }
+
+        this.events($form);
+        return $form;
+    };
+    //----------------------------------------------------------------------------------------------
+    loadForm(uniqueids: any) {
+        var $form;
+
+        $form = this.openForm('EDIT');
+        $form.find('div.fwformfield[data-datafield="InventoryId"] input').val(uniqueids.InventoryId);
+        FwModule.loadForm(this.Module, $form);
+
+        return $form;
+    };
+    //----------------------------------------------------------------------------------------------
+    saveForm($form: any, parameters: any) {
+        FwModule.saveForm(this.Module, $form, parameters);
+    };
+    //----------------------------------------------------------------------------------------------
+    loadAudit($form: any) {
+        var uniqueid;
+        uniqueid = $form.find('div.fwformfield[data-datafield="InventoryId"] input').val();
+        FwModule.loadAudit($form, uniqueid);
+    };
+    //----------------------------------------------------------------------------------------------
     events($form: any): void {
         let classificationValue, trackedByValue;
 
@@ -45,6 +131,193 @@
             }
         });
     }
+    //----------------------------------------------------------------------------------------------
+    addBrowseMenuItems($menuObject: any) {
+        var self = this;
+        var $all: JQuery = FwMenu.generateDropDownViewBtn('All', true);
+        var $item: JQuery = FwMenu.generateDropDownViewBtn('Item', true);
+        var $accessory: JQuery = FwMenu.generateDropDownViewBtn('Accessory', false);
+        var $complete: JQuery = FwMenu.generateDropDownViewBtn('Complete', false);
+        var $kitset: JQuery = FwMenu.generateDropDownViewBtn('Kit', false);
+        var $misc: JQuery = FwMenu.generateDropDownViewBtn('Misc', false);
+        var $container: JQuery = FwMenu.generateDropDownViewBtn('Container', false);
+
+        $all.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'ALL';
+            FwBrowse.search($browse);
+        });
+        $item.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'ITEM';
+            FwBrowse.search($browse);
+        });
+        $accessory.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'ACCESSORY';
+            FwBrowse.search($browse);
+        });
+        $complete.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'COMPLETE';
+            FwBrowse.search($browse);
+        });
+        $kitset.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'KIT';
+            FwBrowse.search($browse);
+        });
+        $misc.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'MISC';
+            FwBrowse.search($browse);
+        });
+        $container.on('click', function () {
+            var $browse;
+            $browse = jQuery(this).closest('.fwbrowse');
+            self.ActiveView = 'CONTAINER';
+            FwBrowse.search($browse);
+        });
+
+        FwMenu.addVerticleSeparator($menuObject);
+
+        var viewSubitems: Array<JQuery> = [];
+        viewSubitems.push($all);
+        viewSubitems.push($item);
+        viewSubitems.push($accessory);
+        viewSubitems.push($complete);
+        viewSubitems.push($kitset);
+        viewSubitems.push($misc);
+        if (this.AvailableFor === "R") {
+            viewSubitems.push($container);
+        }
+        var $view;
+        $view = FwMenu.addViewBtn($menuObject, 'View', viewSubitems);
+
+        return $menuObject;
+    };
+    //----------------------------------------------------------------------------------------------
+    setupNewMode($form: any) {
+        FwFormField.enable($form.find('[data-datafield="Classification"]'));
+
+        $form.find('div[data-datafield="Classification"] .fwformfield-value').on('change', function () {
+            var $this = jQuery(this);
+
+            $form.find('.completeskitstab').show();
+            $form.find('.containertab').hide();
+            $form.find('.completetab').hide();
+            $form.find('.kittab').hide();
+
+            if ($this.prop('checked') === true && $this.val() === 'I') {
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').show();
+            }
+            if ($this.prop('checked') === true && $this.val() === 'A') {
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').show();
+            }
+            if ($this.prop('checked') === true && $this.val() === 'C') {
+                $form.find('.completetab').show();
+                $form.find('.completeskitstab').hide();
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'K') {
+                $form.find('.kittab').show();
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'N') {
+                $form.find('.containertab').show();
+                $form.find('.completeskitstab').hide();
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'S') {
+                FwFormField.enable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').hide();
+                $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+            }
+            if ($this.prop('checked') === true && $this.val() === 'M') {
+                FwFormField.setValueByDataField($form, 'TrackedBy', 'QUANTITY');
+                FwFormField.disable($form.find('div[data-datafield="TrackedBy"]'));
+                $form.find('.tracked-by-column').show();
+            }
+        })
+    }
+    //----------------------------------------------------------------------------------------------
+    afterLoadSetClassification($form: any) {
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'I' || FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'A') {
+            FwFormField.enable($form.find('[data-datafield="Classification"]'));
+            $form.find('.completeradio').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.miscradio').hide();
+        }
+
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'N') {
+            $form.find('.containertab').show();
+            $form.find('.completeskitstab').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.completeradio').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.miscradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'C') {
+            $form.find('.completetab').show();
+            $form.find('.completeskitstab').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.miscradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'K') {
+            $form.find('.kittab').show();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.miscradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'M') {
+            $form.find('.completeradio').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            FwFormField.setValueByDataField($form, 'TrackedBy', 'QUANTITY');
+            FwFormField.disable($form.find('div[data-datafield="TrackedBy"]'));
+        }
+
+        if (FwFormField.getValue($form, 'div[data-datafield="Classification"]') === 'S') {
+            $form.find('.completeradio').hide();
+            $form.find('.kitradio').hide();
+            $form.find('.itemradio').hide();
+            $form.find('.accessoryradio').hide();
+            $form.find('.containerradio').hide();
+            $form.find('.tracked-by-column').hide();
+            $form.find('div[data-datafield="TrackedBy"] input').prop('checked', false);
+        }
+    }
+    //----------------------------------------------------------------------------------------------
 }
 
 var InventoryBaseController = new InventoryBase();
