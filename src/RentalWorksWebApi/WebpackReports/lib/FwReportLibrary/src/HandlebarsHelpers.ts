f91d7c8e5c48e9b4fc472f9563c6fee3db24c968
Jason Hoang <jhoang@4wall.com>
2018-08-27 11:44:36 -0700

    Added HandleBar helper "ifEquals"

--- src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/HandlebarsHelpers.ts
+++ src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/HandlebarsHelpers.ts
@@ -8,5 +8,8 @@ export class HandlebarsHelpers {
                 return result;
             }
         });
+        Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+            return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+        });
     }
 }
