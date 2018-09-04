fb4c9c5e50c4ca2cf552a74e3069104dbbb1ab60
Jason Hoang <jhoang@4wall.com>
2018-09-03 21:41:28 -0700

    Updated OrderStatus Sales and Rental detail grids

--- src/RentalWorksWeb/Source/Grids/OrderStatusSalesDetailGrid/OrderStatusSalesDetailGridController.ts
+++ src/RentalWorksWeb/Source/Grids/OrderStatusSalesDetailGrid/OrderStatusSalesDetailGridController.ts
@@ -6,6 +6,21 @@
         this.Module = 'OrderStatusSalesDetailGrid';
         this.apiurl = 'api/v1/orderstatusdetail';
     }
+
+    generateRow($control, $generatedtr) {
+        FwBrowse.setAfterRenderRowCallback($control, ($tr: JQuery, dt: FwJsonDataTable, rowIndex: number) => {
+            let isSuspendIn = $tr.find('[data-browsedatafield="IsSuspendIn"]').attr('data-originalvalue');
+            let isSuspendOut = $tr.find('[data-browsedatafield="IsSuspendOut"]').attr('data-originalvalue');
+
+            if (isSuspendIn === 'true') {
+                $tr.find('[data-browsedatafield="InContractId"] div.btnpeek').hide();
+            }
+
+            if (isSuspendOut === 'true') {
+                $tr.find('[data-browsedatafield="OutContractId"] div.btnpeek').hide();
+            }
+        });
+    }
 }
 
 var OrderStatusSalesDetailGridController = new OrderStatusSalesDetailGrid();
