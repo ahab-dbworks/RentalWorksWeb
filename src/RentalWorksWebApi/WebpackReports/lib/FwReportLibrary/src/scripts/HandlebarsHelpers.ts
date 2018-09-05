0d9262db32d898f7aa6f4293f15effd8190f143e
Mike Vermilion <mike@dbworks.com>
2018-09-05 02:07:33 -0700

    Makes some code look nicer

--- src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/scripts/HandlebarsHelpers.ts
+++ src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/scripts/HandlebarsHelpers.ts
@@ -2,11 +2,8 @@
 
 export class HandlebarsHelpers {
     static registerHelpers() {
-		Handlebars.registerHelper('gt', function (leftSide, rightSide, options) {
-            if (leftSide > rightSide) {
-                var result = options.fn(this);
-                return result;
-            }
+		Handlebars.registerHelper('gt', function (arg1, arg2, options) {
+            return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
         });
         Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
             return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
