806b1b300858c0c469051b4c8359ba1cb80f5078
Oliver <oliver@dbworks.com>
2018-09-12 14:37:32 -0700

    Sales Inventory Compatibility grid description now renders details after selecting an item

--- src/RentalWorksWeb/Source/Grids/SalesInventoryCompatibilityGrid/SalesInventoryCompatibilityGridController.js
+++ src/RentalWorksWeb/Source/Grids/SalesInventoryCompatibilityGrid/SalesInventoryCompatibilityGridController.js
@@ -1,5 +1,10 @@
 class SalesInventoryCompatibilityGrid {
     constructor() {
+        this.generateRow = ($control, $generatedtr) => {
+            $generatedtr.find('div[data-browsedatafield="CompatibleWithInventoryId"]').data('onchange', $tr => {
+                $generatedtr.find('.field[data-browsedatafield="CompatibleWithDescription"]').text($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            });
+        };
         this.Module = 'SalesInventoryCompatibilityGrid';
         this.apiurl = 'api/v1/inventorycompatible';
     }
--- src/RentalWorksWeb/Source/Grids/SalesInventoryCompatibilityGrid/SalesInventoryCompatibilityGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SalesInventoryCompatibilityGrid/SalesInventoryCompatibilityGridController.ts
@@ -6,6 +6,12 @@
         this.Module = 'SalesInventoryCompatibilityGrid';
         this.apiurl = 'api/v1/inventorycompatible';
     }
+
+    generateRow = ($control, $generatedtr) => {
+        $generatedtr.find('div[data-browsedatafield="CompatibleWithInventoryId"]').data('onchange', $tr => {
+            $generatedtr.find('.field[data-browsedatafield="CompatibleWithDescription"]').text($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+        });
+    };
 }
 
 (<any>window).SalesInventoryCompatibilityGridController = new SalesInventoryCompatibilityGrid();
