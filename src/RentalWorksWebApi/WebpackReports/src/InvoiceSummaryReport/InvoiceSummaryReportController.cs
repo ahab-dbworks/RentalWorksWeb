fafc463c1d5f0c6226718c9a37c035f27e48cc18
Justin Hoffman <justin@dbworks.com>
2018-09-13 13:44:41 -0700

    Added API endpoint for Invoice Summary Report

---
+++ src/RentalWorksWebApi/WebpackReports/src/InvoiceSummaryReport/InvoiceSummaryReportController.cs
@@ -0,0 +1,83 @@
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using FwStandard.Reporting;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Http;
+namespace WebApi.Modules.Reports.InvoiceSummaryReport
+{
+    public class InvoiceSummaryReportRequest
+    {
+        public DateTime FromDate;
+        public DateTime ToDate;
+        public string DateType;
+        public bool? IncludeNoCharge;
+        public string OfficeLocationId;
+        public string DepartmentId;
+        public string CustomerId;
+        public string DealId;
+    }
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    public class InvoiceSummaryReportController : AppReportController
+    {
+        public InvoiceSummaryReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "InvoiceSummaryReport"; }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFriendlyName() { return "Invoice Summary Report"; }
+        //------------------------------------------------------------------------------------ 
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS. 
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            //return request.parameters["xxxxid"].ToString().TrimEnd(); 
+            return "InvoiceSummaryReport";
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/invoicesummaryreport/render 
+        [HttpPost("render")]
+        public async Task<IActionResult> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/invoicesummaryreport/runreport 
+        [HttpPost("runreport")]
+        public async Task<IActionResult> RunReportAsync([FromBody]InvoiceSummaryReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                InvoiceSummaryReportLoader l = new InvoiceSummaryReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                FwJsonDataTable dt = await l.RunReportAsync(request);
+                return new OkObjectResult(dt);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
