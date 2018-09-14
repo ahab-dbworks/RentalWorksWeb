bd6522897e9bbb880fd704c4800277179598ea07
Justin Hoffman <justin@dbworks.com>
2018-09-14 06:20:13 -0700

    Added C# object for working with selected Checkboxlist items

---
+++ lib/Fw/src/FwStandard/Models/SelectCheckBoxListItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace FwStandard.Models
+{
+
+    //------------------------------------------------------------------------------------ 
+    public class SelectedCheckBoxListItem
+    {
+        public string value;
+    }
+    //------------------------------------------------------------------------------------ 
+    public class SelectedCheckBoxListItems : List<SelectedCheckBoxListItem>
+    {
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (SelectedCheckBoxListItem item in this)
+            {
+                if (!sb.Length.Equals(0))
+                {
+                    sb.Append(",");
+                }
+                sb.Append(item.value);
+            }
+            return sb.ToString();
+        }
+    }
+    //------------------------------------------------------------------------------------ 
+}
