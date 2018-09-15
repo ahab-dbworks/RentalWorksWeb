4670de9135d519638548d7860304a7cacf4ea95e
Joshua Pace <jpace@4wall.com>
2018-09-14 17:31:21 -0700

    Adds new Invoice Summary Report WIP

--- src/RentalWorksWeb/Source/Modules/Reports/InvoiceSummary/RwInvoiceSummaryReportController.js
+++ src/RentalWorksWeb/Source/Modules/Reports/InvoiceSummary/RwInvoiceSummaryReportController.js
@@ -1,4 +1,10 @@
-﻿<div class="fwcontrol fwcontainer fwform fwreport invoicesummaryreport" data-control="FwContainer" data-type="form" data-version="1" data-caption="Invoice Summary" data-rendermode="template" data-mode="" data-hasaudit="false" data-controller="RwInvoiceSummaryReportController">
+routes.push({
+    pattern: /^reports\/invoicesummaryreport/, action: function (match) {
+        return RwInvoiceSummaryReportController.getModuleScreen();
+    }
+});
+var invoiceSummaryTemplateFrontEnd = `
+<div class="fwcontrol fwcontainer fwform fwreport invoicesummaryreport" data-control="FwContainer" data-type="form" data-version="1" data-caption="Invoice Summary" data-rendermode="template" data-mode="" data-hasaudit="false" data-controller="RwInvoiceSummaryReportController">
   <div class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
     <div class="tabs" style="margin-right:10px;">
       <div id="generaltab" class="tab" data-tabpageid="generaltabpage" data-caption="General"></div>
@@ -47,17 +53,78 @@
                   <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Customer" data-datafield="CustomerId" data-displayfield="Customer" data-validationname="CustomerValidation" style="float:left;max-width:300px;"></div>
                 </div>
                 <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
-                  <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Deal" data-datafield="DealId" data-displayfield="Deal" data-validationname="DealValidation" style="float:left;max-width:300px;"></div>
+                  <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Deal" data-datafield="DealId" data-displayfield="Deal" data-formbeforevalidate="beforeValidate" data-validationname="DealValidation" style="float:left;max-width:300px;"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div id="exporttabpage" class="tabpage exporttabpage" data-tabid="exporttab">
-      </div>
+      <div id="exporttabpage" class="tabpage exporttabpage" data-tabid="exporttab"></div>
     </div>
   </div>
-</div>
-
-
+</div>`;
+class RwInvoiceSummaryReportClass extends FwWebApiReport {
+    constructor() {
+        super('InvoiceSummaryReport', 'api/v1/invoicesummaryreport', invoiceSummaryTemplateFrontEnd);
+    }
+    getModuleScreen() {
+        let screen = {};
+        screen.$view = FwModule.getModuleControl('Rw' + this.Module + 'Controller');
+        screen.viewModel = {};
+        screen.properties = {};
+        let $form = this.openForm();
+        screen.load = function () {
+            FwModule.openModuleTab($form, $form.attr('data-caption'), false, 'REPORT', true);
+        };
+        screen.unload = function () {
+        };
+        return screen;
+    }
+    openForm() {
+        let $form = this.getFrontEnd();
+        $form.data('getexportrequest', request => {
+            request.parameters = this.getParameters($form);
+            return request;
+        });
+        return $form;
+    }
+    onLoadForm($form) {
+        this.load($form, this.reportOptions);
+        var appOptions = program.getApplicationOptions();
+        var request = { method: "LoadForm" };
+        this.loadLists($form);
+        const department = JSON.parse(sessionStorage.getItem('department'));
+        const location = JSON.parse(sessionStorage.getItem('location'));
+        FwFormField.setValue($form, 'div[data-datafield="DepartmentId"]', department.departmentid, department.department);
+        FwFormField.setValue($form, 'div[data-datafield="OfficeLocationId"]', location.locationid, location.location);
+    }
+    beforeValidate($browse, $form, request) {
+        const validationName = request.module;
+        const customerId = FwFormField.getValueByDataField($form, 'CustomerId');
+        request.uniqueids = {};
+        switch (validationName) {
+            case 'DealValidation':
+                if (customerId !== "") {
+                    request.uniqueids.CustomerId = customerId;
+                }
+                break;
+        }
+        ;
+    }
+    ;
+    loadLists($form) {
+        FwFormField.loadItems($form.find('div[data-datafield="statuslist"]'), [
+            { value: "NEW", text: "New", selected: "T" },
+            { value: "RETURNED", text: "Returned", selected: "T" },
+            { value: "REVISED", text: "Revised", selected: "T" },
+            { value: "APPROVED", text: "Approved", selected: "T" },
+            { value: "PROCESSED", text: "Processed", selected: "T" },
+            { value: "CLOSED", text: "Closed", selected: "T" },
+            { value: "VOID", text: "Void" }
+        ]);
+    }
+}
+;
+var RwInvoiceSummaryReportController = new RwInvoiceSummaryReportClass();
+//# sourceMappingURL=RwInvoiceSummaryReportController.js.map
---
+++ src/RentalWorksWebApi/WebpackReports/src/InvoiceSummaryReport/index.ts
@@ -0,0 +1,74 @@
+﻿import { WebpackReport } from '../../lib/FwReportLibrary/src/scripts/WebpackReport';
+import { CustomField } from '../../lib/FwReportLibrary/src/scripts/CustomField';
+import { DataTable, DataTableColumn, BrowseRequest } from '../../lib/FwReportLibrary/src/scripts/Browse';
+import { Ajax } from '../../lib/FwReportLibrary/src/scripts/Ajax';
+import { HandlebarsHelpers } from '../../lib/FwReportLibrary/src/scripts/HandlebarsHelpers';
+import * as moment from 'moment';
+import '../../lib/FwReportLibrary/src/theme/webpackReports.scss';
+import './index.scss';
+var hbReport = require("./hbReport.hbs");
+var hbFooter = require("./hbFooter.hbs");
+
+export class InvoiceSummaryReportRequest {
+    FromDate: Date;
+    ToDate: Date;
+    DateType: string;
+    Status: Array<string> = [];
+    OfficeLocationId: string;
+    DepartmentId: string;
+    CustomerId: string;
+    DealId: string;
+}
+
+export class InvoiceSummaryReport extends WebpackReport {
+
+    renderReport(apiUrl: string, authorizationHeader: string, parameters: any): void {
+        try {
+            super.renderReport(apiUrl, authorizationHeader, parameters);
+
+            HandlebarsHelpers.registerHelpers();
+            let request = new InvoiceSummaryReportRequest();
+
+            request.DateType = parameters.DateType;
+            request.ToDate = parameters.ToDate;
+            request.FromDate = parameters.FromDate;
+            //request.Status = parameters.IncludeNoCharge;
+            request.OfficeLocationId = parameters.OfficeLocationId
+            request.DepartmentId = parameters.DepartmentId
+            request.CustomerId = parameters.CustomerId
+            request.DealId = parameters.DealId
+
+            let invoiceSummary: any = {};
+
+            let Promise = Ajax.post<DataTable>(`${apiUrl}/api/v1/invoicesummaryreport/runreport`, authorizationHeader, request)
+                .then((response: DataTable) => {
+                    invoiceSummary = DataTable.toObjectList(response);
+                    invoiceSummary.PrintTime = moment().format('YYYY-MM-DD h:mm:ss A');
+                    invoiceSummary.FromDate = parameters.FromDate;
+                    invoiceSummary.ToDate = parameters.ToDate;
+                    invoiceSummary.Report = 'Agent Billing Report';
+                    invoiceSummary.System = 'RENTALWORKS';
+                    invoiceSummary.Company = '4WALL ENTERTAINMENT';
+                    this.renderFooterHtml(invoiceSummary);
+                    if (this.action === 'Preview' || this.action === 'PrintHtml') {
+                        document.getElementById('pageFooter').innerHTML = this.footerHtml;
+                    }
+                    document.getElementById('pageBody').innerHTML = hbReport(invoiceSummary);
+                    console.log('invoiceSummary: ', invoiceSummary )
+                    this.onRenderReportCompleted();
+                })
+                .catch((ex) => {
+                    this.onRenderReportFailed(ex);
+                });
+        } catch (ex) {
+            this.onRenderReportFailed(ex);
+        }
+    }
+
+    renderFooterHtml(model: DataTable): string {
+        this.footerHtml = hbFooter(model);
+        return this.footerHtml;
+    }
+}
+
+(<any>window).report = new InvoiceSummaryReport();
