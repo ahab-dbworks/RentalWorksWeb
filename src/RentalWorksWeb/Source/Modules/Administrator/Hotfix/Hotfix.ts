1edd5578774878ff4b8e7acc500a02bc48ad875d
Joshua Pace <jpace@4wall.com>
2018-09-17 16:30:21 -0700

    Adds Hotfix

---
+++ src/RentalWorksWeb/Source/Modules/Administrator/Hotfix/Hotfix.ts
@@ -0,0 +1,80 @@
+﻿routes.push({ pattern: /^module\/hotfix$/, action: function (match: RegExpExecArray) { return HotfixController.getModuleScreen(); } });
+
+class Hotfix {
+    Module: string = 'Hotfix';
+    apiurl: string = 'api/v1/hotfix';
+    //---------------------------------------------------------------------------------------------
+    getModuleScreen() {
+        var screen, $browse;
+
+        screen = {};
+        screen.$view = FwModule.getModuleControl(this.Module + 'Controller');
+        screen.viewModel = {};
+        screen.properties = {};
+
+        $browse = this.openBrowse();
+
+        screen.load = function () {
+            FwModule.openModuleTab($browse, 'Hotfix', false, 'BROWSE', true);
+            FwBrowse.databind($browse);
+            FwBrowse.screenload($browse);
+        };
+        screen.unload = function () {
+            FwBrowse.screenunload($browse);
+        };
+
+        return screen;
+    }
+    //---------------------------------------------------------------------------------------------
+    openBrowse() {
+        var $browse;
+
+        $browse = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Browse').html());
+        $browse = FwModule.openBrowse($browse);
+
+        return $browse;
+    }
+    //---------------------------------------------------------------------------------------------
+    openForm(mode: string) {
+        var $form;
+
+        $form = jQuery(jQuery('#tmpl-modules-' + this.Module + 'Form').html());
+        $form = FwModule.openForm($form, mode);
+
+
+        if (mode === 'NEW') {
+            FwFormField.enable($form.find('.ifnew'))
+        } else {
+            FwFormField.disable($form.find('.ifnew'))
+        }
+
+        return $form;
+    }
+    //---------------------------------------------------------------------------------------------
+    loadForm(uniqueids: any) {
+        var $form;
+
+        $form = this.openForm('EDIT');
+        $form.find('div.fwformfield[data-datafield="HotfixId"] input').val(uniqueids.HotfixId);
+        FwModule.loadForm(this.Module, $form);
+
+        return $form;
+    }
+    //---------------------------------------------------------------------------------------------
+    saveForm($form: any, parameters: any) {
+        FwModule.saveForm(this.Module, $form, parameters);
+    }
+    //---------------------------------------------------------------------------------------------
+    loadAudit($form: any) {
+        var uniqueid;
+        uniqueid = $form.find('div.fwformfield[data-datafield="HotfixId"] input').val();
+        FwModule.loadAudit($form, uniqueid);
+    }
+    //---------------------------------------------------------------------------------------------
+    afterLoad($form: any) {
+
+    }
+}
+//---------------------------------------------------------------------------------------------
+var HotfixController = new Hotfix();
+
