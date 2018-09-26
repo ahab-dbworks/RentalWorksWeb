b4dec0fd92898633d9a226b5e8b8cce9d6b14f6f
Justin Hoffman <justin@dbworks.com>
2018-09-26 09:49:57 -0700

    Made GeneratorWatts.RowType required

--- src/RentalWorksWebApi/Modules/Settings/GeneratorWatts/GeneratorWattsRecord.cs
+++ src/RentalWorksWebApi/Modules/Settings/GeneratorWatts/GeneratorWattsRecord.cs
@@ -14,7 +14,7 @@ namespace WebApi.Modules.Settings.GeneratorWatts
         [FwSqlDataField(column: "watts", modeltype: FwDataTypes.Text, maxlength: 15, required: true)]
         public string GeneratorWatts { get; set; }
         //------------------------------------------------------------------------------------
-        [FwSqlDataField(column: "rowtype", modeltype: FwDataTypes.Text, maxlength: 20)]
+        [FwSqlDataField(column: "rowtype", modeltype: FwDataTypes.Text, maxlength: 20, required: true)]
         public string RowType { get; set; }
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "inactive", modeltype: FwDataTypes.Boolean)]
