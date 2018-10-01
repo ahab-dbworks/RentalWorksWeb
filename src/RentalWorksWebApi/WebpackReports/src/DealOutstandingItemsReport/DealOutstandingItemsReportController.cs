918bb8dfabd60d80906ff90a593e27586a8f06f9
Justin Hoffman <justin@dbworks.com>
2018-10-01 05:25:07 -0700

    Added API for Deal Outstanding Items Report

---
+++ src/RentalWorksWebApi/WebpackReports/src/DealOutstandingItemsReport/DealOutstandingItemsReportController.cs
@@ -0,0 +1,94 @@
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using FwStandard.Reporting;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Http;
+namespace WebApi.Modules.Reports.DealOutstandingItemsReport
+{
+    public class DealOutstandingItemsReportRequest
+    {
+        public DateTime FromDate;
+        public DateTime ToDate;
+        public string DateType;  //B-Billing Stop Date E-Estimated Rental Stop Date
+        public bool? IncludeBlankPages;
+        public bool? IncludeFullImages;
+        public bool? IncludeThumbnailImages;
+        public string OfficeLocationId;
+        public string DepartmentId;
+        public string CustomerId;
+        public string DealId;
+        public string OrderUnitId;
+        public string OrderTypeId;
+        public string OrderId;
+        public string ContractId;
+        public string InventoryTypeId;
+        public string CategoryId;
+        public string SubCategoryId;
+        public string InventoryId;
+    }
+
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    public class DealOutstandingItemsReportController : AppReportController
+    {
+        public DealOutstandingItemsReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "DealOutstandingItemsReport"; }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFriendlyName() { return "Deal Outstanding Items Report"; }
+        //------------------------------------------------------------------------------------ 
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS. 
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            //return request.parameters["xxxxid"].ToString().TrimEnd(); 
+            return "DealOutstandingItemsReport";
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/dealoutstandingitemsreport/render 
+        [HttpPost("render")]
+        public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/dealoutstandingitemsreport/runreport 
+        [HttpPost("runreport")]
+        public async Task<ActionResult<FwJsonDataTable>> RunReportAsync([FromBody]DealOutstandingItemsReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                DealOutstandingItemsReportLoader l = new DealOutstandingItemsReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                FwJsonDataTable dt = await l.RunReportAsync(request);
+                return new OkObjectResult(dt);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
---
+++ src/RentalWorksWebApi/WebpackReports/src/DealOutstandingItemsReport/DealOutstandingItemsReportLoader.cs
@@ -0,0 +1,205 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Collections.Generic;
+using System;
+using WebLibrary;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+namespace WebApi.Modules.Reports.DealOutstandingItemsReport
+{
+    public class DealOutstandingItemsReportLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rowtype", modeltype: FwDataTypes.Text)]
+        public string RowType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderid", modeltype: FwDataTypes.Text)]
+        public string OrderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "locationid", modeltype: FwDataTypes.Text)]
+        public string OfficeLocationId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "loccode", modeltype: FwDataTypes.Text)]
+        public string OfficeLocationCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "location", modeltype: FwDataTypes.Text)]
+        public string OfficeLocation { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "customerid", modeltype: FwDataTypes.Text)]
+        public string CustomerId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "customer", modeltype: FwDataTypes.Text)]
+        public string Customer { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealid", modeltype: FwDataTypes.Text)]
+        public string DealId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "deal", modeltype: FwDataTypes.Text)]
+        public string Deal { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealno", modeltype: FwDataTypes.Text)]
+        public string DealNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "departmentid", modeltype: FwDataTypes.Text)]
+        public string DepartmentId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "department", modeltype: FwDataTypes.Text)]
+        public string Department { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "ordertype", modeltype: FwDataTypes.Text)]
+        public string OrderType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "pono", modeltype: FwDataTypes.Text)]
+        public string PoNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderno", modeltype: FwDataTypes.Text)]
+        public string OrderNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderdesc", modeltype: FwDataTypes.Text)]
+        public string OrderDescription { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "barcode", modeltype: FwDataTypes.Text)]
+        public string BarCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterno", modeltype: FwDataTypes.Text)]
+        public string ICode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masternodisplay", modeltype: FwDataTypes.Text)]
+        public string ICodeDisplay { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "trackedby", modeltype: FwDataTypes.Text)]
+        public string TrackedBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "description", modeltype: FwDataTypes.Text)]
+        public string Description { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "categoryid", modeltype: FwDataTypes.Text)]
+        public string CategoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rentalitemid", modeltype: FwDataTypes.Text)]
+        public string ItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "quantity", modeltype: FwDataTypes.Decimal)]
+        public decimal? Quantity { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "vendorid", modeltype: FwDataTypes.Text)]
+        public string VendorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "vendor", modeltype: FwDataTypes.Text)]
+        public string Vendor { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterid", modeltype: FwDataTypes.Text)]
+        public string InventoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "warehouseid", modeltype: FwDataTypes.Text)]
+        public string WarehouseId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "whcode", modeltype: FwDataTypes.Text)]
+        public string WarehouseCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "warehouse", modeltype: FwDataTypes.Text)]
+        public string Warehouse { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masteritemid", modeltype: FwDataTypes.Text)]
+        public string OrderItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "primarymasteritemid", modeltype: FwDataTypes.Text)]
+        public string PrimaryOrderItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "itemclass", modeltype: FwDataTypes.Text)]
+        public string ItemClass { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "itemorder", modeltype: FwDataTypes.Text)]
+        public string ItemOrder { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderby", modeltype: FwDataTypes.Text)]
+        public string OrderBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "notes", modeltype: FwDataTypes.Text)]
+        public string Notes { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rectype", modeltype: FwDataTypes.Text)]
+        public string RecType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rectypedisplay", modeltype: FwDataTypes.Text)]
+        public string RecTypeDisplay { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "parentid", modeltype: FwDataTypes.Text)]
+        public string ParentId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "accratio", modeltype: FwDataTypes.Decimal)]
+        public decimal? AccessoryRatio { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "nestedmasteritemid", modeltype: FwDataTypes.Text)]
+        public string NestedOrderItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "containerrentalitemid", modeltype: FwDataTypes.Text)]
+        public string ContainerItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "containerbarcode", modeltype: FwDataTypes.Text)]
+        public string ContainerBarCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "consignorid", modeltype: FwDataTypes.Text)]
+        public string ConsignorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "consignoragreementid", modeltype: FwDataTypes.Text)]
+        public string ConsignorAgreementId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "purchamt", modeltype: FwDataTypes.Decimal)]
+        public decimal? PurchaseAmount { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "manifestvalue", modeltype: FwDataTypes.Decimal)]
+        public decimal? UnitValue { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "replacementcost", modeltype: FwDataTypes.Decimal)]
+        public decimal? ReplacementCost { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "responsiblepersonid", modeltype: FwDataTypes.Text)]
+        public string ResponsiblePersonId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "responsibleperson", modeltype: FwDataTypes.Text)]
+        public string ResponsiblePerson { get; set; }
+        //------------------------------------------------------------------------------------ 
+        public async Task<FwJsonDataTable> RunReportAsync(DealOutstandingItemsReportRequest request)
+        {
+            FwJsonDataTable dt = null;
+            using (FwSqlConnection conn = new FwSqlConnection(AppConfig.DatabaseSettings.ConnectionString))
+            {
+                using (FwSqlCommand qry = new FwSqlCommand(conn, "getdealoutstandingrpt", this.AppConfig.DatabaseSettings.QueryTimeout))
+                {
+                    qry.AddParameter("@fromdate", SqlDbType.Date, ParameterDirection.Input, request.FromDate);
+                    qry.AddParameter("@todate", SqlDbType.Date, ParameterDirection.Input, request.ToDate);
+                    qry.AddParameter("@datetouse", SqlDbType.Text, ParameterDirection.Input, request.DateType);
+                    qry.AddParameter("@shownones", SqlDbType.Text, ParameterDirection.Input, request.IncludeBlankPages.GetValueOrDefault(false) ? "T" : "F");
+                    qry.AddParameter("@returnimagemode", SqlDbType.Text, ParameterDirection.Input, request.IncludeFullImages.GetValueOrDefault(false) ? "F" : request.IncludeThumbnailImages.GetValueOrDefault(false) ? "T" : "");
+                    qry.AddParameter("@locationid", SqlDbType.Text, ParameterDirection.Input, request.OfficeLocationId);
+                    qry.AddParameter("@departmentid", SqlDbType.Text, ParameterDirection.Input, request.DepartmentId);
+                    qry.AddParameter("@customerid", SqlDbType.Text, ParameterDirection.Input, request.CustomerId);
+                    qry.AddParameter("@dealid", SqlDbType.Text, ParameterDirection.Input, request.DealId);
+                    qry.AddParameter("@orderunitid", SqlDbType.Text, ParameterDirection.Input, request.OrderUnitId);
+                    qry.AddParameter("@ordertypeid", SqlDbType.Text, ParameterDirection.Input, request.OrderTypeId);
+                    qry.AddParameter("@orderid", SqlDbType.Text, ParameterDirection.Input, request.OrderId);
+                    qry.AddParameter("@contractid", SqlDbType.Text, ParameterDirection.Input, request.ContractId);
+                    qry.AddParameter("@inventorydepartmentid", SqlDbType.Text, ParameterDirection.Input, request.InventoryTypeId);
+                    qry.AddParameter("@categoryid", SqlDbType.Text, ParameterDirection.Input, request.CategoryId);
+                    qry.AddParameter("@subcategoryid", SqlDbType.Text, ParameterDirection.Input, request.SubCategoryId);
+                    qry.AddParameter("@masterid", SqlDbType.Text, ParameterDirection.Input, request.InventoryId);
+                    AddPropertiesAsQueryColumns(qry);
+                    dt = await qry.QueryToFwJsonTableAsync(false, 0);
+                }
+            }
+            string[] totalFields = new string[] { "Quantity" };
+            dt.InsertSubTotalRows("OfficeLocation", "RowType", totalFields);
+            dt.InsertSubTotalRows("Customer", "RowType", totalFields);
+            dt.InsertSubTotalRows("Deal", "RowType", totalFields);
+            dt.InsertSubTotalRows("OrderNumber", "RowType", totalFields);
+            dt.InsertTotalRow("RowType", "detail", "grandtotal", totalFields);
+            return dt;
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
