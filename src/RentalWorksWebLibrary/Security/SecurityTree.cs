04be0dc389fc3cd7f9dca7bece4f1a8bc3258b28
Jason Hoang <jhoang@4wall.com>
2018-10-03 10:53:24 -0700

    Changes to CustomField module for consistency

--- src/RentalWorksWebLibrary/Security/SecurityTree.cs
+++ src/RentalWorksWebLibrary/Security/SecurityTree.cs
@@ -279,7 +279,8 @@ namespace WebLibrary.Security
 
             // Add Administrator 
             AddModule("Control",             "{B3ADDF49-64EB-4740-AB41-4327E6E56242}", lv1menuAdministrator.Id, "ControlController",        "module/control",        administratoriconbaseurl + "placeholder.png");
-            AddModule("Custom Fields",       "{C98C4CB4-2036-4D70-BC29-8F5A2874B178}", lv1menuAdministrator.Id, "CustomFieldController",   "module/customfields",   administratoriconbaseurl + "placeholder.png");
+            AddModule("Custom Field",        "{C98C4CB4-2036-4D70-BC29-8F5A2874B178}", lv1menuAdministrator.Id, "CustomFieldController",    "module/customfield",   administratoriconbaseurl + "placeholder.png");
+            AddModule("Custom Forms",        "{CB2EF8FF-2E8D-4AD0-B880-07037B839C5E}", lv1menuAdministrator.Id, "WebFormController", "module/webform", administratoriconbaseurl + "placeholder.png");
             AddModule("Duplicate Rules",     "{2E0EA479-AC02-43B1-87FA-CCE2ABA6E934}", lv1menuAdministrator.Id, "DuplicateRulesController", "module/duplicaterules", administratoriconbaseurl + "placeholder.png");
             AddModule("Group",               "{9BE101B6-B406-4253-B2C6-D0571C7E5916}", lv1menuAdministrator.Id, "GroupController",          "module/group",          administratoriconbaseurl + "group.png", "USER");
             AddModule("Hotfix",              "{9D29A5D9-744F-40CE-AE3B-09219611A680}", lv1menuAdministrator.Id, "HotfixController",         "module/hotfix",         administratoriconbaseurl + "placeholder.png");
@@ -287,7 +288,6 @@ namespace WebLibrary.Security
             AddModule("User",                "{79E93B21-8638-483C-B377-3F4D561F1243}", lv1menuAdministrator.Id, "UserController",           "module/user",           administratoriconbaseurl + "user.png", "USER");
             AddModule("Settings",            "{57150967-486A-42DE-978D-A2B0F843341A}", lv1menuAdministrator.Id, "SettingsController",       "module/settings",       administratoriconbaseurl + "placeholder.png");
             AddModule("Reports",             "{3C5C7603-9E7B-47AB-A722-B29CA09B3B8C}", lv1menuAdministrator.Id, "ReportsController",        "module/reports",        administratoriconbaseurl + "placeholder.png");
-            AddModule("Custom Forms",        "{CB2EF8FF-2E8D-4AD0-B880-07037B839C5E}", lv1menuAdministrator.Id, "WebFormController",       "module/webform",     administratoriconbaseurl + "placeholder.png");
             // Add Submodules
             AddSubModule("User Settings", "{A6704904-01E1-4C6B-B75A-C1D3FCB50C01}", lv1menuSubModules.Id, "UserSettingsController");
             AddSubModule("SubWorksheet", "{F24BDA2F-B37C-45C1-B08E-588D02D50B7C}", lv1menuSubModules.Id, "SubWorksheetController");
