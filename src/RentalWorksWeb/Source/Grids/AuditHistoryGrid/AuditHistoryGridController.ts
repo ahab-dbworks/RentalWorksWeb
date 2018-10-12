daab7f12dcc679e94ea07afce541f7736d99d076
Jason Hoang <jhoang@4wall.com>
2018-10-12 13:30:06 -0700

    Updated AuditHistoryGrid

--- src/RentalWorksWeb/Source/Grids/AuditHistoryGrid/AuditHistoryGridController.ts
+++ src/RentalWorksWeb/Source/Grids/AuditHistoryGrid/AuditHistoryGridController.ts
@@ -8,17 +8,26 @@
             let changes = JSON.parse($oldElement.attr('data-originalvalue'));
             let html: any = [];
 
+            function compare(a, b) {
+                if (a.FieldName < b.FieldName)
+                    return -1;
+                if (a.FieldName > b.FieldName)
+                    return 1;
+                return 0;
+            }
+            changes.sort(compare);
+
             html.push(`<ul style="font-size:14px; width:100%; display:flex;">
-                                <span style="font-weight:bold; float:left; flex:0 0 250px; text-decoration:underline; padding-right:1em;">Field Name</span>
-                                <span style="font-weight:bold; float:left; flex:1 0 250px; text-decoration:underline; padding-right:1em; width:250px">New Value</span>
-                                <span style="font-weight:bold; float:left; flex:1 0 250px; text-decoration:underline; width:250px; word-wrap:break-word">Old Value</span>
+                                <span style="font-weight:bold; float:left; width:160px; text-decoration:underline; padding-right:1em;">Field Name</span>
+                                <span style="font-weight:bold; float:left; width:200px; text-decoration:underline; padding-right:1em;">Old Value</span>
+                                <span style="font-weight:bold; float:left; width:200px; text-decoration:underline;">New Value</span>
                           </ul>`);
 
             for (let i = 0; i < changes.length; i++) {
                 html.push(`<ul style="font-size:14px; width:100%; display:flex;">
-                                <span style="font-weight:bold; float:left; flex:0 0 250px; padding-right:1em;">${changes[i].FieldName}:</span>
-                                <span style="flex:1 0 250px; padding-right:1em; width:250px; word-wrap:break-word">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
-                                <span style="flex:1 0 250px; width:250px; word-wrap:break-word">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
+                                <span style="font-weight:bold; float:left; width:160px; padding-right:1em;">${changes[i].FieldName}:</span>
+                                <span style="width:200px; word-wrap:break-word; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
+                                <span style="width:200px; word-wrap:break-word">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
                           </ul>`);
             }
             jQuery($oldElement).replaceWith(html.join(''));
