f43f5f7167af6a589c41fd4e3af18a3a7cf98979
Jason Hoang <jhoang@4wall.com>
2018-10-16 12:31:35 -0700

    Added alternating shading, increased width of field names column and added word-wrapping to AuditHistory grid

--- src/RentalWorksWeb/Source/Grids/AuditHistoryGrid/AuditHistoryGridController.ts
+++ src/RentalWorksWeb/Source/Grids/AuditHistoryGrid/AuditHistoryGridController.ts
@@ -8,17 +8,17 @@
             let changes = JSON.parse($oldElement.attr('data-originalvalue'));
             let html: any = [];
 
-            html.push(`<ul style="font-size:14px; width:100%; display:flex;">
-                                <span style="font-weight:bold; float:left; width:200px; text-decoration:underline; padding-right:1em;">Field Name</span>
+            html.push(`<ul class="auditHeader" style="font-size:14px; display:flex;">
+                                <span style="font-weight:bold; float:left; width:225px; text-decoration:underline; padding-right:1em;">Field Name</span>
                                 <span style="font-weight:bold; float:left; width:200px; text-decoration:underline; padding-right:1em;">Old Value</span>
                                 <span style="font-weight:bold; float:left; width:200px; text-decoration:underline;">New Value</span>
                           </ul>`);
 
             for (let i = 0; i < changes.length; i++) {
-                html.push(`<ul style="font-size:14px; width:100%; display:flex;">
-                                <span style="font-weight:bold; float:left; width:200px; padding-right:1em;">${changes[i].FieldName}:</span>
-                                <span style="width:200px; word-wrap:break-word; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
-                                <span style="width:200px; word-wrap:break-word">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
+                html.push(`<ul style="font-size:14px; word-wrap:break-word; display:flex;">
+                                <span style="font-weight:bold; float:left; width:225px; padding-right:1em;">${changes[i].FieldName}:</span>
+                                <span style="width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
+                                <span style="width:200px;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
                           </ul>`);
             }
             jQuery($oldElement).replaceWith(html.join(''));
