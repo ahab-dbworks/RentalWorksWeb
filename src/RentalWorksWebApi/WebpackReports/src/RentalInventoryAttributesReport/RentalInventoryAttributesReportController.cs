ccbbadf640465c06a9c6b95c41190a822cf9a500
Justin Hoffman <justin@dbworks.com>
2018-10-29 02:00:16 -0700

    added Rental Inventory Attributes Report API

---
+++ src/RentalWorksWebApi/WebpackReports/src/RentalInventoryAttributesReport/RentalInventoryAttributesReportController.cs
@@ -0,0 +1,81 @@
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using FwStandard.Reporting;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Http;
+namespace WebApi.Modules.Reports.RentalInventoryAttributesReport
+{
+    public class RentalInventoryAttributesReportRequest
+    {
+        public string SortBy { get; set; }
+        public string InventoryTypeId { get; set; }
+        public string CategoryId { get; set; }
+        public string SubCategoryId { get; set; }
+        public string InventoryId { get; set; }
+        public string AttributeId { get; set; }
+    }
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    public class RentalInventoryAttributesReportController : AppReportController
+    {
+        public RentalInventoryAttributesReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "RentalInventoryAttributesReport"; }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFriendlyName() { return "Rental Inventory Attributes Report"; }
+        //------------------------------------------------------------------------------------ 
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS. 
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            //return request.parameters["xxxxid"].ToString().TrimEnd(); 
+            return "RentalInventoryAttributesReport";
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/rentalinventoryattributesreport/render 
+        [HttpPost("render")]
+        public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/rentalinventoryattributesreport/runreport 
+        [HttpPost("runreport")]
+        public async Task<ActionResult<FwJsonDataTable>> RunReportAsync([FromBody]RentalInventoryAttributesReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                RentalInventoryAttributesReportLoader l = new RentalInventoryAttributesReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                FwJsonDataTable dt = await l.RunReportAsync(request);
+                return new OkObjectResult(dt);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
---
+++ src/RentalWorksWebApi/WebpackReports/src/RentalInventoryAttributesReport/RentalInventoryAttributesReportLoader.cs
@@ -0,0 +1,108 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+using WebLibrary;
+
+namespace WebApi.Modules.Reports.RentalInventoryAttributesReport
+{
+    [FwSqlTable("dbo.funcinventoryattributerpt()")]
+    public class RentalInventoryAttributesReportLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterid", modeltype: FwDataTypes.Text)]
+        public string InventoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterno", modeltype: FwDataTypes.Text)]
+        public string ICode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "master", modeltype: FwDataTypes.Text)]
+        public string Description { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterorderby", modeltype: FwDataTypes.Integer)]
+        public int? InventoryOrderBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "availfor", modeltype: FwDataTypes.Text)]
+        public string AvailableFor { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "inventorydepartmentid", modeltype: FwDataTypes.Text)]
+        public string InventoryTypeId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "inventorydepartment", modeltype: FwDataTypes.Text)]
+        public string InventoryType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "inventorydepartmentorderby", modeltype: FwDataTypes.Integer)]
+        public int? InventoryTypeOrderby { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "categoryid", modeltype: FwDataTypes.Text)]
+        public string CategoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "category", modeltype: FwDataTypes.Text)]
+        public string Category { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "categoryorderby", modeltype: FwDataTypes.Integer)]
+        public int? CategoryOrderBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "subcategoryid", modeltype: FwDataTypes.Text)]
+        public string SubCategoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "subcategory", modeltype: FwDataTypes.Text)]
+        public string SubCategory { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "subcategoryorderby", modeltype: FwDataTypes.Integer)]
+        public int? SubCategoryOrderBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "attributeid", modeltype: FwDataTypes.Text)]
+        public string AttributeId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "attribute", modeltype: FwDataTypes.Text)]
+        public string Attribute { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "attributevalueid", modeltype: FwDataTypes.Text)]
+        public string AttributevalueId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "attributevalue", modeltype: FwDataTypes.Text)]
+        public string AttributeValue { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "numericvalue", modeltype: FwDataTypes.Decimal)]
+        public decimal? NumericValue { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "numericonly", modeltype: FwDataTypes.Boolean)]
+        public bool? NumericOnly { get; set; }
+        //------------------------------------------------------------------------------------ 
+        public async Task<FwJsonDataTable> RunReportAsync(RentalInventoryAttributesReportRequest request)
+        {
+            useWithNoLock = false;
+            FwJsonDataTable dt = null;
+            using (FwSqlConnection conn = new FwSqlConnection(AppConfig.DatabaseSettings.ConnectionString))
+            {
+                FwSqlSelect select = new FwSqlSelect();
+                select.EnablePaging = false;
+                using (FwSqlCommand qry = new FwSqlCommand(conn, AppConfig.DatabaseSettings.QueryTimeout))
+                {
+                    SetBaseSelectQuery(select, qry);
+                    select.Parse();
+                    select.AddWhere("(availfor = '" + RwConstants.INVENTORY_AVAILABLE_FOR_RENT + "')");
+                    addStringFilterToSelect("inventorydepartmentid", request.InventoryTypeId, select);
+                    addStringFilterToSelect("categoryid", request.CategoryId, select);
+                    addStringFilterToSelect("subcategoryid", request.SubCategoryId, select);
+                    addStringFilterToSelect("masterid", request.InventoryId, select);
+                    addStringFilterToSelect("attributeid", request.AttributeId, select);
+
+                    select.AddOrderBy("inventorydepartmentorderby, categoryorderby, subcategoryorderby, masterorderby, masterno");
+                    dt = await qry.QueryToFwJsonTableAsync(select, false);
+                }
+            }
+            //string[] totalFields = new string[] { "RentalTotal", "SalesTotal" };
+            //dt.InsertSubTotalRows("GroupField1", "RowType", totalFields);
+            //dt.InsertSubTotalRows("GroupField2", "RowType", totalFields);
+            //dt.InsertTotalRow("RowType", "detail", "grandtotal", totalFields);
+            return dt;
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
