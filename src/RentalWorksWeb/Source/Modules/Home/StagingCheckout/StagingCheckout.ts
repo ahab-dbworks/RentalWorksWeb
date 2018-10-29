feb495355e0591f026a3ea380c6e00c517bbb434
Justin Hoffman <justin@dbworks.com>
2018-10-28 23:27:46 -0700

    fixed Staging typo

--- src/RentalWorksWeb/Source/Modules/Home/StagingCheckout/StagingCheckout.ts
+++ src/RentalWorksWeb/Source/Modules/Home/StagingCheckout/StagingCheckout.ts
@@ -2,7 +2,7 @@
 
 class StagingCheckout {
     Module: string = 'StagingCheckout';
-    caption: string = 'Straging / Check-Out';
+    caption: string = 'Staging / Check-Out';
     nav: string = 'module/checkout';
     id: string = 'C3B5EEC9-3654-4660-AD28-20DE8FF9044D';
     showAddItemToOrder: boolean = false;
