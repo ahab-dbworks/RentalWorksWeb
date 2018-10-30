b479d757aaa3bd73a79109df28c5c96cbca18dcb
Justin Hoffman <justin@dbworks.com>
2018-10-30 10:17:05 -0700

    added new checkboxlist class to receive check box list items for report sorting

---
+++ lib/Fw/src/FwStandard/Models/CheckBoxListItems.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace FwStandard.Models
+{
+
+    //------------------------------------------------------------------------------------ 
+    public class CheckBoxListItem
+    {
+        public string value { get; set; }
+        public string text { get; set; }
+        public bool? selected { get; set; }
+    }
+    //------------------------------------------------------------------------------------ 
+    public class CheckBoxListItems : List<CheckBoxListItem>
+    {
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (CheckBoxListItem item in this)
+            {
+                if (item.selected.GetValueOrDefault(false))
+                {
+                    if (!sb.Length.Equals(0))
+                    {
+                        sb.Append(",");
+                    }
+                    sb.Append(item.value);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+    //------------------------------------------------------------------------------------ 
+}
