f293aba4ecbb2e1951b29ce1b67db0eb7c951321
Justin Hoffman <justin@dbworks.com>
2018-11-02 14:21:15 -0700

    added Rental Inventory Purchase History Report api

---
+++ src/RentalWorksWebApi/WebpackReports/src/RentalInventoryPurchaseHistoryReport/RentalInventoryPurchaseHistoryReportLoader.cs
@@ -0,0 +1,143 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+using WebLibrary;
+
+namespace WebApi.Modules.Reports.RentalInventoryPurchaseHistoryReport
+{
+    [FwSqlTable("inventorypurchaseshistoryrptview")]
+    public class RentalInventoryPurchaseHistoryReportLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rowtype", modeltype: FwDataTypes.Text)]
+        public string RowType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterid", modeltype: FwDataTypes.Text)]
+        public string InventoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterno", modeltype: FwDataTypes.Text)]
+        public string ICode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "master", modeltype: FwDataTypes.Text)]
+        public string Description { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "availfor", modeltype: FwDataTypes.Text)]
+        public string AvailableFor { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "trackedby", modeltype: FwDataTypes.Text)]
+        public string TrackedBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rank", modeltype: FwDataTypes.Boolean)]
+        public bool? Rank { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "warehouseid", modeltype: FwDataTypes.Text)]
+        public string WarehouseId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "whcode", modeltype: FwDataTypes.Text)]
+        public string WarehouseCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "warehouse", modeltype: FwDataTypes.Text)]
+        public string Warehouse { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "departmentid", modeltype: FwDataTypes.Text)]
+        public string DepartmentId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "department", modeltype: FwDataTypes.Text)]
+        public string Department { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "inventorydepartmentid", modeltype: FwDataTypes.Text)]
+        public string InventoryTypeId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "inventorydepartment", modeltype: FwDataTypes.Text)]
+        public string InventoryType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "categoryid", modeltype: FwDataTypes.Text)]
+        public string CategoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "category", modeltype: FwDataTypes.Text)]
+        public string Category { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "subcategoryid", modeltype: FwDataTypes.Text)]
+        public string SubCategoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "subcategory", modeltype: FwDataTypes.Text)]
+        public string SubCategory { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "categorysubcategory", modeltype: FwDataTypes.Text)]
+        public string CategorySubCategory { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "departmentcatsub", modeltype: FwDataTypes.Text)]
+        public string InventoryTypeCategorySubCategory { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "purchasedate", modeltype: FwDataTypes.Date)]
+        public string PurchaseDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "receivedate", modeltype: FwDataTypes.Date)]
+        public string ReceiveDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "vendorid", modeltype: FwDataTypes.Text)]
+        public string VendorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "vendor", modeltype: FwDataTypes.Text)]
+        public string Vendor { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "qty", modeltype: FwDataTypes.Integer)]
+        public int? Quantity { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "unitcost", modeltype: FwDataTypes.Decimal)]
+        public decimal? UnitCost { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "extendedcost", modeltype: FwDataTypes.Decimal)]
+        public decimal? ExtendedCost { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "barcode", modeltype: FwDataTypes.Text)]
+        public string BarCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "serialno", modeltype: FwDataTypes.Text)]
+        public string SerialNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rfid", modeltype: FwDataTypes.Text)]
+        public string RfId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        public async Task<FwJsonDataTable> RunReportAsync(RentalInventoryPurchaseHistoryReportRequest request)
+        {
+            FwJsonDataTable dt = null;
+            using (FwSqlConnection conn = new FwSqlConnection(AppConfig.DatabaseSettings.ConnectionString))
+            {
+                FwSqlSelect select = new FwSqlSelect();
+                select.EnablePaging = false;
+                using (FwSqlCommand qry = new FwSqlCommand(conn, AppConfig.DatabaseSettings.QueryTimeout))
+                {
+                    SetBaseSelectQuery(select, qry);
+                    select.Parse();
+                    select.AddWhere("(availfor = '" + RwConstants.INVENTORY_AVAILABLE_FOR_RENT + "')");
+                    addDateFilterToSelect("purchasedate", request.PurchasedFromDate, select, ">=", "purchasefromdate");
+                    addDateFilterToSelect("purchasedate", request.PurchasedToDate, select, "<=", "purchasetodate");
+                    addDateFilterToSelect("receivedate", request.ReceivedFromDate, select, ">=", "receivefromdate");
+                    addDateFilterToSelect("receivedate", request.ReceivedToDate, select, "<=", "receivetodate");
+
+                    addStringFilterToSelect("warehouseid", request.WarehouseId, select);
+                    addStringFilterToSelect("inventorydepartmentid", request.InventoryTypeId, select);
+                    addStringFilterToSelect("categoryid", request.CategoryId, select);
+                    addStringFilterToSelect("subcategoryid", request.SubCategoryId, select);
+                    addStringFilterToSelect("masterid", request.InventoryId, select);
+
+                    select.AddOrderBy("warehouse, inventorydepartment, category, subcategory, masterno, purchasedate, receivedate");
+                    dt = await qry.QueryToFwJsonTableAsync(select, false);
+                }
+            }
+            string[] totalFields = new string[] { "Quantity", "ExtendedCost" };
+            dt.InsertSubTotalRows("Warehouse", "RowType", totalFields);
+            dt.InsertSubTotalRows("InventoryType", "RowType", totalFields);
+            dt.InsertSubTotalRows("Category", "RowType", totalFields);
+            dt.InsertTotalRow("RowType", "detail", "grandtotal", totalFields);
+            return dt;
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
