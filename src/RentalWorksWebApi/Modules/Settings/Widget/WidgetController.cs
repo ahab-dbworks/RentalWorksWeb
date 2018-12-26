d440b7d21714c97e8b6e6679a05dbd2f9b1c8bc4
Justin Hoffman <justin@dbworks.com>
2018-12-26 05:01:31 -0800

    added parameter for dateBehavior on the widget loadbyname endpoint

--- src/RentalWorksWebApi/Modules/Settings/Widget/WidgetController.cs
+++ src/RentalWorksWebApi/Modules/Settings/Widget/WidgetController.cs
@@ -70,12 +70,12 @@ namespace WebApi.Modules.Settings.Widget
         // GET api/v1/widget/loadbyname/ordersbystatus
         [HttpGet("loadbyname/{widgetApiName}")]
         [FwControllerMethod(Id:"CSCjPzhW5pIbB")]
-        public async Task<ActionResult<Widget>> LoadByName([FromRoute]string widgetApiName, int dataPoints, string locationId, string warehouseId, string departmentId, DateTime? fromDate, DateTime? toDate)
+        public async Task<ActionResult<Widget>> LoadByName([FromRoute]string widgetApiName, int dataPoints, string locationId, string warehouseId, string departmentId, string dateBehavior, DateTime? fromDate, DateTime? toDate)
         {
-            return await DoGetWidget(widgetApiName, dataPoints: dataPoints, locationId: locationId, fromDate: fromDate, toDate: toDate);
+            return await DoGetWidget(widgetApiName, dataPoints: dataPoints, locationId: locationId, dateBehavior: dateBehavior, fromDate: fromDate, toDate: toDate);
         }
         //------------------------------------------------------------------------------------
-        private async Task<ActionResult<Widget>> DoGetWidget(string widgetName, int dataPoints = 0, string locationId = "", string warehouseId = "", string departmentId = "", DateTime? fromDate = null, DateTime? toDate = null)
+        private async Task<ActionResult<Widget>> DoGetWidget(string widgetName, int dataPoints = 0, string locationId = "", string warehouseId = "", string departmentId = "", string dateBehavior = "", DateTime? fromDate = null, DateTime? toDate = null)
         {
             try
             {
@@ -98,6 +98,7 @@ namespace WebApi.Modules.Settings.Widget
                     w.sql = l.Sql;
                     w.counterFieldName = l.CounterFieldName;
                     w.labelFieldName = l.LabelFieldName;
+                    w.dateBehavior = dateBehavior;
                     w.fromDate = fromDate;
                     w.toDate = toDate;
                 }
