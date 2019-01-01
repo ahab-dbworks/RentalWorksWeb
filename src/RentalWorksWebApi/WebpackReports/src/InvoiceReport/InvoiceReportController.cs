de69defa3bb44e015565065c345a03b15c26daf8
Justin Hoffman <justin@dbworks.com>
2019-01-01 09:47:15 -0800

    Added Invoice Report api

---
+++ src/RentalWorksWebApi/WebpackReports/src/InvoiceReport/InvoiceReportController.cs
@@ -0,0 +1,80 @@
+using FwStandard.AppManager;
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using FwStandard.Reporting;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Http;
+namespace WebApi.Modules.Reports.InvoiceReport
+{
+    public class InvoiceReportRequest
+    {
+        public string InvoiceId { get; set; }
+    }
+
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    [FwController(Id: "o5nbWmTr7xy0n")]
+    public class InvoiceReportController : AppReportController
+    {
+        public InvoiceReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "InvoiceReport"; }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFriendlyName() { return "Invoice Report"; }
+        //------------------------------------------------------------------------------------ 
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS. 
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            //return request.parameters["xxxxid"].ToString().TrimEnd(); 
+            return "InvoiceReport";
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/orderreport/render 
+        [HttpPost("render")]
+        [FwControllerMethod(Id: "kI7jYtaWlf8ue")]
+        public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/orderreport/runreport 
+        [HttpPost("runreport")]
+        [FwControllerMethod(Id: "jj2IKqYy5ZpPN")]
+        public async Task<ActionResult<InvoiceReportLoader>> RunReportAsync([FromBody]InvoiceReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                InvoiceReportLoader l = new InvoiceReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                return new OkObjectResult(await l.RunReportAsync(request));
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
