fdf84d56c0070b591024062a48f65a358e185f6e
Justin Hoffman <justin@dbworks.com>
2019-01-01 09:46:21 -0800

    Added Retired Rental Inventory Report api

---
+++ src/RentalWorksWebApi/WebpackReports/src/RetiredRentalInventoryReport/RetiredRentalInventoryReportController.cs
@@ -0,0 +1,90 @@
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using FwStandard.Reporting;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Http;
+using FwStandard.AppManager;
+namespace WebApi.Modules.Reports.RetiredRentalInventoryReport
+{
+    public class RetiredRentalInventoryReportRequest
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool? IncludeUnretired { get; set; }
+        public string WarehouseId { get; set; }
+        public string InventoryTypeId { get; set; }
+        public string CategoryId { get; set; }
+        public string SubCategoryId { get; set; }
+        public string InventoryId { get; set; }
+        public string CustomerId { get; set; }
+        public string DealId { get; set; }
+        public string RetiredReasonId { get; set; }
+    }
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    [FwController(Id: "L8qgvB6nFhAV")]
+    public class RetiredRentalInventoryReportController : AppReportController
+    {
+        public RetiredRentalInventoryReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "RetiredRentalInventoryReport"; }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFriendlyName() { return "Retired Rental Inventory Report"; }
+        //------------------------------------------------------------------------------------ 
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS. 
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            //return request.parameters["xxxxid"].ToString().TrimEnd(); 
+            return "RetiredRentalInventoryReport";
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/retiredrentalinventoryreport/render 
+        [HttpPost("render")]
+        [FwControllerMethod(Id: "VJ159JOnP0ge")]
+        public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/retiredrentalinventoryreport/runreport 
+        [HttpPost("runreport")]
+        [FwControllerMethod(Id: "fkp2hgpw8zmD")]
+        public async Task<ActionResult<FwJsonDataTable>> RunReportAsync([FromBody]RetiredRentalInventoryReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                RetiredRentalInventoryReportLoader l = new RetiredRentalInventoryReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                FwJsonDataTable dt = await l.RunReportAsync(request);
+                return new OkObjectResult(dt);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
