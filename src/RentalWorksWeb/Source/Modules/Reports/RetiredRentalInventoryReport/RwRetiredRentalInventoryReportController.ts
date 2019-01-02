34ed7747638afc5f6dbcc7b1a1801488c65b224b
Jason Hoang <jhoang@4wall.com>
2019-01-02 15:01:38 -0800

    Retired Rental Inventory Report (Issue #62 WiP)

---
+++ src/RentalWorksWeb/Source/Modules/Reports/RetiredRentalInventoryReport/RwRetiredRentalInventoryReportController.ts
@@ -0,0 +1,85 @@
+﻿routes.push({
+    pattern: /^reports\/retiredrentalinventoryreport/, action: function (match: RegExpExecArray) {
+        return RwRetiredRentalInventoryReportController.getModuleScreen();
+    }
+});
+
+var templateRetiredRentalInventoryFrontEnd = `
+<div class="fwcontrol fwcontainer fwform fwreport retiredrentalinventoryreport" data-control="FwContainer" data-type="form" data-version="1" data-caption="Retired Rental Inventory" data-rendermode="template" data-mode="" data-hasaudit="false" data-controller="RwRetiredRentalInventoryReportController">
+  <div class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
+    <div class="tabs" style="margin-right:10px;">
+      <div id="generaltab" class="tab" data-tabpageid="generaltabpage" data-caption="General"></div>
+    </div>
+    <div class="tabpages">
+      <div data-type="tabpage" id="generaltabpage" class="tabpage" data-tabid="generaltab">
+        <div class="flexpage">
+          <div class="flexcolumn" style="max-width:450px; float:left;">
+            <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Retired Date Range">
+              <div class="flexrow">
+                <div data-datafield="FromDate" data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="From" style="max-width:150px;"></div>
+                <div data-datafield="ToDate" data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="To" style="max-width:150px;"></div>
+              </div>
+            </div>
+            <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Options">
+              <div data-datafield="IncludeUnretired" data-control="FwFormField" data-type="checkbox" class="fwcontrol fwformfield" data-caption="Include Un-Retired Items" style="max-width:200px;"></div>
+              <div data-datafield="ShowSellInformation" data-control="FwFormField" data-type="checkbox" class="fwcontrol fwformfield" data-caption="Show Sell Information" style="max-width:200px;"></div>
+            </div>
+          </div>
+          <div class="flexcolumn" style="max-width:450px; float:left;">
+            <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Filters">
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Warehouse" data-datafield="WarehouseId" data-displayfield="Warehouse" data-validationname="WarehouseValidation" style="max-width:300px;"></div>
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Inventory Type" data-datafield="InventoryTypeId" data-displayfield="InventoryType" data-validationname="InventoryTypeValidation" style="max-width:300px;"></div>
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Customer" data-datafield="CustomerId" data-displayfield="Customer" data-validationname="CustomerValidation" style="max-width:300px;"></div>
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Deal" data-datafield="DealId" data-displayfield="Deal" data-validationname="DealValidation" style="max-width:300px;"></div>
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Category" data-datafield="CategoryId" data-displayfield="Category" data-validationname="RentalCategoryValidation" style="max-width:300px;"></div>
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="I-Code" data-datafield="InventoryId" data-displayfield="ICode" data-validationname="RentalInventoryValidation" style="max-width:300px;"></div>
+              <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Retired Reason" data-datafield="RetiredReasonId" data-displayfield="RetiredReason" data-validationname="RetiredReasonValidation" style="max-width:300px;"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="exporttabpage" class="tabpage exporttabpage" data-tabid="exporttab">
+      </div>
+    </div>
+  </div>
+</div>
+`;
+//----------------------------------------------------------------------------------------------
+class RwRetiredRentalInventoryReport extends FwWebApiReport {
+    //----------------------------------------------------------------------------------------------
+    constructor() {
+        super('RetiredRentalInventoryReport', 'api/v1/retiredrentalinventoryreport', templateRetiredRentalInventoryFrontEnd);
+    }
+    //----------------------------------------------------------------------------------------------
+    getModuleScreen() {
+        let screen: any = {};
+        screen.$view = FwModule.getModuleControl(`Rw${this.Module}Controller`);
+        screen.viewModel = {};
+        screen.properties = {};
+
+        let $form = this.openForm();
+
+        screen.load = function () {
+            FwModule.openModuleTab($form, $form.attr('data-caption'), false, 'REPORT', true);
+        };
+        screen.unload = function () {
+        };
+        return screen;
+    }
+    //----------------------------------------------------------------------------------------------
+    openForm() {
+        let $form = this.getFrontEnd();
+        return $form;
+    }
+    //----------------------------------------------------------------------------------------------
+    onLoadForm($form) {
+        this.load($form, this.reportOptions);
+        var appOptions: any = program.getApplicationOptions();
+        var request: any = { method: "LoadForm" };
+
+        FwFormField.setValueByDataField($form, 'ShowSellInformation', true);
+    };
+    //----------------------------------------------------------------------------------------------
+};
+
+var RwRetiredRentalInventoryReportController: any = new RwRetiredRentalInventoryReport();
---
+++ src/RentalWorksWebApi/WebpackReports/src/RetiredRentalInventoryReport/index.ts
@@ -0,0 +1,75 @@
+﻿import { WebpackReport } from '../../lib/FwReportLibrary/src/scripts/WebpackReport';
+import { CustomField } from '../../lib/FwReportLibrary/src/scripts/CustomField';
+import { DataTable, DataTableColumn } from '../../lib/FwReportLibrary/src/scripts/Browse';
+import { Ajax } from '../../lib/FwReportLibrary/src/scripts/Ajax';
+import { HandlebarsHelpers } from '../../lib/FwReportLibrary/src/scripts/HandlebarsHelpers';
+import * as moment from 'moment';
+import './index.scss';
+import '../../lib/FwReportLibrary/src/theme/webpackReports.scss';
+var hbReport = require("./hbReport.hbs");
+var hbFooter = require("./hbFooter.hbs");
+
+export class RetiredRentalInventoryReportRequest {
+    FromDate: Date;
+    ToDate: Date;
+    WarehouseId: string;
+    InventoryTypeId: string;
+    CategoryId: string;
+    SubCategoryId: string;
+    InventoryId: string;
+    CustomerId: string;
+    DealId: string;
+    RetiredReasonId: string;
+}
+
+export class RetiredRentalInventoryReport extends WebpackReport {
+    renderReport(apiUrl: string, authorizationHeader: string, parameters: any): void {
+        try {
+            super.renderReport(apiUrl, authorizationHeader, parameters);
+            HandlebarsHelpers.registerHelpers();
+            let retiredRentalInventory: any = {};
+            let request = new RetiredRentalInventoryReportRequest();
+            request.FromDate = parameters.FromDate;
+            request.ToDate = parameters.ToDate;
+            request.WarehouseId = parameters.WarehouseId;
+            request.InventoryTypeId = parameters.InventoryTypeId;
+            request.CategoryId = parameters.CategoryId;
+            request.SubCategoryId = parameters.SubCategoryId;
+            request.InventoryId = parameters.InventoryId;
+            request.CustomerId = parameters.CustomerId;
+            request.DealId = parameters.DealId;
+            request.RetiredReasonId = parameters.RetiredReasonId;
+
+            let Promise = Ajax.post<DataTable>(`${apiUrl}/api/v1/retiredrentalinventoryreport/runreport`, authorizationHeader, request)
+                .then((response: DataTable) => {
+                    retiredRentalInventory.Items = DataTable.toObjectList(response);
+                    this.renderFooterHtml(retiredRentalInventory);
+                    retiredRentalInventory.PrintTime = moment().format('YYYY-MM-DD h:mm:ss A');
+                    retiredRentalInventory.FromDate = parameters.FromDate;
+                    retiredRentalInventory.ToDate = parameters.ToDate;
+                    retiredRentalInventory.Report = 'Retired Rental Inventory Report';
+                    retiredRentalInventory.System = 'RENTALWORKS';
+                    retiredRentalInventory.Company = '4WALL ENTERTAINMENT';
+
+                    console.log(retiredRentalInventory.Items);
+                    if (this.action === 'Preview' || this.action === 'PrintHtml') {
+                        document.getElementById('pageFooter').innerHTML = this.footerHtml;
+                    }
+                    document.getElementById('pageBody').innerHTML = hbReport(retiredRentalInventory);
+
+                    this.onRenderReportCompleted();
+                })
+                .catch((ex) => {
+                    this.onRenderReportFailed(ex);
+                });
+        } catch (ex) {
+            this.onRenderReportFailed(ex);
+        }
+    }
+    renderFooterHtml(model: DataTable): string {
+        this.footerHtml = hbFooter(model);
+        return this.footerHtml;
+    }
+}
+
+(<any>window).report = new RetiredRentalInventoryReport();
