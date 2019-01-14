b35fb1700e699cb46fb3248c9f6fd603ee58c34f
Justin Hoffman <justin@dbworks.com>
2019-01-14 00:00:35 -0800

    Added Complete QC functions to api

---
+++ src/RentalWorksWebApi/Modules/Home/CompleteQc/CompleteQcController.cs
@@ -0,0 +1,87 @@
+using FwStandard.AppManager;
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApi.Modules.Home.CompleteQc
+{
+
+    public class CompleteQcItemRequest
+    {
+        public string Code { get; set; }
+        public bool? QcAnyway { get; set; }
+    }
+
+    public class UpdateQcItemRequest
+    {
+        public string ItemId { get; set; }
+        public string ItemQcId { get; set; }
+        public string ConditionId { get; set; }
+        public string Note { get; set; }
+    }
+
+
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "home-v1")]
+    [FwController(Id: "VwNYsEONLutM")]
+    public class CompleteQcController : AppDataController
+    {
+        public CompleteQcController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+
+
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/completeqc/completeqcitem
+        [HttpPost("completeqcitem")]
+        [FwControllerMethod(Id: "xzYWo1xclDpJ")]
+        public async Task<ActionResult<CompleteQcItemReponse>> CompleteQcItem([FromBody]CompleteQcItemRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                CompleteQcItemReponse response = await CompleteQcFunc.CompleteQcItem(AppConfig, UserSession, request);
+                return new OkObjectResult(response);
+
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/completeqc/updateqcitem
+        [HttpPost("updateqcitem")]
+        [FwControllerMethod(Id: "ge5Y42NIEBGE")]
+        public async Task<ActionResult<UpdateQcItemReponse>> StageItem([FromBody]UpdateQcItemRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                UpdateQcItemReponse response = await CompleteQcFunc.UpdateQcItem(AppConfig, UserSession, request);
+                return new OkObjectResult(response);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
---
+++ src/RentalWorksWebApi/Modules/Home/CompleteQc/CompleteQcFunc.cs
@@ -0,0 +1,87 @@
+﻿using FwStandard.Models;
+using FwStandard.SqlServer;
+using System.Data;
+using System.Threading.Tasks;
+using WebApi.Logic;
+
+namespace WebApi.Modules.Home.CompleteQc
+{
+
+    public class CompleteQcItemReponse : TSpStatusReponse
+    {
+        public string InventoryId{ get; set; }
+        public string ICode{ get; set; }
+        public string Description{ get; set; }
+        public string ConditionId { get; set; }
+        public string Condition { get; set; }
+        public string ItemId{ get; set; }
+        public string ItemQcId { get; set; }
+        public bool CannotQcItemBecauseOfStatus { get; set; }
+        public bool ItemDoesNotNeedQc { get; set; }
+    }
+
+
+    public class UpdateQcItemReponse : TSpStatusReponse
+    {
+    }
+
+    public static class CompleteQcFunc
+    {
+        //-------------------------------------------------------------------------------------------------------
+        public static async Task<CompleteQcItemReponse> CompleteQcItem(FwApplicationConfig appConfig, FwUserSession userSession, CompleteQcItemRequest request)
+        {
+            CompleteQcItemReponse response = new CompleteQcItemReponse();
+            using (FwSqlConnection conn = new FwSqlConnection(appConfig.DatabaseSettings.ConnectionString))
+            {
+                FwSqlCommand qry = new FwSqlCommand(conn, "completeqcitem", appConfig.DatabaseSettings.QueryTimeout);
+                qry.AddParameter("@code", SqlDbType.NVarChar, ParameterDirection.Input, request.Code);
+                qry.AddParameter("@usersid", SqlDbType.NVarChar, ParameterDirection.Input, userSession.UsersId);
+                qry.AddParameter("@qcanyway", SqlDbType.NVarChar, ParameterDirection.Input, (request.QcAnyway.GetValueOrDefault(false) ? "T" : "F"));
+                qry.AddParameter("@masterid", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@masterno", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@description", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@conditionid", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@condition", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@rentalitemid", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@rentalitemqcid", SqlDbType.NVarChar, ParameterDirection.Output);
+                qry.AddParameter("@status", SqlDbType.Int, ParameterDirection.Output);
+                qry.AddParameter("@msg", SqlDbType.NVarChar, ParameterDirection.Output);
+                await qry.ExecuteNonQueryAsync(true);
+                response.InventoryId = qry.GetParameter("@masterid").ToString();
+                response.ICode = qry.GetParameter("@masterno").ToString();
+                response.Description = qry.GetParameter("@description").ToString();
+                response.ConditionId = qry.GetParameter("@conditionid").ToString();
+                response.Condition = qry.GetParameter("@condition").ToString();
+                response.ItemId = qry.GetParameter("@rentalitemid").ToString();
+                response.ItemQcId = qry.GetParameter("@rentalitemqcid").ToString();
+                response.CannotQcItemBecauseOfStatus = qry.GetParameter("@status").ToInt32().Equals(1250);
+                response.ItemDoesNotNeedQc = qry.GetParameter("@status").ToInt32().Equals(1251);
+                response.status = qry.GetParameter("@status").ToInt32();
+                response.success = (response.status == 0);
+                response.msg = qry.GetParameter("@msg").ToString();
+            }
+            return response;
+        }
+        //-------------------------------------------------------------------------------------------------------
+        public static async Task<UpdateQcItemReponse> UpdateQcItem(FwApplicationConfig appConfig, FwUserSession userSession, UpdateQcItemRequest request)
+        {
+            UpdateQcItemReponse response = new UpdateQcItemReponse();
+            using (FwSqlConnection conn = new FwSqlConnection(appConfig.DatabaseSettings.ConnectionString))
+            {
+                FwSqlCommand qry = new FwSqlCommand(conn, "updateqcitem", appConfig.DatabaseSettings.QueryTimeout);
+                qry.AddParameter("@rentalitemid", SqlDbType.NVarChar, ParameterDirection.Input, request.ItemId);
+                qry.AddParameter("@rentalitemqcid", SqlDbType.NVarChar, ParameterDirection.Input, request.ItemQcId);
+                qry.AddParameter("@conditionid", SqlDbType.NVarChar, ParameterDirection.Input, request.ConditionId);
+                qry.AddParameter("@note", SqlDbType.NVarChar, ParameterDirection.Input, request.Note);
+                qry.AddParameter("@status", SqlDbType.Int, ParameterDirection.Output);
+                qry.AddParameter("@msg", SqlDbType.NVarChar, ParameterDirection.Output);
+                await qry.ExecuteNonQueryAsync(true);
+                response.status = qry.GetParameter("@status").ToInt32();
+                response.success = (response.status == 0);
+                response.msg = qry.GetParameter("@msg").ToString();
+            }
+            return response;
+        }
+        //-------------------------------------------------------------------------------------------------------
+    }
+}
