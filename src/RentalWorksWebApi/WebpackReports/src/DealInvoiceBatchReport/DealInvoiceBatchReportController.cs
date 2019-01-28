dddeddf76394f8c7592e746ea974de9e6691ec86
Jason Hoang <jhoang@4wall.com>
2019-01-28 15:48:33 -0800

    Added DealInvoiceBatch Report API - Issue #32

---
+++ src/RentalWorksWebApi/WebpackReports/src/DealInvoiceBatchReport/DealInvoiceBatchReportController.cs
@@ -0,0 +1,81 @@
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using System;
+using FwStandard.Reporting;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Http;
+using FwStandard.AppManager;
+namespace WebApi.Modules.Reports.DealInvoiceBatchReport
+{
+    public class DealInvoiceBatchReportRequest
+    {
+        public string BatchId { get; set; }
+
+    }
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    [FwController(Id: "t3byQWNa3hZ4H")]
+    public class DealInvoiceBatchReportController : AppReportController
+    {
+        public DealInvoiceBatchReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "DealInvoiceBatchReport"; }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFriendlyName() { return "Deal Invoice Batch"; }
+        //------------------------------------------------------------------------------------ 
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS. 
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            //return request.parameters["xxxxid"].ToString().TrimEnd(); 
+            return "DealInvoiceBatchReport";
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/dealinvoicebatchreport/render 
+        [HttpPost("render")]
+        [FwControllerMethod(Id: "TayhUDJZ1gqjx")]
+        public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/dealinvoicebatchreport/runreport 
+        [HttpPost("runreport")]
+        [FwControllerMethod(Id: "yvXJUqbHRbd09")]
+        public async Task<ActionResult<FwJsonDataTable>> RunReportAsync([FromBody]DealInvoiceBatchReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                DealInvoiceBatchReportLoader l = new DealInvoiceBatchReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                FwJsonDataTable dt = await l.RunReportAsync(request);
+                return new OkObjectResult(dt);
+            }
+            catch (Exception ex)
+            {
+                FwApiException jsonException = new FwApiException();
+                jsonException.StatusCode = StatusCodes.Status500InternalServerError;
+                jsonException.Message = ex.Message;
+                jsonException.StackTrace = ex.StackTrace;
+                return StatusCode(jsonException.StatusCode, jsonException);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
