8af328f7e5a6798f59f839308bdfb7206492b910
Jason Hoang <jhoang@4wall.com>
2019-01-29 16:57:31 -0800

    Added ReceiptBatch API and Report - Issue #32

---
+++ src/RentalWorksWebApi/WebpackReports/src/ReceiptBatchReport/ReceiptBatchReportLoader.cs
@@ -0,0 +1,91 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+namespace WebApi.Modules.Reports.ReceiptBatchReport
+{
+    [FwSqlTable("receiptbatchview")]
+    public class ReceiptBatchReportLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rowtype", modeltype: FwDataTypes.Text)]
+        public string RowType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "chgbatchid", modeltype: FwDataTypes.Text)]
+        public string BatchId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "chgbatchno", modeltype: FwDataTypes.Text)]
+        public string BatchNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "chgbatchdate", modeltype: FwDataTypes.Date)]
+        public string BatchDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "arid", modeltype: FwDataTypes.Text)]
+        public string ReceiptId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "locationid", modeltype: FwDataTypes.Text)]
+        public string LocationId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "location", modeltype: FwDataTypes.Text)]
+        public string Location { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "paymentby", modeltype: FwDataTypes.Text)]
+        public string PaymentBy { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealid", modeltype: FwDataTypes.Text)]
+        public string DealId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "customerid", modeltype: FwDataTypes.Text)]
+        public string CustomerId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "customer", modeltype: FwDataTypes.Text)]
+        public string Customer { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "ardate", modeltype: FwDataTypes.Date)]
+        public string ReceiptDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "checkno", modeltype: FwDataTypes.Text)]
+        public string CheckNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "paytypeid", modeltype: FwDataTypes.Text)]
+        public string PaymentTypeId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "paytype", modeltype: FwDataTypes.Text)]
+        public string PaymentType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "amount", modeltype: FwDataTypes.Decimal)]
+        public decimal? Amount { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "deal", modeltype: FwDataTypes.Text)]
+        public string Deal { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealno", modeltype: FwDataTypes.Text)]
+        public string DealNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        public async Task<FwJsonDataTable> RunReportAsync(ReceiptBatchReportRequest request)
+        {
+            FwJsonDataTable dt = null;
+            using (FwSqlConnection conn = new FwSqlConnection(AppConfig.DatabaseSettings.ConnectionString))
+            {
+                FwSqlSelect select = new FwSqlSelect();
+                select.EnablePaging = false;
+                using (FwSqlCommand qry = new FwSqlCommand(conn, AppConfig.DatabaseSettings.QueryTimeout))
+                {
+                    SetBaseSelectQuery(select, qry);
+                    select.Parse();
+                    addStringFilterToSelect("chgbatchid", request.BatchId, select);
+                    select.AddOrderBy("chgbatchdate, customer, deal, ardate, checkno");
+                    dt = await qry.QueryToFwJsonTableAsync(select, false);
+                }
+                string[] totalFields = new string[] { "Amount" };
+                dt.InsertTotalRow("RowType", "detail", "receipttotal", totalFields);
+            }
+            return dt;
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
---
+++ src/RentalWorksWebApi/WebpackReports/src/ReceiptBatchReport/index.ts
@@ -0,0 +1,60 @@
+﻿import { WebpackReport } from '../../lib/FwReportLibrary/src/scripts/WebpackReport';
+import { CustomField } from '../../lib/FwReportLibrary/src/scripts/CustomField';
+import { DataTable, DataTableColumn } from '../../lib/FwReportLibrary/src/scripts/Browse';
+import { Ajax } from '../../lib/FwReportLibrary/src/scripts/Ajax';
+import { HandlebarsHelpers } from '../../lib/FwReportLibrary/src/scripts/HandlebarsHelpers';
+import * as moment from 'moment';
+import './index.scss';
+import '../../lib/FwReportLibrary/src/theme/webpackReports.scss';
+var hbReport = require("./hbReport.hbs");
+var hbFooter = require("./hbFooter.hbs");
+
+export class ReceiptBatchReportRequest {
+    BatchId: string;
+    BatchNumber: string;
+    OfficeLocation: string;
+}
+
+export class ReceiptBatchReport extends WebpackReport {
+    renderReport(apiUrl: string, authorizationHeader: string, parameters: any): void {
+        try {
+            super.renderReport(apiUrl, authorizationHeader, parameters);
+            HandlebarsHelpers.registerHelpers();
+            let report: any = {};
+            let request = new ReceiptBatchReportRequest();
+            request.BatchId = parameters.BatchId;
+
+            let Promise = Ajax.post<DataTable>(`${apiUrl}/api/v1/receiptbatchreport/runreport`, authorizationHeader, request)
+                .then((response: DataTable) => {
+                    report.Items = DataTable.toObjectList(response);
+                    this.renderFooterHtml(report);
+                    report.PrintTime = moment().format('YYYY-MM-DD h:mm:ss A');
+                    report.Date = moment().format('MM/DD/YYYY');
+                    report.Report = 'Receipt Batch Report';
+                    report.System = 'RENTALWORKS';
+                    report.Company = '4WALL ENTERTAINMENT';
+                    report.BatchNumber = report.Items[0].BatchNumber;
+                    report.OfficeLocation = report.Items[0].Location;
+                    console.log(report);
+
+                    if (this.action === 'Preview' || this.action === 'PrintHtml') {
+                        document.getElementById('pageFooter').innerHTML = this.footerHtml;
+                    }
+                    document.getElementById('pageBody').innerHTML = hbReport(report);
+
+                    this.onRenderReportCompleted();
+                })
+                .catch((ex) => {
+                    this.onRenderReportFailed(ex);
+                });
+        } catch (ex) {
+            this.onRenderReportFailed(ex);
+        }
+    }
+    renderFooterHtml(model: DataTable): string {
+        this.footerHtml = hbFooter(model);
+        return this.footerHtml;
+    }
+}
+
+(<any>window).report = new ReceiptBatchReport();
