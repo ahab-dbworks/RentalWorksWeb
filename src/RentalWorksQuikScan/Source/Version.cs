0cc14b982967dbe52a29b0c6a3765d9b1fe57f52
Mike Vermilion <mike@dbworks.com>
2019-03-08 13:51:41 -0800

    QS: Updates required database version, which seems not to be used currently.

--- src/RentalWorksQuikScan/Source/Version.cs
+++ src/RentalWorksQuikScan/Source/Version.cs
@@ -2,7 +2,7 @@
 {
     static class Version
     {
-        public const string RequiredDatabaseVersion = "2017.001";
+        public const string RequiredDatabaseVersion = "2018.001";
         public const string RequiredHotfixFilename  = "";
         public const string MinIosAppVersion       = "2015.1.1.130";
     }
