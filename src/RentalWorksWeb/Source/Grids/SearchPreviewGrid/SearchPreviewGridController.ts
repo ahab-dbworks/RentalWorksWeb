c31c812747d0a34737c350be451e28888cbc4241
Jason Hoang <jhoang@4wall.com>
2019-03-11 13:04:59 -0700

    Fixed issue with RefreshAvailability in the Search Preview grid.  - Issue #276

--- src/RentalWorksWeb/Source/Grids/SearchPreviewGrid/SearchPreviewGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SearchPreviewGrid/SearchPreviewGridController.ts
@@ -6,30 +6,17 @@
 //Refresh Availability
 FwApplicationTree.clickEvents['{3756AF3A-1611-4BCD-BBD9-E3233F5A772E}'] = function (e) {
     const $grid = jQuery(this).closest('[data-type="Grid"]');
-    const $form = jQuery(this).closest('.fwform');
-    const module = $form.attr('data-controller').replace('Controller', '');
-    const recType = jQuery('#searchpopup').find('[data-datafield="InventoryType"] input:checked').val();
-    var defaultRows = sessionStorage.getItem('browsedefaultrows');
-
-    const pageNumber = $grid.attr('data-pageno');
+    const $popup = jQuery('#searchpopup');
+    const parentId = FwFormField.getValueByDataField($popup, 'ParentFormId');
     $grid.data('ondatabind', function (request) {
-        request.uniqueids = {
-            OrderId: FwFormField.getValueByDataField($form, `${module}Id`),
-            RecType: recType,
-            RefreshAvailability: true
-        };
-        request.pagesize = defaultRows;
-        request.pageno = pageNumber;
+        request.SessionId = parentId;
+        request.RefreshAvailability = true;
     });
 
     FwBrowse.search($grid);
     //sets refreshavailability back to its default
     $grid.data('ondatabind', function (request) {
-        request.uniqueids = {
-            OrderId: FwFormField.getValueByDataField($form, `${module}Id`),
-            RecType: recType
-        };
-        request.pagesize = defaultRows;
+        request.SessionId = parentId;
     });
 }
 //----------------------------------------------------------------------------------------------
