732d615c0563e3853c2daa700e70f81d5c05b9f4
Justin Hoffman <justin@dbworks.com>
2019-03-10 22:46:26 -0700

    added new legend behavior to inventory validations

--- src/RentalWorksWeb/Source/Validations/RentalInventoryValidation/RentalInventoryValidationController.ts
+++ src/RentalWorksWeb/Source/Validations/RentalInventoryValidation/RentalInventoryValidationController.ts
@@ -2,13 +2,20 @@
     Module: string = 'RentalInventoryValidation';
     apiurl: string = 'api/v1/rentalinventory';
 
+
+
     addLegend($control) {
-        FwBrowse.addLegend($control, 'Item', '#FFFFFF');
-        FwBrowse.addLegend($control, 'Accessory', '#fffa00');
-        FwBrowse.addLegend($control, 'Complete', '#0080ff');
-        FwBrowse.addLegend($control, 'Kit', '#00c400');
-        FwBrowse.addLegend($control, 'Misc', '#ff0080');
-        FwBrowse.addLegend($control, 'Container', '#FF8040');
+        try {
+            FwAppData.apiMethod(true, 'GET', `api/v1/legend/rentalinventory`, null, FwServices.defaultTimeout, function onSuccess(response) {
+                for (var key in response) {
+                    FwBrowse.addLegend($control, key, response[key]);
+                }
+            }, function onError(response) {
+                FwFunc.showError(response);
+            }, $control)
+        } catch (ex) {
+            FwFunc.showError(ex);
+        }
     }
 }
 
