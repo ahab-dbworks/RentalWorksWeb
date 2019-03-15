8d2e04b1eb923ac15f17107a25d12b9b6c034976
Joshua Pace <jpace@4wall.com>
2019-03-15 12:37:21 -0700

    Billing Progress Report rewrite, increases font size on all reports using global scss - issue #247

--- src/RentalWorksWebApi/WebpackReports/src/BillingProgressReport/index.ts
+++ src/RentalWorksWebApi/WebpackReports/src/BillingProgressReport/index.ts
@@ -16,10 +16,9 @@ export class BillingProgressReport extends WebpackReport {
 
             Ajax.post<DataTable>(`${apiUrl}/api/v1/billingprogressreport/runreport`, authorizationHeader, parameters)
                 .then((response: DataTable) => {
-                    const data: any = {};
-                    data.Rows = DataTable.toObjectList(response);
+                    const data: any = DataTable.toObjectList(response);
                     data.PrintTime = `Printed on ${moment().format('MM/DD/YYYY')} at ${moment().format('h:mm:ss A')}`;
-                    data.ToDate = parameters.ToDate;
+                    data.AsOfDate = parameters.AsOfDate;
                     data.Report = 'Billing Progress Report';
                     data.System = 'RENTALWORKS';
                     data.Company = '4WALL ENTERTAINMENT';
