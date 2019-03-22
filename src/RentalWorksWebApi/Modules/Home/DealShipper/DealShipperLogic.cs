548c641f572a2292a6511180a107da0d96035b44
Justin Hoffman <justin@dbworks.com>
2019-03-22 00:17:06 -0700

    Resolved #311: Fixed bug where saving new records in the Deal Carrier grid would give an error: VendorId is required.

--- src/RentalWorksWebApi/Modules/Home/DealShipper/DealShipperLogic.cs
+++ src/RentalWorksWebApi/Modules/Home/DealShipper/DealShipperLogic.cs
@@ -2,7 +2,7 @@ using FwStandard.AppManager;
 using WebApi.Logic;
 namespace WebApi.Modules.Home.DealShipper
 {
-    [FwLogic(Id:"Fto5kQYOglEN")]
+    [FwLogic(Id: "Fto5kQYOglEN")]
     public class DealShipperLogic : AppBusinessLogic
     {
         //------------------------------------------------------------------------------------ 
@@ -14,34 +14,34 @@ namespace WebApi.Modules.Home.DealShipper
             dataLoader = dealShipperLoader;
         }
         //------------------------------------------------------------------------------------ 
-        [FwLogicProperty(Id:"a1GpNe3LmHZo", IsPrimaryKey:true)]
+        [FwLogicProperty(Id: "a1GpNe3LmHZo", IsPrimaryKey: true)]
         public string DealShipperId { get { return dealShipper.DealShipperId; } set { dealShipper.DealShipperId = value; } }
 
-        [FwLogicProperty(Id:"tJLHZgzYyrXS")]
+        [FwLogicProperty(Id: "tJLHZgzYyrXS")]
         public string DealId { get { return dealShipper.DealId; } set { dealShipper.DealId = value; } }
 
-        [FwLogicProperty(Id:"NrQB01Iy7IiW")]
+        [FwLogicProperty(Id: "NrQB01Iy7IiW")]
         public string VendorId { get { return dealShipper.VendorId; } set { dealShipper.VendorId = value; } }
 
-        [FwLogicProperty(Id:"etXOei1dFetX", IsReadOnly:true)]
-        public string CarrierId { get; set; }
+        [FwLogicProperty(Id: "etXOei1dFetX")]
+        public string CarrierId { get { return VendorId; } set { this.VendorId = value; } }
 
-        [FwLogicProperty(Id:"etXOei1dFetX", IsReadOnly:true)]
+        [FwLogicProperty(Id: "etXOei1dFetX", IsReadOnly: true)]
         public string Carrier { get; set; }
 
-        [FwLogicProperty(Id:"XsfG2fvN0d8G")]
+        [FwLogicProperty(Id: "XsfG2fvN0d8G")]
         public string ShipperAcct { get { return dealShipper.ShipperAcct; } set { dealShipper.ShipperAcct = value; } }
 
-        [FwLogicProperty(Id:"wZzLQvhWRtpW")]
+        [FwLogicProperty(Id: "wZzLQvhWRtpW")]
         public bool? IsPrimary { get { return dealShipper.IsPrimary; } set { dealShipper.IsPrimary = value; } }
 
-        [FwLogicProperty(Id:"fQKWbapmJ51c")]
+        [FwLogicProperty(Id: "fQKWbapmJ51c")]
         public string ShipViaId { get { return dealShipper.ShipViaId; } set { dealShipper.ShipViaId = value; } }
 
-        [FwLogicProperty(Id:"3y6AIQyyyJqI", IsReadOnly:true)]
+        [FwLogicProperty(Id: "3y6AIQyyyJqI", IsReadOnly: true)]
         public string ShipVia { get; set; }
 
-        [FwLogicProperty(Id:"zNqbxgrXcKP6")]
+        [FwLogicProperty(Id: "zNqbxgrXcKP6")]
         public string DateStamp { get { return dealShipper.DateStamp; } set { dealShipper.DateStamp = value; } }
 
         //------------------------------------------------------------------------------------ 
