7c684db256937b87cfc5c3e4646612b84877b4cb
Mike Vermilion <mike@dbworks.com>
2019-04-01 10:34:28 -0700

    QS: Adds RentalWorks back into the title on the Home screen.  Resolves #315

--- src/RentalWorksQuikScan/Modules/Home/Home.js
+++ src/RentalWorksQuikScan/Modules/Home/Home.js
@@ -3,7 +3,7 @@ var RwHome = {};
 RwHome.getHomeScreen = function(viewModel, properties) {
     var combinedViewModel, screen, applicationOptions, $menuObject, nodeModule;
     combinedViewModel = jQuery.extend({
-        captionPageTitle:         '<div class="apptitle bgothm center" style="padding-top:10px 0;font-size:18px;color:#f2f2f2;">RentalWorks</div>',
+        captionPageTitle:         '<div class="apptitle bgothm center" style="padding-top:10px 0;font-size:18px;color:#f2f2f2;">RentalWorks QuikScan</div>',
         captionPageSubTitle:      ''
     }, viewModel);
     combinedViewModel.htmlPageBody = Mustache.render(jQuery('#tmpl-home').html(), combinedViewModel);
