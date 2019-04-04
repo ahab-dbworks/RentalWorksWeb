aaf80e0bd55e0af6a59cdcf592040e3e28bb6a3a
Joshua Pace <jpace@4wall.com>
2019-04-04 09:40:34 -0700

    changes col type to money in customer / deal credit browse

--- src/RentalWorksWeb/Source/Modules/Home/DealCredit/DealCredit.ts
+++ src/RentalWorksWeb/Source/Modules/Home/DealCredit/DealCredit.ts
@@ -106,16 +106,16 @@ class DealCredit {
                <div class="field" data-caption="Ref/Check" data-datafield="CheckNumber" data-browsedatatype="text" data-sort="off"></div>
              </div>
              <div class="column flexcolumn" max-width="150px" data-visible="true">
-               <div class="field" data-caption="Amount" data-datafield="Amount" data-browsedatatype="number" data-sort="off"></div>
+               <div class="field" data-caption="Amount" data-datafield="Amount" data-browsedatatype="money" data-sort="off"></div>
              </div>
              <div class="column flexcolumn" max-width="150px" data-visible="true">
-               <div class="field" data-caption="Applied" data-datafield="Applied" data-browsedatatype="number" data-sort="off"></div>
+               <div class="field" data-caption="Applied" data-datafield="Applied" data-browsedatatype="money" data-sort="off"></div>
              </div>
              <div class="column flexcolumn" max-width="150px" data-visible="true">
-               <div class="field" data-caption="Refunded" data-datafield="Refunded" data-browsedatatype="number" data-sort="off"></div>
+               <div class="field" data-caption="Refunded" data-datafield="Refunded" data-browsedatatype="money" data-sort="off"></div>
              </div>
              <div class="column flexcolumn" max-width="150px" data-visible="true">
-               <div class="field" data-caption="Remaining" data-datafield="Remaining" data-browsedatatype="number" data-sort="off"></div>
+               <div class="field" data-caption="Remaining" data-datafield="Remaining" data-browsedatatype="money" data-sort="off"></div>
              </div>
              <div class="column flexcolumn" data-width="0" data-visible="false">
                <div class="field" data-datafield="Inactive" data-browsedatatype="text" data-visible="false"></div>
