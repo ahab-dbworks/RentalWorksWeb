a326e2c96f7e49ddc3612107270acb77c8862baf
Jason Hoang <jhoang@4wall.com>
2019-04-06 11:34:28 -0700

    Defaults QuantityOrdered to 1 in InventoryContainerGrid - Resolves #369

--- src/RentalWorksWeb/Source/Grids/InventoryContainerGrid/InventoryContainerGridController.ts
+++ src/RentalWorksWeb/Source/Grids/InventoryContainerGrid/InventoryContainerGridController.ts
@@ -3,8 +3,9 @@
     apiurl: string = 'api/v1/inventorycontaineritem';
 
     generateRow($control, $generatedtr) {
-        $generatedtr.find('div[data-browsedatafield="InventoryId"]').data('onchange', function ($tr) {
+        $generatedtr.find('div[data-browsedatafield="InventoryId"]').data('onchange', $tr => {
             $generatedtr.find('.field[data-browsedatafield="Description"] input').val($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="QuantityOrdered"] input').val(1);
         });
     };
 }
