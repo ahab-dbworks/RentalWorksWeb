7c3468f51166593424edf4caae59651bc71195d8
Oliver <oliver@dbworks.com>
2019-04-06 19:43:04 -0700

    Updates detailed scheduler time header with date instead of day of week and removes id from tooltip - Resolves #378 Resolves #377

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwSchedulerDetailed.js
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwSchedulerDetailed.js
@@ -103,7 +103,7 @@ class FwSchedulerDetailedClass {
         dpscheduler.scale = "Day";
         dpscheduler.timeHeaders = [
             { groupBy: "Month" },
-            { groupBy: "Day", format: "dddd" }
+            { groupBy: "Day", format: "d" }
         ];
         dpscheduler.treeEnabled = true;
         dpscheduler.bubble = new DayPilot.Bubble({
@@ -111,7 +111,7 @@ class FwSchedulerDetailedClass {
             onLoad: function (args) {
                 var ev = args.source;
                 args.async = true;
-                args.html = "<div style='font-weight:bold'>" + ev.text() + "</div><div>Order Number: " + ev.data.orderNumber + "</div><div>Order Status: " + ev.data.orderStatus + "</div><div>Deal: " + ev.data.deal + "</div><div>Start: " + ev.start().toString("MM/dd/yyyy HH:mm") + "</div><div>End: " + ev.end().toString("MM/dd/yyyy HH:mm") + "</div><div>Id: " + ev.id() + "</div>";
+                args.html = "<div style='font-weight:bold'>" + ev.text() + "</div><div>Order Number: " + ev.data.orderNumber + "</div><div>Order Status: " + ev.data.orderStatus + "</div><div>Deal: " + ev.data.deal + "</div><div>Start: " + ev.start().toString("MM/dd/yyyy HH:mm") + "</div><div>End: " + ev.end().toString("MM/dd/yyyy HH:mm") + "</div>";
                 args.loaded();
             }
         });
