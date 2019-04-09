34e2f0eb3fd38a13095f088c71d261d0a1a1f4f4
Mike Vermilion <mike@dbworks.com>
2019-04-09 10:43:59 -0700

    QS: Inherits AppImageHandler, so all handlers go through RentalWorksQuikScan.dll

---
+++ src/RentalWorksQuikScan/HttpHandlers/AppImageHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalWorksQuikScan.HttpHandlers
+{
+    public class AppImageHandler : Fw.Json.HttpHandlers.FwAppImageHandler
+    {
+    }
+}
