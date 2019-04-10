22a8dea0f25bd76530805beeefff24dfeda604be
Justin Hoffman <justin@dbworks.com>
2019-04-10 12:01:21 -0700

    added comments to InventoryWarehouseGrid controllers to document potential issue

--- src/RentalWorksWeb/Source/Grids/SalesInventoryWarehouseGrid/SalesInventoryWarehouseGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SalesInventoryWarehouseGrid/SalesInventoryWarehouseGridController.ts
@@ -1,5 +1,5 @@
 ﻿class SalesInventoryWarehouseGrid {
-    Module: string = 'InventoryWarehouseGrid';
+    Module: string = 'InventoryWarehouseGrid';  //justin 04/10/2019 potential issue with shared Module value. See Issue #401
     apiurl: string = 'api/v1/inventorywarehouse';
 }
 
