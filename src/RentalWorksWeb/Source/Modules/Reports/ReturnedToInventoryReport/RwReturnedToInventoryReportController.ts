c8e8485357d3f0862c1c3ec83fc2f76631b42e72
Joshua Pace <jpace@4wall.com>
2019-04-22 14:44:08 -0700

    Adds  Returned To Inventory report FE #429

---
+++ src/RentalWorksWeb/Source/Modules/Reports/ReturnedToInventoryReport/RwReturnedToInventoryReportController.ts
@@ -0,0 +1,108 @@
+﻿routes.push({
+    pattern: /^reports\/returnedtoinventoryreport/, action: function (match: RegExpExecArray) {
+        return RwReturnedToInventoryReportController.getModuleScreen();
+    }
+});
+
+const returnedInventoryTemplate = `
+<div class="fwcontrol fwcontainer fwform fwreport" data-control="FwContainer" data-type="form" data-version="1" data-caption="Returned To Inventory" data-rendermode="template" data-mode="" data-hasaudit="false" data-controller="RwReturnedToInventoryReportController">
+  <div class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
+    <div class="tabs" style="margin-right:10px;">
+      <div id="generaltab" class="tab" data-tabpageid="generaltabpage" data-caption="General"></div>
+    </div>
+    <div class="tabpages">
+      <div data-type="tabpage" id="generaltabpage" class="tabpage" data-tabid="generaltab">
+        <div class="flexpage">
+          <div class="flexcolumn" style="max-width:450px; float:left;">
+            <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Returned Date Range">
+              <div class="flexrow">
+                <div data-datafield="FromDate" data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-required="true" data-caption="From" style="max-width:150px;"></div>
+                <div data-datafield="ToDate" data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-required="true" data-caption="To" style="max-width:150px;"></div>
+              </div>
+            </div>
+          </div>
+          <div class="flexcolumn" style="max-width:600px; float:left;">
+            <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Filters">
+              <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Warehouse" data-datafield="WarehouseId" data-displayfield="Warehouse" data-validationname="WarehouseValidation" style="min-width:400px;"></div>
+              <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Inventory Type" data-formbeforevalidate="beforeValidate" data-datafield="InventoryTypeId" data-displayfield="InventoryType" data-validationname="InventoryTypeValidation" style="min-width:400px;"></div>
+              <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Deal" data-datafield="DealId" data-formbeforevalidate="beforeValidate" data-displayfield="Deal" data-validationname="DealValidation" style="min-width:400px;"></div>
+              <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Category" data-datafield="CategoryId" data-formbeforevalidate="beforeValidate" data-displayfield="Category" data-validationname="RentalCategoryValidation" style="min-width:400px;"></div>
+              <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="I-Code" data-formbeforevalidate="beforeValidate"  data-datafield="InventoryId" data-displayfield="ICode" data-validationname="RentalInventoryValidation" style="min-width:400px;"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="exporttabpage" class="tabpage exporttabpage" data-tabid="exporttab">
+      </div>
+    </div>
+  </div>
+</div>
+`;
+//----------------------------------------------------------------------------------------------
+class RwReturnedToInventoryReport extends FwWebApiReport {
+    //----------------------------------------------------------------------------------------------
+    constructor() {
+        super('ReturnedToInventoryReport', 'api/v1/returnedtoinventoryreport', returnedInventoryTemplate);
+    }
+    //----------------------------------------------------------------------------------------------
+    getModuleScreen() {
+        const screen: any = {};
+        screen.$view = FwModule.getModuleControl(`Rw${this.Module}Controller`);
+        screen.viewModel = {};
+        screen.properties = {};
+
+        const $form = this.openForm();
+
+        screen.load = function () {
+            FwModule.openModuleTab($form, $form.attr('data-caption'), false, 'REPORT', true);
+        };
+        screen.unload = function () { };
+        return screen;
+    }
+    //----------------------------------------------------------------------------------------------
+    openForm() {
+        const $form = this.getFrontEnd();
+        return $form;
+    }
+    //----------------------------------------------------------------------------------------------
+    onLoadForm($form) {
+        this.load($form, this.reportOptions);
+        const warehouse = JSON.parse(sessionStorage.getItem('warehouse'));
+        FwFormField.setValue($form, 'div[data-datafield="WarehouseId"]', warehouse.warehouseid, warehouse.warehouse);
+    }
+    //----------------------------------------------------------------------------------------------
+    convertParameters(parameters: any) {
+        return parameters;
+    }
+    //----------------------------------------------------------------------------------------------
+    beforeValidate = function ($browse, $form, request) {
+        var validationName = request.module;
+        if (validationName != null) {
+            const inventoryTypeId = FwFormField.getValueByDataField($form, 'InventoryTypeId');
+            const categoryId = FwFormField.getValueByDataField($form, 'CategoryId');
+            request.uniqueids = {};
+
+            switch (validationName) {
+                case 'InventoryTypeValidation':
+                    request.uniqueids.Rental = true;
+                    break;
+                case 'RentalCategoryValidation':
+                    if (inventoryTypeId !== "") {
+                        request.uniqueids.InventoryTypeId = inventoryTypeId;
+                    }
+                    break;
+                case 'RentalInventoryValidation':
+                    if (inventoryTypeId !== "") {
+                        request.uniqueids.InventoryTypeId = inventoryTypeId;
+                    };
+                    if (categoryId !== "") {
+                        request.uniqueids.CategoryId = categoryId;
+                    };
+                    break;
+            }
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+};
+
+var RwReturnedToInventoryReportController: any = new RwReturnedToInventoryReport();
