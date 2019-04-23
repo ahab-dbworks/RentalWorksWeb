ef38a99f169d34cb6d2fddf96accf42e7f51b095
Mike Vermilion <mike@dbworks.com>
2019-04-22 21:54:20 -0700

    Rw/Tw/Qs: Updates JQuery TypeScript interface

--- src/RentalWorksQuikScan/@types/JQuery/index.d.ts
+++ src/RentalWorksQuikScan/@types/JQuery/index.d.ts
@@ -1,3 +1,11 @@
 ﻿interface JQuery {
+    ckeditor();
+    clockpicker(options: any);
     colResizable();
+    colpick(options: any);
+    colpickSetColor(col: string | object, setCurrent?: boolean);
+    colpickHide();
+    colpickShow(e: JQuery.Event);
+    datepicker(options: any);
+    inputmask(arg1: any, arg2?: any);
 }
