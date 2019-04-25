512eca2bf0598c080653114a6fcecb8f12894950
Mike Vermilion <mike@dbworks.com>
2019-04-25 16:19:15 -0700

    Removes excluded files across several projects
    git ls-files -z --ignored --exclude-standard | xargs -0 git rm -r --cached

--- src/TrakItWorksWeb/Source/Grids/CheckInExceptionGrid/CheckInExceptionGridController.js
+++
@@ -1,8 +0,0 @@
-class CheckInExceptionGrid {
-    constructor() {
-        this.Module = 'CheckInExceptionGrid';
-        this.apiurl = 'api/v1/checkinexception';
-    }
-}
-var CheckInExceptionGridController = new CheckInExceptionGrid();
-//# sourceMappingURL=CheckInExceptionGridController.js.map
--- src/TrakItWorksWeb/Source/Validations/GeneralItemValidation/GeneralItemValidationController.js
+++
@@ -1,8 +0,0 @@
-class GeneralItemValidation {
-    constructor() {
-        this.Module = 'GeneralItemValidation';
-        this.apiurl = 'api/v1/generalitem';
-    }
-}
-var GeneralItemValidationController = new GeneralItemValidation();
-//# sourceMappingURL=GeneralItemValidationController.js.map
