48b7df62ffd8846396814ba11ff1836b90c1be21
Justin Hoffman <justin@dbworks.com>
2019-05-02 13:30:44 -0700

    added error to getByBarCode for #543

--- src/RentalWorksWebApi/Modules/Home/Item/ItemController.cs
+++ src/RentalWorksWebApi/Modules/Home/Item/ItemController.cs
@@ -1,25 +1,26 @@
 using FwStandard.AppManager;
 using FwStandard.SqlServer;
 using System.Collections.Generic;
-using FwStandard.Models; 
-using Microsoft.AspNetCore.Mvc; 
-using Microsoft.Extensions.Options; 
-using WebApi.Controllers; 
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
 using System.Threading.Tasks;
 using WebApi.Logic;
+using System;
 
 namespace WebApi.Modules.Home.Item
 {
     [Route("api/v1/[controller]")]
     [ApiExplorerSettings(GroupName = "home-v1")]
-    [FwController(Id:"kSugPLvkuNsH")]
+    [FwController(Id: "kSugPLvkuNsH")]
     public class ItemController : AppDataController
     {
         public ItemController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { logicType = typeof(ItemLogic); }
         //------------------------------------------------------------------------------------ 
         // POST api/v1/item/browse 
         [HttpPost("browse")]
-        [FwControllerMethod(Id:"8EjRJqgdgpgQ")]
+        [FwControllerMethod(Id: "8EjRJqgdgpgQ")]
         public async Task<ActionResult<FwJsonDataTable>> BrowseAsync([FromBody]BrowseRequest browseRequest)
         {
             return await DoBrowseAsync(browseRequest);
@@ -27,7 +28,7 @@ namespace WebApi.Modules.Home.Item
         //------------------------------------------------------------------------------------ 
         // POST api/v1/modulename/exportexcelxlsx/filedownloadname 
         [HttpPost("exportexcelxlsx/{fileDownloadName}")]
-        [FwControllerMethod(Id:"Y5fE4iVc3UhZ")]
+        [FwControllerMethod(Id: "Y5fE4iVc3UhZ")]
         public async Task<ActionResult<DoExportExcelXlsxExportFileAsyncResult>> ExportExcelXlsxFileAsync([FromBody]BrowseRequest browseRequest)
         {
             return await DoExportExcelXlsxFileAsync(browseRequest);
@@ -35,7 +36,7 @@ namespace WebApi.Modules.Home.Item
         //------------------------------------------------------------------------------------ 
         // GET api/v1/item 
         [HttpGet]
-        [FwControllerMethod(Id:"EsvBT0cfnwU2")]
+        [FwControllerMethod(Id: "EsvBT0cfnwU2")]
         public async Task<ActionResult<IEnumerable<ItemLogic>>> GetManyAsync([FromQuery]int pageno, [FromQuery]int pagesize, [FromQuery]string sort)
         {
             return await DoGetAsync<ItemLogic>(pageno, pagesize, sort);
@@ -43,7 +44,7 @@ namespace WebApi.Modules.Home.Item
         //------------------------------------------------------------------------------------ 
         // GET api/v1/item/A0000001 
         [HttpGet("{id}")]
-        [FwControllerMethod(Id:"zjCQVTktDrdU")]
+        [FwControllerMethod(Id: "zjCQVTktDrdU")]
         public async Task<ActionResult<ItemLogic>> GetOneAsync([FromRoute]string id)
         {
             return await DoGetAsync<ItemLogic>(id);
@@ -55,12 +56,20 @@ namespace WebApi.Modules.Home.Item
         public async Task<ActionResult<ItemLogic>> GetOneByBarCodeAsync(string barCode)
         {
             string itemId = AppFunc.GetStringDataAsync(AppConfig, "rentalitem", "barcode", barCode, "rentalitemid").Result;
-            return await DoGetAsync<ItemLogic>(itemId);
+            if (string.IsNullOrEmpty(itemId))
+            {
+                SystemException ex = new SystemException($"Invalid Bar Code {barCode}");
+                return GetApiExceptionResult(ex);
+            }
+            else
+            {
+                return await DoGetAsync<ItemLogic>(itemId);
+            }
         }
         //------------------------------------------------------------------------------------ 
         // POST api/v1/item 
         [HttpPost]
-        [FwControllerMethod(Id:"vf0mEqWKxcv3")]
+        [FwControllerMethod(Id: "vf0mEqWKxcv3")]
         public async Task<ActionResult<ItemLogic>> PostAsync([FromBody]ItemLogic l)
         {
             return await DoPostAsync<ItemLogic>(l);
