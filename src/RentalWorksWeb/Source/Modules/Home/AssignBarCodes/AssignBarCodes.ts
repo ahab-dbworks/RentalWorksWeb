d4848a0a4cbbd8b0a61e9b4a41bca36788a5bae6
Mike Vermilion <mike@dbworks.com>
2019-05-03 13:33:51 -0700

    Tw/Rw: Improves layout of AssignBarCodes
    asdf
    
    
    asdf

--- src/RentalWorksWeb/Source/Modules/Home/AssignBarCodes/AssignBarCodes.ts
+++ src/RentalWorksWeb/Source/Modules/Home/AssignBarCodes/AssignBarCodes.ts
@@ -145,33 +145,25 @@ class AssignBarCodes {
     //----------------------------------------------------------------------------------------------
     getFormTemplate(): string {
         return `
-        <div id="assignbarcodesform" class="fwcontrol fwcontainer fwform" data-control="FwContainer" data-type="form" data-version="1" data-caption="Assign Bar Codes" data-rendermode="template" data-tablename="" data-mode="" data-hasaudit="false" data-controller="AssignBarCodesController">
-          <div id="checkinform-tabcontrol" class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
-            <div class="tabs">
-            </div>
-            <div class="tabpages">
-              <div class="flexpage">
+        <div id="assignbarcodesform" class="fwcontrol fwcontainer fwform" data-control="FwContainer" data-type="form" data-caption="Assign Barcodes" data-hasaudit="false" data-controller="AssignBarcodesController">
+          <div class="flexpage">
+            <div class="flexrow">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Barcodes / Serial Numbers" style="flex:1 1 750px;">
                 <div class="flexrow">
-                  <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Assign Bar Codes">
-                    <div class="flexrow">
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="PO No." data-datafield="PurchaseOrderId" data-displayfield="PurchaseOrderNumber" data-validationname="PurchaseOrderValidation" data-formbeforevalidate="beforeValidatePONumber" style="float:left; flex:0 1 200px;"></div>
-                      <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="PO Date" data-datafield="PODate" style="float:left; flex:0 1 150px;" data-enabled="false"></div>
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Vendor" data-datafield="VendorId" data-displayfield="Vendor" data-validationname="VendorValidation" style="float:left; flex:0 1 250px;" data-enabled="false"></div>
-                      <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Description" data-datafield="Description" style="float:left; flex:1 1 250px;" data-enabled="false"></div>
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Department" data-datafield="DepartmentId" data-displayfield="Department" data-validationname="DepartmentValidation" style="float:left; flex:0 1 225px;" data-enabled="false"></div>
-                    </div>
-                    <div class="flexrow">
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Contract No." data-datafield="ContractId" data-displayfield="ContractNumber" data-validationname="ContractValidation" data-formbeforevalidate="beforeValidateContractNumber" style="float:left; flex:0 1 200px;"></div>
-                      <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="Contract Date" data-datafield="ContractDate" style="float:left;  flex:0 1 150px;" data-enabled="false"></div>
-                    </div>
-                    <div class="flexrow" style="min-width:1400px;">
-                      <div data-control="FwGrid" data-grid="POReceiveBarCodeGrid" data-securitycaption="Purchase Order Receive Bar Code"></div>
-                    </div>
-                    <div class="formrow" style="min-width:1400px;">
-                      <div class="fwformcontrol assignbarcodes" data-type="button" style="float:left;">Assign Bar Codes</div>
-                      <div class="fwformcontrol additems" data-type="button" style="float:right;">Add Items</div>
-                    </div>
-                  </div>
+                  <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="PO No." data-datafield="PurchaseOrderId" data-displayfield="PurchaseOrderNumber" data-validationname="PurchaseOrderValidation" data-formbeforevalidate="beforeValidatePONumber" style="flex:1 1 125px;"></div>
+                  <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="PO Date" data-datafield="PODate" style="flex:1 1 125px;" data-enabled="false"></div>
+                  <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Vendor" data-datafield="VendorId" data-displayfield="Vendor" data-validationname="VendorValidation" style="flex:2 1 350px;" data-enabled="false"></div>
+                </div>
+                <div class="flexrow">
+                  <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Description" data-datafield="Description" style="flex:2 1 350px;" data-enabled="false"></div>
+                  <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Department" data-datafield="DepartmentId" data-displayfield="Department" data-validationname="DepartmentValidation" style="flex:1 1 225px;" data-enabled="false"></div>
+                </div>
+                <div class="flexrow">
+                  <div data-control="FwGrid" data-grid="POReceiveBarCodeGrid" data-securitycaption="Purchase Order Receive Bar Code"></div>
+                </div>
+                <div class="flexrow" style="margin-top:15px;justify-content:center;">
+                  <div class="fwformcontrol assignbarcodes" data-type="button" style="flex: 0 0 150px;">Assign Barcodes</div>
+                  <div class="fwformcontrol additems" data-type="button" style="flex: 0 0 150px;margin:0 0 0 20px;">Add Items</div>
                 </div>
               </div>
             </div>
--- src/TrakitWorksWeb/Source/Modules/Home/AssignBarCodes/AssignBarCodes.ts
+++ src/TrakitWorksWeb/Source/Modules/Home/AssignBarCodes/AssignBarCodes.ts
@@ -146,34 +146,28 @@ class AssignBarCodes {
     getFormTemplate(): string {
         return `
         <div id="assignbarcodesform" class="fwcontrol fwcontainer fwform" data-control="FwContainer" data-type="form" data-version="1" data-caption="Assign Bar Codes" data-rendermode="template" data-tablename="" data-mode="" data-hasaudit="false" data-controller="AssignBarCodesController">
-          <div id="checkinform-tabcontrol" class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
-            <div class="tabs">
-            </div>
-            <div class="tabpages">
-              <div class="flexpage">
+          <div class="flexpage">
+            <div class="flexrow">
+                <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Assign Bar Codes">
                 <div class="flexrow">
-                  <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Assign Bar Codes">
-                    <div class="flexrow">
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="PO No." data-datafield="PurchaseOrderId" data-displayfield="PurchaseOrderNumber" data-validationname="PurchaseOrderValidation" data-formbeforevalidate="beforeValidatePONumber" style="float:left; flex:0 1 200px;"></div>
-                      <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="PO Date" data-datafield="PODate" style="float:left; flex:0 1 150px;" data-enabled="false"></div>
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Vendor" data-datafield="VendorId" data-displayfield="Vendor" data-validationname="VendorValidation" style="float:left; flex:0 1 250px;" data-enabled="false"></div>
-                      <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Description" data-datafield="Description" style="float:left; flex:1 1 250px;" data-enabled="false"></div>
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Department" data-datafield="DepartmentId" data-displayfield="Department" data-validationname="DepartmentValidation" style="float:left; flex:0 1 225px;" data-enabled="false"></div>
-                    </div>
-                    <div class="flexrow">
-                      <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Contract No." data-datafield="ContractId" data-displayfield="ContractNumber" data-validationname="ContractValidation" data-formbeforevalidate="beforeValidateContractNumber" style="float:left; flex:0 1 200px;"></div>
-                      <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="Contract Date" data-datafield="ContractDate" style="float:left;  flex:0 1 150px;" data-enabled="false"></div>
-                    </div>
-                    <div class="flexrow" style="min-width:1400px;">
-                      <div data-control="FwGrid" data-grid="POReceiveBarCodeGrid" data-securitycaption="Purchase Order Receive Bar Code"></div>
-                    </div>
-                    <div class="formrow" style="min-width:1400px;">
-                      <div class="fwformcontrol assignbarcodes" data-type="button" style="float:left;">Assign Bar Codes</div>
-                      <div class="fwformcontrol additems" data-type="button" style="float:right;">Add Items</div>
-                    </div>
-                  </div>
+                    <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="PO No." data-datafield="PurchaseOrderId" data-displayfield="PurchaseOrderNumber" data-validationname="PurchaseOrderValidation" data-formbeforevalidate="beforeValidatePONumber" style="float:left; flex:0 1 200px;"></div>
+                    <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="PO Date" data-datafield="PODate" style="float:left; flex:0 1 150px;" data-enabled="false"></div>
+                    <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Vendor" data-datafield="VendorId" data-displayfield="Vendor" data-validationname="VendorValidation" style="float:left; flex:0 1 250px;" data-enabled="false"></div>
+                    <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Description" data-datafield="Description" style="float:left; flex:1 1 250px;" data-enabled="false"></div>
+                    <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Department" data-datafield="DepartmentId" data-displayfield="Department" data-validationname="DepartmentValidation" style="float:left; flex:0 1 225px;" data-enabled="false"></div>
                 </div>
-              </div>
+                <div class="flexrow">
+                    <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Contract No." data-datafield="ContractId" data-displayfield="ContractNumber" data-validationname="ContractValidation" data-formbeforevalidate="beforeValidateContractNumber" style="float:left; flex:0 1 200px;"></div>
+                    <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="Contract Date" data-datafield="ContractDate" style="float:left;  flex:0 1 150px;" data-enabled="false"></div>
+                </div>
+                <div class="flexrow" style="min-width:1400px;">
+                    <div data-control="FwGrid" data-grid="POReceiveBarCodeGrid" data-securitycaption="Purchase Order Receive Bar Code"></div>
+                </div>
+                <div class="formrow" style="min-width:1400px;">
+                    <div class="fwformcontrol assignbarcodes" data-type="button" style="float:left;">Assign Bar Codes</div>
+                    <div class="fwformcontrol additems" data-type="button" style="float:right;">Add Items</div>
+                </div>
+                </div>
             </div>
           </div>
         </div>`;
