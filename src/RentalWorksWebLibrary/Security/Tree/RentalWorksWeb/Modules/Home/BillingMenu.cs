d60ff303aa49163560eaf1ba56ba4b872edb76c1
Jason <jhoang@4wall.com>
2019-05-07 10:33:49 -0700

    Added export option to Billing module - Resolves #552

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/BillingMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/BillingMenu.cs
@@ -13,17 +13,8 @@ namespace WebLibrary.Security.Tree.RentalWorksWeb.Modules.Settings
             var nodeBrowse = tree.AddBrowse("{C355874E-FD6D-45A9-AE2D-45623F5A705E}", MODULEID);
             var nodeBrowseMenuBar = tree.AddMenuBar("{F94FDA27-C427-468E-BE01-A395A8032F10}", nodeBrowse.Id);
             var nodeBrowseSubMenu = tree.AddSubMenu("{FD2FF91F-EE58-4AF6-BC67-D23FEE1E0D87}", nodeBrowseMenuBar.Id);
-            //tree.AddNewMenuBarButton("{47F0F323-EC4B-409C-87B8-D7042541227F}", nodeBrowseMenuBar.Id);
-            //tree.AddViewMenuBarButton("{0F5DC392-5895-4E19-A6FF-3E194FF4CAAC}", nodeBrowseMenuBar.Id);
-            //tree.AddEditMenuBarButton("{5230AAE3-E2DF-4DFC-9DA9-FCFFCB13CA8A}", nodeBrowseMenuBar.Id);
-            //tree.AddDeleteMenuBarButton("{12021AED-2868-4ADD-B5A1-6194581A058B}", nodeBrowseMenuBar.Id);
-
-            // Form
-            //var nodeForm = tree.AddForm("{23AA845C-B254-488E-93BD-E710681186C2}", MODULEID);
-            //var nodeFormMenuBar = tree.AddMenuBar("{EFA3370F-7B1A-4555-82A7-030B7C9CDEE1}", nodeForm.Id);
-            //tree.AddSaveMenuBarButton("{79BB5C05-6E28-420D-AB1A-7EC9275D6017}", nodeFormMenuBar.Id);
-            //var nodeFormSubMenu = tree.AddSubMenu("{56AF631A-2969-48FE-AEA8-53574D37FD58}", nodeFormMenuBar.Id);
-          
+            var nodeBrowseExport = tree.AddSubMenuGroup("Export", "{CE60F0E5-2995-43F1-A72D-E798B08D3B81}", nodeBrowseSubMenu.Id);
+            tree.AddDownloadExcelSubMenuItem("{B70C682C-E983-4840-A418-8C449C048158}", nodeBrowseExport.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
