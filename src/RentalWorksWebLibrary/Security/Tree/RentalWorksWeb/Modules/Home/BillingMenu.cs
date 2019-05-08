4fd8dfc73bacfaee8d3b1a83d9766b72a61b4b3a
Jason <jhoang@4wall.com>
2019-05-08 09:52:12 -0700

    Renamed "Export" to "Options" in Billing browse  - Resolves #552

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/BillingMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/BillingMenu.cs
@@ -13,7 +13,7 @@ namespace WebLibrary.Security.Tree.RentalWorksWeb.Modules.Settings
             var nodeBrowse = tree.AddBrowse("{C355874E-FD6D-45A9-AE2D-45623F5A705E}", MODULEID);
             var nodeBrowseMenuBar = tree.AddMenuBar("{F94FDA27-C427-468E-BE01-A395A8032F10}", nodeBrowse.Id);
             var nodeBrowseSubMenu = tree.AddSubMenu("{FD2FF91F-EE58-4AF6-BC67-D23FEE1E0D87}", nodeBrowseMenuBar.Id);
-            var nodeBrowseExport = tree.AddSubMenuGroup("Export", "{CE60F0E5-2995-43F1-A72D-E798B08D3B81}", nodeBrowseSubMenu.Id);
+            var nodeBrowseExport = tree.AddSubMenuGroup("Options", "{CE60F0E5-2995-43F1-A72D-E798B08D3B81}", nodeBrowseSubMenu.Id);
             tree.AddDownloadExcelSubMenuItem("{B70C682C-E983-4840-A418-8C449C048158}", nodeBrowseExport.Id);
         }
         //---------------------------------------------------------------------------------------------
