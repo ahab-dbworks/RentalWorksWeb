f9d10183b494f92a700726f0fe9c6751a72e108b
Mike Vermilion <mike@dbworks.com>
2019-05-10 16:48:29 -0700

    WebApi: Removes legacy comment

--- src/RentalWorksWebApi/Controllers/AppController.cs
+++ src/RentalWorksWebApi/Controllers/AppController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 namespace WebApi.Controllers
 {
     [Route("api/v1/[controller]")]
-    public class AppController : FwController  //todo: create FwController to inherit from
+    public class AppController : FwController
     {
         public AppController(IOptions<FwApplicationConfig> appConfig) : base(appConfig)
         {
