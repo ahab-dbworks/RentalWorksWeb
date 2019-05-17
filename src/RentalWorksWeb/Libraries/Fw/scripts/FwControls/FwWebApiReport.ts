9e1aa4eab782ec3e8cb8422d1f13e44b13106bff
Joshua Pace <jpace@4wall.com>
2019-05-17 13:12:47 -0700

    reworks company name in FwWebApiReports.ts #576

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwWebApiReport.ts
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwWebApiReport.ts
@@ -70,7 +70,16 @@ abstract class FwWebApiReport {
         const urlHtmlReport = `${applicationConfig.apiurl}Reports/${this.reportName}/index.html`;
         const apiUrl = applicationConfig.apiurl.substring(0, applicationConfig.apiurl.length - 1);
         const authorizationHeader = `Bearer ${sessionStorage.getItem('apiToken')}`;
-
+        let companyName;
+        if (this.reportName === 'OrderReport' || this.reportName === 'OutContractReport' || this.reportName === 'PickListReport' || this.reportName === 'InvoiceReport') {
+            if (JSON.parse(sessionStorage.getItem('location')).companyname != null) {
+                companyName = JSON.parse(sessionStorage.getItem('location')).companyname;
+            }
+        } else {
+            if (JSON.parse(sessionStorage.getItem('controldefaults')).companyname != null) {
+                companyName = JSON.parse(sessionStorage.getItem('controldefaults')).companyname;
+            }
+        }
         // Preview Button
         if ((typeof reportOptions.HasExportHtml === 'undefined') || (reportOptions.HasExportHtml === true)) {
             const $btnPreview = FwMenu.addStandardBtn($menuObject, 'Preview');
@@ -80,7 +89,7 @@ abstract class FwWebApiReport {
                     const request: any = this.getRenderRequest($form);
                     request.renderMode = 'Html';
                     request.parameters = this.convertParameters(this.getParameters($form));
-                    request.parameters.companyName = JSON.parse(sessionStorage.getItem('controldefaults')).companyname;
+                    request.parameters.companyName = companyName;
                     const reportPageMessage = new ReportPageMessage();
                     reportPageMessage.action = 'Preview';
                     reportPageMessage.apiUrl = apiUrl;
@@ -117,7 +126,7 @@ abstract class FwWebApiReport {
                     const request: any = this.getRenderRequest($form);
                     request.renderMode = 'Html';
                     request.parameters = this.convertParameters(this.getParameters($form));
-                    request.parameters.companyName = JSON.parse(sessionStorage.getItem('controldefaults')).companyname;
+                    request.parameters.companyName = companyName;
                     const $iframe = jQuery(`<iframe src="${urlHtmlReport}" style="display:none;"></iframe>`);
                     jQuery('.application').append($iframe);
                     $iframe.on('load', () => {
@@ -205,7 +214,7 @@ abstract class FwWebApiReport {
                     request.renderMode = 'Pdf';
                     request.downloadPdfAsAttachment = false;
                     request.parameters = this.convertParameters(this.getParameters($form));
-                    request.parameters.companyName = JSON.parse(sessionStorage.getItem('controldefaults')).companyname;
+                    request.parameters.companyName = companyName;
                     const win = window.open('', '_blank');
                     const head = win.document.head || win.document.getElementsByTagName('head')[0];
                     const loader = jQuery(win.document.body.innerHTML = '<div class="loader-container"><div class="loader"></div></div>');
@@ -310,7 +319,7 @@ abstract class FwWebApiReport {
                     request.email.subject = '[reportname]';
                     request.email.body = '';
                     request.parameters = this.convertParameters(this.getParameters($form));
-                    request.parameters.companyName = JSON.parse(sessionStorage.getItem('controldefaults')).companyname;
+                    request.parameters.companyName = companyName;
                     FwAppData.apiMethod(true, 'POST', `${this.apiurl}/render`, request, timeout,
                         (successResponse: RenderResponse) => {
                             try {
@@ -364,7 +373,7 @@ abstract class FwWebApiReport {
                                 requestEmailPdf.email.subject = FwFormField.getValueByDataField($confirmation, 'subject');
                                 requestEmailPdf.email.body = FwFormField.getValueByDataField($confirmation, 'body');
                                 requestEmailPdf.parameters = this.convertParameters(this.getParameters($form));
-                                requestEmailPdf.parameters.companyName = JSON.parse(sessionStorage.getItem('controldefaults')).companyname;
+                                requestEmailPdf.parameters.companyName = companyName;
                                 FwAppData.apiMethod(true, 'POST', `${this.apiurl}/render`, requestEmailPdf, timeout,
                                     (successResponse) => {
                                         try {
