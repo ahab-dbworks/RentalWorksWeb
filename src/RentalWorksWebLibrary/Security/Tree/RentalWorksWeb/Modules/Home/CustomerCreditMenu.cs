a7f38084ae9b464fc03eda195ec9638c1b185816
Joshua Pace <jpace@4wall.com>
2019-05-22 15:40:31 -0700

    removes view option from dealcredit and customercredit menu options #582

--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/CustomerCreditMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/CustomerCreditMenu.cs
@@ -15,7 +15,6 @@ namespace WebLibrary.Security.Tree.RentalWorksWeb.Modules.Home
             var nodeBrowseSubMenu = tree.AddSubMenu("{E444C88B-1E9C-4E29-9FC0-2D3FD46CE763}", nodeBrowseMenuBar.Id);
             var nodeBrowseExport = tree.AddSubMenuGroup("Export", "{E98DD846-8E9A-4314-BD71-5C7CCA290385}", nodeBrowseSubMenu.Id);
             tree.AddDownloadExcelSubMenuItem("{40CFC7D0-7478-4E6B-8A45-0CA3D5744BF0}", nodeBrowseExport.Id);
-            tree.AddViewMenuBarButton("{D4D1AA0A-B8F6-4C2C-A2AC-CA0D16C9A58C}", nodeBrowseMenuBar.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
--- src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/DealCreditMenu.cs
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Modules/Home/DealCreditMenu.cs
@@ -15,7 +15,6 @@ namespace WebLibrary.Security.Tree.RentalWorksWeb.Modules.Home
             var nodeBrowseSubMenu = tree.AddSubMenu("{2DB305BC-8212-45F0-A8CA-BB38DEFA23ED}", nodeBrowseMenuBar.Id);
             var nodeBrowseExport = tree.AddSubMenuGroup("Export", "{4B5A17CC-0601-4A1D-AF1B-B65F7F54B6AA}", nodeBrowseSubMenu.Id);
             tree.AddDownloadExcelSubMenuItem("{D4A81F48-F486-45F2-8C96-11DF046FD1E6}", nodeBrowseExport.Id);
-            tree.AddViewMenuBarButton("{16113D75-B757-45A4-8A3B-52557348E295}",   nodeBrowseMenuBar.Id);
         }
         //---------------------------------------------------------------------------------------------
     }
