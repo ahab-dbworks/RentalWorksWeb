ab2c263695ba307b32da4c4c625d3296db4b999d
Mike Vermilion <mike@dbworks.com>
2019-05-25 10:22:46 -0700

    Tw: Adds CompanyValidation (required by ContactCompanies grid)

---
+++ src/TrakitWorksWeb/Source/Validations/CompanyValidation/CompanyValidationController.ts
@@ -0,0 +1,14 @@
+﻿class CompanyValidation {
+    Module: string = 'CompanyValidation';
+    apiurl: string = 'api/v1/company';
+
+    addLegend($control) {
+        FwBrowse.addLegend($control, 'Lead', '#ff8040');
+        FwBrowse.addLegend($control, 'Prospect', '#ff0080');
+        FwBrowse.addLegend($control, 'Customer', '#ffff80');
+        FwBrowse.addLegend($control, 'Deal', '#03de3a');
+        FwBrowse.addLegend($control, 'Vendor', '#20b7ff');
+    }
+}
+
+var CompanyValidationController = new CompanyValidation();
