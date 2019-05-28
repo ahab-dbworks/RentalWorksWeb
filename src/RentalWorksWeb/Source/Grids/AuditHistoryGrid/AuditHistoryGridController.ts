e1d9d80d21e5eaa11a7ae82a1deda3746efd1dbf
Jason <jhoang@4wall.com>
2019-05-28 16:26:50 -0700

    Updated Audit History grid - Issue #611

--- src/RentalWorksWeb/Source/Grids/AuditHistoryGrid/AuditHistoryGridController.ts
+++ src/RentalWorksWeb/Source/Grids/AuditHistoryGrid/AuditHistoryGridController.ts
@@ -9,9 +9,9 @@
             let html: Array<string> = [];
             let htmlAnchor = `< <a style="color:blue;">HTML</a> >`; 
             html.push(`<ul class="auditHeader" style="font-size:14px; display:flex;">
-                         <span style="font-weight:bold; float:left; width:225px; text-decoration:underline; padding-right:1em;">Field Name</span>
-                         <span style="font-weight:bold; float:left; width:200px; text-decoration:underline; padding-right:1em;">Old Value</span>
-                         <span style="font-weight:bold; float:left; width:200px; text-decoration:underline;">New Value</span>
+                         <span style="font-weight:bold; float:left; min-width:350px; text-decoration:underline; padding-right:1em;">Field Name</span>
+                         <span style="font-weight:bold; float:left; min-width:200px; text-decoration:underline; padding-right:1em;">Old Value</span>
+                         <span style="font-weight:bold; float:left; min-width:200px; text-decoration:underline;">New Value</span>
                        </ul>`);
 
 
@@ -26,13 +26,13 @@
                                 if (changes[i].OldValue === " ") {
                                     changes[i].OldValue = "";
                                 }
-                                html.push(`<ul style="font-size:14px; word-wrap:break-word; display:flex;">
-                                                <span style="font-weight:bold; float:left; width:225px; padding-right:1em;">${changes[i].FieldName}:</span>
-                                                <span class="oldHtml" style="width:200px; padding-right:1em;">
+                                html.push(`<ul style="font-size:14px; display:flex;">
+                                                <span style="font-weight:bold; float:left; min-width:350px; padding-right:1em;">${changes[i].FieldName}:</span>
+                                                <span class="oldHtml" style="min-width:200px; padding-right:1em;">
                                                     <textarea class="value" style="display:none;">${changes[i].OldValue}</textarea>
                                                     <span style="font-weight:bolder; cursor:pointer; display:contents;">${changes[i].OldValue === "" ? "&#160;" : htmlAnchor}</span>
                                                 </span>
-                                                <span class="newHtml" style="width:200px; padding-right:4em;">
+                                                <span class="newHtml" style="min-width:200px; padding-right:4em;">
                                                     <textarea class="value" style="display:none;">${changes[i].NewValue}</textarea>
                                                     <span style="font-weight:bolder; cursor:pointer; display:contents;">${changes[i].NewValue === "" ? "&#160;" : htmlAnchor}</span>
                                                 </span>
@@ -40,26 +40,26 @@
                                            </ul>`);
 
                             } else {
-                                html.push(`<ul style="font-size:14px; word-wrap:break-word; display:flex;">
-                                         <span style="font-weight:bold; float:left; width:225px; padding-right:1em;">${changes[i].FieldName}:</span>
-                                         <span style="width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
-                                         <span style="width:200px; padding-right:4em;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
+                                html.push(`<ul style="font-size:14px; display:flex;">
+                                         <span style="font-weight:bold; float:left; min-width:350px; padding-right:1em;">${changes[i].FieldName}:</span>
+                                         <span style="min-width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
+                                         <span style="min-width:200px; padding-right:4em;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
                                          <span class="auditSpacer" style="flex:1 1 0"></span>
                                        </ul>`);
                             }
                         } else {
-                            html.push(`<ul style="font-size:14px; word-wrap:break-word; display:flex;">
-                                         <span style="font-weight:bold; float:left; width:225px; padding-right:1em;">${changes[i].FieldName}:</span>
-                                         <span style="width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}<span style="border-radius:3px;padding-left:18px;margin-left:5px;border:.75px solid black;background-color:${changes[i].OldValue}"></span></span>
-                                         <span style="width:200px; padding-right:4em;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}<span style="border-radius:3px;padding-left:18px;margin-left:5px;border:.75px solid black;background-color:${changes[i].NewValue}"></span></span>
+                            html.push(`<ul style="font-size:14px; display:flex;">
+                                         <span style="font-weight:bold; float:left; min-width:350px; padding-right:1em;">${changes[i].FieldName}:</span>
+                                         <span style="min-width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}<span style="border-radius:3px;padding-left:18px;margin-left:5px;border:.75px solid black;background-color:${changes[i].OldValue}"></span></span>
+                                         <span style="min-width:200px; padding-right:4em;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}<span style="border-radius:3px;padding-left:18px;margin-left:5px;border:.75px solid black;background-color:${changes[i].NewValue}"></span></span>
                                          <span class="auditSpacer" style="flex:1 1 0"></span>
                                        </ul>`);
                         }
                     } else {
-                        html.push(`<ul style="font-size:14px; word-wrap:break-word; display:flex;">
-                                     <span style="font-weight:bold; float:left; width:225px; padding-right:1em;">${changes[i].FieldName}:</span>
-                                     <span style="width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
-                                     <span style="width:200px; padding-right:4em;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
+                        html.push(`<ul style="font-size:14px; display:flex;">
+                                     <span style="font-weight:bold; float:left; min-width:350px; padding-right:1em;">${changes[i].FieldName}:</span>
+                                     <span style="min-width:200px; padding-right:1em;">${changes[i].OldValue === "" ? "&#160;" : changes[i].OldValue}</span>
+                                     <span style="min-width:200px; padding-right:4em;">${changes[i].NewValue === "" ? "&#160;" : changes[i].NewValue}</span>
                                      <span class="auditSpacer" style="flex:1 1 0"></span>
                                    </ul>`);
                     }
@@ -70,10 +70,10 @@
 
             $newElement.on('click', '.oldHtml span, .newHtml span', e => {
                 e.stopPropagation();
-                let $confirmation, $close, controlhtml;
+                let $confirmation, controlhtml;
                 let $htmlChanges = jQuery(e.currentTarget).siblings('textarea.value')[0].textContent;
                 $confirmation = FwConfirmation.renderConfirmation('HTML', '');
-                $close = FwConfirmation.addButton($confirmation, 'Close', true);
+                FwConfirmation.addButton($confirmation, 'Close', true);
                 controlhtml = [];
                 controlhtml.push('<div data-control="FwFormField" data-type="textarea" class="fwcontrol fwformfield html" data-caption="HTML" data-enabled="false" data-datafield=""></div>');
                 FwConfirmation.addControls($confirmation, controlhtml.join('\n'));
