ef9dcc0f9e0e8b1c486480ec0f47fcc2329457f1
Jason <jhoang@4wall.com>
2019-05-27 22:30:49 -0700

    Band-aid for issue where custom fields don't load properly in the Asset module for Custom Forms - Issue #618

--- src/RentalWorksWeb/Source/Modules/Administrator/CustomField/CustomField.ts
+++ src/RentalWorksWeb/Source/Modules/Administrator/CustomField/CustomField.ts
@@ -78,7 +78,26 @@ class CustomField {
             if (moduleNav == 'CustomField') {
                 break;
             }
+            if (moduleNav == 'Asset') {
+                moduleNav = 'Item';
+            }
             allModules.push({ value: moduleNav, text: moduleCaption });
+
+            //const controller = modules[i].properties.controller;
+            //const moduleCaption = modules[i].properties.caption;
+            //if (typeof window[controller] !== 'undefined') {
+            //    if (window[controller].hasOwnProperty('apiurl')) {
+            //        let moduleNav = window[controller].nav;
+            //        if (moduleNav) {
+            //            const sliceIndex = moduleNav.lastIndexOf('/');
+            //            moduleNav = moduleNav.slice(sliceIndex + 1);
+            //            if (moduleNav == 'customfield') {
+            //                break;
+            //            }
+            //            allModules.push({ value: moduleNav, text: moduleCaption });
+            //        }
+            //    }
+            //}
         };
         for (var i = 0; i < settingsModules.length; i++) {
             var settingsModuleNav = settingsModules[i].properties.controller.slice(0, -10);
