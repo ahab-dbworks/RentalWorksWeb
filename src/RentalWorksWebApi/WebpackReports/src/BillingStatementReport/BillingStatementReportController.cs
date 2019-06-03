48d9301312534698b6ac81d838f6d12127c32ec5
Joshua Pace <jpace@4wall.com>
2019-06-03 14:10:36 -0700

    Billing Statement report BE #405

--- src/RentalWorksWebApi/WebpackReports/src/BillingStatementReport/BillingStatementReportController.cs
+++ src/RentalWorksWebApi/WebpackReports/src/BillingStatementReport/BillingStatementReportController.cs
@@ -1,4 +1,3 @@
-using FwStandard.AppManager;
 using FwStandard.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -10,36 +9,33 @@ using PuppeteerSharp;
 using PuppeteerSharp.Media;
 using FwStandard.SqlServer;
 using Microsoft.AspNetCore.Http;
+using FwStandard.AppManager;
 using static FwCore.Controllers.FwDataController;
-
-namespace WebApi.Modules.Reports.BillingProgressReport
+namespace WebApi.Modules.Reports.BillingStatementReport
 {
-
-    public class BillingProgressReportRequest : AppReportRequest
+    public class BillingStatementReportRequest : AppReportRequest
     {
-        public DateTime AsOfDate { get; set; }
-        public SelectedCheckBoxListItems Statuses { get; set; } = new SelectedCheckBoxListItems();
-        public bool? IncludeCredits { get; set; }
-        public bool? ExcludeBilled100 { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool? IncludeNoCharge { get; set; }
+        public bool? IncludePaidInvoices { get; set; }
+        public bool? IncludeZeroBalance { get; set; }
+        public bool? ReduceBalance { get; set; }
         public string OfficeLocationId { get; set; }
-        public string DepartmentId { get; set; }
-        public string DealCsrId { get; set; }
-        public string CustomerId { get; set; }
+        public string DealStatusId { get; set; }
         public string DealTypeId { get; set; }
+        public string CustomerId { get; set; }
         public string DealId { get; set; }
-        public string AgentId { get; set; }
     }
-
-
     [Route("api/v1/[controller]")]
     [ApiExplorerSettings(GroupName = "reports-v1")]
-    [FwController(Id: "Y5ssGcXnxL2")]
-    public class BillingProgressReportController : AppReportController
+    [FwController(Id: "wd7t1jPI4ztQH")]
+    public class BillingStatementReportController : AppReportController
     {
-        public BillingProgressReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
-        protected override string GetReportFileName() { return "BillingProgressReport"; }
+        public BillingStatementReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
+        protected override string GetReportFileName() { return "BillingStatementReport"; }
         //------------------------------------------------------------------------------------ 
-        protected override string GetReportFriendlyName() { return "Billing Progress Report"; }
+        protected override string GetReportFriendlyName() { return "Billing Statement Report"; }
         //------------------------------------------------------------------------------------ 
         protected override PdfOptions GetPdfOptions()
         {
@@ -52,12 +48,12 @@ namespace WebApi.Modules.Reports.BillingProgressReport
         protected override string GetUniqueId(FwReportRenderRequest request)
         {
             //return request.parameters["xxxxid"].ToString().TrimEnd(); 
-            return "BillingProgressReport";
+            return "BillingStatementReport";
         }
         //------------------------------------------------------------------------------------ 
-        // POST api/v1/billingprogressreport/render 
+        // POST api/v1/billingstatementreport/render 
         [HttpPost("render")]
-        [FwControllerMethod(Id: "XD5izGut00O")]
+        [FwControllerMethod(Id: "pK5VJkSdVrsDw")]
         public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
         {
             if (!this.ModelState.IsValid) return BadRequest();
@@ -65,20 +61,20 @@ namespace WebApi.Modules.Reports.BillingProgressReport
             return new OkObjectResult(response);
         }
         //------------------------------------------------------------------------------------ 
-        // POST api/v1/modulename/exportexcelxlsx/filedownloadname 
+        // POST api/v1/billingstatementreport/exportexcelxlsx/filedownloadname 
         [HttpPost("exportexcelxlsx/{fileDownloadName}")]
-        [FwControllerMethod(Id: "w4Jb1f6fLJMp")]
-        public async Task<ActionResult<DoExportExcelXlsxExportFileAsyncResult>> ExportExcelXlsxFileAsync([FromBody]BillingProgressReportRequest request)
+        [FwControllerMethod(Id: "f2Cw7y6ZtvAjM")]
+        public async Task<ActionResult<DoExportExcelXlsxExportFileAsyncResult>> ExportExcelXlsxFileAsync([FromBody]BillingStatementReportRequest request)
         {
             ActionResult<FwJsonDataTable> actionResult = await RunReportAsync(request);
             FwJsonDataTable dt = (FwJsonDataTable)((OkObjectResult)(actionResult.Result)).Value;
-            return await DoExportExcelXlsxFileAsync(dt, includeIdColumns: request.IncludeIdColumns);
+            return await DoExportExcelXlsxFileAsync(dt);
         }
-        //------------------------------------------------------------------------------------
-        // POST api/v1/billingprogressreport/runreport 
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/billingstatementreport/runreport 
         [HttpPost("runreport")]
-        [FwControllerMethod(Id: "AOjPM7jV7pi")]
-        public async Task<ActionResult<FwJsonDataTable>> RunReportAsync([FromBody]BillingProgressReportRequest request)
+        [FwControllerMethod(Id: "SaC3eGowJ2Krg")]
+        public async Task<ActionResult<FwJsonDataTable>> RunReportAsync([FromBody]BillingStatementReportRequest request)
         {
             if (!ModelState.IsValid)
             {
@@ -86,7 +82,7 @@ namespace WebApi.Modules.Reports.BillingProgressReport
             }
             try
             {
-                BillingProgressReportLoader l = new BillingProgressReportLoader();
+                BillingStatementReportLoader l = new BillingStatementReportLoader();
                 l.SetDependencies(this.AppConfig, this.UserSession);
                 FwJsonDataTable dt = await l.RunReportAsync(request);
                 return new OkObjectResult(dt);
---
+++ src/RentalWorksWebApi/WebpackReports/src/BillingStatementReport/BillingStatementReportLoader.cs
@@ -0,0 +1,172 @@
+using FwStandard.DataLayer;
+using FwStandard.Models;
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+namespace WebApi.Modules.Reports.BillingStatementReport
+{
+    [FwSqlTable("tmpreporttable")]
+    public class BillingStatementReportLoader : AppDataLoadRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(calculatedColumnSql: "'detail'", modeltype: FwDataTypes.Text, isVisible: false)]
+        public string RowType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "id", modeltype: FwDataTypes.Integer)]
+        public int? Id { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "locationid", modeltype: FwDataTypes.Text)]
+        public string OfficeLocationId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "location", modeltype: FwDataTypes.Text)]
+        public string Location { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealid", modeltype: FwDataTypes.Text)]
+        public string DealId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "deal", modeltype: FwDataTypes.Text)]
+        public string Deal { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "attention", modeltype: FwDataTypes.Text)]
+        public string BillToAttention { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billadd1", modeltype: FwDataTypes.Text)]
+        public string BillToAddress1 { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billadd2", modeltype: FwDataTypes.Text)]
+        public string BilltoAddress2 { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billcity", modeltype: FwDataTypes.Text)]
+        public string BillToCity { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billstate", modeltype: FwDataTypes.Text)]
+        public string BillToState { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billzip", modeltype: FwDataTypes.Text)]
+        public string BillToZipCode { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billcountry", modeltype: FwDataTypes.Text)]
+        public string BillToCountry { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderid", modeltype: FwDataTypes.Text)]
+        public string OrderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "invoiceid", modeltype: FwDataTypes.Text)]
+        public string InvoiceId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "arid", modeltype: FwDataTypes.Text)]
+        public string ReceiptId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "invoicedate", modeltype: FwDataTypes.Date)]
+        public string InvoiceDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "invoiceno", modeltype: FwDataTypes.Text)]
+        public string InvoiceNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "nocharge", modeltype: FwDataTypes.Text)]
+        public string NoCharge { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "pono", modeltype: FwDataTypes.Text)]
+        public string PurchaseOrderNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "wano", modeltype: FwDataTypes.Text)]
+        public string WarehouseNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "orderno", modeltype: FwDataTypes.Text)]
+        public string OrderNumber { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "invoicedesc", modeltype: FwDataTypes.Text)]
+        public string InvoiceDescription { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "due", modeltype: FwDataTypes.Decimal)]
+        public decimal? Due { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "received", modeltype: FwDataTypes.Decimal)]
+        public decimal? Received { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "remaining", modeltype: FwDataTypes.Decimal)]
+        public decimal? Remaining { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "pmtdate", modeltype: FwDataTypes.Date)]
+        public string PaymentDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "pmtdesc", modeltype: FwDataTypes.Text)]
+        public string PaymentDescription { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "pmtamt", modeltype: FwDataTypes.Decimal)]
+        public decimal? PaymentAmount { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "arrectype", modeltype: FwDataTypes.Boolean)]
+        public bool? ArrecType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "rectype", modeltype: FwDataTypes.Text)]
+        public string RecType { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "haspayments", modeltype: FwDataTypes.Boolean)]
+        public bool? HasPayments { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "pendingfinancecharge", modeltype: FwDataTypes.Decimal)]
+        public decimal? PendingFinanceCharge { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "dealtotal", modeltype: FwDataTypes.Decimal)]
+        public decimal? DealTotal { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "ytdfinancecharge", modeltype: FwDataTypes.Decimal)]
+        public decimal? YearToDateFinanceCharge { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "nonbillable", modeltype: FwDataTypes.Boolean)]
+        public bool? NonBillable { get; set; }
+        //------------------------------------------------------------------------------------ 
+        public async Task<FwJsonDataTable> RunReportAsync(BillingStatementReportRequest request)
+        {
+            useWithNoLock = false;
+            FwJsonDataTable dt = null;
+            using (FwSqlConnection conn = new FwSqlConnection(AppConfig.DatabaseSettings.ConnectionString))
+            {
+
+                using (FwSqlCommand qry = new FwSqlCommand(conn, "getbillingstatementweb", this.AppConfig.DatabaseSettings.ReportTimeout))
+                {
+                    qry.AddParameter("@fromdate", SqlDbType.Date, ParameterDirection.Input, request.FromDate);
+                    qry.AddParameter("@todate", SqlDbType.Date, ParameterDirection.Input, request.ToDate);
+                    qry.AddParameter("@locationid", SqlDbType.Text, ParameterDirection.Input, request.OfficeLocationId);
+                    qry.AddParameter("@dealstatusid", SqlDbType.Text, ParameterDirection.Input, request.DealStatusId);
+                    qry.AddParameter("@dealtypeid", SqlDbType.Text, ParameterDirection.Input, request.DealTypeId);
+                    qry.AddParameter("@customerid", SqlDbType.Text, ParameterDirection.Input, request.CustomerId);
+                    qry.AddParameter("@dealid", SqlDbType.Text, ParameterDirection.Input, request.DealId);
+
+                    if (!request.IncludeNoCharge.GetValueOrDefault(false))
+                    {
+                       
+                    }
+                    if (!request.IncludePaidInvoices.GetValueOrDefault(false))
+                    {
+
+                    }
+                    if (!request.IncludeZeroBalance.GetValueOrDefault(false))
+                    {
+
+                    }
+                    if (!request.ReduceBalance.GetValueOrDefault(false))
+                    {
+
+                    }
+                    AddPropertiesAsQueryColumns(qry);
+                    dt = await qry.QueryToFwJsonTableAsync(false, 0);
+                }
+                //--------------------------------------------------------------------------------- 
+            }
+            if (request.IncludeSubHeadingsAndSubTotals)
+            {
+                string[] totalFields = new string[] { "RentalTotal", "SalesTotal" };
+                dt.InsertSubTotalRows("GroupField1", "RowType", totalFields);
+                dt.InsertSubTotalRows("GroupField2", "RowType", totalFields);
+                dt.InsertTotalRow("RowType", "detail", "grandtotal", totalFields);
+            }
+            return dt;
+        }
+        //------------------------------------------------------------------------------------ 
+    }
+}
