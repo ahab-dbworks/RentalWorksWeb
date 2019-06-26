5e48e99c9b5d0ce627fc9436288709052d144696
Joshua Pace <jpace@4wall.com>
2019-06-26 14:10:11 -0700

    Adds new filters to order conflict report #413

--- src/RentalWorksWeb/Source/Modules/Reports/OrderConflictReport/OrderConflictReportController.ts
+++ src/RentalWorksWeb/Source/Modules/Reports/OrderConflictReport/OrderConflictReportController.ts
@@ -42,6 +42,27 @@ const orderConflictTemplate = `
                 </div>
               </div>
             </div>
+           <div class="flexcolumn" style="max-width:200px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Classification">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-datafield="Classifications" data-control="FwFormField" data-type="checkboxlist" class="fwcontrol fwformfield" data-caption="" style="float:left;max-width:200px;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:200px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Tracked By">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-datafield="TrackedBys" data-control="FwFormField" data-type="checkboxlist" class="fwcontrol fwformfield" data-caption="" style="float:left;max-width:200px;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:75px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Rank">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-datafield="Ranks" data-control="FwFormField" data-type="checkboxlist" class="fwcontrol fwformfield" data-caption="" style="float:left;max-width:75px;"></div>
+                </div>
+              </div>
+            </div>
             <div class="flexcolumn" style="max-width:600px;">
               <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Filters">
                 <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
@@ -55,6 +76,18 @@ const orderConflictTemplate = `
                 </div>
                 <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
                   <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Order" data-datafield="OrderId" data-formbeforevalidate="beforeValidate" data-displayfield="Order" data-validationname="OrderValidation" data-validationpeek="false" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Inventory Type" data-datafield="InventoryTypeId" data-displayfield="InventoryType" data-formbeforevalidate="beforeValidate" data-validationname="InventoryTypeValidation" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Category" data-datafield="CategoryId" data-displayfield="Category" data-formbeforevalidate="beforeValidate" data-validationname="RentalCategoryValidation" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Sub-Category" data-datafield="SubCategoryId" data-formbeforevalidate="beforeValidate" data-displayfield="SubCategory" data-validationname="SubCategoryValidation" data-validationpeek="false" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="I-Code" data-datafield="InventoryId" data-formbeforevalidate="beforeValidate" data-displayfield="ICode" data-validationname="RentalInventoryValidation" style="float:left;min-width:400px;"></div>
                 </div>
               </div>
             </div>
@@ -96,23 +129,40 @@ class OrderConflictReport extends FwWebApiReport {
     //----------------------------------------------------------------------------------------------
     onLoadForm($form) {
         this.load($form, this.reportOptions);
+        this.loadLists($form);
 
         const warehouse = JSON.parse(sessionStorage.getItem('warehouse'));
         FwFormField.setValue($form, 'div[data-datafield="WarehouseId"]', warehouse.warehouseid, warehouse.warehouse);
-        const today = FwFunc.getDate();
-        FwFormField.setValueByDataField($form, 'FromDate', today);
 
+    }
+    afterLoad($form) {
+        const today = FwFunc.getDate();
+        const twoWeeks = FwFunc.getDate(today, 14);
+        FwFormField.setValueByDataField($form, 'FromDate', today);
+        FwFormField.setValueByDataField($form, 'ToDate', twoWeeks);
+
+        //$form.find('div[data-datafield="FromDate"]').change()
         $form.find('.date-field').on('changeDate', event => {
             this.dateValidation($form, event);
         });
+        FwFormField.setValueByDataField($form, 'ConflictType', 'N');
     }
     //----------------------------------------------------------------------------------------------
     convertParameters(parameters: any) {
         return parameters;
     }
     //----------------------------------------------------------------------------------------------
+    loadLists($form) {
+        FwFormField.loadItems($form.find('div[data-datafield="Classifications"]'), [{ value: "I", text: "Item", selected: "T" }, { value: "A", text: "Accessory", selected: "T" }, { value: "C", text: "Complete", selected: "T" }, { value: "K", text: "Kit", selected: "T" }, { value: "N", text: "Container", selected: "T" }, { value: "M", text: "Miscellaneous", selected: "F" }]);
+        FwFormField.loadItems($form.find('div[data-datafield="TrackedBys"]'), [{ value: "BARCODE", text: "Barcode", selected: "T" }, { value: "QUANTITY", text: "Quantity", selected: "T" }, { value: "SERIALNO", text: "Serial Number", selected: "T" }]);
+        FwFormField.loadItems($form.find('div[data-datafield="Ranks"]'), [{ value: "A", text: "A", selected: "T" }, { value: "B", text: "B", selected: "T" }, { value: "C", text: "C", selected: "T" }, { value: "D", text: "D", selected: "T" }, { value: "E", text: "E", selected: "T" }, { value: "F", text: "F", selected: "T" }, { value: "G", text: "G", selected: "T" }]);
+    }
+    //----------------------------------------------------------------------------------------------
     beforeValidate = function ($browse, $form, request) {
         const validationName = request.module;
+        const inventoryTypeId = FwFormField.getValueByDataField($form, 'InventoryTypeId');
+        const categoryId = FwFormField.getValueByDataField($form, 'CategoryId');
+        const subCategoryId = FwFormField.getValueByDataField($form, 'SubCategoryId');
         if (validationName != null) {
             const dealId = FwFormField.getValueByDataField($form, 'DealId');
             request.uniqueids = {};
@@ -122,6 +172,34 @@ class OrderConflictReport extends FwWebApiReport {
                     if (dealId !== "") {
                         request.uniqueids.DealId = dealId;
                     }
+                    break;
+                case 'InventoryTypeValidation':
+                    request.uniqueids.Rental = true;
+                    break;
+                case 'RentalCategoryValidation':
+                    if (inventoryTypeId !== "") {
+                        request.uniqueids.InventoryTypeId = inventoryTypeId;
+                    }
+                    break;
+                case 'SubCategoryValidation':
+                    request.uniqueids.Rental = true;
+                    if (inventoryTypeId !== "") {
+                        request.uniqueids.InventoryTypeId = inventoryTypeId;
+                    }
+                    if (categoryId !== "") {
+                        request.uniqueids.CategoryId = categoryId;
+                    }
+                    break;
+                case 'RentalInventoryValidation':
+                    if (inventoryTypeId !== "") {
+                        request.uniqueids.InventoryTypeId = inventoryTypeId;
+                    };
+                    if (categoryId !== "") {
+                        request.uniqueids.CategoryId = categoryId;
+                    };
+                    if (subCategoryId !== "") {
+                        request.uniqueids.SubCategoryId = subCategoryId;
+                    };
                     break;
             }
         }
