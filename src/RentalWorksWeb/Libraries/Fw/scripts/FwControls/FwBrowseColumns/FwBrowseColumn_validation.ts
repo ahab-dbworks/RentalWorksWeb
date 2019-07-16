99c309f3659d12928d16d8b1db9cf54d2605ef99
Joshua Pace <jpace@4wall.com>
2019-07-16 16:18:37 -0700

    updates fw

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwBrowseColumns/FwBrowseColumn_validation.ts
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwBrowseColumns/FwBrowseColumn_validation.ts
@@ -83,6 +83,7 @@ class FwBrowseColumn_validationClass implements IFwBrowseColumn {
         html.push('<div class="btnvalidate"><i class="material-icons">&#xE8B6;</i></div>');
         // push hidden spinner
         html.push('<div class="sk-fading-circle validation-loader"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>');
+
         const isWebAdmin = JSON.parse(sessionStorage.getItem('userid')).webadministrator;
         if (!($field.attr('data-validationpeek') === 'false')) {
             if (isWebAdmin === 'true') {
