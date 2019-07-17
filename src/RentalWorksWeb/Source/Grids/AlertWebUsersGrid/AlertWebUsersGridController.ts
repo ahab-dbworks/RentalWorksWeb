4526f6e5aea0e063c4730fb9f7ffd884184c1ab1
Jason <jhoang@4wall.com>
2019-07-17 09:54:26 -0700

    Defaults email when selecting a user in AlertWebUsersGrid - Resolves #730

--- src/RentalWorksWeb/Source/Grids/AlertWebUsersGrid/AlertWebUsersGridController.ts
+++ src/RentalWorksWeb/Source/Grids/AlertWebUsersGrid/AlertWebUsersGridController.ts
@@ -1,6 +1,13 @@
 ﻿class AlertWebUsersGrid {
     Module: string = 'AlertWebUsers';
     apiurl: string = 'api/v1/alertwebusers';
+
+    generateRow($control, $generatedtr) {
+        $generatedtr.find('div[data-browsedatafield="UserId"]').data('onchange', $tr => {
+            const email = $tr.find('.field[data-browsedatafield="Email"]').attr('data-originalvalue');
+            $generatedtr.find('.field[data-browsedatafield="Email"]').text(email);
+        });
+    }
 }
 
 var AlertWebUsersGridController = new AlertWebUsersGrid();
