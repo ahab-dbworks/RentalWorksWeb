0b9b67ac8138104cb22fdd3dcc6b43a024733b8f
Joshua Pace <jpace@4wall.com>
2019-07-18 11:49:20 -0700

    adds wip deal invoice detail report FE #434

---
+++ src/RentalWorksWeb/Source/Modules/Reports/DealInvoiceDetailReport/DealInvoiceDetailReportController.ts
@@ -0,0 +1,172 @@
+﻿routes.push({
+    pattern: /^reports\/dealinvoicedetailreport/, action: function (match: RegExpExecArray) {
+        return DealInvoiceDetailReportController.getModuleScreen();
+    }
+});
+
+const dealInvoiceDetailTemplate = `
+<div class="fwcontrol fwcontainer fwform fwreport" data-control="FwContainer" data-type="form" data-version="1" data-caption="" data-rendermode="template" data-mode="" data-hasaudit="false" data-controller="DealInvoiceDetailReportController">
+  <div class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
+    <div class="tabs" style="margin-right:10px;">
+      <div id="generaltab" class="tab" data-tabpageid="generaltabpage" data-caption="General"></div>
+    </div>
+    <div class="tabpages">
+      <div data-type="tabpage" id="generaltabpage" class="tabpage" data-tabid="generaltab">
+        <div class="formpage">
+          <div class="row" style="display:flex;flex-wrap:wrap;">
+            <div class="flexcolumn" style="max-width:200px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Date Range">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="From:" data-datafield="FromDate" data-required="true" style="float:left;max-width:200px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="date" class="fwcontrol fwformfield" data-caption="To:" data-datafield="ToDate" data-required="true" style="float:left;max-width:200px;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:200px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Date Type">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="radio" class="fwcontrol fwformfield datatype" data-datafield="DateType" style="float:left;max-width:200px;">
+                    <div data-value="INVOICE_DATE" data-caption="Invoice Date"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="Billing Start Date"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:233px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="No Charge Invoices">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="radio" class="fwcontrol fwformfield datatype" data-datafield="DateType" style="float:left;max-width:210px;">
+                    <div data-value="INVOICE_DATE" data-caption="Include &quot;No Charge&quot; Invoices"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="Exclude &quot;No Charge&quot; Invoices"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="&quot;No Charge&quot; Invoices Only"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:200px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Hiatus Invoices">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="radio" class="fwcontrol fwformfield datatype" data-datafield="DateType" style="float:left;max-width:200px;">
+                    <div data-value="INVOICE_DATE" data-caption="Include &quot;Hiatus&quot; Invoices"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="Exclude &quot;Hiatus&quot; Invoices"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="&quot;Hiatus&quot; Invoices Only"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:210px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Flat PO Invoices">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="radio" class="fwcontrol fwformfield datatype" data-datafield="DateType" style="float:left;max-width:210px;">
+                    <div data-value="INVOICE_DATE" data-caption="Include &quot;Flat PO&quot; Invoices"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="Exclude &quot;Flat PO&quot; Invoices"></div>
+                    <div data-value="BILLING_START_DATE" data-caption="&quot;Flat PO&quot; Invoices Only"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:200px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Status">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="checkboxlist" class="fwcontrol fwformfield" data-caption="" data-datafield="Statuses" style="float:left;max-width:200px;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="flexcolumn" style="max-width:600px;">
+              <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Filters">
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Office Location" data-datafield="OfficeLocationId" data-displayfield="OfficeLocation" data-validationname="OfficeLocationValidation" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Department" data-datafield="DepartmentId" data-displayfield="Department" data-validationname="DepartmentValidation" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Customer" data-datafield="CustomerId" data-displayfield="Customer" data-validationname="CustomerValidation" style="float:left;min-width:400px;"></div>
+                </div>
+                <div class="fwcontrol fwcontainer fwform-fieldrow" data-control="FwContainer" data-type="fieldrow">
+                  <div data-control="FwFormField" data-type="multiselectvalidation" class="fwcontrol fwformfield" data-caption="Deal" data-datafield="DealId" data-displayfield="Deal" data-validationname="DealValidation" data-formbeforevalidate="beforeValidate" style="float:left;min-width:400px;"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+//----------------------------------------------------------------------------------------------
+class DealInvoiceDetailReport extends FwWebApiReport {
+    //----------------------------------------------------------------------------------------------
+    constructor() {
+        super('DealInvoiceDetailReport', 'api/v1/dealinvoicedetailreport', dealInvoiceDetailTemplate);
+        this.reportOptions.HasDownloadExcel = true;
+    }
+    //----------------------------------------------------------------------------------------------
+    getModuleScreen() {
+        const screen: any = {};
+        screen.$view = FwModule.getModuleControl(`${this.Module}Controller`);
+        screen.viewModel = {};
+        screen.properties = {};
+
+        const $form = this.openForm();
+        screen.load = function () {
+            FwModule.openModuleTab($form, $form.attr('data-caption'), false, 'REPORT', true);
+        };
+        screen.unload = function () { };
+        return screen;
+    }
+    //----------------------------------------------------------------------------------------------
+    convertParameters(parameters: any): any {
+        return parameters;
+    }
+    //----------------------------------------------------------------------------------------------
+    openForm() {
+        const $form = this.getFrontEnd();
+        return $form;
+    }
+    //----------------------------------------------------------------------------------------------
+    onLoadForm($form) {
+        this.load($form, this.reportOptions);
+
+        const department = JSON.parse(sessionStorage.getItem('department'));
+        const location = JSON.parse(sessionStorage.getItem('location'));
+
+        FwFormField.setValue($form, 'div[data-datafield="DepartmentId"]', department.departmentid, department.department);
+        FwFormField.setValue($form, 'div[data-datafield="OfficeLocationId"]', location.locationid, location.location);
+
+        this.loadLists($form);
+    }
+    //----------------------------------------------------------------------------------------------
+    beforeValidate($browse, $form, request) {
+        const validationName = request.module;
+        const customerId = FwFormField.getValueByDataField($form, 'CustomerId');
+        request.uniqueids = {};
+
+        switch (validationName) {
+            case 'DealValidation':
+                if (customerId !== "") {
+                    request.uniqueids.CustomerId = customerId;
+                }
+                break;
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    loadLists($form: JQuery): void {
+        FwFormField.loadItems($form.find('div[data-datafield="Statuses"]'), [
+            { value: "NEW", text: "New", selected: "T" },
+            { value: "RETURNED", text: "Returned", selected: "T" },
+            { value: "REVISED", text: "Revised", selected: "T" },
+            { value: "APPROVED", text: "Approved", selected: "T" },
+            { value: "PROCESSED", text: "Processed", selected: "T" },
+            { value: "CLOSED", text: "Closed", selected: "T" },
+            { value: "VOID", text: "Void", selected: "T" },
+        ]);
+    }
+    //----------------------------------------------------------------------------------------------
+}
+
+var DealInvoiceDetailReportController: any = new DealInvoiceDetailReport();
+//----------------------------------------------------------------------------------------------
