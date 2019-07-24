27a4691804b1c962c7bb322c507cfa9048f021f3
Jason <jhoang@4wall.com>
2019-07-24 15:25:40 -0700

    Added PhysicalInventoryGrid - Issue #758

---
+++ src/RentalWorksWeb/Source/Grids/PhysicalInventoryGrid/PhysicalInventoryGridController.ts
@@ -0,0 +1,14 @@
+﻿class PhysicalInventoryGrid {
+    Module: string = 'PhysicalInventoryGrid';
+    apiurl: string = 'api/v1/physicalinventoryinventory';
+
+    generateRow($control, $generatedtr) {
+        $generatedtr.find('div[data-browsedatafield="InventoryId"]').data('onchange', $tr => {
+            $generatedtr.find('.field[data-browsedatafield="Description"]').text($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="AvailableFor"]').text($tr.find('.field[data-browsedatafield="AvailFor"]').attr('data-originalvalue'));
+        });
+    }
+}
+
+var PhysicalInventoryGridController = new PhysicalInventoryGrid();
+//----------------------------------------------------------------------------------------------
---
+++ src/RentalWorksWebLibrary/Security/Tree/RentalWorksWeb/Grids/PhysicalInventoryGridMenu.cs
@@ -0,0 +1,22 @@
+﻿using FwStandard.Security;
+
+namespace WebLibrary.Security.Tree.RentalWorksWeb.Grids
+{
+    public class PhysicalInventoryGridMenu : FwSecurityTreeBranch
+    {
+        //---------------------------------------------------------------------------------------------
+        public PhysicalInventoryGridMenu() : base("{B4256DE8-60A8-4C7F-B35E-FB6971263793}") { }
+        //---------------------------------------------------------------------------------------------
+        public override void BuildBranch(FwSecurityTree tree)
+        {
+            var nodeGridMenuBar = tree.AddMenuBar("{CA53ED73-4CF2-456F-AEF8-CBC0B68100EC}", MODULEID);
+            var nodeGridSubMenu = tree.AddSubMenu("{930C65C7-82EC-4097-984B-B4838626A641}", nodeGridMenuBar.Id);
+            var nodeBrowseOptions = tree.AddSubMenuGroup("Options", "{700321C2-783C-433C-9C7A-85931DBFAA29}", nodeGridSubMenu.Id);
+            tree.AddDownloadExcelSubMenuItem("{B6D018D9-7DCE-4E21-B96E-2BB0AEE70AB4}", nodeBrowseOptions.Id);
+            tree.AddNewMenuBarButton("{83BF2014-D8CC-4EA7-BEBF-93692D9E85C4}", nodeGridMenuBar.Id);
+            tree.AddEditMenuBarButton("{586CF89C-B64E-4622-82EE-4C9F92156C4A}", nodeGridMenuBar.Id);
+            tree.AddDeleteMenuBarButton("{971DF574-B265-4AC8-901F-D4AAF2664AE7}", nodeGridMenuBar.Id);
+        }
+        //---------------------------------------------------------------------------------------------
+    }
+}
