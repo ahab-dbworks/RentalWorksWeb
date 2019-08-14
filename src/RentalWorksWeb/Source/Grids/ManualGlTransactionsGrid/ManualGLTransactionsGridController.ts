106c8a1f41a7f60b31cb6cb5fa1b0a8b3176aa03
Jason <jhoang@4wall.com>
2019-08-14 10:00:27 -0700

    Resolved #865: Defaulted date to match InvoiceDate and updated Amount column to display decimals

--- src/RentalWorksWeb/Source/Grids/ManualGlTransactionsGrid/ManualGLTransactionsGridController.ts
+++ src/RentalWorksWeb/Source/Grids/ManualGlTransactionsGrid/ManualGLTransactionsGridController.ts
@@ -2,8 +2,13 @@
     Module: string = 'ManualGlTransactionsGrid';
     apiurl: string = 'api/v1/glmanual';
 
-    generateRow($control, $generatedtr) {
+    onRowNewMode($control: JQuery, $tr: JQuery) {
         const $form = $control.closest('.fwform');
+        const invoiceDate = FwFormField.getValueByDataField($form, 'InvoiceDate');
+        FwBrowse.setFieldValue($control, $tr, 'GlDate', { value: invoiceDate });
+    }
+
+    generateRow($control, $generatedtr) {
         $generatedtr.find('div[data-browsedatafield="DebitGlAccountId"]').data('onchange', $tr => {
             $generatedtr.find('.field[data-browsedatafield="DebitGlAccountDescription"]').text($tr.find('.field[data-browsedatafield="GlAccountDescription"]').attr('data-originalvalue'));
         });
