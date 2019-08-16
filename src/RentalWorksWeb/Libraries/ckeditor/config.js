034b79cacd1f0e4d59cc991ccbdba6f6522c4c76
Joshua Pace <jpace@4wall.com>
2019-08-16 16:30:25 -0700

    Updates CKEditor to version 4.12.1 and adds Font and Font Size to toolbar #873

--- src/RentalWorksWeb/Libraries/ckeditor/config.js
+++ src/RentalWorksWeb/Libraries/ckeditor/config.js
@@ -1,29 +1,31 @@
 /**
-* @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
-* For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
- 
-CKEDITOR.editorConfig = function( config ) {
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.uiColor = '#AADC6E';
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+    config.extraPlugins = 'font';
     config.toolbar_Full = [
-        { name: 'document',    groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source'/*, 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', 'Templates', 'document'*/ ] },
-        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', 'Undo', 'Redo' ] },
-        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', 'SelectAll', /*'Scayt'*/ ] },
-        { name: 'insert', items: [ /*'CreatePlaceholder', 'Image',*//* 'Flash', */'Table', 'HorizontalRule',/* 'Smiley', */'SpecialChar', /*'PageBreak',*//* 'Iframe', *//*'InsertPre'*/ ] },
+        { name: 'document', groups: ['mode', 'document', 'doctools'], items: ['Source'/*, 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', 'Templates', 'document'*/] },
+        { name: 'clipboard', groups: ['clipboard', 'undo'], items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', 'Undo', 'Redo'] },
+        { name: 'editing', groups: ['find', 'selection', 'spellchecker'], items: ['Find', 'Replace', 'SelectAll', /*'Scayt'*/] },
+        { name: 'insert', items: [ /*'CreatePlaceholder', 'Image',*//* 'Flash', */'Table', 'HorizontalRule',/* 'Smiley', */'SpecialChar', /*'PageBreak',*//* 'Iframe', *//*'InsertPre'*/] },
         //{ name: 'forms', items: [ /*'Form',*/ 'Checkbox', /*'Radio',*/ 'TextField', /*'Textarea',*/ /*'Select','Button', 'ImageButton', 'HiddenField'*/ ] },
         '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat' ] },
-        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'/*, 'BidiLtr', 'BidiRtl'*/ ] },
-        { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'], items: ['Font', 'FontSize', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat'] },
+        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align'], items: ['NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'/*, 'BidiLtr', 'BidiRtl'*/] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
         '/',
-        { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-        { name: 'tools', items: [ 'UIColor', 'Maximize', 'ShowBlocks' ] },
+        { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'tools', items: ['UIColor', 'Maximize', 'ShowBlocks'] },
         //{ name: 'about', items: [ 'About' ] }
     ];
- 
+    config.font_names =
+        'Arial/Arial, Helvetica, sans-serif;' +
+        'Times New Roman/Times New Roman, Times, serif;' +
+    'Verdana;' + 'Courier New;' + 'Helvetica;';
+    config.fontSize_sizes = '8/8px;10/10px;12/12px;16/16px;20/20px;24/24px;32/32px;48/48px;';
     config.toolbar = "Full";
     config.enterMode = CKEDITOR.ENTER_BR;
     config.tabSpaces = 4;
