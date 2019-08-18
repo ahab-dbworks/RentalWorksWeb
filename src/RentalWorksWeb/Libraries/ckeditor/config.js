ca75f83cd918e24acbeefa9397540016ac2960a8
Joshua Pace <jpace@4wall.com>
2019-08-18 09:09:40 -0700

    Removes duplicate font and font size selectors from CKEditor #873

--- src/RentalWorksWeb/Libraries/ckeditor/config.js
+++ src/RentalWorksWeb/Libraries/ckeditor/config.js
@@ -12,7 +12,7 @@ CKEDITOR.editorConfig = function (config) {
         { name: 'insert', items: [ /*'CreatePlaceholder', 'Image',*//* 'Flash', */'Table', 'HorizontalRule',/* 'Smiley', */'SpecialChar', /*'PageBreak',*//* 'Iframe', *//*'InsertPre'*/] },
         //{ name: 'forms', items: [ /*'Form',*/ 'Checkbox', /*'Radio',*/ 'TextField', /*'Textarea',*/ /*'Select','Button', 'ImageButton', 'HiddenField'*/ ] },
         '/',
-        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'], items: ['Font', 'FontSize', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat'] },
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'], items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat'] },
         { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align'], items: ['NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'/*, 'BidiLtr', 'BidiRtl'*/] },
         { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
         '/',
