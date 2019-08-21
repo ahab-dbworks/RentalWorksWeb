3215b4d6a8334f16d6d938580335304df16c349d
Joshua Pace <jpace@4wall.com>
2019-08-21 16:09:07 -0700

    includes some config files for jest

--- src/JestTest/jest-puppeteer.config.js
+++ src/JestTest/jest-puppeteer.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     launch: {
-      dumpio: true,
-      //headless: process.env.HEADLESS !== 'false',
-      headless: false,
-      args: [`--window-size=${1600},${1080}`]
+        dumpio: true,
+        //headless: process.env.HEADLESS !== 'false',
+        headless: false,
+        args: [`--window-size=${1600},${1080}`]
     },
     browserContext: 'default'
-  }
+};
