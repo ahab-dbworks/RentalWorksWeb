81fbfaae928c9ad2530f62567bbac8b86f21c6f8
Justin Hoffman <justin@dbworks.com>
2019-08-28 11:54:32 -0700

    example of reflection for #899

--- src/RentalWorksWebApi/WebpackReports/src/AgentBillingReport/AgentBillingReportLoader.cs
+++ src/RentalWorksWebApi/WebpackReports/src/AgentBillingReport/AgentBillingReportLoader.cs
@@ -4,6 +4,8 @@ using WebApi.Data;
 using WebLibrary;
 using System.Threading.Tasks;
 using System.Text;
+using System.Reflection;
+using System;
 
 namespace WebApi.Modules.Reports.AgentBillingReport
 {
@@ -211,6 +213,35 @@ namespace WebApi.Modules.Reports.AgentBillingReport
                 }
                 dt.InsertTotalRow("RowType", "detail", "grandtotal", totalFields);
             }
+
+
+            //PropertyInfo[] properties = this.GetType().GetProperties();   // get an array of all of the properties (fields) on this object (AgentBillingReportLoader)
+            //foreach (FwJsonDataTableColumn col in dt.Columns)             // iterate through each column in the FwJsonDataTable
+            //{
+            //    foreach (PropertyInfo property in properties)             // iterate through each Property trying to find one that matches the name of the column
+            //    {
+            //        if (property.Name.Equals(col.DataField))              // we found the Property whose name matches the column name
+            //        {
+            //            if (property.IsDefined(typeof(FwSqlDataFieldAttribute)))     // make sure the Property has a FwSqlDataFieldAttribute defined on it
+            //            {
+            //                foreach (Attribute attribute in property.GetCustomAttributes())   // iterate through each attribute to find the one that is FwSqlDataFieldAttribute
+            //                {
+            //                    if (attribute.GetType() == typeof(FwSqlDataFieldAttribute))   // we found the one attribute that is FwSqlDataFieldAttribute
+            //                    {
+            //                        FwSqlDataFieldAttribute fieldAttribute = (FwSqlDataFieldAttribute)attribute;  // create a variable to hold the attribute info
+            //                        if (fieldAttribute.HideInSummary)                                             // if the Property (field) is defined as "HideInSummary" then make the column invisible
+            //                        {
+            //                            col.IsVisible = false;
+            //                        }
+            //                        break;
+            //                    }
+            //                }
+            //            }
+            //            break;
+            //        }
+            //    }
+            //}
+
 
             return dt;
         }
