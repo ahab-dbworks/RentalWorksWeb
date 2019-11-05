80918ed67f708bc87059bcb90edf81a6f18fe44a
Joshua Pace <jpace@4wall.com>
2019-11-05 08:08:55 -0800

    Fixes duplicate field defaulting when validating in the inventoryprepgrid - closes #1255

--- src/RentalWorksWeb/Source/Grids/InventoryPrepGrid/InventoryPrepGridController.ts
+++ src/RentalWorksWeb/Source/Grids/InventoryPrepGrid/InventoryPrepGridController.ts
@@ -5,7 +5,7 @@
     generateRow($control, $generatedtr) {
         $generatedtr.find('div[data-browsedatafield="PrepRateId"]').data('onchange', function ($tr) {
             $generatedtr.find('.field[data-browsedatafield="PrepDescription"]').text($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
-            $generatedtr.find('.field[data-browsedatafield="PrepUnit"]').text($tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="PrepUnit"]').text($tr.find('.field[data-browsedatafield="Unit"]').attr('data-originalvalue'));
         });
     };
 }
