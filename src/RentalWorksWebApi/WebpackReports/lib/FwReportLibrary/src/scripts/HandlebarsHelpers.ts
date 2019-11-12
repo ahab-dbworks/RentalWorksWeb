c19ae74518305030ae20f56705e3b7fff16d5f6f
Jason <jhoang@4wall.com>
2019-11-12 13:33:24 -0800

    Updated HandlebarsHelpers to support both runtime and compile

--- src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/scripts/HandlebarsHelpers.ts
+++ src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/scripts/HandlebarsHelpers.ts
@@ -1,24 +1,30 @@
-﻿import * as Handlebars from 'handlebars/dist/cjs/handlebars';
+﻿//import * as  Handlebars from 'handlebars/dist/cjs/handlebars';
 
 export class HandlebarsHelpers {
-    static registerHelpers() {
-		Handlebars.registerHelper('gt', function (arg1, arg2, options) {
+    static registerHelpers(custom:boolean = false) {
+        let Handlebars;
+        if (custom) {
+            Handlebars = require('handlebars/dist/cjs/handlebars');
+        } else {
+            Handlebars = require('handlebars/runtime');
+        }
+        Handlebars.registerHelper('gt', function (arg1: any, arg2: any, options: any) {
             return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
         });
-        Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+        Handlebars.registerHelper('ifEquals', function (arg1: any, arg2: any, options: any) {
             return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
         });
-        Handlebars.registerHelper('ifNotEquals', function (arg1, arg2, options) {
+        Handlebars.registerHelper('ifNotEquals', function (arg1: any, arg2: any, options: any) {
             return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
         });
-        Handlebars.registerHelper('checkLength', function (arg1, arg2, options) {
+        Handlebars.registerHelper('checkLength', function (arg1: any, arg2: any, options: any) {
             'use strict';
             if (arg1.length > arg2) {
                 return options.fn(this);
             }
             return options.inverse(this);
         });
-        Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+        Handlebars.registerHelper('ifCond', function (v1: any, operator: any, v2: any, options: any) {
 
             switch (operator) {
                 case '==':
