7652c34a32298b9c45b7620f8399213300efda99
Justin Hoffman <justin@dbworks.com>
2019-11-19 17:00:31 -0800

    Resolved #1325: fixed syntax error when using the "Find" function on a Custom Field with spaces in the field name.

--- lib/Fw/src/FwStandard/Data/FwDataRecord.cs
+++ lib/Fw/src/FwStandard/Data/FwDataRecord.cs
@@ -570,7 +570,8 @@ namespace FwStandard.Data
                         //string parameterName = "@" + columns[request.searchfields[i]].Replace('.', '_');
                         //string parameterName = "@" + columns[request.searchfields[i]].Replace('.', '_').Replace("[", "").Replace("]", ""); // remove square brackets to work with custom fields
                         //string parameterName = "@" + columns[request.searchfields[i]].Replace('.', '_').Replace("[", "").Replace("]", "") + i.ToString(); // remove square brackets to work with custom fields
-                        string parameterName = "@" + request.searchfields[i].Replace('.', '_').Replace("[", "").Replace("]", "") + i.ToString(); // remove square brackets to work with custom fields
+                        //string parameterName = "@" + request.searchfields[i].Replace('.', '_').Replace("[", "").Replace("]", "") + i.ToString(); // remove square brackets to work with custom fields
+                        string parameterName = "@" + request.searchfields[i].Replace('.', '_').Replace("[", "").Replace("]", "").Replace(" ", "_") + i.ToString(); // remove square brackets to work with custom fields
 
                         bool doUpper = true;
                         string searchField = columns[request.searchfields[i]];
