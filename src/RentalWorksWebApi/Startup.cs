c6368071260e807760714d04ab0768509904cc4f
Mike Vermilion <mike@dbworks.com>
2019-12-10 16:05:28 -0800

    Moves creation of swagger documents and endpoints from FwStartup to Startup

--- src/RentalWorksWebApi/Startup.cs
+++ src/RentalWorksWebApi/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Logging;
 using WebApi.ApplicationManager;
 using WebApi.Modules.HomeControls.InventoryAvailability;
 using WebApi;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace WebApi
 {
@@ -35,6 +38,26 @@ namespace WebApi
         public override void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             base.Configure(app, env, loggerFactory);
+        }
+        //------------------------------------------------------------------------------------
+        protected override void AddSwaggerDocs(SwaggerGenOptions options)
+        {
+            options.SwaggerDoc("accountservices-v1", new Info { Title = SystemName + " Account Services API v1", Version = "v1" });
+            options.SwaggerDoc("home-v1", new Info { Title = SystemName + " Home API v1", Version = "v1" });
+            options.SwaggerDoc("settings-v1", new Info { Title = SystemName + " Settings API v1", Version = "v1" });
+            options.SwaggerDoc("reports-v1", new Info { Title = SystemName + "  Reports API v1", Version = "v1" });
+            options.SwaggerDoc("utilities-v1", new Info { Title = SystemName + "  Utilities API v1", Version = "v1" });
+            options.SwaggerDoc("administrator-v1", new Info { Title = SystemName + " Administrator API v1", Version = "v1" });
+        }
+        //------------------------------------------------------------------------------------
+        protected override void AddSwaggerEndPoints(SwaggerUIOptions options)
+        {
+            options.SwaggerEndpoint(Configuration["ApplicationConfig:VirtualDirectory"] + "/swagger/accountservices-v1/swagger.json", SystemName + " Account Services API v1");
+            options.SwaggerEndpoint(Configuration["ApplicationConfig:VirtualDirectory"] + "/swagger/home-v1/swagger.json", SystemName + " Home API v1");
+            options.SwaggerEndpoint(Configuration["ApplicationConfig:VirtualDirectory"] + "/swagger/settings-v1/swagger.json", SystemName + " Settings API v1");
+            options.SwaggerEndpoint(Configuration["ApplicationConfig:VirtualDirectory"] + "/swagger/reports-v1/swagger.json", SystemName + " Reports API v1");
+            options.SwaggerEndpoint(Configuration["ApplicationConfig:VirtualDirectory"] + "/swagger/utilities-v1/swagger.json", SystemName + " Utilities API v1");
+            options.SwaggerEndpoint(Configuration["ApplicationConfig:VirtualDirectory"] + "/swagger/administrator-v1/swagger.json", SystemName + " Administrator API v1");
         }
         //------------------------------------------------------------------------------------
     }
