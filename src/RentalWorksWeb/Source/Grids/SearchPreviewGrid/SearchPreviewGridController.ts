136022cc3024daa2b389fcfa4ee47b4fcdbe9bb6
Jason <jhoang@4wall.com>
2019-12-17 15:31:22 -0800

    Resolved #1489: Added indenting to QuikSearch Preview grid

--- src/RentalWorksWeb/Source/Grids/SearchPreviewGrid/SearchPreviewGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SearchPreviewGrid/SearchPreviewGridController.ts
@@ -6,6 +6,23 @@
          FwBrowse.setAfterRenderRowCallback($control, ($tr: JQuery, dt: FwJsonDataTable, rowIndex: number) => {
              const availabilityState = $generatedtr.find('[data-browsedatafield="AvailabilityState"]').attr('data-originalvalue');
              $generatedtr.find('[data-browsedatafield="QuantityAvailable"]').attr('data-state', availabilityState);
+
+             const parentIdIndex = dt.ColumnIndex.ParentId;
+             const grandParentIdIndex = dt.ColumnIndex.GrandParentId;
+             const parentId = dt.Rows[rowIndex][parentIdIndex];
+             const grandParentId = dt.Rows[rowIndex][grandParentIdIndex];
+             const iCodeColumn = $tr.find('[data-browsedatafield="ICode"]');
+             const descriptionColumn = $tr.find('[data-browsedatafield="Description"]');
+
+             if (parentId != "") {
+                 if (grandParentId != "") {
+                     iCodeColumn.css('text-indent', '2em');
+                     descriptionColumn.css('text-indent', '2em');
+                 } else {
+                     iCodeColumn.css('text-indent', '1em');
+                     descriptionColumn.css('text-indent', '1em');
+                 }
+             }
          });
      }
 }
