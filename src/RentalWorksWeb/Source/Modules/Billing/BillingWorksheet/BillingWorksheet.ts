685c52dbccaf1d79b2cd2d1df3a2f3e43e1efee5
Joshua Pace <jpace@4wall.com>
2019-12-31 08:25:38 -0800

    Adds defaulting for Deal in billingworksheet #1541

--- src/RentalWorksWeb/Source/Modules/Billing/BillingWorksheet/BillingWorksheet.ts
+++ src/RentalWorksWeb/Source/Modules/Billing/BillingWorksheet/BillingWorksheet.ts
@@ -744,6 +744,8 @@ class BillingWorksheet {
         });
         $form.find('div[data-datafield="OrderId"]').data('onchange', $tr => {
             FwFormField.setValue($form, 'div[data-datafield="WorksheetDescription"]', $tr.find('.field[data-browsedatafield="Description"]').attr('data-originalvalue'));
+            FwFormField.setValue($form, 'div[data-datafield="DealId"]', $tr.find('.field[data-browsedatafield="DealId"]').attr('data-originalvalue'), $tr.find('.field[data-browsedatafield="Deal"]').attr('data-originalvalue'));
+
         });
     }
     //----------------------------------------------------------------------------------------------
