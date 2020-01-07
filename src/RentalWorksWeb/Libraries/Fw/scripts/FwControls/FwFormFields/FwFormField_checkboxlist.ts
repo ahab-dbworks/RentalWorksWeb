7bdf03839329f28ae99763a03e635dea6ce3fb87
Joshua Pace <jpace@4wall.com>
2020-01-07 12:09:21 -0800

    Adds condition in checkboxlist to prevent undefined err

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwFormFields/FwFormField_checkboxlist.ts
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwFormFields/FwFormField_checkboxlist.ts
@@ -1,18 +1,16 @@
 ﻿class FwFormField_checkboxlistClass implements IFwFormField {
     //---------------------------------------------------------------------------------
     renderDesignerHtml($control: JQuery, html: string[]): void {
-
     }
     //---------------------------------------------------------------------------------
     renderRuntimeHtml($control: JQuery, html: string[]): void {
-        let options: any = {};
-        let $form = $control.closest('.fwform');
         html.push('<div class="fwformfield-caption">' + $control.attr('data-caption') + '</div>');;
         html.push('<div class="fwformfield-control">');
         html.push('  <ol style="min-height:1200px;min-width:200px;">');
         html.push('  </ol>');
         html.push('</div>');
         $control.html(html.join(''));
+        const options: any = {};
         if ($control.attr('data-share') === 'true') {
             options.group = 'shared';
             options.onAdd = function (evt) {
@@ -31,32 +29,30 @@
             }
             options.onAdd = function (evt) {
                 this.el.removeChild(evt.item);
+                const $form = $control.closest('.fwform');
                 $form.attr('data-modified', 'true');
                 $form.find('.btn[data-type="SaveMenuBarButton"]').removeClass('disabled');
             }
         }
         if ($control.attr('data-sortable') === 'true') {
             $control.find('ol').addClass('sortable');
-            var ol = $control.find('ol');
+            const ol = $control.find('ol');
             if (ol.length > 0) {
-                let olElement = ol.get(0);
+                const olElement = ol.get(0);
                 Sortable.create(olElement, options);
             }
         }
     }
     //---------------------------------------------------------------------------------
     loadItems($control: JQuery<HTMLElement>, items: any, hideEmptyItem: boolean): void {
-        var html, hasorderby, checkboxid;
-
-        html = [];
+        const html = [];
         if ((typeof items !== 'undefined') && (items !== null)) {
             if ($control.attr('data-type') === 'orderby') {
                 $control.attr('data-sortable', 'true');
                 $control.attr('data-orderby', 'true');
             }
-            hasorderby = ((typeof $control.attr('data-orderby') === 'string') && ($control.attr('data-orderby') === 'true'));
-            for (var i = 0; i < items.length; i++) {
-                checkboxid = FwControl.generateControlId('cb' + i.toString());
+            for (let i = 0; i < items.length; i++) {
+                const checkboxid = FwControl.generateControlId('cb' + i.toString());
                 if (typeof items[i].selected !== 'string') items[i].selected = 'F';
                 if (typeof items[i].orderbydirection !== 'string') items[i].orderbydirection = '';
                 html.push('<li data-value="');
@@ -71,6 +67,7 @@
                 }
                 html.push('>');
                 html.push('<div class="wrapper">');
+                const hasorderby = ((typeof $control.attr('data-orderby') === 'string') && ($control.attr('data-orderby') === 'true'));
                 if (hasorderby) {
                     html.push('<div class="handle">::</div>');
                 }
@@ -98,15 +95,13 @@
         }
         $control.find('ol').html(html.join(''));
         $control.find('ol .checkbox').on('change', function () {
-            var $this, $li;
-            $this = jQuery(this);
-            $li = $this.closest('li');
+            const $this = jQuery(this);
+            const $li = $this.closest('li');;
             $li.attr('data-selected', $this.prop('checked') ? 'T' : 'F');
         });
         $control.find('ol .orderbydirection').on('click', function () {
-            var $this, $li;
-            $this = jQuery(this);
-            $li = $this.closest('li');
+            const $this = jQuery(this);
+            const $li = $this.closest('li');
             switch ($li.attr('data-orderbydirection')) {
                 case 'asc': $li.attr('data-orderbydirection', 'desc'); break;
                 case 'desc': $li.attr('data-orderbydirection', 'asc'); break;
@@ -115,12 +110,10 @@
     }
     //---------------------------------------------------------------------------------
     loadForm($fwformfield: JQuery<HTMLElement>, table: string, field: string, value: any, text) {
-        var html, hasorderby, checkboxid;
-
-        html = [];
+        const html = [];
         if ((typeof value !== 'undefined') && (value !== null)) {
-            for (var i = 0; i < value.length; i++) {
-                checkboxid = FwControl.generateControlId('cb' + i.toString());
+            for (let i = 0; i < value.length; i++) {
+                const checkboxid = FwControl.generateControlId('cb' + i.toString());
                 if (value[i].selected) {
                     value[i].selected = 'T';
                 } else {
@@ -161,6 +154,7 @@
                 if ($fwformfield.attr('data-listtype') === 'standard') {
                     html.push('<div class="settings"><i class="material-icons">settings</i></div>')
                 }
+                const hasorderby = ((typeof $fwformfield.attr('data-orderby') === 'string') && ($fwformfield.attr('data-orderby') === 'true'));
                 if (hasorderby) {
                     html.push('<div class="orderbydirection"></div>');
                 }
@@ -175,15 +169,13 @@
         }
 
         $fwformfield.find('ol .checkbox').on('change', function () {
-            var $this, $li;
-            $this = jQuery(this);
-            $li = $this.closest('li');
+            const $this = jQuery(this);
+            const $li = $this.closest('li');
             $li.attr('data-selected', $this.prop('checked') ? 'T' : 'F');
         });
         $fwformfield.find('ol .orderbydirection').on('click', function () {
-            var $this, $li;
-            $this = jQuery(this);
-            $li = $this.closest('li');
+            const $this = jQuery(this);
+            const $li = $this.closest('li');
             switch ($li.attr('data-orderbydirection')) {
                 case 'asc': $li.attr('data-orderbydirection', 'desc'); break;
                 case 'desc': $li.attr('data-orderbydirection', 'asc'); break;
@@ -203,9 +195,8 @@
         let value = [];
         if ($fwformfield.data('checkboxlist') === 'persist') {
             $fwformfield.find('li[data-selected="T"]').each(function (index, element) {
-                var $li, item;
-                $li = jQuery(element);
-                item = {};
+                const $li = jQuery(element);
+                const item: any = {};
                 item.value = $li.attr('data-value');
                 item.text = $li.find('label').text();
                 item.userWidgetId = $li.attr('data-userwidgetid');
@@ -214,9 +205,8 @@
                 value.push(item);
             });
             $fwformfield.find('li[data-selected="F"]').each(function (index, element) {
-                var $li, item;
-                $li = jQuery(element);
-                item = {};
+                const $li = jQuery(element);
+                const item: any = {};
                 item.value = $li.attr('data-value');
                 item.text = $li.find('label').text();
                 item.userWidgetId = $li.attr('data-userwidgetid');
@@ -225,9 +215,8 @@
             });
         } else {
             $fwformfield.find('li[data-selected="T"]').each(function (index, element) {
-                var $li, item;
-                $li = jQuery(element);
-                item = {};
+                const $li = jQuery(element);
+                const item: any = {};
                 item.value = $li.attr('data-value');
                 if (typeof $li.attr('data-orderbydirection') === 'string') {
                     item.orderbydirection = $li.attr('data-orderbydirection');
@@ -235,8 +224,8 @@
                 value.push(item);
             });
         }
-        if ($fwformfield.data('returncsv').toString() === 'true') {
-            let values: string[] = [];
+        if ($fwformfield.data('returncsv') && $fwformfield.data('returncsv').toString() === 'true') {
+            const values: string[] = [];
             value = value.map((item, index, array) => {
                 values.push(item.value.toString());
             });
@@ -247,7 +236,7 @@
     }
     //---------------------------------------------------------------------------------
     setValue($fwformfield: JQuery<HTMLElement>, value: any, text: string, firechangeevent: boolean): void {
-        var $inputvalue = $fwformfield.find('.fwformfield-value');
+        const $inputvalue = $fwformfield.find('.fwformfield-value');
         $inputvalue.val(value);
         if (firechangeevent) $inputvalue.change();
     }
