3f1da739093d4a2edde35e7dc69c461a1259f49d
Jason <jhoang@4wall.com>
2020-01-09 13:14:16 -0800

    Resolved #1590:  Added a menu option to cancel a Transfer In Session

--- src/RentalWorksWeb/Source/Modules/Transfers/TransferIn/TransferIn.ts
+++ src/RentalWorksWeb/Source/Modules/Transfers/TransferIn/TransferIn.ts
@@ -10,8 +10,45 @@ class TransferIn extends CheckInBase {
     addFormMenuItems(options: IAddFormMenuOptions) {
         options.hasSave = false;
         FwMenu.addFormMenuButtons(options);
+
+        FwMenu.addSubMenuItem(options.$groupOptions, 'Cancel Transfer In Session', '', (e: JQuery.ClickEvent) => {
+            try {
+                this.cancelTransferIn(options.$form);
+            } catch (ex) {
+                FwFunc.showError(ex);
+            }
+        });
     }
     //----------------------------------------------------------------------------------------------
+    cancelTransferIn($form: JQuery): void {
+        try {
+            const contractId = FwFormField.getValueByDataField($form, 'ContractId');
+            if (contractId != '') {
+                const $confirmation = FwConfirmation.renderConfirmation('Cancel Transfer In Session', 'Cancelling this Transfer In Session will cause all transacted items to be cancelled. Continue?');
+                const $yes = FwConfirmation.addButton($confirmation, 'Yes', false);
+                FwConfirmation.addButton($confirmation, 'No', true);
+
+                $yes.on('click', () => {
+                    try {
+                        const request: any = { ContractId: contractId };
+                        FwAppData.apiMethod(true, 'POST', `api/v1/contract/cancelcontract`, request, FwServices.defaultTimeout,
+                            response => {
+                                FwConfirmation.destroyConfirmation($confirmation);
+                                this.resetForm($form);
+                                FwNotification.renderNotification('SUCCESS', 'Session succesfully cancelled.');
+                            },
+                            ex => FwFunc.showError(ex),
+                            $confirmation.find('.fwconfirmationbox'));
+                    } catch (ex) {
+                        FwFunc.showError(ex);
+                    }
+                });
+            }
+        } catch (ex) {
+            FwFunc.showError(ex);
+        }
+    }
+        //----------------------------------------------------------------------------------------------
     //beforeValidateOrder($browse: any, $form: any, request: any) {
     //    const warehouseId = JSON.parse(sessionStorage.getItem('warehouse')).warehouseid;
     //    request.miscfields = {
