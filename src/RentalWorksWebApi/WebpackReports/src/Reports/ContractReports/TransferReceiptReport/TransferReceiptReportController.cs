18042099c3f5753af97ecf236305b36126d75881
Joshua Pace <jpace@4wall.com>
2020-01-09 13:32:33 -0800

    Adds TransferReceiptReport API #1560

---
+++ src/RentalWorksWebApi/WebpackReports/src/Reports/ContractReports/TransferReceiptReport/TransferReceiptReportController.cs
@@ -0,0 +1,92 @@
+using FwStandard.AppManager;
+using FwStandard.Models;
+using FwStandard.Reporting;
+using FwStandard.SqlServer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using PuppeteerSharp;
+using System;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+using WebApi.Data;
+using WebApi.Modules.Warehouse.Contract;
+
+namespace WebApi.Modules.Reports.ContractReports.TransferReceiptReport
+{
+    public class TransferReceiptReportRequest : AppReportRequest
+    {
+        public string ContractId { get; set; }
+    }
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "reports-v1")]
+    [FwController(Id: "F6XtBsrOH4cjm")]
+    public class TransferReceiptReportController : AppReportController
+    {
+        //------------------------------------------------------------------------------------ 
+        public TransferReceiptReportController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { loaderType = typeof(TransferReceiptReportLoader); }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetReportFileName() { return "TransferReceiptReport"; }
+        protected override string GetReportFriendlyName() { return "Lost Contract Report"; }
+        protected override PdfOptions GetPdfOptions()
+        {
+            // Configures Chromium for printing. Some of these properties are better to set in the @page section in CSS.
+            PdfOptions pdfOptions = new PdfOptions();
+            pdfOptions.DisplayHeaderFooter = true;
+            return pdfOptions;
+        }
+        //------------------------------------------------------------------------------------ 
+        protected override string GetUniqueId(FwReportRenderRequest request)
+        {
+            return request.parameters["ContractId"].ToString().TrimEnd();
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/transferreceiptreport/render 
+        [HttpPost("render")]
+        [FwControllerMethod(Id: "ZtZcIsvTh2iA5")]
+        public async Task<ActionResult<FwReportRenderResponse>> Render([FromBody]FwReportRenderRequest request)
+        {
+            if (!this.ModelState.IsValid) return BadRequest();
+            FwReportRenderResponse response = await DoRender(request);
+            return new OkObjectResult(response);
+        }
+        //------------------------------------------------------------------------------------ 
+        //[HttpGet("{contractid}")]
+        //[FwControllerMethod(Id:"HCPKa35FPjMY")]
+        //public async Task<ActionResult<TransferReceiptReport>> GetContract([FromRoute] string contractid)
+        //{
+        //    TransferReceiptReportLogic contractReportRepo = new TransferReceiptReportLogic(this.AppConfig, this.UserSession);
+        //    var contractReport = await contractReportRepo.Get(contractid);
+        //    return new OkObjectResult(contractReport);
+        //}
+        ////------------------------------------------------------------------------------------ 
+        // POST api/v1/transferreceiptreport/runreport 
+        [HttpPost("runreport")]
+        [FwControllerMethod(Id: "KRcMT7QPVMb9t")]
+        public async Task<ActionResult<TransferReceiptReportLoader>> RunReportAsync([FromBody]TransferReceiptReportRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                TransferReceiptReportLoader l = new TransferReceiptReportLoader();
+                l.SetDependencies(this.AppConfig, this.UserSession);
+                return new OkObjectResult(await l.RunReportAsync(request));
+            }
+            catch (Exception ex)
+            {
+                return GetApiExceptionResult(ex);
+            }
+        }
+        //------------------------------------------------------------------------------------ 
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/transferreceiptreport/validatecontract/browse 
+        [HttpPost("validatecontract/browse")]
+        [FwControllerMethod(Id: "KdooVKhTCi4qq", ActionType: FwControllerActionTypes.Browse)]
+        public async Task<ActionResult<FwJsonDataTable>> ValidateContractBrowseAsync([FromBody]BrowseRequest browseRequest)
+        {
+            return await DoBrowseAsync<ContractLogic>(browseRequest);
+        }
+    }
+}
---
+++ src/RentalWorksWebApi/WebpackReports/src/Reports/ContractReports/TransferReceiptReport/index.ts
@@ -0,0 +1,138 @@
+import { WebpackReport } from '../../../../lib/FwReportLibrary/src/scripts/WebpackReport';
+import { CustomField } from '../../../../lib/FwReportLibrary/src/scripts/CustomField';
+import { DataTable } from '../../../../lib/FwReportLibrary/src/scripts/Browse';
+import { Ajax } from '../../../../lib/FwReportLibrary/src/scripts/Ajax';
+import * as moment from 'moment';
+import '../../../../lib/FwReportLibrary/src/theme/webpackReports.scss';
+import './index.scss';
+const hbReport = require("./hbReport.hbs");
+const hbFooter = require("./hbFooter.hbs");
+
+export class TransferReceiptReport extends WebpackReport {
+    contract: TransferReceipt = null;
+    renderReport(apiUrl: string, authorizationHeader: string, parameters: any): void {
+        try {
+            super.renderReport(apiUrl, authorizationHeader, parameters);
+            Ajax.get<DataTable>(`${apiUrl}/api/v1/logosettings/1`, authorizationHeader)
+                .then((response: DataTable) => {
+                    const logoObject: any = response;
+                    Ajax.post<TransferReceipt>(`${apiUrl}/api/v1/transferreceiptreport/runreport`, authorizationHeader, parameters)
+                        .then((response: TransferReceipt) => {
+                            const data: any = response;
+                            data.Items = DataTable.toObjectList(response.Items);
+                            data.PrintTime = `Printed on ${moment().format('MM/DD/YYYY')} at ${moment().format('h:mm:ss A')}`;
+                            data.System = 'RENTALWORKS';
+                            data.Report = 'TRANSFER RECEIPT';
+                            if (logoObject.LogoImage != '') {
+                                data.Logosrc = logoObject.LogoImage;
+                            }
+                            this.renderFooterHtml(data);
+                            if (this.action === 'Preview' || this.action === 'PrintHtml') {
+                                document.getElementById('pageFooter').innerHTML = this.footerHtml;
+                            }
+                            if (parameters.isCustomReport) {
+                                document.getElementById('pageBody').innerHTML = parameters.CustomReport(data);
+                            } else {
+                                document.getElementById('pageBody').innerHTML = hbReport(data);
+                            }
+
+                            // want to add
+                            //if (data.TermsAndConditions !== null || data.TermsAndConditions !== '') {
+                            //    const termEl = document.getElementById('terms');
+                            //    termEl.innerHTML = data.TermsAndConditions;
+                            //    if (data.TermsAndConditionsNewPage) {
+                            //        const termsRow = document.getElementById('termsRow');
+                            //        termsRow.style.cssText = "page-break-before:always;";
+                            //    }
+                            //}
+
+
+                            this.onRenderReportCompleted();
+                        })
+                        .catch((ex) => {
+                            this.onRenderReportFailed(ex);
+                        });
+                })
+                .catch((ex) => {
+                    console.log('exception: ', ex)
+                });
+        } catch (ex) {
+            this.onRenderReportFailed(ex);
+        }
+    }
+
+    renderFooterHtml(model: TransferReceipt): string {
+        this.footerHtml = hbFooter(model);
+        return this.footerHtml;
+    }
+}
+
+(<any>window).report = new TransferReceiptReport();
+
+class TransferReceipt {
+    _Custom = new Array<CustomField>();
+    RowType: string;
+    ContractId: string;
+    ContractNumber: string;
+    ContractDate: string;
+    ContractTime: string;
+    ContractDateAndTime: string;
+    ContractType: string;
+    HasPendingExchange: true;
+    InputByUserId: string;
+    BillingDate: string;
+    Warehouse: string;
+    WarehouseAddress1: string;
+    WarehouseAddress2: string;
+    WarehouseCityStateZipCode: string;
+    WarehouseCityStateZipCodeCountry: string;
+    WarehousePhone: string;
+    WarehouseFax: string;
+    DealId: string;
+    Deal: string;
+    DealNumber: string;
+    DealNumberAndDeal: string;
+    OrderId: string;
+    OrderNumber: string;
+    OrderDate: string;
+    OrderPoNumber: string;
+    OrderType: string;
+    OrderDescription: string;
+    OrderNumberAndDescription: string;
+    ContainerBarCode: string;
+    UsageDates: string;
+    BillingDates: true;
+    BillingCycle: string;
+    OrderLocation: string;
+    PaymentTerms: string;
+    Agent: string;
+    AgentPhoneAndExtension: string;
+    AgentFax: string;
+    Department: string;
+    Vendor: string;
+    VendorAddress1: string;
+    VendorAddress2: string;
+    VendorCity: string;
+    VendorState: string;
+    VendorZipCode: string;
+    VendorPhone: string;
+    VendorFax: string;
+    VendorContact: string;
+    PurchaseOrderId: string;
+    PurchaseOrderNumber: string;
+    DeliveryContact: string;
+    DeliveryLocation: string;
+    DeliveryAddress1: string;
+    DeliveryAddress2: string;
+    DeliveryCityStateZipCode: string;
+    DeliveryCountry: string;
+    DeliveryContactPhone: string;
+    TermsAndConditionsId: string;
+    TermsAndConditionsFileName: string;
+    TermsAndConditionsNewPage: true;
+    ResponsiblePersonId: string;
+    ResponsiblePerson: string;
+    Items: any;
+    PrintTime: string;
+}
+
