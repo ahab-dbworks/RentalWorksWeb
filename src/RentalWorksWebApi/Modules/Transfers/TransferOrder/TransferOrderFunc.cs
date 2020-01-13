9c5c99375a1f4d466fd8917ccd90317887efd0e2
Justin Hoffman <justin@dbworks.com>
2020-01-13 04:04:10 -0800

    Fixed the availability calculation delay problem for #1623

--- src/RentalWorksWebApi/Modules/Transfers/TransferOrder/TransferOrderFunc.cs
+++ src/RentalWorksWebApi/Modules/Transfers/TransferOrder/TransferOrderFunc.cs
@@ -1,5 +1,6 @@
 ﻿using FwStandard.Models;
 using FwStandard.SqlServer;
+using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
 
@@ -18,6 +19,29 @@ namespace WebApi.Modules.Transfers.TransferOrder
                 qry.AddParameter("@usersid", SqlDbType.NVarChar, ParameterDirection.Input, userSession.UsersId);
                 await qry.ExecuteNonQueryAsync();
                 success = true;
+            }
+
+            if (success)
+            {
+                using (FwSqlConnection conn = new FwSqlConnection(appConfig.DatabaseSettings.ConnectionString))
+                {
+                    FwSqlCommand qryItems = new FwSqlCommand(conn, appConfig.DatabaseSettings.QueryTimeout);
+                    qryItems.Add("select distinct mi.masterid, mi.warehouseid, m.class            ");
+                    qryItems.Add(" from  masteritem mi                                            ");
+                    qryItems.Add("            join master m on (mi.masterid = m.masterid)         ");
+                    qryItems.Add(" where mi.orderid = @orderid                                    ");
+                    qryItems.AddParameter("@orderid", id);
+                    FwJsonDataTable dt = await qryItems.QueryToFwJsonTableAsync();
+
+                    foreach (List<object> row in dt.Rows)
+                    {
+                        string inventoryId = row[dt.GetColumnNo("masterid")].ToString();
+                        string warehouseId = row[dt.GetColumnNo("warehouseid")].ToString();
+                        string classification = row[dt.GetColumnNo("class")].ToString();
+                        HomeControls.InventoryAvailability.InventoryAvailabilityFunc.RequestRecalc(inventoryId, warehouseId, classification);
+                    }
+                }
+
             }
             return success;
         }
