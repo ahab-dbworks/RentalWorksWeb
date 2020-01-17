54460e0fefc4a5818558e12f196a8686738a2a1c
Joshua Pace <jpace@4wall.com>
2020-01-16 18:06:48 -0800

    Don't render peek button for browse column fields with no data #1603

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwFormFields/FwFormField_validation.ts
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwFormFields/FwFormField_validation.ts
@@ -34,26 +34,11 @@
         }
         html.push(' />');
         html.push('<i class="material-icons btnvalidate">search</i>');
-
         // push hidden spinning loader
         html.push('<div class="sk-fading-circle validation-loader"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>');
-
-        //const isWebAdmin = (sessionStorage.getItem('userid') !== null) ? JSON.parse(sessionStorage.getItem('userid')).webadministrator === 'true' : false;
-        //let showPeek = false;
-        //if (!($control.attr('data-validationpeek') === 'false')) {
-        //    if (isWebAdmin === true) {
-        //        showPeek = true;
-        //    }
-        //    else if (isWebAdmin === false && FwValidation.isValidationWithPeek($control)) {        
-        //        showPeek = true;
-        //    }
-        //}
-
-        //if (showPeek) {
         html.push('<i class="material-icons btnpeek" style="display:none;">more_horiz</i>');
-        // }
-
         html.push('</div>');
+
         $control.html(html.join(''));
         FwValidation.init($control);
     }
