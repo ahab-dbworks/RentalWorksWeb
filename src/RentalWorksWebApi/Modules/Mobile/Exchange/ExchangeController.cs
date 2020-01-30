eef89f6e158416af3183b2cc7c0c45173df40259
Justin Hoffman <justin@dbworks.com>
2020-01-30 14:26:57 -0800

    Resolved #1791: Fixed 404 error when trying to Cancel a Check-Out, Exchange, or Check-In session

--- src/RentalWorksWebApi/Modules/Mobile/Exchange/ExchangeController.cs
+++ src/RentalWorksWebApi/Modules/Mobile/Exchange/ExchangeController.cs
@@ -2,7 +2,11 @@ using FwStandard.AppManager;
 using FwStandard.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
 using WebApi.Controllers;
+using WebApi.Logic;
+using WebApi.Modules.Warehouse.Contract;
 
 namespace WebApi.Modules.Mobile.Exchange
 {
@@ -13,5 +17,25 @@ namespace WebApi.Modules.Mobile.Exchange
     {
         public ExchangeController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { }
         //------------------------------------------------------------------------------------ 
+        // POST api/v1/exchange/cancelcontract
+        [HttpPost("cancelcontract")]
+        [FwControllerMethod(Id: "w8HPRde15rzxn", ActionType: FwControllerActionTypes.Option, Caption: "Cancel Contract")]
+        public async Task<ActionResult<TSpStatusResponse>> CancelContract([FromBody]CancelContractRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                TSpStatusResponse response = await ContractFunc.CancelContract(AppConfig, UserSession, request);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return GetApiExceptionResult(ex);
+            }
+        }
+        //------------------------------------------------------------------------------------
     }
 }
