f1901f9f6c2f6bc733756cb36ea3fbec9b4955b6
Ahab Guirguis <ahab@dbworks.com>
2020-01-30 15:11:11 -0800

    Added Manifest properties to Inventory

--- src/RentalWorksWebApi/Modules/Agent/Order/Controls/Manifest/OrderManifestController.cs
+++ src/RentalWorksWebApi/Modules/Agent/Order/Controls/Manifest/OrderManifestController.cs
@@ -14,15 +14,6 @@ namespace WebApi.Modules.Agent.Order
     [FwControl(Caption: "Order Manifiest", SecurityId: "8uhwXXJ95d3o", ControlType: FwControlTypes.Grid)]
     public partial class OrderController : AppDataController
     {
-        //public class OrderManifestRequest
-        //{
-        //    public string OrderId { get; set; }
-        //    public string RentalValue { get; set; }
-        //    public string SalesValue { get; set; }
-        //    public string AppLanguageId { get; set; }
-        //    public string OutOnly { get; set; }
-        //    public string Excel { get; set; }
-        //}
         //------------------------------------------------------------------------------------ 
         /// <summary>
         /// Get Order Manifest
@@ -32,7 +23,7 @@ namespace WebApi.Modules.Agent.Order
         /// <returns></returns>
         // POST api/v1/order/{orderId}/manifest/browse 
         [HttpPost("{orderid}/manifest/browse")]
-        [FwControllerMethod(Id: "QCdz6hgmh7Dh", FwControllerActionTypes.ControlBrowse, ParentId: "U8Zlahz3ke9i")]
+        [FwControllerMethod(Id: "QCdz6hgmh7Dh", FwControllerActionTypes.ControlBrowse, ParentId: "8uhwXXJ95d3o")]
         public async Task<ActionResult<FwJsonDataTable>> ManifestBrowseAsync([FromBody]BrowseRequest browseRequest)
         {
             return await DoBrowseAsync(browseRequest);
@@ -46,7 +37,7 @@ namespace WebApi.Modules.Agent.Order
         /// <returns></returns>
         // POST api/v1/restrictedperson/{id}/aka/exportexcelxlsx 
         [HttpPost("{orderid}/manifest/exportexcelxlsx")]
-        [FwControllerMethod(Id: "KVAhgRQdvHQt", FwControllerActionTypes.ControlBrowse, ParentId: "U8Zlahz3ke9i")]
+        [FwControllerMethod(Id: "KVAhgRQdvHQt", FwControllerActionTypes.ControlBrowse, ParentId: "8uhwXXJ95d3o")]
         public async Task<ActionResult<DoExportExcelXlsxExportFileAsyncResult>> ManifestExportExcelXlsxFileAsync([FromBody]BrowseRequest browseRequest)
         {
             return await DoExportExcelXlsxFileAsync(browseRequest);
