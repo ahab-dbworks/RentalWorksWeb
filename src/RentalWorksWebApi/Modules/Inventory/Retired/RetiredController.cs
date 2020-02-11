aeef1acdc68a2eb1485dda095ff4b35af0500f3d
Justin Hoffman <justin@dbworks.com>
2020-02-11 11:33:10 -0800

    added Retired api controller for #1874

---
+++ src/RentalWorksWebApi/Modules/Inventory/Retired/RetiredController.cs
@@ -0,0 +1,75 @@
+using FwStandard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Controllers;
+using System.Threading.Tasks;
+using FwStandard.SqlServer;
+using System.Collections.Generic;
+using FwStandard.AppManager;
+namespace WebApi.Modules.Home.Retired
+{
+    [Route("api/v1/[controller]")]
+    [ApiExplorerSettings(GroupName = "home-v1")]
+    [FwController(Id: "I9OA43GGHPNFf")]
+    public class RetiredController : AppDataController
+    {
+        public RetiredController(IOptions<FwApplicationConfig> appConfig) : base(appConfig) { logicType = typeof(RetiredLogic); }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/retired/browse 
+        [HttpPost("browse")]
+        [FwControllerMethod(Id: "IAJakjbK04PHQ", ActionType: FwControllerActionTypes.Browse)]
+        public async Task<ActionResult<FwJsonDataTable>> BrowseAsync([FromBody]BrowseRequest browseRequest)
+        {
+            return await DoBrowseAsync(browseRequest);
+        }
+        //------------------------------------------------------------------------------------ 
+        // POST api/v1/retired/exportexcelxlsx/filedownloadname 
+        [HttpPost("exportexcelxlsx/{fileDownloadName}")]
+        [FwControllerMethod(Id: "ibDi8oSWvmOXb", ActionType: FwControllerActionTypes.Browse)]
+        public async Task<ActionResult<DoExportExcelXlsxExportFileAsyncResult>> ExportExcelXlsxFileAsync([FromBody]BrowseRequest browseRequest)
+        {
+            return await DoExportExcelXlsxFileAsync(browseRequest);
+        }
+        //------------------------------------------------------------------------------------ 
+        // GET api/v1/retired 
+        [HttpGet]
+        [FwControllerMethod(Id: "IBJi23JaPug2X", ActionType: FwControllerActionTypes.Browse)]
+        public async Task<ActionResult<IEnumerable<RetiredLogic>>> GetManyAsync([FromQuery]int pageno, [FromQuery]int pagesize, [FromQuery]string sort)
+        {
+            return await DoGetAsync<RetiredLogic>(pageno, pagesize, sort);
+        }
+        //------------------------------------------------------------------------------------ 
+        // GET api/v1/retired/A0000001 
+        [HttpGet("{id}")]
+        [FwControllerMethod(Id: "iCI01PTIilYmw", ActionType: FwControllerActionTypes.View)]
+        public async Task<ActionResult<RetiredLogic>> GetOneAsync([FromRoute]string id)
+        {
+            return await DoGetAsync<RetiredLogic>(id);
+        }
+        //------------------------------------------------------------------------------------ 
+        //// POST api/v1/retired 
+        //[HttpPost]
+        //[FwControllerMethod(Id: "ICjk6uImSscUK", ActionType: FwControllerActionTypes.New)]
+        //public async Task<ActionResult<RetiredLogic>> NewAsync([FromBody]RetiredLogic l)
+        //{
+        //    return await DoNewAsync<RetiredLogic>(l);
+        //}
+        ////------------------------------------------------------------------------------------ 
+        //// PUT api/v1/retired/A0000001 
+        //[HttpPut("{id}")]
+        //[FwControllerMethod(Id: "IcTJYDlKIrDK6", ActionType: FwControllerActionTypes.Edit)]
+        //public async Task<ActionResult<RetiredLogic>> EditAsync([FromRoute] string id, [FromBody]RetiredLogic l)
+        //{
+        //    return await DoEditAsync<RetiredLogic>(l);
+        //}
+        ////------------------------------------------------------------------------------------ 
+        //// DELETE api/v1/retired/A0000001 
+        //[HttpDelete("{id}")]
+        //[FwControllerMethod(Id: "IcyBZnWQlffM4", ActionType: FwControllerActionTypes.Delete)]
+        //public async Task<ActionResult<bool>> DeleteAsync([FromRoute]string id)
+        //{
+        //    return await DoDeleteAsync<RetiredLogic>(id);
+        //}
+        ////------------------------------------------------------------------------------------ 
+    }
+}
---
+++ src/RentalWorksWebApi/Modules/Inventory/Retired/RetiredRecord.cs
@@ -0,0 +1,161 @@
+using FwStandard.SqlServer;
+using FwStandard.SqlServer.Attributes;
+using WebApi.Data;
+namespace WebApi.Modules.Home.Retired
+{
+    [FwSqlTable("retired")]
+    public class RetiredRecord : AppDataReadWriteRecord
+    {
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "retiredid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8, isPrimaryKey: true)]
+        public string RetiredId { get; set; } = "";
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "reprentalitemid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string ReprentalitemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "physicalitemid", modeltype: FwDataTypes.Integer, sqltype: "int")]
+        public int? PhysicalInventoryItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "physicalid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string PhysicalInventoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "retiredreasonid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string RetiredReasonId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "soldtoorderid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string SoldToOrderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "warehouseid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string WarehouseId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "donateorderid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string DonateorderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostorderid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string LostOrderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostvendorid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string LostvendorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "tradedpoid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string TradedpoId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenvendorid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string StolenvendorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "tradedvendorid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string TradedvendorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billedtoorderid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string BilledtoorderId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostamountpaid", modeltype: FwDataTypes.Decimal, sqltype: "numeric", precision: 9, scale: 2)]
+        public decimal? LostamountpaId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostpolice", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 12)]
+        public string Lostpolice { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostfiled", modeltype: FwDataTypes.Date, sqltype: "smalldatetime")]
+        public string Lostfiled { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostadjuster", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 30)]
+        public string Lostadjuster { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostclaimno", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 15)]
+        public string Lostclaimno { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "lostreplacedate", modeltype: FwDataTypes.Date, sqltype: "smalldatetime")]
+        public string Lostreplacedate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "loststatus", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 15)]
+        public string Loststatus { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "tradedreplacedate", modeltype: FwDataTypes.Date, sqltype: "smalldatetime")]
+        public string Tradedreplacedate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "tradeamt", modeltype: FwDataTypes.Decimal, sqltype: "numeric", precision: 9, scale: 2)]
+        public decimal? Tradeamt { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "donatedvalue", modeltype: FwDataTypes.Decimal, sqltype: "numeric", precision: 9, scale: 2)]
+        public decimal? Donatedvalue { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "retirednotes", modeltype: FwDataTypes.Text, sqltype: "varchar", maxlength: 255)]
+        public string RetiredNotes { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenfiled", modeltype: FwDataTypes.Date, sqltype: "smalldatetime")]
+        public string Stolenfiled { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenadjuster", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 30)]
+        public string Stolenadjuster { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenstatus", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 15)]
+        public string Stolenstatus { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenreplacedate", modeltype: FwDataTypes.Date, sqltype: "smalldatetime")]
+        public string Stolenreplacedate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "modbyid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string ModifiedByUserId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenamount", modeltype: FwDataTypes.Decimal, sqltype: "numeric", precision: 9, scale: 2)]
+        public decimal? Stolenamount { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "moddate", modeltype: FwDataTypes.Date, sqltype: "smalldatetime")]
+        public string ModifiedDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "retiredbyid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string RetiredByUserId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "retireddate", modeltype: FwDataTypes.Date, sqltype: "datetime")]
+        public string RetiredDate { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "masterid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string InventoryId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "soldtomasteritemid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string SoldToOrderItemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenpolice", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 20)]
+        public string Stolenpolice { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "stolenclaimno", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 20)]
+        public string Stolenclaimno { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "billedtomasteritemid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string BilledtomasteritemId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "consignoragreementid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string ConsignorAgreementId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "consignorid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string ConsignorId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "consignorpoid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string ConsignorpoId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "contractid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string ContractId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "qty", modeltype: FwDataTypes.Decimal, sqltype: "numeric", precision: 12, scale: 2)]
+        public decimal? Quantity { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "unretiredqty", modeltype: FwDataTypes.Decimal, sqltype: "numeric", precision: 12, scale: 2)]
+        public decimal? UnretiredQuantity { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "ordertranid", modeltype: FwDataTypes.Integer, sqltype: "int")]
+        public int? OrderTranId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "internalchar", modeltype: FwDataTypes.Boolean, sqltype: "char")]
+        public bool? InternalChar { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "repmasterid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string RepmasterId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "adjid", modeltype: FwDataTypes.Text, sqltype: "char", maxlength: 8)]
+        public string AdjustmentId { get; set; }
+        //------------------------------------------------------------------------------------ 
+        [FwSqlDataField(column: "datestamp", modeltype: FwDataTypes.UTCDateTime, sqltype: "datetime")]
+        public string DateStamp { get; set; }
+        //------------------------------------------------------------------------------------ 
+    }
+}
