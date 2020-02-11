4df0b13676ab23bdd0d7189c8a6e8eb307c9850b
Justin Hoffman <justin@dbworks.com>
2020-02-11 14:55:18 -0800

    Resolved #1889: Enabled modifying the Description for Miscellaneous and Labor line-items on Quote/Order

--- src/RentalWorksWebApi/Modules/SharedControls/HomeControls/OrderItem/OrderItemLogic.cs
+++ src/RentalWorksWebApi/Modules/SharedControls/HomeControls/OrderItem/OrderItemLogic.cs
@@ -1176,12 +1176,14 @@ namespace WebApi.Modules.HomeControls.OrderItem
             }
 
             string inventoryClass = "";
+            string inventoryAvailFor = "";
             string inventoryDescription = "";
             if (!string.IsNullOrEmpty(inventoryId))
             {
-                string[] inventoryData = AppFunc.GetStringDataAsync(AppConfig, "master", new string[] { "masterid" }, new string[] { inventoryId }, new string[] { "class", "master" }).Result;
+                string[] inventoryData = AppFunc.GetStringDataAsync(AppConfig, "master", new string[] { "masterid" }, new string[] { inventoryId }, new string[] { "class", "master", "availfor" }).Result;
                 inventoryClass = inventoryData[0];
                 inventoryDescription = inventoryData[1];
+                inventoryAvailFor = inventoryData[2];
             }
 
             if (e.SaveMode == TDataRecordSaveMode.smInsert)
@@ -1192,9 +1194,9 @@ namespace WebApi.Modules.HomeControls.OrderItem
                     OrderItemId = OrderFunc.InsertPackage(AppConfig, UserSession, this).Result;
                     e.PerformSave = false;  // all framework save functions will be skipped
                 }
-                if (!inventoryClass.Equals(RwConstants.ITEMCLASS_MISCELLANEOUS))
+                if ((!inventoryClass.Equals(RwConstants.ITEMCLASS_MISCELLANEOUS)) && (!(inventoryAvailFor.Equals(RwConstants.RATE_AVAILABLE_FOR_LABOR) || inventoryAvailFor.Equals(RwConstants.RATE_AVAILABLE_FOR_MISC))))
                 {
-                    Description = inventoryDescription; // don't let user change the description on a new row, unless MISC
+                    Description = inventoryDescription; // don't let user change the description on a new row, unless MISC, or unless Misc/Labor
                 }
                 ItemClass = inventoryClass;
                 ItemOrder = "";
@@ -1202,10 +1204,10 @@ namespace WebApi.Modules.HomeControls.OrderItem
             }
             else  // updating
             {
-                // don't let user change the description on existing row, unless MISC, GROUPHEADING, TEXT, or SUBTOTAL
+                // don't let user change the description on existing row, unless MISC, GROUPHEADING, TEXT, or SUBTOTAL, or unless Misc/Labor
                 if (orig != null)
                 {
-                    if (!(orig.ItemClass.Equals(RwConstants.ITEMCLASS_MISCELLANEOUS) || orig.ItemClass.Equals(RwConstants.ITEMCLASS_GROUP_HEADING) || orig.ItemClass.Equals(RwConstants.ITEMCLASS_TEXT) || orig.ItemClass.Equals(RwConstants.ITEMCLASS_SUBTOTAL)))
+                    if (!(orig.ItemClass.Equals(RwConstants.ITEMCLASS_MISCELLANEOUS) || orig.ItemClass.Equals(RwConstants.ITEMCLASS_GROUP_HEADING) || orig.ItemClass.Equals(RwConstants.ITEMCLASS_TEXT) || orig.ItemClass.Equals(RwConstants.ITEMCLASS_SUBTOTAL) || orig.RecType.Equals(RwConstants.RECTYPE_LABOR) || orig.RecType.Equals(RwConstants.RECTYPE_MISCELLANEOUS)))
                     {
                         Description = orig.Description;
                     }
@@ -1372,7 +1374,7 @@ namespace WebApi.Modules.HomeControls.OrderItem
         //------------------------------------------------------------------------------------
         public void OnAfterDelete(object sender, AfterDeleteEventArgs e)
         {
-            
+
             string itemClass = ItemClass ?? "";
             if (itemClass.Equals(RwConstants.INVENTORY_CLASSIFICATION_KIT) || itemClass.Equals(RwConstants.INVENTORY_CLASSIFICATION_COMPLETE) || itemClass.Equals(RwConstants.INVENTORY_CLASSIFICATION_CONTAINER))
             {
