88efd92f497592eef5aa43ac420a3f8cc3a06a60
Justin Hoffman <justin@dbworks.com>
2020-02-25 14:38:16 -0800

    removed forced permissions command from hotfix batch script

--- applyhotfixes.bat
+++ applyhotfixes.bat
@@ -1,2 +1,1 @@
-osql.exe -S justin4\sql2012 -d rentalworks_dfs_2019_001 -U dbworks -P db2424 -Q "exec fw_installhotfixes 'O'"
-osql.exe -S justin4\sql2012 -d rentalworks_dfs_2019_001 -U dbworks -P db2424 -Q "exec fw_grantall 'rentalworksweb'"
+osql.exe -S <<sesrvername> -d <<databasename>> -U dbworks -P <<password>> -Q "exec fw_installhotfixes 'O'"
