82a9892cf28cee7f39cbbbfa21eb4055f90bf212
Justin Hoffman <justin@dbworks.com>
2020-02-26 16:48:50 -0800

    Revert "Revert "added missing OrderBy field for Inventory Type, for sorting #1839""
    This reverts commit 338dceae739de4412cf28db1b9d3d95c49bcf8bd.

--- src/RentalWorksWebApi/Modules/Settings/InventorySettings/InventoryType/InventoryTypeRecord.cs
+++ src/RentalWorksWebApi/Modules/Settings/InventorySettings/InventoryType/InventoryTypeRecord.cs
@@ -9,7 +9,6 @@ namespace WebApi.Modules.Settings.InventorySettings.InventoryType
     {
 
         /* TODO:
-        orderby
         inventoryappreportdesignerid
         rentalitemappreportdesignerid
         orderbypicklist
@@ -72,6 +71,9 @@ namespace WebApi.Modules.Settings.InventorySettings.InventoryType
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "profitlossgroup", modeltype: FwDataTypes.Boolean)]
         public bool? GroupProfitLoss { get; set; }
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "orderby", modeltype: FwDataTypes.Decimal, precision: 5, scale: 1)]
+        public decimal? OrderBy { get; set; }
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "inactive", modeltype: FwDataTypes.Boolean)]
         public bool? Inactive { get; set; }
