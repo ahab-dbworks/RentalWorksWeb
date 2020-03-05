593001e09e3a2e96b07281879e8d5eb3077a422a
Joshua Pace <jpace@4wall.com>
2020-03-05 11:21:26 -0800

    deletes unused method in sound

--- src/RentalWorksWeb/Source/Modules/Settings/UserSettings/Sound/Sound.ts
+++ src/RentalWorksWeb/Source/Modules/Settings/UserSettings/Sound/Sound.ts
@@ -73,20 +73,7 @@ class Sound {
             const sound = new Audio(soundFileName);
             sound.play();
         });
-        //function dataURItoBlob(dataURI) {
-        //    var byteString = atob(dataURI.split(',')[1]);
 
-        //    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-        //    var ab = new ArrayBuffer(byteString.length);
-        //    var ia = new Uint8Array(ab);
-        //    for (var i = 0; i < byteString.length; i++) {
-        //        ia[i] = byteString.charCodeAt(i);
-        //    }
-
-        //    var bb = new Blob([ab], { "type": mimeString });
-        //    return bb;
-        //}
         $form.find('#soundInput').on('change', e => {
 
             // if NEW vs EDIT
