b8bda1f05e9770afb7513a762d2a636ce42c48b7
Mike Vermilion <mike@dbworks.com>
2020-03-09 12:28:03 -0700

    #2138 Makes the security logic more strict about kicking out a user with an expired security group.
    It's hard to see the change, when looking at the diff, but what this does is there was an if statement about validate security group and it was checking if the group was expired only when using the don't validate security group option.  Really it needs to always check if the group is expired, but only check on the inidividual security node when that option is used.

--- lib/Fw/src/FwCore/AppManager/FwAmAuthorizationFilter.cs
+++ lib/Fw/src/FwCore/AppManager/FwAmAuthorizationFilter.cs
@@ -66,42 +66,39 @@ namespace FwCore.AppManager
                         context.Result = new ForbidResult();
                         return Task.CompletedTask;
                     }
+
+                    // Validate Product Edition
+                    var editions = controllerMethodAttribute.Editions;
+                    if (editions == null)
+                    {
+                        editions = FwAppManager.Tree.GetDefaultEditions();
+                    }
+                    if (!FwAppManager.Tree.HasProductEdition(editions, FwAppManager.CurrentProduct, FwAppManager.CurrentProductEdition))
+                    {
+                        // reject the request because the Controller Action is not supported in this product edition
+                        context.Result = new ForbidResult();
+                        return Task.CompletedTask;
+                    }
+
+                    // Validate that the User's secrity group exists and is not expired
+                    var groupsid = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == AuthenticationClaimsTypes.GroupsId).Value;
+                    var groupsdatestamp = Convert.ToDateTime(context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == AuthenticationClaimsTypes.GroupsDateStamp).Value);
+                    var groupTree = FwAppManager.Tree.GetGroupsTreeAsync(groupsid, true).Result;
+                    if (groupTree == null)
+                    {
+                        // reject the request because the group does not exist
+                        context.Result = new UnauthorizedResult();
+                        return Task.CompletedTask;
+                    }
+                    if (groupsdatestamp != groupTree.DateStamp)
+                    {
+                        // reject the request because someone has modified the user's group since they last logged in
+                        context.Result = new UnauthorizedResult();
+                        return Task.CompletedTask;
+                    }
                     if (controllerMethodAttribute.ValidateSecurityGroup)
                     {
-                        var editions = controllerMethodAttribute.Editions;
-                        if (editions == null)
-                        {
-                            editions = FwAppManager.Tree.GetDefaultEditions();
-                        }
-                        if (!FwAppManager.Tree.HasProductEdition(editions, FwAppManager.CurrentProduct, FwAppManager.CurrentProductEdition))
-                        {
-                            // reject the request because the Controller Action is not supported in this product edition
-                            context.Result = new ForbidResult();
-                            return Task.CompletedTask;
-                        }
-                        var groupsid = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == AuthenticationClaimsTypes.GroupsId).Value;
-                        var groupsdatestamp = Convert.ToDateTime(context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == AuthenticationClaimsTypes.GroupsDateStamp).Value);
-                        var groupTree = FwAppManager.Tree.GetGroupsTreeAsync(groupsid, true).Result;
-                        if (groupTree == null)
-                        {
-                            // reject the request because the group does not exist
-                            context.Result = new UnauthorizedResult();
-                            return Task.CompletedTask;
-                        }
                         var groupTreeNode = groupTree.RootNode.FindById(controllerMethodAttribute.Id);
-                        if (groupsdatestamp != groupTree.DateStamp)
-                        {
-                            // reject the request because someone has modified the user's group since they last logged in
-                            context.Result = new UnauthorizedResult();
-                            return Task.CompletedTask;
-                        }
-
-                        // hack to allow validations to work without configuring security
-                        // this causes the validation filter to ignore security settings on any method named BrowseAsync
-                        //if (groupTreeNode.Caption == "BrowseAsync")
-                        //{
-                        //    return Task.CompletedTask;
-                        //}
                         if (groupTreeNode == null || !groupTreeNode.Properties.ContainsKey("visible") || groupTreeNode.Properties["visible"] == "F")
                         {
                             context.Result = new ForbidResult();
