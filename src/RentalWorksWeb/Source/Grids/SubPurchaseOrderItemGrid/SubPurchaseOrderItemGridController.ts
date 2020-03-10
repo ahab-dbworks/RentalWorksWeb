83b5ffb8d4baadf9d05ddad945bd418c83413e77
Joshua Pace <jpace@4wall.com>
2020-03-09 17:19:20 -0700

    style changes for subpo grid #2048

--- src/RentalWorksWeb/Source/Grids/SubPurchaseOrderItemGrid/SubPurchaseOrderItemGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SubPurchaseOrderItemGrid/SubPurchaseOrderItemGridController.ts
@@ -2,11 +2,9 @@
     Module: string = 'SubPurchaseOrderItemGrid';
      apiurl: string = 'api/v1/subpurchaseorderitem';
 
-    // addLegend($control) {
-     //    FwBrowse.addLegend($control, 'Vendor', '#ffece6'); // blue
-      //   FwBrowse.addLegend($control, 'Deal/Order', '#ffd666'); // orange
-
-  //   }
+     addLegend($control) {
+         FwBrowse.addLegend($control, 'Vendor', '#ffd666'); // orange
+     }
 };
 //----------------------------------------------------------------------------------------------
 var SubPurchaseOrderItemGridController = new SubPurchaseOrderItemGrid();
