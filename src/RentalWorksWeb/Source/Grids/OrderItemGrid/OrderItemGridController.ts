1d515eb57d13bea76b4e279b7dd8c6364c74f03c
Justin Hoffman <justin@dbworks.com>
2020-03-20 07:17:59 -0700

    Resolved #2219: modified legend color for "add to order"

--- src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.ts
+++ src/RentalWorksWeb/Source/Grids/OrderItemGrid/OrderItemGridController.ts
@@ -816,7 +816,7 @@ class OrderItemGrid {
                     <div class="flexrow" style="margin-bottom:5px"><div data-browsedatafield="QuantityAvailable" data-state="negative"><div class="fieldvalue">-7</div></div><div>Inventory Shortage</div></div>
                     <div class="flexrow" style="margin-bottom:5px"><div data-browsedatafield="QuantityAvailable" data-state="low"><div class="fieldvalue">4</div></div><div>Low Availability</div></div>
                     <div style="margin-bottom:10px; margin-top: 1em; font-weight:bold;">Quantity</div>
-                    <div class="flexrow" style="margin-bottom:5px"><div data-browsedatafield="QuantityAvailable"><div class="fieldvalue" style="background-color:#f44336; height:.8em; margin-top:1em;"></div></div><div>Quantity modified at</br> Staging/Check-Out</div></div>
+                    <div class="flexrow" style="margin-bottom:5px"><div data-browsedatafield="QuantityAvailable"><div class="fieldvalue" style="background-color:#ff6f6f; height:.8em; margin-top:1em;"></div></div><div>Quantity modified at</br> Staging/Check-Out</div></div>
                     <div style="margin-bottom:10px; margin-top: 1em; font-weight:bold;">Sub-Quantity</div>
                     <div class="flexrow" style="margin-bottom:5px"><div data-browsedatafield="QuantityAvailable"><div class="fieldvalue" style="background-color:#ffe5b4; height:.8em;"></div></div><div>Sub Purchase Order created</div></div>
                 </div>`;
