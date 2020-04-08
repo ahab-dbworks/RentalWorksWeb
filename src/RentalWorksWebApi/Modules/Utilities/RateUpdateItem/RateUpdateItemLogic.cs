871d2f6c18e1bfb7cef698dae6dced7421a68e1b
Jason <jhoang@4wall.com>
2020-04-07 18:29:16 -0700

    Updated RateUpdateItem API

--- src/RentalWorksWebApi/Modules/Utilities/RateUpdateItem/RateUpdateItemLogic.cs
+++ src/RentalWorksWebApi/Modules/Utilities/RateUpdateItem/RateUpdateItemLogic.cs
@@ -7,27 +7,67 @@ namespace WebApi.Modules.Utilities.RateUpdateItem
     {
         //------------------------------------------------------------------------------------ 
         RateUpdateItemRecord rateUpdateItem = new RateUpdateItemRecord();
+        RateUpdateItemLoader rateUpdateItemLoader = new RateUpdateItemLoader();
         public RateUpdateItemLogic()
         {
             dataRecords.Add(rateUpdateItem);
+            dataLoader = rateUpdateItemLoader;
         }
+
         //------------------------------------------------------------------------------------ 
         [FwLogicProperty(Id: "J1n135Cf0ZQI")]
         public string InventoryId { get { return rateUpdateItem.InventoryId; } set { rateUpdateItem.InventoryId = value; } }
         [FwLogicProperty(Id: "qyo8o5YEgnRC")]
         public string WarehouseId { get { return rateUpdateItem.WarehouseId; } set { rateUpdateItem.WarehouseId = value; } }
-        [FwLogicProperty(Id: "MzJtgJByqaoz")]
+        [FwLogicProperty(Id: "RD0YTpsLQLCn")]
         public string Description { get { return rateUpdateItem.Description; } set { rateUpdateItem.Description = value; } }
+
+        [FwLogicProperty(Id: "MzJtgJByqaoz", IsReadOnly: true)]
+        public string ICode { get; set; }
+        [FwLogicProperty(Id: "6Fl8x1jNZdrc", IsReadOnly: true)]
+        public string WarehouseCode { get; set; }
+        [FwLogicProperty(Id: "OWfOcUWYESaP", IsReadOnly: true)]
+        public string Warehouse { get; set; }
+        [FwLogicProperty(Id: "xSN9sz5G0lxp", IsReadOnly: true)]
+        public string AvailableFor { get; set; }
+        [FwLogicProperty(Id: "F6tt94SwSzyC", IsReadOnly: true)]
+        public string Rank { get; set; }
+        [FwLogicProperty(Id: "ft32BAyeROQS", IsReadOnly: true)]
+        public string Class { get; set; }
+        [FwLogicProperty(Id: "zLNzBjF9z3KY", IsReadOnly: true)]
+        public string InventoryTypeId { get; set; }
+        [FwLogicProperty(Id: "tsONHDyZlXRD", IsReadOnly: true)]
+        public string InventoryType { get; set; }
+        [FwLogicProperty(Id: "uAOA2Mcry74J", IsReadOnly: true)]
+        public string CategoryId { get; set; }
+        [FwLogicProperty(Id: "FxPTd16ERBcx", IsReadOnly: true)]
+        public string SubCategoryId { get; set; }
+        [FwLogicProperty(Id: "k2KjgP1RHqB2", IsReadOnly: true)]
+        public string UnitId { get; set; }
+        [FwLogicProperty(Id: "9O51vzxfEMUa", IsReadOnly: true)]
+        public string PartNumber { get; set; }
+
+        [FwLogicProperty(Id: "31Vzzc6ITOvz")]
+        public decimal? Cost { get { return rateUpdateItem.Cost; } set { rateUpdateItem.Cost = value; } }
+        [FwLogicProperty(Id: "NirOywh8ChYP")]
+        public decimal? NewCost { get { return rateUpdateItem.NewCost; } set { rateUpdateItem.NewCost = value; } }
+        [FwLogicProperty(Id: "8rmAX5SLW7Lg")]
+        public decimal? DefaultCost { get { return rateUpdateItem.DefaultCost; } set { rateUpdateItem.DefaultCost = value; } }
+        [FwLogicProperty(Id: "4zIBRjTWssFp")]
+        public decimal? NewDefaultCost { get { return rateUpdateItem.NewDefaultCost; } set { rateUpdateItem.NewDefaultCost = value; } }
+        [FwLogicProperty(Id: "w9Kbn3h6vBRR")]
+        public decimal? Price { get { return rateUpdateItem.Price; } set { rateUpdateItem.Price = value; } }
+        [FwLogicProperty(Id: "CrrmBzvJo51C")]
+        public decimal? NewPrice { get { return rateUpdateItem.NewPrice; } set { rateUpdateItem.NewPrice = value; } }
 
         [FwLogicProperty(Id: "MIYfFqE7WSmY")]
         public decimal? HourlyRate { get { return rateUpdateItem.HourlyRate; } set { rateUpdateItem.HourlyRate = value; } }
         [FwLogicProperty(Id: "ns54VM7vN4Yu")]
         public decimal? DailyRate { get { return rateUpdateItem.DailyRate; } set { rateUpdateItem.DailyRate = value; } }
-        [FwLogicProperty(Id: "sFOTCggAzoJO")]
-        public decimal? MonthlyRate { get { return rateUpdateItem.MonthlyRate; } set { rateUpdateItem.MonthlyRate = value; } }
         [FwLogicProperty(Id: "CTTjRFOrSB1k")]
         public decimal? WeeklyRate { get { return rateUpdateItem.WeeklyRate; } set { rateUpdateItem.WeeklyRate = value; } }
-
+        [FwLogicProperty(Id: "sFOTCggAzoJO")]
+        public decimal? MonthlyRate { get { return rateUpdateItem.MonthlyRate; } set { rateUpdateItem.MonthlyRate = value; } }
 
         [FwLogicProperty(Id: "MMLsJrSIUR3I")]
         public decimal? NewHourlyRate { get { return rateUpdateItem.NewHourlyRate; } set { rateUpdateItem.NewHourlyRate = value; } }
@@ -46,7 +86,7 @@ namespace WebApi.Modules.Utilities.RateUpdateItem
         public decimal? Week4Rate { get { return rateUpdateItem.Week4Rate; } set { rateUpdateItem.Week4Rate = value; } }
         [FwLogicProperty(Id: "4N1SfE3i9Nt0")]
         public decimal? Week5Rate { get { return rateUpdateItem.Week5Rate; } set { rateUpdateItem.Week5Rate = value; } }
-       
+
         [FwLogicProperty(Id: "0BitRrJkRiiy")]
         public decimal? NewWeek2Rate { get { return rateUpdateItem.NewWeek2Rate; } set { rateUpdateItem.NewWeek2Rate = value; } }
         [FwLogicProperty(Id: "m2WY6hwU9Iuw")]
@@ -61,6 +101,32 @@ namespace WebApi.Modules.Utilities.RateUpdateItem
         [FwLogicProperty(Id: "1QaP8BYmXob1")]
         public decimal? NewMaxDiscount { get { return rateUpdateItem.NewMaxDiscount; } set { rateUpdateItem.NewMaxDiscount = value; } }
 
+        [FwLogicProperty(Id: "txC2HWJv1cNi")]
+        public decimal? HourlyCost { get { return rateUpdateItem.HourlyCost; } set { rateUpdateItem.HourlyCost = value; } }
+        [FwLogicProperty(Id: "wziTi3DiAXNW")]
+        public decimal? NewHourlyCost { get { return rateUpdateItem.NewHourlyCost; } set { rateUpdateItem.NewHourlyCost = value; } }
+
+        [FwLogicProperty(Id: "OsYoioKqVDTu")]
+        public decimal? DailyCost { get { return rateUpdateItem.DailyCost; } set { rateUpdateItem.DailyCost = value; } }
+        [FwLogicProperty(Id: "ARHS0yEig2Wk")]
+        public decimal? NewDailyCost { get { return rateUpdateItem.NewDailyCost; } set { rateUpdateItem.NewDailyCost = value; } }
+        [FwLogicProperty(Id: "le8WaehConra")]
+        public decimal? WeeklyCost { get { return rateUpdateItem.WeeklyCost; } set { rateUpdateItem.WeeklyCost = value; } }
+        [FwLogicProperty(Id: "PSEFxrBtEW0O")]
+        public decimal? NewWeeklyCost { get { return rateUpdateItem.NewWeeklyCost; } set { rateUpdateItem.NewWeeklyCost = value; } }
+        [FwLogicProperty(Id: "l78Z4ox5OjXg")]
+        public decimal? MonthlyCost { get { return rateUpdateItem.MonthlyCost; } set { rateUpdateItem.MonthlyCost = value; } }
+        [FwLogicProperty(Id: "koSH1gTXrJD4")]
+        public decimal? NewMonthlyCost { get { return rateUpdateItem.NewMonthlyCost; } set { rateUpdateItem.NewMonthlyCost = value; } }
+        [FwLogicProperty(Id: "ufSistFUcYdJ")]
+        public decimal? ManifestValue { get { return rateUpdateItem.ManifestValue; } set { rateUpdateItem.ManifestValue = value; } }
+        [FwLogicProperty(Id: "kRj2DGxZs26O")]
+        public decimal? NewManifestValue { get { return rateUpdateItem.NewManifestValue; } set { rateUpdateItem.NewManifestValue = value; } }
+        [FwLogicProperty(Id: "lVDiGi37UWlm")]
+        public decimal? ReplacementCost { get { return rateUpdateItem.ReplacementCost; } set { rateUpdateItem.ReplacementCost = value; } }
+        [FwLogicProperty(Id: "OLzETL4A0rUz")]
+        public decimal? NewReplacementCost { get { return rateUpdateItem.NewReplacementCost; } set { rateUpdateItem.NewReplacementCost = value; } }
+
         [FwLogicProperty(Id: "PzpSGvAUSAcy")]
         public decimal? Retail { get { return rateUpdateItem.Retail; } set { rateUpdateItem.Retail = value; } }
         [FwLogicProperty(Id: "weBi0FRqPYlF")]
@@ -71,145 +137,9 @@ namespace WebApi.Modules.Utilities.RateUpdateItem
         [FwLogicProperty(Id: "NR05t3p2EaNx")]
         public decimal? NewMinDaysPerWeek { get { return rateUpdateItem.NewMinDaysPerWeek; } set { rateUpdateItem.NewMinDaysPerWeek = value; } }
 
-       
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Price { get { return rateUpdateItem.Price; } set { rateUpdateItem.Price = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Cost { get { return rateUpdateItem.Cost; } set { rateUpdateItem.Cost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public int? Reorderpoint { get { return rateUpdateItem.Reorderpoint; } set { rateUpdateItem.Reorderpoint = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public string Shelfloc { get { return rateUpdateItem.Shelfloc; } set { rateUpdateItem.Shelfloc = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public string Aisleloc { get { return rateUpdateItem.Aisleloc; } set { rateUpdateItem.Aisleloc = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldmonthlyrate { get { return rateUpdateItem.Oldmonthlyrate; } set { rateUpdateItem.Oldmonthlyrate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public int? LastQuantityonhand { get { return rateUpdateItem.LastQuantityonhand; } set { rateUpdateItem.LastQuantityonhand = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Olddailyrate { get { return rateUpdateItem.Olddailyrate; } set { rateUpdateItem.Olddailyrate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldweeklyrate { get { return rateUpdateItem.Oldweeklyrate; } set { rateUpdateItem.Oldweeklyrate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newprice { get { return rateUpdateItem.Newprice; } set { rateUpdateItem.Newprice = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldprice { get { return rateUpdateItem.Oldprice; } set { rateUpdateItem.Oldprice = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Dailycost { get { return rateUpdateItem.Dailycost; } set { rateUpdateItem.Dailycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Monthlycost { get { return rateUpdateItem.Monthlycost; } set { rateUpdateItem.Monthlycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Weeklycost { get { return rateUpdateItem.Weeklycost; } set { rateUpdateItem.Weeklycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newcost { get { return rateUpdateItem.Newcost; } set { rateUpdateItem.Newcost = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldretail { get { return rateUpdateItem.Oldretail; } set { rateUpdateItem.Oldretail = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldcost { get { return rateUpdateItem.Oldcost; } set { rateUpdateItem.Oldcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Defaultcost { get { return rateUpdateItem.Defaultcost; } set { rateUpdateItem.Defaultcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newdefaultcost { get { return rateUpdateItem.Newdefaultcost; } set { rateUpdateItem.Newdefaultcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Olddefaultcost { get { return rateUpdateItem.Olddefaultcost; } set { rateUpdateItem.Olddefaultcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public string ModbyusersId { get { return rateUpdateItem.ModbyusersId; } set { rateUpdateItem.ModbyusersId = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public int? ReorderQuantity { get { return rateUpdateItem.ReorderQuantity; } set { rateUpdateItem.ReorderQuantity = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public bool? Qcrequired { get { return rateUpdateItem.Qcrequired; } set { rateUpdateItem.Qcrequired = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Hourlycost { get { return rateUpdateItem.Hourlycost; } set { rateUpdateItem.Hourlycost = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldweek2rate { get { return rateUpdateItem.Oldweek2rate; } set { rateUpdateItem.Oldweek2rate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldweek3rate { get { return rateUpdateItem.Oldweek3rate; } set { rateUpdateItem.Oldweek3rate = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public bool? Availbyhour { get { return rateUpdateItem.Availbyhour; } set { rateUpdateItem.Availbyhour = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public bool? Availbydeal { get { return rateUpdateItem.Availbydeal; } set { rateUpdateItem.Availbydeal = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldhourlyrate { get { return rateUpdateItem.Oldhourlyrate; } set { rateUpdateItem.Oldhourlyrate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldhourlycost { get { return rateUpdateItem.Oldhourlycost; } set { rateUpdateItem.Oldhourlycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newhourlycost { get { return rateUpdateItem.Newhourlycost; } set { rateUpdateItem.Newhourlycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Olddailycost { get { return rateUpdateItem.Olddailycost; } set { rateUpdateItem.Olddailycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newdailycost { get { return rateUpdateItem.Newdailycost; } set { rateUpdateItem.Newdailycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldweeklycost { get { return rateUpdateItem.Oldweeklycost; } set { rateUpdateItem.Oldweeklycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newweeklycost { get { return rateUpdateItem.Newweeklycost; } set { rateUpdateItem.Newweeklycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldmonthlycost { get { return rateUpdateItem.Oldmonthlycost; } set { rateUpdateItem.Oldmonthlycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newmonthlycost { get { return rateUpdateItem.Newmonthlycost; } set { rateUpdateItem.Newmonthlycost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldmaxdiscount { get { return rateUpdateItem.Oldmaxdiscount; } set { rateUpdateItem.Oldmaxdiscount = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldweek4rate { get { return rateUpdateItem.Oldweek4rate; } set { rateUpdateItem.Oldweek4rate = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldweek5rate { get { return rateUpdateItem.Oldweek5rate; } set { rateUpdateItem.Oldweek5rate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public bool? Availbyasset { get { return rateUpdateItem.Availbyasset; } set { rateUpdateItem.Availbyasset = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public string Starttime { get { return rateUpdateItem.Starttime; } set { rateUpdateItem.Starttime = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public string Stoptime { get { return rateUpdateItem.Stoptime; } set { rateUpdateItem.Stoptime = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public int? Availqcdelay { get { return rateUpdateItem.Availqcdelay; } set { rateUpdateItem.Availqcdelay = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public bool? Allowallusersaddtoorder { get { return rateUpdateItem.Allowallusersaddtoorder; } set { rateUpdateItem.Allowallusersaddtoorder = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public string DefaultpurchasecurrencyId { get { return rateUpdateItem.DefaultpurchasecurrencyId; } set { rateUpdateItem.DefaultpurchasecurrencyId = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public bool? Hastieredcost { get { return rateUpdateItem.Hastieredcost; } set { rateUpdateItem.Hastieredcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Manifestvalue { get { return rateUpdateItem.Manifestvalue; } set { rateUpdateItem.Manifestvalue = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newmanifestvalue { get { return rateUpdateItem.Newmanifestvalue; } set { rateUpdateItem.Newmanifestvalue = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Newreplacementcost { get { return rateUpdateItem.Newreplacementcost; } set { rateUpdateItem.Newreplacementcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldmanifestvalue { get { return rateUpdateItem.Oldmanifestvalue; } set { rateUpdateItem.Oldmanifestvalue = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldreplacementcost { get { return rateUpdateItem.Oldreplacementcost; } set { rateUpdateItem.Oldreplacementcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Replacementcost { get { return rateUpdateItem.Replacementcost; } set { rateUpdateItem.Replacementcost = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Restockfee { get { return rateUpdateItem.Restockfee; } set { rateUpdateItem.Restockfee = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Restockpercent { get { return rateUpdateItem.Restockpercent; } set { rateUpdateItem.Restockpercent = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Maxdw { get { return rateUpdateItem.Maxdw; } set { rateUpdateItem.Maxdw = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Seriesrate { get { return rateUpdateItem.Seriesrate; } set { rateUpdateItem.Seriesrate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Year2leaserate { get { return rateUpdateItem.Year2leaserate; } set { rateUpdateItem.Year2leaserate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Year3leaserate { get { return rateUpdateItem.Year3leaserate; } set { rateUpdateItem.Year3leaserate = value; } }
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Year4leaserate { get { return rateUpdateItem.Year4leaserate; } set { rateUpdateItem.Year4leaserate = value; } }
-
-        //[FwLogicProperty(Id: "")]
-        //public decimal? Oldmindw { get { return rateUpdateItem.Oldmindw; } set { rateUpdateItem.Oldmindw = value; } }
-     
-        //[FwLogicProperty(Id: "")]
-        //public string PhysicalId { get { return rateUpdateItem.PhysicalId; } set { rateUpdateItem.PhysicalId = value; } }
         [FwLogicProperty(Id: "DlEo0K6orV5N")]
         public string DateStamp { get { return rateUpdateItem.DateStamp; } set { rateUpdateItem.DateStamp = value; } }
+
         //------------------------------------------------------------------------------------ 
         //protected override bool Validate(TDataRecordSaveMode saveMode, FwBusinessLogic original, ref string validateMsg) 
         //{ 
