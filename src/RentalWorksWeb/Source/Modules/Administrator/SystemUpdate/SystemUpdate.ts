2ed144658f4cd60db2f12f15f1901edb0b020788
Joshua Pace <jpace@4wall.com>
2020-04-09 12:58:46 -0700

    add update method

--- src/RentalWorksWeb/Source/Modules/Administrator/SystemUpdate/SystemUpdate.ts
+++ src/RentalWorksWeb/Source/Modules/Administrator/SystemUpdate/SystemUpdate.ts
@@ -37,7 +37,7 @@ class SystemUpdate {
         //disables asterisk and save prompt
         $form.off('change keyup', '.fwformfield[data-enabled="true"]:not([data-isuniqueid="true"][data-datafield=""])');
 
-        FwFormField.loadItems($form.find('div[data-datafield="UpdateVersion"]'), [
+        FwFormField.loadItems($form.find('div[data-datafield="ToVersion"]'), [
             { value: '2019.1.2.94', text: '2019.1.2.94' },
             { value: '2019.1.2.95', text: '2019.1.2.95' },
             { value: '2019.1.2.96', text: '2019.1.2.96' },
@@ -50,7 +50,21 @@ class SystemUpdate {
     };
     //----------------------------------------------------------------------------------------------
     events($form) {
-
+        $form.find('div[data-type="button"].update-now').on('click', e => {
+            const toVersion = FwFormField.getValueByDataField($form, 'ToVersion');
+            if (toVersion !== '') {
+                const request: any = {
+                    ToVersion: toVersion,
+                };
+                FwAppData.apiMethod(true, 'POST', `api/v1/update/applyupdate`, request, FwServices.defaultTimeout, response => {
+                   
+                }, function onError(response) {
+                    FwFunc.showError(response);
+                }, null);
+            } else {
+                FwNotification.renderNotification('WARNING', 'Select a version in order to update RentalWorks.')
+            }
+        });
     }
     //----------------------------------------------------------------------------------------------
     afterLoad($form) {
@@ -66,12 +80,17 @@ class SystemUpdate {
             <div id="systemupdateform" class="fwcontrol fwcontainer fwform" data-control="FwContainer" data-type="form" data-version="1" data-caption="System Update" data-rendermode="template" data-tablename="" data-mode="" data-hasaudit="false" data-controller="SystemUpdateController">
               <div id="systemupdateform-tabcontrol" class="fwcontrol fwtabs" data-control="FwTabs" data-type="">
                 <div class="tabpages">
-                  <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="System Update" style="max-width:200px">
-                    <div class="flexrow">
-                        <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Current Version" data-datafield="CurrentVersion" data-enabled="false" style="flex: 0 1 200px;">
-                    </div>
-                    <div class="flexrow">
-                        <div data-control="FwFormField" data-type="select" class="fwcontrol fwformfield" data-caption="Update to Version" data-datafield="UpdateVersion" style="flex: 0 1 200px;">
+                  <div class="flexrow">
+                    <div class="flexcolumn" style="max-width:135px;">
+                      <div class="flexrow">
+                        <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Current Version" data-datafield="CurrentVersion" data-enabled="false" style="flex: 0 1 135px;">
+                      </div>
+                      <div class="flexrow">
+                        <div data-control="FwFormField" data-type="select" class="fwcontrol fwformfield" data-caption="Update to Version" data-datafield="ToVersion" style="flex: 0 1 135px;">
+                      </div>
+                      <div class="flexrow">
+                        <div class="update-now fwformcontrol" data-type="button" style="float:right;margin-top:15px;">Update Now</div>
+                      </div>
                     </div>
                   </div>
                 </div>
