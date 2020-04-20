d1637b58107958f6e24c49087e05eb6bf4ea5cd1
Joshua Pace <jpace@4wall.com>
2020-04-20 13:50:13 -0700

    Adds beforeValidate filters for InventoryVendorGrid Vendor validation #2348

--- src/RentalWorksWeb/Source/Grids/InventoryVendorGrid/InventoryVendorGridController.ts
+++ src/RentalWorksWeb/Source/Grids/InventoryVendorGrid/InventoryVendorGridController.ts
@@ -5,6 +5,15 @@
     beforeValidate(datafield: string, request: any, $validationbrowse: JQuery, $form: JQuery, $tr: JQuery) {
         switch (datafield) {
             case 'VendorId':
+                if ($form.attr('data-controller') === 'RentalInventoryController') {
+                    request.uniqueids = {
+                        SubRent: true,
+                    };
+                } else if ($form.attr('data-controller') === 'SalesInventoryController') {
+                    request.uniqueids = {
+                        SubSalet: true,
+                    };
+                }
                 $validationbrowse.attr('data-apiurl', `${this.apiurl}/validatevendor`);
                 break;
         }
