ea83196d33363705f7a70df6c57a757796cb9403
Joshua Pace <jpace@4wall.com>
2020-05-15 10:59:17 -0700

    Changing the Tax Option defaults the tax rates #2493

--- src/RentalWorksWeb/Source/Grids/CompanyTaxOptionGrid/CompanyTaxOptionGridController.ts
+++ src/RentalWorksWeb/Source/Grids/CompanyTaxOptionGrid/CompanyTaxOptionGridController.ts
@@ -2,6 +2,16 @@
     Module: string = 'CompanyTaxOptionGrid';
     apiurl: string = 'api/v1/companytaxoption';
 
+    generateRow($control, $generatedtr) {
+        const $form = $control.closest('.fwform');
+
+        $generatedtr.find('div[data-browsedatafield="TaxOptionId"]').data('onchange', $tr => {
+            $generatedtr.find('.field[data-browsedatafield="RentalTaxRate1"]').text($tr.find('.field[data-browsedatafield="RentalTaxRate1"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="SalesTaxRate1"]').text($tr.find('.field[data-browsedatafield="SalesTaxRate1"]').attr('data-originalvalue'));
+            $generatedtr.find('.field[data-browsedatafield="LaborTaxRate1"]').text($tr.find('.field[data-browsedatafield="LaborTaxRate1"]').attr('data-originalvalue'));
+        });
+    }
+
     beforeValidate(datafield: string, request: any, $validationbrowse: JQuery, $gridbrowse: JQuery, $tr: JQuery) {
         switch (datafield) {
             case 'TaxOptionId':
