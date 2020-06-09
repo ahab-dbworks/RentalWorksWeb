bfa5534c5b9744dcae014d09d078d4f94dc19216
Joshua Pace <jpace@4wall.com>
2020-06-09 16:20:44 -0700

    Adds RateType to invoice report #2588

--- src/RentalWorksWebApi/WebpackReports/src/Reports/BillingReports/InvoiceReport/InvoiceReportLoader.cs
+++ src/RentalWorksWebApi/WebpackReports/src/Reports/BillingReports/InvoiceReport/InvoiceReportLoader.cs
@@ -672,7 +672,9 @@ namespace WebApi.Modules.Reports.Billing.InvoiceReport
         //------------------------------------------------------------------------------------
         [FwSqlDataField(column: "orderedbyemail", modeltype: FwDataTypes.Text)]
         public string OrderedByEmail { get; set; }
-
+        //------------------------------------------------------------------------------------
+        [FwSqlDataField(column: "ratetype", modeltype: FwDataTypes.Text)]
+        public string RateType { get; set; }
 
 
         //------------------------------------------------------------------------------------
