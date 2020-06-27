d4f1f0c2f97ed2964e9d414d3d205d9e17ea444c
Joshua Pace <jpace@4wall.com>
2020-06-26 17:02:41 -0700

    fixes download excel template

--- src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwBrowse.ts
+++ src/RentalWorksWeb/Libraries/Fw/scripts/FwControls/FwBrowse.ts
@@ -4260,23 +4260,104 @@ class FwBrowseClass {
             FwAppData.apiMethod(true, 'GET', `${(<any>window[controller]).apiurl}/emptyobject`, null, FwServices.defaultTimeout, function onSuccess(response) {
 
                 const fields = [];
-                for (let key in response) {
-                    if (!key.startsWith('_')) {
-                        fields.push({ key });
-                    }
+                const resFields = response._Fields
+                //for (let key in response._Fields) {
+                //    if (!key.startsWith('_')) {
+                //        console.log('key: ', {key: 'h'})
+                //        fields.push({ key: '' });
+                //    }
+                //}
+
+                const obj = {}
+                for (let i = 0; i < resFields.length; i++) {
+                    const curr = resFields[i];
+                    obj[curr.Name] = "";
                 }
+                fields.push(obj)
                 console.log('fields', JSON.stringify(fields));
 
                 const sortedFields = fields.slice();
                 sortedFields.sort(function (a, b) { return (a.text > b.text) ? 1 : ((b.text > a.text) ? -1 : 0); });
                 console.log('sortedFields', sortedFields);
 
+                //function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+                //    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+                //    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+                //    var CSV = 'sep=,' + '\r\n\n';
+
+                //    //This condition will generate the Label/Header
+                //    if (ShowLabel) {
+                //        var row = "";
+
+                //        //This loop will extract the label from 1st index of on array
+                //        for (var index in arrData[0]) {
+
+                //            //Now convert each value to string and comma-seprated
+                //            row += index + ',';
+                //        }
+
+                //        row = row.slice(0, -1);
+
+                //        //append Label row with line break
+                //        CSV += row + '\r\n';
+                //    }
+
+                //    //1st loop is to extract each row
+                //    for (var i = 0; i < arrData.length; i++) {
+                //        var row = "";
+
+                //        //2nd loop will extract each column and convert it in string comma-seprated
+                //        for (var index in arrData[i]) {
+                //            row += '"' + arrData[i][index] + '",';
+                //        }
+
+                //        row.slice(0, row.length - 1);
+
+                //        //add a line break after each row
+                //        CSV += row + '\r\n';
+                //    }
+
+                //    if (CSV == '') {
+                //        alert("Invalid data");
+                //        return;
+                //    }
+
+                //    //Generate a file name
+                //    var fileName = "MyReport_";
+                //    //this will remove the blank-spaces from the title and replace it with an underscore
+                //    fileName += ReportTitle.replace(/ /g, "_");
+
+                //    //Initialize file format you want csv or xls
+                //    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+                //    // Now the little tricky part.
+                //    // you can use either>> window.open(uri);
+                //    // but this will not work in some browsers
+                //    // or you will not get the correct file extension    
+
+                //    //this trick will generate a temp <a /> tag
+                //    var link = document.createElement("a");
+                //    link.href = uri;
+
+                //    //set the visibility hidden so it will not effect on your web-layout
+                //   // link.style = "visibility:hidden";
+                //    link.download = fileName + ".csv";
+
+                //    //this part will append the anchor tag and remove it after automatic click
+                //    document.body.appendChild(link);
+                //    link.click();
+                //    document.body.removeChild(link);
+                //}
+
+                //JSONToCSVConvertor(fields, "ORDER", true);
+
                 let myExcelXML = (function () {
                     let Workbook, WorkbookStart = '<?xml version="1.0"?><ss:Workbook  xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40">';
                     const WorkbookEnd = '</ss:Workbook>';
                     let fs, SheetName = 'SHEET 1',
                         styleID = 1, columnWidth = 80, uri, link;
-                    let fileName = `${controller.substring(0, controller.length - 10)}Browse-Fields`;
+                    let fileName = `${controller.substring(0, controller.length - 10)}Browse-Template`;
                     class myExcelXML {
                         constructor(o) {
                             let respArray = JSON.parse(o);
@@ -4380,7 +4461,7 @@ class FwBrowseClass {
                         const HeadEnd = '</ss:Row>';
 
                         for (let i = 0; i < h.length; i++) {
-                            const Cell = myXMLCell(h[i].toUpperCase());
+                            const Cell = myXMLCell(h[i]);
                             HeadStart += Cell;
                         }
 
