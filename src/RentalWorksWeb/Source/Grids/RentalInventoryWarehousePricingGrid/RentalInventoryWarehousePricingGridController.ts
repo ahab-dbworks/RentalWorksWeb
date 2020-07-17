cf109d0d9dde7f99e868605baa68e4dd22bd0d28
Jason Hoang <jhoang@4wall.com>
2020-07-17 13:31:02 -0700

    Resolved #2732: Added Replacement Cost calculation to Warehouse Pricing grid

--- src/RentalWorksWeb/Source/Grids/RentalInventoryWarehousePricingGrid/RentalInventoryWarehousePricingGridController.ts
+++ src/RentalWorksWeb/Source/Grids/RentalInventoryWarehousePricingGrid/RentalInventoryWarehousePricingGridController.ts
@@ -1,6 +1,23 @@
 ﻿class RentalInventoryWarehousePricingGrid {
     Module: string = 'InventoryWarehouseGrid';  //justin 04/10/2019 potential issue with shared Module value. See Issue #401
     apiurl: string = 'api/v1/inventorywarehouse';
+
+    generateRow($control, $generatedtr) {
+        $generatedtr.find('div[data-browsedatafield="UnitValue"]').on('change', 'input.value', e => {
+            calculateReplacementCost($control, $generatedtr);
+        });
+
+        const calculateReplacementCost = ($control: any, $tr: any) => {
+            const markupReplacementCost = FwBrowse.getValueByDataField($control, $tr, 'MarkupReplacementCost');
+            if (markupReplacementCost) {
+                const markupPercent = parseFloat(FwBrowse.getValueByDataField($control, $tr, 'ReplacementCostMarkupPercent')) / 100;
+                let unitValue = parseFloat(FwBrowse.getValueByDataField($control, $tr, 'UnitValue'));
+                unitValue += unitValue * markupPercent;
+                FwBrowse.setFieldValue($control, $tr, 'ReplacementCost', { value: unitValue.toString() });
+            }
+        };
+    }
+
 }
 
 var RentalInventoryWarehousePricingGridController = new RentalInventoryWarehousePricingGrid();
