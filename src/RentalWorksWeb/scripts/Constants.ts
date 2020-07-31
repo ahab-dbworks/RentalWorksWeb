1f45a16fd30b55140959ef83f8e50e0385dadbfa
Joshua Pace <jpace@4wall.com>
2020-07-31 16:46:41 -0700

    Adds wip Repair Tag "report" #2781

--- src/RentalWorksWeb/scripts/Constants.ts
+++ src/RentalWorksWeb/scripts/Constants.ts
@@ -282,7 +282,8 @@ var Constants = {
                     nodetype: 'Category',
                     children: {
                         RentalEquipmentRepairHistoryReport: { id: 'bLX4YBftwJvw',  caption: 'Rental Equipment Repair History', nav: 'reports/rentalequipmentrepairhistoryreport', nodetype: 'Module', description: 'List all Rental Inventory that has been In Repair over a specified date range.' },
-                        RepairOrderStatusReport:            { id: 'KFP6Nq17ZuDPO', caption: 'Repair Order Status',             nav: 'reports/repairorderstatusreport',            nodetype: 'Module', description: 'List all Repair Orders, including Damage Notes.  Filter by Status, Type, Priority, Days In Repair, and more.' }
+                        RepairOrderStatusReport: { id: 'KFP6Nq17ZuDPO', caption: 'Repair Order Status', nav: 'reports/repairorderstatusreport', nodetype: 'Module', description: 'List all Repair Orders, including Damage Notes.  Filter by Status, Type, Priority, Days In Repair, and more.' },
+                        RepairTag: { id: 'TNvVB0kI42ngF', caption: 'Repair Tag', nav: 'reports/repairtag', nodetype: 'Module', description: 'List all Repair Tags' },
                     }
                 },
                 SalesInventoryReports: {
