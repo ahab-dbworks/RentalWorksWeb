461e30d01bffb75e8e1030f9d2dbfadbcc18ba35
Justin Hoffman <justin@dbworks.com>
2020-08-07 16:45:50 -0600

    web: 2019.1.2.115

--- src/RentalWorksWeb/Properties/AssemblyInfo.cs
+++ src/RentalWorksWeb/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2019.1.2.114")]
+[assembly: AssemblyVersion("2019.1.2.115")]
