651146c257485b70fce969f3f597502c45c4c2b1
Mike Vermilion <mike@dbworks.com>
2020-08-13 08:01:09 -0700

    #2846 Adds DefaultApplicationConfig.js to tracking.

---
+++ src/RentalWorksWebApi/QuikScan/scripts/DefaultApplicationConfig.js
@@ -0,0 +1,26 @@
+﻿var applicationConfig = {
+    appbaseurl: '',
+    fwvirtualdirectory: '',
+    appvirtualdirectory: '',
+    debugMode: false,
+    designMode: false,
+    demoMode: false,
+    devMode: false,
+    demoEmail: 'qsdemo@dbworks.com',
+    demoPassword: 'QSDEMO',
+    ajaxTimeoutSeconds: 20,
+    version: '',
+    apiurl: '/',
+    photoWidth: 1024,
+    photoHeight: 1024,
+    photoQuality: 100,
+    iPodPhotoQuality: 100,
+    iPhonePhotoQuality: 100,
+    defaultPeek: false,
+    quikIn: {
+        enableQuikInSessionSearch: false,
+        enableSessionInItemSearch: true,
+        enableCancelItem: true
+    },
+    allowDisableBarcodeFieldInStaging: true
+};
