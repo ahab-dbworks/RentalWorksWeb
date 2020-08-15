0f256b909d2aa93eaa8c0c134ece347e4ef74171
Justin Hoffman <justin@dbworks.com>
2020-08-14 19:47:27 -0600

    Added Currency to Inventory Warehouse Pricing grids #2859

--- src/RentalWorksWeb/Source/Grids/SalesInventoryWarehousePricingGrid/SalesInventoryWarehousePricingGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SalesInventoryWarehousePricingGrid/SalesInventoryWarehousePricingGridController.ts
@@ -1,6 +1,7 @@
 ﻿class SalesInventoryWarehousePricingGrid {
     Module: string = 'InventoryWarehouseGrid';  //justin 04/10/2019 potential issue with shared Module value. See Issue #401
-    apiurl: string = 'api/v1/inventorywarehouse';
+    //apiurl: string = 'api/v1/inventorywarehouse';
+    apiurl: string = 'api/v1/pricing';
 }
 
 var SalesInventoryWarehousePricingGridController = new SalesInventoryWarehousePricingGrid();
