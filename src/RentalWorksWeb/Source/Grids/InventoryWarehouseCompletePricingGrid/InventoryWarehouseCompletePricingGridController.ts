01240e3c9612d5f7af5eb75cbed9bd056146928b
Justin Hoffman <justin@dbworks.com>
2020-08-14 20:31:51 -0600

    Currency on Complete/Kit grids #2859

--- src/RentalWorksWeb/Source/Grids/InventoryWarehouseCompletePricingGrid/InventoryWarehouseCompletePricingGridController.ts
+++ src/RentalWorksWeb/Source/Grids/InventoryWarehouseCompletePricingGrid/InventoryWarehouseCompletePricingGridController.ts
@@ -1,6 +1,7 @@
 ﻿class InventoryWarehouseCompletePricingGrid {
     Module: string = 'InventoryWarehouseGrid';  //justin 04/10/2019 potential issue with shared Module value. See Issue #401
-    apiurl: string = 'api/v1/inventorywarehouse';
+    //apiurl: string = 'api/v1/inventorywarehouse';
+    apiurl: string = 'api/v1/pricing';
 }
 
 var InventoryWarehouseCompletePricingGridController = new InventoryWarehouseCompletePricingGrid();
