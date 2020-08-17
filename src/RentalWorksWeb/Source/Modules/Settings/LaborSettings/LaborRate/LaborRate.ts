e6ec01bbb418fc834956cf4a49313733609b7aa8
Joshua Pace <jpace@4wall.com>
2020-08-17 14:37:26 -0700

    Adds currency view menu options to grids in labor and misc rate forms #2890

--- src/RentalWorksWeb/Source/Modules/Settings/LaborSettings/LaborRate/LaborRate.ts
+++ src/RentalWorksWeb/Source/Modules/Settings/LaborSettings/LaborRate/LaborRate.ts
@@ -181,6 +181,29 @@ class RwLaborRate {
             addGridMenu: (options: IAddGridMenuOptions) => {
                 options.hasNew = false;
                 options.hasDelete = false;
+                const $viewcolumn = FwMenu.addSubMenuColumn(options.$menu);
+                const $viewgroup = FwMenu.addSubMenuGroup($viewcolumn, 'View', 'securityid1');
+                FwMenu.addSubMenuItem($viewgroup, 'View Rates in local Currencies', '', (e: JQuery.ClickEvent) => {
+                    try {
+                        this.currencyViewForPricingGrids(e, 'local');
+                    } catch (ex) {
+                        FwFunc.showError(ex);
+                    }
+                });
+                FwMenu.addSubMenuItem($viewgroup, 'View Rates in a specific Currency', '', (e: JQuery.ClickEvent) => {
+                    try {
+                        this.currencyViewForPricingGrids(e, 'specific');
+                    } catch (ex) {
+                        FwFunc.showError(ex);
+                    }
+                });
+                FwMenu.addSubMenuItem($viewgroup, 'View Rates in All Currencies', '', (e: JQuery.ClickEvent) => {
+                    try {
+                        this.currencyViewForPricingGrids(e, 'all');
+                    } catch (ex) {
+                        FwFunc.showError(ex);
+                    }
+                });
             },
             onDataBind: (request: any) => {
                 request.uniqueids = {
