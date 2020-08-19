c09a1db429c97037c0ed6a2d7885f98d325b6d8f
Mike Vermilion <mike@dbworks.com>
2020-08-19 10:50:11 -0700

    Fixes an issue with DefaultApplicationConfig

--- src/RentalWorksWeb/DefaultApplicationConfig.ts
+++
@@ -1,7 +0,0 @@
-﻿class ApplicationConfig extends FwApplicationConfig {
-    customLogin: boolean = false;
-    client: string = '';
-    defaultPeek: boolean = true;
-    allCaps: boolean = false;
-}
-var applicationConfig = new ApplicationConfig();
