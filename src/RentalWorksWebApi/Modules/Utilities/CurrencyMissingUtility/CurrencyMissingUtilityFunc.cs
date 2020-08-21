f70bc25d5d1e727b0838d8f11c72899f2d91542b
Justin Hoffman <justin@dbworks.com>
2020-08-21 14:47:42 -0600

    Added stored procedure call for ApplyProposedCurrencies #2905

--- src/RentalWorksWebApi/Modules/Utilities/CurrencyMissingUtility/CurrencyMissingUtilityFunc.cs
+++ src/RentalWorksWebApi/Modules/Utilities/CurrencyMissingUtility/CurrencyMissingUtilityFunc.cs
@@ -1,5 +1,6 @@
 ﻿using FwStandard.Models;
 using FwStandard.SqlServer;
+using System.Data;
 using System.Threading.Tasks;
 using WebApi.Logic;
 
@@ -17,10 +18,13 @@ namespace WebApi.Modules.Utilities.CurrencyMissingUtility
             ApplyProposedCurrenciesResponse response = new ApplyProposedCurrenciesResponse();
             using (FwSqlConnection conn = new FwSqlConnection(appConfig.DatabaseSettings.ConnectionString))
             {
-                FwSqlCommand qry = new FwSqlCommand(conn, "xx", appConfig.DatabaseSettings.QueryTimeout);
-                //qry.AddParameter("@code", SqlDbType.NVarChar, ParameterDirection.Input, request.Code);
+                FwSqlCommand qry = new FwSqlCommand(conn, "setmissingcurrency", appConfig.DatabaseSettings.QueryTimeout);
+                qry.AddParameter("@status", SqlDbType.Int, ParameterDirection.Output);
+                qry.AddParameter("@msg", SqlDbType.NVarChar, ParameterDirection.Output);
                 await qry.ExecuteNonQueryAsync();
-                response.success = true;
+                response.status = qry.GetParameter("@status").ToInt32();
+                response.success = (response.status == 0);
+                response.msg = qry.GetParameter("@msg").ToString();
             }
             return response;
         }
