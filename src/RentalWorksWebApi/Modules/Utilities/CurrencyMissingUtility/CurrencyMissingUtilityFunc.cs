85cc17f800a65ef8008d7d14bef9944b083afa15
Justin Hoffman <justin@dbworks.com>
2020-08-28 16:43:25 -0600

    fixed api currency provisioning utility #2905

--- src/RentalWorksWebApi/Modules/Utilities/CurrencyMissingUtility/CurrencyMissingUtilityFunc.cs
+++ src/RentalWorksWebApi/Modules/Utilities/CurrencyMissingUtility/CurrencyMissingUtilityFunc.cs
@@ -1,6 +1,7 @@
 ﻿using FwStandard.Models;
 using FwStandard.SqlServer;
 using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using WebApi.Logic;
 
@@ -15,16 +16,24 @@ namespace WebApi.Modules.Utilities.CurrencyMissingUtility
         //-------------------------------------------------------------------------------------------------------
         public static async Task<ApplyProposedCurrenciesResponse> ApplyProposedCurrencies(FwApplicationConfig appConfig, FwUserSession userSession, ApplyProposedCurrenciesRequest request)
         {
+
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder(appConfig.DatabaseSettings.ConnectionString);
+            string dbworksConnectionString = "Server=" + connectionStringBuilder.DataSource + ";Database=" + connectionStringBuilder.InitialCatalog + ";User Id=dbworks;Password=db2424;";  // user/pass hard-coded for now
+
             ApplyProposedCurrenciesResponse response = new ApplyProposedCurrenciesResponse();
-            using (FwSqlConnection conn = new FwSqlConnection(appConfig.DatabaseSettings.ConnectionString))
+            using (SqlConnection conn = new SqlConnection(dbworksConnectionString))
             {
-                FwSqlCommand qry = new FwSqlCommand(conn, "setmissingcurrency", appConfig.DatabaseSettings.QueryTimeout);
-                qry.AddParameter("@status", SqlDbType.Int, ParameterDirection.Output);
-                qry.AddParameter("@msg", SqlDbType.NVarChar, ParameterDirection.Output);
+                SqlCommand qry = new SqlCommand("setmissingcurrency", conn);
+                SqlParameter p1 = new SqlParameter("@status", SqlDbType.Int, 10, ParameterDirection.Output, true, 1, 1, "", DataRowVersion.Current, null);
+                qry.Parameters.Add(p1);
+                SqlParameter p2 = new SqlParameter("@msg", SqlDbType.NVarChar, 255, ParameterDirection.Output, true, 1, 1, "", DataRowVersion.Current, null);
+                qry.Parameters.Add(p2);
+                conn.Open();
                 await qry.ExecuteNonQueryAsync();
-                response.status = qry.GetParameter("@status").ToInt32();
+                conn.Close();
+                response.status = FwConvert.ToInt32(qry.Parameters[0].Value);
                 response.success = (response.status == 0);
-                response.msg = qry.GetParameter("@msg").ToString();
+                response.msg = qry.Parameters[1].ToString();
             }
             return response;
         }
