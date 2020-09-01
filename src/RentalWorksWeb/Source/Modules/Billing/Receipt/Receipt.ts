c561ff54593f069ce776ce3e1c5afe80620d213c
Joshua Pace <jpace@4wall.com>
2020-09-01 13:04:48 -0700

    adds controller currencysymbol to receipts #2884

--- src/RentalWorksWeb/Source/Modules/Billing/Receipt/Receipt.ts
+++ src/RentalWorksWeb/Source/Modules/Billing/Receipt/Receipt.ts
@@ -121,7 +121,6 @@ class Receipt {
                 const paymentTypeType = FwFormField.getValueByDataField($form, 'PaymentTypeType');
                 if (paymentTypeType !== '' && paymentTypeType === 'REFUND CHECK') {
                     this.loadReceiptCreditGrid($form);
-
                 } else {
                     this.loadReceiptInvoiceGrid($form);
                 }
@@ -355,6 +354,10 @@ class Receipt {
         });
         // ----------
         $form.find('div[data-datafield="CurrencyId"]').data('onchange', $tr => {
+            const currencySymbol = $tr.find('.field[data-formdatafield="CurrencySymbol"]').attr('data-originalvalue');
+            if (currencySymbol) {
+                this.currencySymbol = currencySymbol;
+            }
             this.paymentTypes($form);
         });
         // ----------
@@ -632,7 +635,6 @@ class Receipt {
         if ($form.attr('data-mode') === 'NEW') {
             $form.find('.table-rows').html('<tr class="empty-row" style="height:33px;"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
         }
-        let currencySymbol;
         const calculateInvoiceTotals = ($form, event?) => {
             let amountValBefore;
             if (event != undefined) {
@@ -793,12 +795,6 @@ class Receipt {
                 const htmlRows: Array<string> = [];
                 if (rows.length) {
                     for (let i = 0; i < rows.length; i++) {
-                        currencySymbol = rows[i][res.ColumnIndex.CurrencySymbol] || '';
-                        if (currencySymbol !== '') {
-                            if (!this.currencySymbol) {
-                                this.currencySymbol = currencySymbol;
-                            }
-                        }
                         htmlRows.push(`<tr class="row"><td data-validationname="Deal" data-datafield="${rows[i][res.ColumnIndex.DealId]}" data-displayfield="${rows[i][res.ColumnIndex.Deal]}" class="text">${rows[i][res.ColumnIndex.Deal]}<i class="material-icons btnpeek">more_horiz</i></td><td class="text InvoiceId" style="display:none;">${rows[i][res.ColumnIndex.InvoiceId]}</td><td class="text InvoiceReceiptId" style="display:none;">${rows[i][res.ColumnIndex.InvoiceReceiptId]}</td><td data-validationname="Invoice" data-datafield="${rows[i][res.ColumnIndex.InvoiceId]}" data-displayfield="${rows[i][res.ColumnIndex.InvoiceNumber]}" class="text">${rows[i][res.ColumnIndex.InvoiceNumber]}<i class="material-icons btnpeek">more_horiz</i></td><td class="text">${rows[i][res.ColumnIndex.InvoiceDate]}</td><td data-validationname="Order" data-datafield="${rows[i][res.ColumnIndex.OrderId]}" data-displayfield="${rows[i][res.ColumnIndex.Description]}" class="text">${rows[i][res.ColumnIndex.OrderNumber]}<i class="material-icons btnpeek">more_horiz</i></td><td class="text">${rows[i][res.ColumnIndex.Description]}</td><td style="text-align:right;" data-invoicefield="InvoiceTotal" class="decimal static-amount">${rows[i][res.ColumnIndex.Total]}</td><td style="text-align:right;" data-invoicefield="InvoiceApplied" class="decimal static-amount">${rows[i][res.ColumnIndex.Applied]}</td><td style="text-align:right;" data-invoicefield="InvoiceDue" class="decimal static-amount">${rows[i][res.ColumnIndex.Due]}</td><td data-enabled="true" data-isuniqueid="false" data-datafield="InvoiceAmount" data-invoicefield="InvoiceAmount" class="decimal fwformfield pay-amount invoice-amount"><input class="decimal fwformfield fwformfield-value" style="font-size:inherit;" type="text" autocapitalize="none" row-index="${i}" value="${rows[i][res.ColumnIndex.Amount]}"></td><td><div class="fwformcontrol apply-btn" row-index="${i}" data-type="button" style="height:27px;padding:.3rem;line-height:13px;font-size:14px;">Apply All</div></td></tr>`);
                     }
                     $form.find('.table-rows').html('');
@@ -908,7 +904,6 @@ class Receipt {
             $form.find('.credit-table-rows').html('<tr class="credit-empty-row" style="height:33px;"><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
             isNew = true;
         }
-        let currencySymbol;
         const calculateInvoiceCreditTotals = ($form, event?) => {
             let amountValBefore;
             if (event != undefined) {
@@ -1051,12 +1046,6 @@ class Receipt {
                 const htmlRows: Array<string> = [];
                 if (rows.length) {
                     for (let i = 0; i < rows.length; i++) {
-                        currencySymbol = rows[i][res.ColumnIndex.CurrencySymbol] || '€';
-                        if (currencySymbol !== '') {
-                            if (!this.currencySymbol) {
-                                this.currencySymbol = currencySymbol;
-                            }
-                        }
                         let buttonPeek;
                         const isWebAdmin = JSON.parse(sessionStorage.getItem('userid')).webadministrator;
 
