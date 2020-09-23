4620fd99987c400a807d4dbdcb3f0696acc3ef8e
Justin Hoffman <justin@dbworks.com>
2020-09-23 15:27:50 -0600

    Resolved #3064: Added lots more fields for the Vendor Invoice Export

--- src/RentalWorksWebApi/Exports/VendorInvoiceBatchExport/VendorInvoiceBatchExportLoader.cs
+++ src/RentalWorksWebApi/Exports/VendorInvoiceBatchExport/VendorInvoiceBatchExportLoader.cs
@@ -18,26 +18,42 @@ namespace WebApi.Modules.Exports.VendorInvoiceBatchExport
 
         public class VendorInvoiceItem
         {
+            public string OrderNumber { get; set; }
+            public string OrderDescription { get; set; }
             public string ICode { get; set; }
             public string Description { get; set; }
+            public string DescriptionWithoutDoubleQuotes { get; set; }
             public decimal? Quantity { get; set; }
             public decimal? QuantityNegative { get; set; }
-            public decimal? Rate { get; set; }
-            public decimal? RateNegative { get; set; }
+            public decimal? UnitCost { get; set; }
+            public decimal? UnitCostNegative { get; set; }
             public decimal? Extended { get; set; }
             public decimal? ExtendedNegative { get; set; }
             public bool Taxable { get; set; }
             public string TaxableYesNo { get; set; }
-            public string ExpenseAccountNumber { get; set; }
-            public string ExpenseAccountDescription { get; set; }
+            public string TaxableYN { get; set; }
+            public string AccountNumber { get; set; }
+            public string AccountDescription { get; set; }
         }
 
         public class BatchInvoice
         {
             public string VendorInvoiceId { get; set; }
             public string InvoiceNumber { get; set; }
+            public string InvoiceDate { get; set; }
+            public string InvoiceDueDate { get; set; }
             public string Vendor { get; set; }
             public string VendorNumber { get; set; }
+            public string PurchaseOrderNumber { get; set; }
+            public string PurchaseOrderDescription { get; set; }
+            public decimal? InvoiceSubTotal { get; set; }
+            public decimal? InvoiceSubTotalNegative { get; set; }
+            public decimal? InvoiceTax { get; set; }
+            public decimal? InvoiceTaxNegative { get; set; }
+            public decimal? InvoiceTotal { get; set; }
+            public decimal? InvoiceTotalNegative { get; set; }
+            public string PaymentTerms { get; set; }
+            public string PaymentTermsCode { get; set; }
             public List<VendorInvoiceItem> Items = new List<VendorInvoiceItem>(new VendorInvoiceItem[] { new VendorInvoiceItem() });
         }
 
@@ -82,25 +98,20 @@ namespace WebApi.Modules.Exports.VendorInvoiceBatchExport
                     BatchInvoice i = new BatchInvoice();
                     i.VendorInvoiceId = row[dt.GetColumnNo("vendorinvoiceid")].ToString();
                     i.InvoiceNumber = row[dt.GetColumnNo("invno")].ToString();
-                    //i.InvoiceDate = FwConvert.ToUSShortDate(row[dt.GetColumnNo("invoicedate")].ToString());   // US specific #jhtodo: add format for user
-                    //i.InvoiceDueDate = FwConvert.ToUSShortDate(row[dt.GetColumnNo("invoiceduedate")].ToString());   // US specific #jhtodo: add format for user
-                    //i.InvoiceDescription = row[dt.GetColumnNo("invoicedesc")].ToString();
-                    //i.InvoiceType = row[dt.GetColumnNo("invoicetype")].ToString();
-                    //i.InvoiceTypeForQuickBooks = row[dt.GetColumnNo("invoicetypeqbo")].ToString();
-                    //i.InvoiceClass = row[dt.GetColumnNo("invoiceclass")].ToString();
-                    //i.Customer = row[dt.GetColumnNo("customer")].ToString();
-                    //i.CustomerNumber = row[dt.GetColumnNo("customernumber")].ToString();
+                    i.InvoiceDate = FwConvert.ToUSShortDate(row[dt.GetColumnNo("invoicedate")].ToString());   // US specific #jhtodo: add format for user
+                    i.InvoiceDueDate = FwConvert.ToUSShortDate(row[dt.GetColumnNo("invoiceduedate")].ToString());   // US specific #jhtodo: add format for user
+                    i.PurchaseOrderNumber= row[dt.GetColumnNo("pono")].ToString();
+                    i.PurchaseOrderDescription= row[dt.GetColumnNo("podesc")].ToString();
                     i.Vendor = row[dt.GetColumnNo("vendor")].ToString();
                     i.VendorNumber = row[dt.GetColumnNo("vendornumber")].ToString();
-                    //i.InvoiceSubTotal = FwConvert.ToDecimal(row[dt.GetColumnNo("invoicesubtotal")].ToString());
-                    //i.InvoiceSubTotalNegative = (-1) * i.InvoiceSubTotal;
-                    //i.InvoiceTax = FwConvert.ToDecimal(row[dt.GetColumnNo("invoicetax")].ToString());
-                    //i.InvoiceTaxNegative = (-1) * i.InvoiceTax;
-                    //i.InvoiceTotal = FwConvert.ToDecimal(row[dt.GetColumnNo("invoicetotal")].ToString());
-                    //i.InvoiceTotalNegative = (-1) * i.InvoiceTotal;
-                    //
-                    //i.PaymentTerms = row[dt.GetColumnNo("payterms")].ToString();
-                    //i.PaymentTermsCode = row[dt.GetColumnNo("paytermscode")].ToString();
+                    i.InvoiceSubTotal = FwConvert.ToDecimal(row[dt.GetColumnNo("invoicesubtotal")].ToString());
+                    i.InvoiceSubTotalNegative = (-1) * i.InvoiceSubTotal;
+                    i.InvoiceTax = FwConvert.ToDecimal(row[dt.GetColumnNo("invoicetax")].ToString());
+                    i.InvoiceTaxNegative = (-1) * i.InvoiceTax;
+                    i.InvoiceTotal = FwConvert.ToDecimal(row[dt.GetColumnNo("invoicetotal")].ToString());
+                    i.InvoiceTotalNegative = (-1) * i.InvoiceTotal;
+                    i.PaymentTerms = row[dt.GetColumnNo("payterms")].ToString();
+                    i.PaymentTermsCode = row[dt.GetColumnNo("paytermscode")].ToString();
                     //i.AccountsReceivableAccountNumber = row[dt.GetColumnNo("arglno")].ToString();
                     //i.AccountsReceivableAccountDescription = row[dt.GetColumnNo("argldesc")].ToString();
                     //i.PurchaseOrderNumber = row[dt.GetColumnNo("pono")].ToString();
@@ -113,6 +124,46 @@ namespace WebApi.Modules.Exports.VendorInvoiceBatchExport
                     //i.BillToCountry = row[dt.GetColumnNo("billcountry")].ToString();
 
                     VendorInvoices.Add(i);
+                }
+            }
+
+            foreach (BatchInvoice i in VendorInvoices)
+            {
+                if (!string.IsNullOrEmpty(i.VendorInvoiceId))
+                {
+                    i.Items.Clear();
+                    using (FwSqlConnection conn = new FwSqlConnection(AppConfig.DatabaseSettings.ConnectionString))
+                    {
+                        FwSqlCommand qry = new FwSqlCommand(conn, AppConfig.DatabaseSettings.QueryTimeout);
+                        qry.Add("select *                                                                            ");
+                        qry.Add(" from  vendorinvoiceitemexportview vii                                              ");
+                        qry.Add(" where vii.vendorinvoiceid = @vendorinvoiceid                                        ");
+                        qry.Add("order by vii.itemorder                                                              ");
+                        qry.AddParameter("@vendorinvoiceid", i.VendorInvoiceId);
+                        FwJsonDataTable dt = await qry.QueryToFwJsonTableAsync();
+
+                        foreach (List<object> row in dt.Rows)
+                        {
+                            VendorInvoiceItem ii = new VendorInvoiceItem();
+                            ii.OrderNumber= row[dt.GetColumnNo("poorderno")].ToString();
+                            ii.OrderDescription= row[dt.GetColumnNo("poorderdesc")].ToString();
+                            ii.ICode = row[dt.GetColumnNo("masterno")].ToString();
+                            ii.Description = row[dt.GetColumnNo("description")].ToString();
+                            ii.DescriptionWithoutDoubleQuotes = ii.Description.Replace("\"", "");
+                            ii.Quantity = FwConvert.ToDecimal(row[dt.GetColumnNo("qty")].ToString());
+                            ii.QuantityNegative = (-1) * ii.Quantity;
+                            ii.UnitCost = FwConvert.ToDecimal(row[dt.GetColumnNo("cost")].ToString());
+                            ii.UnitCostNegative = (-1) * ii.UnitCost;
+                            ii.Extended = FwConvert.ToDecimal(row[dt.GetColumnNo("extended")].ToString());
+                            ii.ExtendedNegative = (-1) * ii.Extended;
+                            ii.Taxable = FwConvert.ToBoolean(row[dt.GetColumnNo("taxable")].ToString());
+                            ii.TaxableYesNo = ii.Taxable ? "Yes" : "No";
+                            ii.TaxableYN = ii.Taxable ? "Y" : "N";
+                            ii.AccountNumber = row[dt.GetColumnNo("glno")].ToString();
+                            ii.AccountDescription = row[dt.GetColumnNo("glacctdesc")].ToString();
+                            i.Items.Add(ii);
+                        }
+                    }
                 }
             }
 
