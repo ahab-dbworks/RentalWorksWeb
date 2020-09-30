f2219037c40f89045eeeb183881ab304f290a083
Joshua Pace <jpace@4wall.com>
2020-09-30 10:18:38 -0700

    fixes overlap for checkbox in Vendor currency #3015

--- src/RentalWorksWeb/Source/Modules/Agent/Vendor/Vendor.ts
+++ src/RentalWorksWeb/Source/Modules/Agent/Vendor/Vendor.ts
@@ -693,13 +693,11 @@ class Vendor {
                       </div>>
                       <div class="flexrow">
                         <div data-control="FwFormField" data-type="validation" class="fwcontrol fwformfield" data-caption="Currency" data-datafield="DefaultCurrencyId" data-displayfield="DefaultCurrencyCode" data-validationname="CurrencyValidation" data-required="true" style="flex:1 1 275px;"></div>
-                        <div data-datafield="MultipleCurrencies" data-control="FwFormField" data-type="checkbox" class="fwcontrol fwformfield" data-caption="This Vendor uses multiple Currencies. The Office Location default Currency will be used when creating new Purchase Orders." style="flex:1 1 200px;"></div>
-                      </div>
-                      <div class="flexrow">
                         <div data-control="FwFormField" data-type="text" class="fwcontrol fwformfield" data-caption="Description" data-datafield="DefaultCurrency" data-enabled="false" style="flex:1 1 275px;"></div>
                       </div>
                     </div>
                   </div>
+       
                   <!-- Address section -->
                   <div class="flexcolumn" style="flex:0 1 325px;">
                     <div class="fwcontrol fwcontainer fwform-section" data-control="FwContainer" data-type="section" data-caption="Remit To Address">
@@ -734,6 +732,9 @@ class Vendor {
                     </div>
                   </div>
                 </div>
+               <div class="flexrow">
+                        <div data-datafield="MultipleCurrencies" data-control="FwFormField" data-type="checkbox" class="fwcontrol fwformfield" data-caption="This Vendor uses multiple Currencies. The Office Location default Currency will be used when creating new Purchase Orders." style="flex:1 1 200px;"></div>
+                      </div>
               </div>
             </div>
 
