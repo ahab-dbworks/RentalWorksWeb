1429636793e7a3028fed02a705b55ef63b43dc22
Jason Hoang <jhoang@4wall.com>
2020-10-02 12:54:40 -0700

    Resolved #2553: Changing the primary item's quantity updates Accessory quantities

--- src/RentalWorksWeb/Source/Grids/SubPurchaseOrderItemGrid/SubPurchaseOrderItemGridController.ts
+++ src/RentalWorksWeb/Source/Grids/SubPurchaseOrderItemGrid/SubPurchaseOrderItemGridController.ts
@@ -1,10 +1,19 @@
-﻿﻿class SubPurchaseOrderItemGrid {
+﻿class SubPurchaseOrderItemGrid {
     Module: string = 'SubPurchaseOrderItemGrid';
-     apiurl: string = 'api/v1/subpurchaseorderitem';
+    apiurl: string = 'api/v1/subpurchaseorderitem';
 
-     addLegend($control) {
-         FwBrowse.addLegend($control, 'Vendor', '#ffd666'); // orange
-     }
+    addLegend($control) {
+        FwBrowse.addLegend($control, 'Vendor', '#ffd666'); // orange
+    }
+
+    generateRow($control, $generatedtr) {
+        $generatedtr.find('div[data-browsedatafield="QuantityOrdered"]').on('change', 'input.value', e => {
+            const itemClass = FwBrowse.getValueByDataField($control, $generatedtr, 'ItemClass');
+            if (itemClass == 'K' || itemClass == 'C' || itemClass == 'N') {
+                OrderItemGridController.updateAccessoryQuantities($control, $generatedtr, e, 'QuantityOrdered');
+            }
+        });
+    }
 };
 //----------------------------------------------------------------------------------------------
 var SubPurchaseOrderItemGridController = new SubPurchaseOrderItemGrid();
