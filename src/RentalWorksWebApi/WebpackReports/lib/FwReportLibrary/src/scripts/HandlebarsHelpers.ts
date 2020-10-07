89fca975a0ec7a784ca26b33b359ab6137bf2e2f
Justin Hoffman <justin@dbworks.com>
2020-10-07 16:52:49 -0600

    wip handlebar helper #3160

--- src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/scripts/HandlebarsHelpers.ts
+++ src/RentalWorksWebApi/WebpackReports/lib/FwReportLibrary/src/scripts/HandlebarsHelpers.ts
@@ -8,15 +8,19 @@ export class HandlebarsHelpers {
         } else {
             Handlebars = require('handlebars/runtime');
         }
+        //--------------------------------------------------------------------------------------------------------------
         Handlebars.registerHelper('gt', function (arg1: any, arg2: any, options: any) {
             return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
         });
+        //--------------------------------------------------------------------------------------------------------------
         Handlebars.registerHelper('ifEquals', function (arg1: any, arg2: any, options: any) {
             return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
         });
+        //--------------------------------------------------------------------------------------------------------------
         Handlebars.registerHelper('ifNotEquals', function (arg1: any, arg2: any, options: any) {
             return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
         });
+        //--------------------------------------------------------------------------------------------------------------
         Handlebars.registerHelper('checkLength', function (arg1: any, arg2: any, options: any) {
             'use strict';
             if (arg1.length > arg2) {
@@ -24,6 +28,7 @@ export class HandlebarsHelpers {
             }
             return options.inverse(this);
         });
+        //--------------------------------------------------------------------------------------------------------------
         Handlebars.registerHelper('ifCond', function (v1: any, operator: any, v2: any, options: any) {
 
             switch (operator) {
@@ -51,11 +56,67 @@ export class HandlebarsHelpers {
                     return options.inverse(this);
             }
         });
-
+        //--------------------------------------------------------------------------------------------------------------
         Handlebars.registerHelper('ifInList', function (arg1: string, arg2: string, options: any) {
             var stringArray = arg2.split(',');
             return (stringArray.indexOf(arg1) !== -1) ? options.fn(this) : options.inverse(this);
         });
-
+        //--------------------------------------------------------------------------------------------------------------
+        Handlebars.registerHelper('ifSameAsNext', function (array, currentIndex, options) {
+            let currentValue: any = array[currentIndex];
+            let nextValue: any = '';
+            if (array.length >=currentIndex) {
+                nextValue = array[currentIndex + 1];
+            }
+            if (currentValue == nextValue) {
+                return options.fn(this);
+            }
+            else {
+                return options.inverse(this);
+            }
+        });
+        //--------------------------------------------------------------------------------------------------------------
+        Handlebars.registerHelper('ifNotSameAsNext', function (array, currentIndex, options) {
+            let currentValue: any = array[currentIndex];
+            let nextValue: any = '';
+            if (array.length >= currentIndex) {
+                nextValue = array[currentIndex + 1];
+            }
+            if (currentValue == nextValue) {
+                return options.inverse(this);
+            }
+            else {
+                return options.fn(this);
+            }
+        });
+        //--------------------------------------------------------------------------------------------------------------
+        Handlebars.registerHelper('ifSameAsPrevious', function (array, currentIndex, options) {
+            let currentValue: any = array[currentIndex];
+            let previousValue: any = '';
+            if (currentIndex > 0) {
+                previousValue = array[currentIndex + 1];
+            }
+            if (currentValue == previousValue) {
+                return options.fn(this);
+            }
+            else {
+                return options.inverse(this);
+            }
+        });
+        //--------------------------------------------------------------------------------------------------------------
+        Handlebars.registerHelper('ifNotSameAsPrevious', function (array, currentIndex, options) {
+            let currentValue: any = array[currentIndex];
+            let previousValue: any = '';
+            if (currentIndex > 0) {
+                previousValue = array[currentIndex + 1];
+            }
+            if (currentValue == previousValue) {
+                return options.inverse(this);
+            }
+            else {
+                return options.fn(this);
+            }
+        });
+        //--------------------------------------------------------------------------------------------------------------
     }
 }
