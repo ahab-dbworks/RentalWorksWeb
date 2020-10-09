b5fccf1e42851ce7e43b82c79359e92571915618
Justin Hoffman <justin@dbworks.com>
2020-10-09 05:11:14 -0600

    modifications for warehouse-specific loader #1723

--- src/RentalWorksWebApi/Modules/SharedControls/HomeControls/InventoryPackageInventory/InventoryPackageInventoryLoader.cs
+++ src/RentalWorksWebApi/Modules/SharedControls/HomeControls/InventoryPackageInventory/InventoryPackageInventoryLoader.cs
@@ -134,7 +134,7 @@ namespace WebApi.Modules.HomeControls.InventoryPackageInventory
         {
             useWithNoLock = false;
             string packageId = "";
-            string warehouseId = "xnonex";
+            string warehouseId = RwConstants.NONE;
             string currencyId = "";
             if ((request != null) && (request.uniqueids != null))
             {
@@ -162,6 +162,18 @@ namespace WebApi.Modules.HomeControls.InventoryPackageInventory
                 }
             }
 
+            if (warehouseId.Equals(RwConstants.NONE))
+            {
+                if (!string.IsNullOrEmpty(InventoryPackageInventoryId))
+                {
+                    warehouseId = AppFunc.GetStringDataAsync(AppConfig, "packageitem", "packageitemid", InventoryPackageInventoryId, "warehouseid").Result;
+                }
+            }
+
+            if (warehouseId.Equals(RwConstants.NONE))
+            {
+                warehouseId = "";
+            }
 
             base.SetBaseSelectQuery(select, qry, customFields, request);
             select.Parse();
