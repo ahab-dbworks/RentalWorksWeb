cab55456cb2c06af42bc3ffcdedbf915ba094cac
Jason Hoang <jhoang@4wall.com>
2020-10-21 15:52:14 -0700

    View Corresponding Deal Invoices menu option wip #3217

--- src/RentalWorksWeb/Source/Grids/VendorInvoiceItemGrid/VendorInvoiceItemGridController.ts
+++ src/RentalWorksWeb/Source/Grids/VendorInvoiceItemGrid/VendorInvoiceItemGridController.ts
@@ -1,18 +1,78 @@
-﻿﻿class VendorInvoiceItemGrid {
+﻿class VendorInvoiceItemGrid {
     Module: string = 'VendorInvoiceItemGrid';
-     apiurl: string = 'api/v1/vendorinvoiceitem';
-
+    apiurl: string = 'api/v1/vendorinvoiceitem';
+    //----------------------------------------------------------------------------------------------
     generateRow($control, $generatedtr) {
+        FwBrowse.setAfterRenderRowCallback($control, ($tr: JQuery, dt: FwJsonDataTable, rowIndex: number) => {
+            const $browsecontextmenu = $tr.find('.browsecontextmenu');
+            $browsecontextmenu.data('contextmenuoptions', $tr => {
+                //View Corresponding Deal Invoices
+                FwContextMenu.addMenuItem($browsecontextmenu, `View Corresponding Deal Invoices`, () => {
+                    try {
+                        this.renderInvoicePopup($control, $tr);
+                    } catch (ex) {
+                        FwFunc.showError(ex);
+                    }
+                });
+            });
+        });
+
         $generatedtr.find('div[data-browsedatafield="FromDate"]').on('change', 'input.value', function ($tr) {
         });
-     }
-     beforeValidate(datafield: string, request: any, $validationbrowse: JQuery, $gridbrowse: JQuery, $tr: JQuery) {
-         switch (datafield) {
-             case 'GlAccountId':
-                 $validationbrowse.attr('data-apiurl', `${this.apiurl}/validateglaccount`);
-                 break;
-         }
-     }
+    }
+    //----------------------------------------------------------------------------------------------
+    beforeValidate(datafield: string, request: any, $validationbrowse: JQuery, $gridbrowse: JQuery, $tr: JQuery) {
+        switch (datafield) {
+            case 'GlAccountId':
+                $validationbrowse.attr('data-apiurl', `${this.apiurl}/validateglaccount`);
+                break;
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    renderInvoicePopup($control: JQuery, $tr: JQuery) {
+        let HTML: Array<string> = [], $popupHtml, $popup;
+        let type;
+
+        HTML.push(
+            `<div class="fwcontrol fwcontainer fwform popup" data-control="FwContainer" data-type="form">
+                <div style="float:right;" class="close-modal"><i class="material-icons">clear</i><div class="btn-text">Close</div></div>
+                <div class="wideflexrow">
+                   <div data-control="FwGrid" data-grid="VendorInvoiceItemCorrespondingDealInvoicesGrid" data-securitycaption=""></div>
+                 </div>
+            </div>`);
+        $popupHtml = HTML.join('');
+        $popup = FwPopup.renderPopup(jQuery($popupHtml), { ismodal: true });
+        FwControl.renderRuntimeControls($popup.find('.fwcontrol'));
+        FwPopup.showPopup($popup);
+
+        const $grid = FwBrowse.renderGrid({
+            nameGrid: `VendorInvoiceItemCorrespondingDealInvoicesGrid`,
+            gridSecurityId: '',
+            moduleSecurityId: '',
+            $form: $popup,
+            addGridMenu: (options: IAddGridMenuOptions) => {
+                options.hasNew = false;
+                options.hasEdit = false;
+                options.hasDelete = false;
+            },
+            onDataBind: (request: any) => {
+                request.uniqueids = {
+                    OrderId: FwBrowse.getValueByDataField($control, $tr, 'OrderId')
+                };
+            }
+        });
+
+        //FwBrowse.disableGrid($grid);
+        FwBrowse.search($grid);
+
+        // Close modal
+        $popup.find('.close-modal').one('click', e => {
+            FwPopup.destroyPopup($popup);
+            jQuery(document).find('.fwpopup').off('click');
+            jQuery(document).off('keydown');
+        });
+    }
+    //----------------------------------------------------------------------------------------------
 }
 //----------------------------------------------------------------------------------------------
 var VendorInvoiceItemGridController = new VendorInvoiceItemGrid();
