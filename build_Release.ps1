949647b1b4b9579b25e503573292323cef4a920b
Mike Vermilion <mike@dbworks.com>
2020-11-04 23:33:32 -0800

    Removes legacy powershell scripts.

--- build_Release.ps1
+++
@@ -1,2 +0,0 @@
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-Start-Process -FilePath $msbuild -ArgumentList "/p:Configuration=Release RentalWorksWeb.sln" -NoNewWindow
--- update_Fw.ps1
+++
@@ -1,2 +0,0 @@
-robocopy C:\gitprojects\RentalWorksWeb\lib\Fw C:\gitprojects\Fw /mir /xd .git .vs /xf .git* update_Fw_*
-#pause
