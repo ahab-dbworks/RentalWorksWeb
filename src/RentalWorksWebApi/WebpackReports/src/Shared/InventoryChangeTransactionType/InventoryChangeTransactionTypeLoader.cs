f8c95802efb52efb35d58a14c06353bdd2f0eb8d
Justin Hoffman <justin@dbworks.com>
2020-11-16 06:29:50 -0700

    Resolved #3250: Fixed error when clicking on the Transaction Type validation of the Inventory Change Report front-ends

--- src/RentalWorksWebApi/WebpackReports/src/Shared/InventoryChangeTransactionType/InventoryChangeTransactionTypeLoader.cs
+++ src/RentalWorksWebApi/WebpackReports/src/Shared/InventoryChangeTransactionType/InventoryChangeTransactionTypeLoader.cs
@@ -5,31 +5,17 @@ using FwStandard.SqlServer.Attributes;
 using WebApi.Data;
 namespace WebApi.Modules.Reports.Shared.InventoryChangeTransactionType
 {
-    [FwSqlTable("dbo.dwinventorychangetype()")]
+    [FwSqlTable("inventorychangetypeview")]
     public class InventoryChangeTransactionTypeLoader : AppDataLoadRecord
     {
         //------------------------------------------------------------------------------------ 
-        [FwSqlDataField(column: "TransactionType", modeltype: FwDataTypes.Text)]
+        [FwSqlDataField(column: "type", modeltype: FwDataTypes.Text)]
         public string TransactionType { get; set; }
         //------------------------------------------------------------------------------------ 
-        public override FwSqlConnection GetDatabaseConnection()
-        {
-            return new FwSqlConnection(AppConfig.DataWarehouseDatabaseSettings.ConnectionString);
-        }
-        //------------------------------------------------------------------------------------
         protected override void SetBaseSelectQuery(FwSqlSelect select, FwSqlCommand qry, FwCustomFields customFields = null, BrowseRequest request = null)
         {
-            useWithNoLock = false;
-            //string paramString = GetUniqueIdAsString("ParamString", request) ?? ""; 
-            //DateTime paramDate = GetUniqueIdAsDateTime("ParamDate", request) ?? DateTime.MinValue; 
-            //bool paramBoolean = GetUniqueIdAsBoolean("ParamBoolean", request) ?? false; 
             base.SetBaseSelectQuery(select, qry, customFields, request);
             select.Parse();
-            //select.AddWhere("(xxxtype = 'ABCDEF')"); 
-            //addFilterToSelect("UniqueId", "uniqueid", select, request); 
-            //select.AddParameter("@paramstring", paramString); 
-            //select.AddParameter("@paramdate", paramDate); 
-            //select.AddParameter("@paramboolean", paramBoolean); 
         }
         //------------------------------------------------------------------------------------ 
     }
