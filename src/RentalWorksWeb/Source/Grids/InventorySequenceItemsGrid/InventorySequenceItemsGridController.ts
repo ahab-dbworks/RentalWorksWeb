8a4fb2292d9b61a575ec76dc046f2c11ddf338ad
Jason Hoang <jhoang@4wall.com>
2020-11-17 14:55:20 -0800

    Resolved #3360: Added ICode and Description sorting options to Inventory Sequence Items grid

--- src/RentalWorksWeb/Source/Grids/InventorySequenceItemsGrid/InventorySequenceItemsGridController.ts
+++ src/RentalWorksWeb/Source/Grids/InventorySequenceItemsGrid/InventorySequenceItemsGridController.ts
@@ -1,7 +1,22 @@
 ﻿class InventorySequenceItemsGrid {
     Module: string = 'InventorySequenceItemsGrid';
     apiurl: string = 'api/v1/generalitem';
-}
+    //----------------------------------------------------------------------------------------------
+    sortItems($form: any, $grid: any, sortByField: string) {
+        if (!$grid.hasClass('sort-mode')) {
+            $grid.find('div.btn-manualsort').click();
+        }
+        const $items = $grid.find('tbody tr');
+        const $sortedArray = $items.sort((a, b) => jQuery(a).find(`[data-browsedatafield="${sortByField}"]`).attr('data-originalvalue')
+                            > jQuery(b).find(`[data-browsedatafield="${sortByField}"]`).attr('data-originalvalue')
+                            ? 1 : -1);
 
+        $grid.find('tbody').append($sortedArray);
+        const $gridMenu = $grid.find('[data-control="FwMenu"]');
+        $gridMenu.find('.sorting').show();
+    }
+    //----------------------------------------------------------------------------------------------
+}
+//----------------------------------------------------------------------------------------------
 var InventorySequenceItemsGridController = new InventorySequenceItemsGrid();
 //----------------------------------------------------------------------------------------------
