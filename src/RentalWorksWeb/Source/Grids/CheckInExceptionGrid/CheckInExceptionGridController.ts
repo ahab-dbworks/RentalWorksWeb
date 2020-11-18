67e0707ec8a2e4b5ff2d99331f097c07059539cd
Jason Hoang <jhoang@4wall.com>
2020-11-17 16:06:29 -0800

    Resolved #3362: Updated Check-In Exceptions grid with dynamic validation peeks

--- src/RentalWorksWeb/Source/Grids/CheckInExceptionGrid/CheckInExceptionGridController.ts
+++ src/RentalWorksWeb/Source/Grids/CheckInExceptionGrid/CheckInExceptionGridController.ts
@@ -1,6 +1,33 @@
 ﻿class CheckInExceptionGrid {
     Module: string = 'CheckInExceptionGrid';
     apiurl: string = 'api/v1/checkinexception';
+
+    generateRow($control, $generatedtr) {
+        FwBrowse.setAfterRenderRowCallback($control, ($tr: JQuery, dt: FwJsonDataTable, rowIndex: number) => {
+            //set validation dynamically
+            const recType = FwBrowse.getValueByDataField($control, $tr, 'RecType');
+            const $td = $tr.find('[data-browsedatafield="InventoryId"]');
+            let peekForm;
+            switch (recType) {
+                case 'R':
+                    peekForm = 'RentalInventory';
+                    break;
+                case 'S':
+                    peekForm = 'SalesInventory';
+                    break;
+                case 'P':
+                    peekForm = 'PartsInventory';
+                    break;
+                case 'M':
+                    peekForm = 'MiscRate';
+                    break;
+                case 'L':
+                    peekForm = 'LaborRate';
+                    break;
+            }
+            $td.attr('data-peekForm', peekForm);
+        });
+    }
 }
 
 var CheckInExceptionGridController = new CheckInExceptionGrid();
