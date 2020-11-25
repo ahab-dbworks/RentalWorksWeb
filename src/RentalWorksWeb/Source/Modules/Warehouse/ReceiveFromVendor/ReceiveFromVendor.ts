e3a2840f378b706b1b5ecf1eb556e80b8103c88d
Joshua Pace <jpace@4wall.com>
2020-11-25 14:19:14 -0800

    ReceiveFromVendor fully received item filter for grid init #3394

--- src/RentalWorksWeb/Source/Modules/Warehouse/ReceiveFromVendor/ReceiveFromVendor.ts
+++ src/RentalWorksWeb/Source/Modules/Warehouse/ReceiveFromVendor/ReceiveFromVendor.ts
@@ -202,9 +202,9 @@ class ReceiveFromVendor {
     events($form: any): void {
         // Create Contract
         $form.find('.createcontract').on('click', e => {
-            let contractId = FwFormField.getValueByDataField($form, 'ContractId');
-            let automaticallyCreateCheckOut: boolean = FwFormField.getValueByDataField($form, 'AutomaticallyCreateCheckOut');
-            let requestBody: any = {
+            const contractId = FwFormField.getValueByDataField($form, 'ContractId');
+            const automaticallyCreateCheckOut: boolean = FwFormField.getValueByDataField($form, 'AutomaticallyCreateCheckOut');
+            const requestBody: any = {
                 CreateOutContracts: automaticallyCreateCheckOut
             };
             if (contractId) {
@@ -272,6 +272,21 @@ class ReceiveFromVendor {
         $form.find('.options').toggle();
         $optionToggle.on('click', () => {
             $form.find('.options').toggle();
+        });
+        $form.find('div[data-datafield="ItemsFullyReceived"]').on('change', e => {
+            const itemsFullyReceived: boolean = FwFormField.getValueByDataField($form, 'ItemsFullyReceived');
+            const $receiveItemsGridControl = $form.find('div[data-name="POReceiveItemGrid"]');
+
+            $receiveItemsGridControl.data('ondatabind', request => {
+                request.uniqueids = {
+                    ShowFullyReceived: `${itemsFullyReceived}`,
+                    ContractId: FwFormField.getValueByDataField($form, 'ContractId'),
+                    PurchaseOrderId: FwFormField.getValueByDataField($form, 'PurchaseOrderId'),
+                    WarehouseId: JSON.parse(sessionStorage.getItem('warehouse')).warehouseid,
+                }
+                let here;
+            });
+            FwBrowse.search($receiveItemsGridControl);
         });
     };
     //----------------------------------------------------------------------------------------------
@@ -387,9 +402,12 @@ class ReceiveFromVendor {
             </div>
             <div class="flexrow">
                 <div class="flexcolumn" style="flex:1 1 450px;">
-                <div class="flexrow">
-                    <div data-control="FwFormField" data-type="checkbox" class="options fwcontrol fwformfield" data-caption="Automatically Create CHECK-OUT Contract for Sub Rental and Sub Sale items received" data-datafield="AutomaticallyCreateCheckOut" style="flex:1 1 150px;"></div>
-                </div>
+                    <div class="flexrow">
+                        <div data-control="FwFormField" data-type="checkbox" class="options fwcontrol fwformfield" data-caption="Automatically Create CHECK-OUT Contract for Sub Rental and Sub Sale items received" data-datafield="AutomaticallyCreateCheckOut" style="flex:1 1 150px;"></div>
+                    </div>
+                    <div class="flexrow">
+                        <div data-control="FwFormField" data-type="checkbox" class="options fwcontrol fwformfield" data-caption="Show items that have been fully received" data-datafield="ItemsFullyReceived" style="flex:1 1 150px;"></div>
+                    </div>
                 </div>
             </div>
           </div>
