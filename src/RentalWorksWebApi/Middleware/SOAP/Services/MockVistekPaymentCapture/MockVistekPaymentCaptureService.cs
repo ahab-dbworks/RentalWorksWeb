baead86cfe4981849a942bf1ddd8430b75ad9cb2
Mike Vermilion <mike@dbworks.com>
2020-12-22 12:05:49 -0800

    Adds support for Refund to MockVisitekPaymentCaptureService.  Currently all refunds will be Approved.

--- src/RentalWorksWebApi/Middleware/SOAP/Services/MockVistekPaymentCapture/MockVistekPaymentCaptureService.cs
+++ src/RentalWorksWebApi/Middleware/SOAP/Services/MockVistekPaymentCapture/MockVistekPaymentCaptureService.cs
@@ -1,5 +1,6 @@
 ﻿using FwStandard.Models;
 using FwStandard.SqlServer;
+using shortid;
 using System;
 using System.ServiceModel;
 using System.Threading.Tasks;
@@ -11,42 +12,60 @@ namespace WebApi.Middleware.SOAP.Services.MockVistekPaymentCapture
         public FwApplicationConfig AppConfig { get; set; } = null;
         public async Task<MockVistekProcessCardPayment_Result> ProcessCardPayment(string pINPadNo, int transactionTypeOpt, string amount, string docRefNo, string storeCode, string salespersonCode, string billToCustomerNo, string pPaymentRefNo, string pCardType, string pCardNo, string pAuthCode)
         {
-            decimal totals_weekly_grandtotal = 0;
-            decimal totals_period_grandtotal = 0;
-            decimal totals_replacement_replacementcost = 0;
-            decimal totals_replacement_depositpercentage = 0;
-            decimal totals_replacement_depositdue = 0;
+            if (transactionTypeOpt == 1)
+            {
+                // Depleting Deposit
+                decimal totals_weekly_grandtotal = 0;
+                decimal totals_period_grandtotal = 0;
+                decimal totals_replacement_replacementcost = 0;
+                decimal totals_replacement_depositpercentage = 0;
+                decimal totals_replacement_depositdue = 0;
 
-            using (FwSqlConnection conn = new FwSqlConnection(this.AppConfig.DatabaseSettings.ConnectionString))
-            {
-                using (FwSqlCommand qry = new FwSqlCommand(conn, this.AppConfig.DatabaseSettings.QueryTimeout))
+                using (FwSqlConnection conn = new FwSqlConnection(this.AppConfig.DatabaseSettings.ConnectionString))
                 {
-                    qry.Add("select totals_weekly_grandtotal, totals_period_grandtotal, totals_replacement_replacementcost, totals_replacement_depositpercentage");
-                    qry.Add("from processcreditcardloadview with (nolock)");
-                    qry.Add("where orderid = @orderid");
-                    qry.AddParameter("@orderid", docRefNo);
-                    await qry.ExecuteAsync();
-                    totals_weekly_grandtotal = qry.GetField("totals_weekly_grandtotal").ToDecimal();
-                    totals_period_grandtotal = qry.GetField("totals_period_grandtotal").ToDecimal();
-                    totals_replacement_replacementcost = qry.GetField("totals_replacement_replacementcost").ToDecimal();
-                    totals_replacement_depositpercentage = qry.GetField("totals_replacement_depositpercentage").ToDecimal();
-                    totals_replacement_depositdue = (totals_replacement_replacementcost * totals_replacement_depositpercentage) / 100.00m;
+                    using (FwSqlCommand qry = new FwSqlCommand(conn, this.AppConfig.DatabaseSettings.QueryTimeout))
+                    {
+                        qry.Add("select totals_weekly_grandtotal, totals_period_grandtotal, totals_replacement_replacementcost, totals_replacement_depositpercentage");
+                        qry.Add("from processcreditcardloadview with (nolock)");
+                        qry.Add("where orderid = @orderid");
+                        qry.AddParameter("@orderid", docRefNo);
+                        await qry.ExecuteAsync();
+                        totals_weekly_grandtotal = qry.GetField("totals_weekly_grandtotal").ToDecimal();
+                        totals_period_grandtotal = qry.GetField("totals_period_grandtotal").ToDecimal();
+                        totals_replacement_replacementcost = qry.GetField("totals_replacement_replacementcost").ToDecimal();
+                        totals_replacement_depositpercentage = qry.GetField("totals_replacement_depositpercentage").ToDecimal();
+                        totals_replacement_depositdue = (totals_replacement_replacementcost * totals_replacement_depositpercentage) / 100.00m;
+                    }
                 }
+
+                Random rnd = new Random();
+                int transactionId = rnd.Next(100000, 999999);
+                int last4CardNumbers = rnd.Next(1000, 9999);
+                MockVistekProcessCardPayment_Result result = new MockVistekProcessCardPayment_Result();
+                decimal paymentAmount = FwConvert.ToDecimal(amount);
+                if ((paymentAmount == totals_weekly_grandtotal) ||
+                    (paymentAmount == totals_period_grandtotal) ||
+                    (paymentAmount == totals_replacement_depositdue))
+                {
+                    result.return_value = $"Approved|APPROVED|Captured|VISA|************${last4CardNumbers}|${transactionId}|{amount}";
+                }
+                else
+                {
+                    result.return_value = $"Declined|DECLINED|Captured|VISA|************${last4CardNumbers}|${transactionId}|{amount}";
+                }
+                return result;
             }
-            
-            MockVistekProcessCardPayment_Result result = new MockVistekProcessCardPayment_Result();
-            decimal paymentAmount = FwConvert.ToDecimal(amount);
-            if ((paymentAmount == totals_weekly_grandtotal) || 
-                (paymentAmount == totals_period_grandtotal) ||
-                (paymentAmount == totals_replacement_depositdue))
+            else if (transactionTypeOpt == 2)
             {
-                result.return_value = $"Approved|APPROVED|Captured|VISA|************0085|119994|{amount}";
+                // Refund
+                Random rnd = new Random();
+                int transactionId = rnd.Next(100000, 999999);
+                int last4CardNumbers = rnd.Next(1000, 9999);
+                MockVistekProcessCardPayment_Result result = new MockVistekProcessCardPayment_Result();
+                result.return_value = $"Approved|APPROVED|Captured|VISA|************0085|${transactionId}|{amount}";
+                return result;
             }
-            else
-            {
-                result.return_value = $"Declined|DECLINED|Captured|VISA|************0085|119994|{amount}";
-            }
-            return result;
+            throw new NotSupportedException($"Unsupported transactionTypeOpt: ${transactionTypeOpt}");
         }
     }
 }
